6d4b7a1f2e20a81decd81c3771272d4938c2ff2f
Saffith <absutman@gmail.com>
2016-07-13 14:07:54 -0400

    Left a few files out somehow.

---
+++ src/zq_rules.h
@@ -0,0 +1,13 @@
+//--------------------------------------------------------
+//  Zelda Classic
+//  by Jeremy Craner, 1999-2000
+//
+//  zquest.cc
+//
+//  Main code for the quest editor.
+//
+//--------------------------------------------------------
+
+int onAnimationRules();
+void center_zq_rules_dialog();
+ 
