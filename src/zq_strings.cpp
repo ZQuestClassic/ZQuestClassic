64ff13274f2b31da081380dcc5c2f69bf3e4737b
Saffith <absutman@gmail.com>
2016-07-28 12:01:09 -0400

    Fixed a possible crash when editing a message.

--- src/zq_strings.cpp
+++ src/zq_strings.cpp
@@ -756,7 +756,7 @@ int onStrings()
         
         if(index>0 && doedit)
         {
-            int lp = MsgStrings[addAfter].listpos;
+            int lp = addAfter>=0 ? MsgStrings[addAfter].listpos : -1;
             editmsg(index, addAfter);
             
             if(MsgStrings[index].listpos!=msg_count) // Created new string
@@ -770,9 +770,8 @@ int onStrings()
                 }
                 editmsg_dlg[27].flags=(MsgStrings[index].stringflags&STRINGFLAG_CONT)?D_SELECTED:0;
             }
-            else // Canceled or edited an existing string
-                // Select previously selected message
-                strlist_dlg[2].d1 = lp;
+            else if(lp>=0)              // Canceled or edited an existing string
+                strlist_dlg[2].d1 = lp; // Select previously selected message
             
             // Fix the quick-category menu
             strlist_dlg[17].d1=0;
