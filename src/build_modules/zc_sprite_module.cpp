f999cc44dd9bd69bc61ed68eeeac159c0115dfe9
Gleeok <arpeggiodragon@gmail.com>
2016-12-27 22:43:54 -0800

    Preliminary unity build files for ZC and ZQ.

---
+++ src/build_modules/zc_sprite_module.cpp
@@ -0,0 +1,18 @@
+
+#include "precompiled.h"
+
+
+// sprite
+#include "zc_sprite.cpp"
+#include "guys.cpp"
+#include "weapons.cpp"
+#include "link.cpp"
+#include "particles.cpp"
+#include "items.cpp"
+#include "sprite.cpp"
+#include "tiles.cpp"
+#include "maps.cpp"
+#include "script_drawing.cpp"
+
+
+
---
+++ src/build_modules/zq_misc_module.cpp
@@ -0,0 +1,26 @@
+
+#include "precompiled.h"
+
+#define NOMINMAX
+
+
+#include "items.cpp"
+#include "questReport.cpp"
+
+#include "ffasm.cpp"
+#include "zquest.cpp"
+//#include "jwin.cpp"
+#include "gui.cpp"
+#include "jwinfsel.cpp"
+#include "tab_ctl.cpp"
+#include "tiles.cpp"
+
+#include "load_gif.cpp"
+#include "win32.cpp"
+
+
+
+
+
+
+
