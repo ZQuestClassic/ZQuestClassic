590ba2bc1a690c5a3278d2bde974049e519c710a
gwx <grayswx@gmail.com>
2017-01-14 09:54:51 -0500

    Rearranged code.

--- src/parser/SymbolVisitors.h
+++ src/parser/SymbolVisitors.h
@@ -10,24 +10,21 @@ class BuildScriptSymbols : public RecursiveVisitor
 {
 public:
     BuildScriptSymbols() : failure(false), deprecateGlobals(false) {}
-    virtual void caseDefault(void *);
-    virtual void caseScript(ASTScript &host,void *param);
-    virtual void caseArrayDecl(ASTArrayDecl &host, void *param);
-    virtual void caseVarDecl(ASTVarDecl &host, void *param);
-    virtual void caseVarDeclInitializer(ASTVarDeclInitializer &host, void *param);
-    virtual void caseFuncDecl(ASTFuncDecl &host, void *param);
-    virtual void caseExprDot(ASTExprDot &host, void *param);
-    virtual void caseExprArrow(ASTExprArrow &host, void *param);
-    virtual void caseExprArray(ASTExprArray &host, void *param);
-    virtual void caseFuncCall(ASTFuncCall &host, void *param);
-    bool isOK()
-    {
-        return !failure;
-    }
-    void enableDeprecationWarnings()
-    {
-        deprecateGlobals = true;
-    }
+    void caseDefault(void *);
+	// Declarations
+    void caseScript(ASTScript &host,void *param);
+    void caseFuncDecl(ASTFuncDecl &host, void *param);
+    void caseArrayDecl(ASTArrayDecl &host, void *param);
+    void caseVarDecl(ASTVarDecl &host, void *param);
+    void caseVarDeclInitializer(ASTVarDeclInitializer &host, void *param);
+	// Expressions
+    void caseExprDot(ASTExprDot &host, void *param);
+    void caseExprArrow(ASTExprArrow &host, void *param);
+    void caseExprArray(ASTExprArray &host, void *param);
+    void caseFuncCall(ASTFuncCall &host, void *param);
+
+    bool isOK() {return !failure;}
+    void enableDeprecationWarnings() {deprecateGlobals = true;}
 private:
     bool failure;
     bool deprecateGlobals;
@@ -37,32 +34,24 @@ class BuildFunctionSymbols : public RecursiveVisitor
 {
 public:
     BuildFunctionSymbols() : failure(false), thisvid(-1) {}
-    virtual void caseDefault(void *param)
-    {
-        param=param; /*these are here to bypass compiler warnings about unused arguments*/
-    }
-    virtual void caseFuncDecl(ASTFuncDecl &host, void *param);
-    virtual void caseVarDecl(ASTVarDecl &host, void *param);
-    virtual void caseArrayDecl(ASTArrayDecl &host, void *param);
-    virtual void caseVarDeclInitializer(ASTVarDeclInitializer &host, void *param)
-    {
-        host.getInitializer()->execute(*this,param);
-        caseVarDecl(host, param);
-    }
-    virtual void caseBlock(ASTBlock &host, void *param);
-    virtual void caseStmtFor(ASTStmtFor &host, void *param);
-    virtual void caseFuncCall(ASTFuncCall &host, void *param);
-    virtual void caseExprDot(ASTExprDot &host, void *param);
-    virtual void caseExprArrow(ASTExprArrow &host, void *param);
-    virtual void caseExprArray(ASTExprArray &host, void *param);
-    bool isOK()
-    {
-        return !failure;
-    }
-    int getThisVID()
-    {
-        return thisvid;
-    }
+
+    void caseDefault(void *) {}
+	// Statements
+    void caseBlock(ASTBlock &host, void *param);
+    void caseStmtFor(ASTStmtFor &host, void *param);
+	// Declarations
+    void caseFuncDecl(ASTFuncDecl &host, void *param);
+    void caseArrayDecl(ASTArrayDecl &host, void *param);
+    void caseVarDecl(ASTVarDecl &host, void *param);
+    void caseVarDeclInitializer(ASTVarDeclInitializer &host, void *param);
+	// Expressions
+    void caseFuncCall(ASTFuncCall &host, void *param);
+    void caseExprDot(ASTExprDot &host, void *param);
+    void caseExprArrow(ASTExprArrow &host, void *param);
+    void caseExprArray(ASTExprArray &host, void *param);
+
+    bool isOK() {return !failure;}
+    int getThisVID() {return thisvid;}
 private:
     bool failure;
     int thisvid;
