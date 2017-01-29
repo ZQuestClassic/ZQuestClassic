bdc9b8bf0afbff2743183f38166386dccc66d90e
Gleeok <arpeggiodragon@gmail.com>
2017-01-29 01:09:24 -0800

    [Experimental] Created bind files for all 2.5 script types as an in-place todo.

---
+++ src/experimental/asBindings/Legacy/asFFC.hpp
@@ -0,0 +1,97 @@
+
+#pragma once
+
+
+namespace FFCBindings
+{
+	int32 Data
+	{
+		//todo
+	}
+
+	int32 Script
+	{
+		//todo
+	}
+
+	int32 CSet
+	{
+		//todo
+	}
+
+	int32 Delay
+	{
+		//todo
+	}
+
+	float X
+	{
+		//todo
+	}
+
+	float Y
+	{
+		//todo
+	}
+
+	float Vx
+	{
+		//todo
+	}
+
+	float Vy
+	{
+		//todo
+	}
+
+	float Ax
+	{
+		//todo
+	}
+
+	float Ay
+	{
+		//todo
+	}
+
+	bool Flags[]
+	{
+		//todo
+	}
+
+	int32 TileWidth
+	{
+		//todo
+	}
+
+	int32 TileHeight
+	{
+		//todo
+	}
+
+	int32 EffectWidth
+	{
+		//todo
+	}
+
+	int32 EffectHeight
+	{
+		//todo
+	}
+
+	int32 Link
+	{
+		//todo
+	}
+
+	float InitD[]
+	{
+		//todo
+	}
+
+	float Misc[]
+	{
+		//todo
+	}
+}
+
---
+++ src/experimental/asBindings/Legacy/asNPC.hpp
@@ -0,0 +1,227 @@
+
+#pragma once
+
+
+namespace NPCBindings
+{
+	bool isValid()
+	{
+		//todo
+	}
+
+	void GetName(int32 buffer[])
+	{
+		//todo
+	}
+
+	int32 ID
+	{
+		//todo
+	}
+
+	int32 Type
+	{
+		//todo
+	}
+
+	int32 X
+	{
+		//todo
+	}
+
+	int32 Y
+	{
+		//todo
+	}
+
+	int32 Z
+	{
+		//todo
+	}
+
+	int32 Jump
+	{
+		//todo
+	}
+
+	int32 Dir
+	{
+		//todo
+	}
+
+	int32 Rate
+	{
+		//todo
+	}
+
+	int32 Haltrate
+	{
+		//todo
+	}
+
+	int32 Homing
+	{
+		//todo
+	}
+
+	int32 Hunger
+	{
+		//todo
+	}
+
+	int32 Step
+	{
+		//todo
+	}
+
+	bool CollDetection
+	{
+		//todo
+	}
+
+	int32 ASpeed
+	{
+		//todo
+	}
+
+	int32 DrawStyle
+	{
+		//todo
+	}
+
+	int32 HP
+	{
+		//todo
+	}
+
+	int32 Damage
+	{
+		//todo
+	}
+
+	int32 WeaponDamage
+	{
+		//todo
+	}
+
+	int32 Stun
+	{
+		//todo
+	}
+
+	int32 OriginalTile
+	{
+		//todo
+	}
+
+	int32 Tile
+	{
+		//todo
+	}
+
+	int32 Weapon
+	{
+		//todo
+	}
+
+	int32 ItemSet
+	{
+		//todo
+	}
+
+	int32 CSet
+	{
+		//todo
+	}
+
+	int32 BossPal
+	{
+		//todo
+	}
+
+	int32 SFX
+	{
+		//todo
+	}
+
+	int32 Extend
+	{
+		//todo
+	}
+
+	int32 TileWidth
+	{
+		//todo
+	}
+
+	int32 TileHeight
+	{
+		//todo
+	}
+
+	int32 HitWidth
+	{
+		//todo
+	}
+
+	int32 HitHeight
+	{
+		//todo
+	}
+
+	int32 HitZHeight
+	{
+		//todo
+	}
+
+	int32 HitXOffset
+	{
+		//todo
+	}
+
+	int32 HitYOffset
+	{
+		//todo
+	}
+
+	int32 DrawXOffset
+	{
+		//todo
+	}
+
+	int32 DrawYOffset
+	{
+		//todo
+	}
+
+	int32 DrawZOffset
+	{
+		//todo
+	}
+
+	int32 Defense[]
+	{
+		//todo
+	}
+
+	int32 Attributes[]
+	{
+		//todo
+	}
+
+	int32 MiscFlags
+	{
+		//todo
+	}
+
+	float_or_int32 Misc[]
+	{
+		//todo
+	}
+
+	void BreakShield()
+	{
+		//todo
+	}
+
+}
