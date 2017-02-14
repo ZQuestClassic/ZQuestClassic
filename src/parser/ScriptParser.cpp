7d6ff8d43b0d3019605b28ba3bd622ba7c8a51ff
gwx <grayswx@gmail.com>
2017-02-13 19:23:29 -0500

    Moved types into Types.cpp/h.

--- src/parser/ScriptParser.cpp
+++ src/parser/ScriptParser.cpp
@@ -20,40 +20,6 @@
 using namespace std;
 //#define PARSER_DEBUG
 
-// Standard Type definitions.
-ZVarTypeSimple const ZVarType::VOID(ZVARTYPEID_VOID, "void");
-ZVarTypeSimple const ZVarType::FLOAT(ZVARTYPEID_FLOAT, "float");
-ZVarTypeSimple const ZVarType::BOOL(ZVARTYPEID_BOOL, "bool");
-ZVarTypeSimple const ZVarType::FFC(ZVARTYPEID_FFC, "ffc");
-ZVarTypeSimple const ZVarType::ITEM(ZVARTYPEID_ITEM, "item");
-ZVarTypeSimple const ZVarType::ITEMCLASS(ZVARTYPEID_ITEMCLASS, "itemclass");
-ZVarTypeSimple const ZVarType::NPC(ZVARTYPEID_NPC, "npc");
-ZVarTypeSimple const ZVarType::LWPN(ZVARTYPEID_LWPN, "lwpn");
-ZVarTypeSimple const ZVarType::EWPN(ZVARTYPEID_EWPN, "ewpn");
-ZVarTypeSimple const ZVarType::GAME(ZVARTYPEID_GAME, "game");
-ZVarTypeSimple const ZVarType::LINK(ZVARTYPEID_LINK, "link");
-ZVarTypeSimple const ZVarType::SCREEN(ZVARTYPEID_SCREEN, "screen");
-
-ZVarType const* ZVarType::get(ZVarTypeId id)
-{
-	switch (id)
-	{
-		case ZVARTYPEID_VOID: return &VOID;
-		case ZVARTYPEID_FLOAT: return &FLOAT;
-		case ZVARTYPEID_BOOL: return &BOOL;
-		case ZVARTYPEID_FFC: return &FFC;
-		case ZVARTYPEID_ITEM: return &ITEM;
-		case ZVARTYPEID_ITEMCLASS: return &ITEMCLASS;
-		case ZVARTYPEID_NPC: return &NPC;
-		case ZVARTYPEID_LWPN: return &LWPN;
-		case ZVARTYPEID_EWPN: return &EWPN;
-		case ZVARTYPEID_GAME: return &GAME;
-		case ZVARTYPEID_LINK: return &LINK;
-		case ZVARTYPEID_SCREEN: return &SCREEN;
-		default: return NULL;
-	}
-}
-
 AST *resAST;
 
 ScriptsData * compile(const char *filename);
---
+++ src/parser/Types.h
@@ -0,0 +1,76 @@
+#ifndef ZSPARSER_TYPES_H
+#define ZSPARSER_TYPES_H
+
+#include <string>
+
+using std::string;
+
+////////////////////////////////////////////////////////////////
+// Script Types
+
+enum ScriptType {SCRIPTTYPE_VOID, SCRIPTTYPE_GLOBAL, SCRIPTTYPE_FFC, SCRIPTTYPE_ITEM};
+
+////////////////////////////////////////////////////////////////
+// Variable Types
+
+typedef int ZVarTypeId;
+
+enum ZVarTypeIdSimple
+{
+    ZVARTYPEID_VOID, ZVARTYPEID_FLOAT, ZVARTYPEID_BOOL,
+    ZVARTYPEID_FFC, ZVARTYPEID_ITEM, ZVARTYPEID_ITEMCLASS, ZVARTYPEID_NPC, ZVARTYPEID_LWPN, ZVARTYPEID_EWPN,
+    ZVARTYPEID_GAME, ZVARTYPEID_LINK, ZVARTYPEID_SCREEN,
+	ZVARTYPEID_END
+};
+
+class ZVarTypeSimple;
+
+class ZVarType
+{
+public:
+	virtual ZVarType* clone() const = 0;
+	virtual string getName() const = 0;
+	int compare(ZVarType const& other) const;
+	bool operator==(ZVarType const& other) const {return compare(other) == 0;}
+	bool operator<(ZVarType const& other) const {return compare(other) < 0;}
+
+	struct PointerLess : public std::less<ZVarType*> {
+		bool operator() (ZVarType* const& a, ZVarType* const& b) const {return *a < *b;}
+	};
+protected:
+	virtual int classCompareId() const = 0;
+	virtual int selfCompare(ZVarType const& other) const = 0;
+
+// Standard Types.
+public:
+	static ZVarTypeSimple const VOID;
+	static ZVarTypeSimple const FLOAT;
+	static ZVarTypeSimple const BOOL;
+	static ZVarTypeSimple const FFC;
+	static ZVarTypeSimple const ITEM;
+	static ZVarTypeSimple const ITEMCLASS;
+	static ZVarTypeSimple const NPC;
+	static ZVarTypeSimple const LWPN;
+	static ZVarTypeSimple const EWPN;
+	static ZVarTypeSimple const GAME;
+	static ZVarTypeSimple const LINK;
+	static ZVarTypeSimple const SCREEN;
+	static ZVarType const* get(ZVarTypeId id);
+};
+
+class ZVarTypeSimple : public ZVarType
+{
+public:
+	ZVarTypeSimple(ZVarTypeIdSimple simpleId, string const& name) : simpleId(simpleId), name(name) {}
+	ZVarTypeSimple* clone() const {return new ZVarTypeSimple(*this);}
+	string getName() const {return name;}
+	ZVarTypeIdSimple getId() const {return simpleId;}
+protected:
+	int classCompareId() const {return 0;}
+	int selfCompare(ZVarType const& other) const;
+private:
+	ZVarTypeIdSimple simpleId;
+	string name;
+};
+
+#endif
