ZQuestClassic/ZQuestClassic

commit 4dc575300d5c0563d75a3e44bc4ff8df98e9a044

Added separate scopes for the builtin classes.
Tracks getters/setters instead of name mangling.

All the hardcoded function names in GlobalSymbols.cpp are still written
in the mangled format - they are now unmangled when loaded into the
scopes. This should eventually be fixed, but not now.

src/parser/GlobalSymbols.h
@@ -30,16 +30,22 @@ struct AccessorTable
 class LibrarySymbols
 {
 public:
+	static LibrarySymbols* getTypeInstance(ZVarTypeId typeId);
+
     virtual void addSymbolsToScope(Scope& scope);
     virtual map<int, vector<Opcode *> > addSymbolsCode(LinkTable &lt);
-    virtual int matchFunction(string name);
+    virtual int matchFunction(string const& name);
+    virtual int matchGetter(string const& name);
+    virtual int matchSetter(string const& name);
     virtual ~LibrarySymbols();
 protected:
     AccessorTable *table;
     LibrarySymbols() {}
     int firstid;
     int refVar;
-    map<string, int> memberids;
+    map<string, int> functions;
+	map<string, int> getters;
+	map<string, int> setters;
     virtual vector<Opcode *> getVariable(LinkTable &lt, int id, int var);
     virtual vector<Opcode *> setVariable(LinkTable &lt, int id, int var);
     virtual vector<Opcode *> setBoolVariable(LinkTable &lt, int id, int var);

src/parser/DataStructs.cpp
@@ -5,6 +5,7 @@
 #include "Types.h"
 #include "ParseError.h"
 #include "GlobalSymbols.h"
+#include "Scope.h"
 #include "../zsyssimple.h"
 #include <assert.h>
 #include <iostream>
@@ -69,6 +70,8 @@ SymbolTable::SymbolTable() : nodeIds()
 SymbolTable::~SymbolTable()
 {
 	for (vector<ZVarType*>::iterator it = types.begin(); it != types.end(); ++it)
+		delete *it;
+	for (vector<ZClass*>::iterator it = classes.begin(); it != classes.end(); ++it)
 		delete *it;
 }
 
@@ -150,6 +153,21 @@ ZVarTypeId SymbolTable::getOrAssignTypeId(ZVarType const& type)
 	types.push_back(storedType);
 	typeIds[storedType] = id;
 	return id;
+}
+
+// Classes
+
+ZClass* SymbolTable::getClass(int classId) const
+{
+	if (classId < 0 || classId > classes.size()) return NULL;
+	return classes[classId];
+}
+
+ZClass* SymbolTable::createClass()
+{
+	ZClass* klass = new ZClass(*this, classes.size());
+	classes.push_back(klass);
+	return klass;
 }
 
 // Variables

src/parser/DataStructs.h
@@ -35,6 +35,8 @@ public:
 	static FunctionTypeIds const null;
 };
 
+class ZClass;
+
 class SymbolTable
 {
 public:
@@ -51,6 +53,9 @@ public:
 	ZVarTypeId getTypeId(ZVarType const& type) const;
 	ZVarTypeId assignTypeId(ZVarType const& type);
 	ZVarTypeId getOrAssignTypeId(ZVarType const& type);
+	// Classes
+	ZClass* getClass(int classId) const;
+	ZClass* createClass();
 	// Variables
     ZVarTypeId getVarTypeId(int varId) const;
     ZVarTypeId getVarTypeId(AST* node) const;
@@ -80,6 +85,7 @@ private:
     map<AST*, int> nodeIds;
 	vector<ZVarType*> types;
 	map<ZVarType*, ZVarTypeId, ZVarType::PointerLess> typeIds;
+	vector<ZClass*> classes;
     map<AST*, vector<int> > possibleNodeFuncIds;
     map<int, ZVarTypeId> varTypes;
     map<int, long> inlinedConstants;

src/parser/Scope.cpp
@@ -9,17 +9,18 @@ Scope* Scope::makeGlobalScope(SymbolTable& table)
 {
 	Scope* global = new BasicScope(table);
 
-	// Add library functions to the top level scope.
+	// Add global library functions.
     GlobalSymbols::getInst().addSymbolsToScope(*global);
-    FFCSymbols::getInst().addSymbolsToScope(*global);
-    ItemSymbols::getInst().addSymbolsToScope(*global);
-    ItemclassSymbols::getInst().addSymbolsToScope(*global);
-    LinkSymbols::getInst().addSymbolsToScope(*global);
-    ScreenSymbols::getInst().addSymbolsToScope(*global);
-    GameSymbols::getInst().addSymbolsToScope(*global);
-    NPCSymbols::getInst().addSymbolsToScope(*global);
-    LinkWeaponSymbols::getInst().addSymbolsToScope(*global);
-    EnemyWeaponSymbols::getInst().addSymbolsToScope(*global);
+
+	// Create builtin classes (skip void, float, and bool).
+	for (ZVarTypeId typeId = ZVARTYPEID_FFC; typeId < ZVARTYPEID_END; ++typeId)
+	{
+		ZVarTypeSimple const& type = *(ZVarTypeSimple const*)ZVarType::get(typeId);
+		int classId = global->addClass(type.getUpName());
+		ZClass& klass = *table.getClass(classId);
+		LibrarySymbols& library = *LibrarySymbols::getTypeInstance(typeId);
+		library.addSymbolsToScope(klass);
+	}
 
 	// Add global pointers.
     table.addGlobalPointer(global->addVariable("Link", ZVARTYPEID_LINK));
@@ -88,6 +89,36 @@ int Scope::addType(string const& name, ZVarType const& type)
 	return addType(name, table.getOrAssignTypeId(type), NULL);
 }
 
+// Classes
+
+int Scope::getClassId(string const& name) const
+{
+	int classId = getLocalClassId(name);
+	Scope* parent = getParent();
+	if (classId == -1 && parent)
+		classId = parent->getClassId(name);
+	return classId;
+}
+
+ZClass* Scope::getLocalClass(string const& name) const
+{
+	int classId = getLocalClassId(name);
+	if (classId == -1) return NULL;
+	return table.getClass(classId);
+}
+
+ZClass* Scope::getClass(string const& name) const
+{
+	int classId = getClassId(name);
+	if (classId == -1) return NULL;
+	return table.getClass(classId);
+}
+
+int Scope::addClass(string const& name)
+{
+	return addClass(name, NULL);
+}
+
 // Variables
 
 int Scope::getVariableId(string const& name) const
@@ -112,6 +143,54 @@ int Scope::addVariable(string const& name, ZVarTypeId typeId)
 int Scope::addVariable(string const& name, ZVarType const& type)
 {
 	return addVariable(name, table.getOrAssignTypeId(type), NULL);
+}
+
+// Properties
+
+int Scope::getGetterId(int varId) const
+{
+	int id = getLocalGetterId(varId);
+	Scope* parent = getParent();
+	if (id == -1 && parent)
+		id = parent->getGetterId(varId);
+	return id;
+}
+
+int Scope::getSetterId(int varId) const
+{
+	int id = getLocalSetterId(varId);
+	Scope* parent = getParent();
+	if (id == -1 && parent)
+		id = parent->getSetterId(varId);
+	return id;
+}
+
+Scope::VariableAccess Scope::getRead(string const& name) const
+{
+	VariableAccess r;
+	r.variableId = getVariableId(name);
+	if (r.variableId != -1)
+		r.functionId = getGetterId(r.variableId);
+	return r;
+}
+
+Scope::VariableAccess Scope::getWrite(string const& name) const
+{
+	VariableAccess r;
+	r.variableId = getVariableId(name);
+	if (r.variableId != -1)
+		r.functionId = getSetterId(r.variableId);
+	return r;
+}
+
+int Scope::addGetter(int varId, vector<ZVarTypeId> const& paramTypeIds)
+{
+	return addGetter(varId, paramTypeIds, NULL);
+}
+
+int Scope::addSetter(int varId, vector<ZVarTypeId> const& paramTypeIds)
+{
+	return addSetter(varId, paramTypeIds, NULL);
 }
 
 // Functions
@@ -214,6 +293,26 @@ int BasicScope::addType(string const& name, ZVarTypeId typeId, AST* node)
 	return typeId;
 }
 
+// Classes
+
+int BasicScope::getLocalClassId(string const& name) const
+{
+	map<string, int>::const_iterator it = classes.find(name);
+	if (it != classes.end()) return it->second;
+	return -1;
+}
+
+int BasicScope::addClass(string const& name, AST* node)
+{
+	map<string, int>::const_iterator it = classes.find(name);
+	if (it != classes.end()) return -1;
+
+	int classId = table.createClass()->id;
+	classes[name] = classId;
+	if (node) table.putNodeId(node, classId);
+	return classId;
+}
+
 // Variables
 
 int BasicScope::getLocalVariableId(string const& name) const
@@ -232,6 +331,45 @@ int BasicScope::addVariable(string const& name, ZVarTypeId typeId, AST* node)
 	table.putVarTypeId(varId, typeId);
 	if (node) table.putNodeId(node, varId);
 	return varId;
+}
+
+// Properties
+
+int BasicScope::getLocalGetterId(int varId) const
+{
+	map<int, int>::const_iterator it = getters.find(varId);
+	if (it != getters.end()) return it->second;
+	return -1;
+}
+
+int BasicScope::getLocalSetterId(int varId) const
+{
+	map<int, int>::const_iterator it = setters.find(varId);
+	if (it != setters.end()) return it->second;
+	return -1;
+}
+
+int BasicScope::addGetter(int varId, vector<ZVarTypeId> const& paramTypeIds, AST* node)
+{
+	map<int, int>::const_iterator it = getters.find(varId);
+	if (it != getters.end()) return -1;
+	ZVarTypeId returnTypeId = table.getVarTypeId(varId);
+	int getterId = ScriptParser::getUniqueFuncID();
+	getters[varId] = getterId;
+	table.putFuncTypeIds(getterId, returnTypeId, paramTypeIds);
+	if (node) table.putNodeId(node, getterId);
+	return getterId;
+}
+
+int BasicScope::addSetter(int varId, vector<ZVarTypeId> const& paramTypeIds, AST* node)
+{
+	map<int, int>::const_iterator it = setters.find(varId);
+	if (it != setters.end()) return -1;
+	int setterId = ScriptParser::getUniqueFuncID();
+	setters[varId] = setterId;
+	table.putFuncTypeIds(setterId, table.getTypeId(ZVarType::VOID), paramTypeIds);
+	if (node) table.putNodeId(node, setterId);
+	return setterId;
 }
 
 // Functions
@@ -264,3 +402,7 @@ int BasicScope::addFunction(string const& name, ZVarTypeId returnTypeId, vector<
 	return funcId;
 }
 
+////////////////////////////////////////////////////////////////
+// ZClass
+
+ZClass::ZClass(SymbolTable& table, int id) : BasicScope(table), id(id) {}

src/parser/Scope.h
@@ -6,6 +6,13 @@
 class Scope
 {
 public:
+	struct VariableAccess
+	{
+		VariableAccess() : variableId(-1), functionId(-1) {}
+		int variableId;
+		int functionId;
+	};
+
 	static Scope* makeGlobalScope(SymbolTable& table);
 
 	Scope(SymbolTable& table);
@@ -24,9 +31,17 @@ public:
 	// Types
 	virtual int getLocalTypeId(string const& name) const = 0;
 	virtual int addType(string const& name, ZVarTypeId typeId, AST* node) = 0;
+	// Classes
+	virtual int getLocalClassId(string const& name) const = 0;
+	virtual int addClass(string const& name, AST* node) = 0;
 	// Variables
 	virtual int getLocalVariableId(string const& name) const = 0;
 	virtual int addVariable(string const& name, ZVarTypeId typeId, AST* node) = 0;
+	// Properties
+	virtual int getLocalGetterId(int varId) const = 0;
+	virtual int getLocalSetterId(int varId) const = 0;
+	virtual int addGetter(int varId, vector<ZVarTypeId> const& paramTypeIds, AST* node) = 0;
+	virtual int addSetter(int varId, vector<ZVarTypeId> const& paramTypeIds, AST* node) = 0;
 	// Functions
 	virtual void getLocalFunctionIds(vector<int>& ids, string const& name) const = 0;
 	virtual int getLocalFunctionId(FunctionSignature const& signature) const = 0;
@@ -45,11 +60,23 @@ public:
 	int addType(string const& name, ZVarType const& type, AST* node);
 	int addType(string const& name, ZVarTypeId typeId);
 	int addType(string const& name, ZVarType const& type);
+	// Classes
+	int getClassId(string const& name) const;
+	ZClass* getLocalClass(string const& name) const;
+	ZClass* getClass(string const& name) const;
+	int addClass(string const& name);
 	// Variables
 	int getVariableId(string const& name) const;
 	int addVariable(string const& name, ZVarType const& type, AST* node);
 	int addVariable(string const& name, ZVarTypeId typeId);
 	int addVariable(string const& name, ZVarType const& type);
+	// Properties
+	int getGetterId(int varId) const;
+	int getSetterId(int varId) const;
+	VariableAccess getRead(string const& name) const;
+	VariableAccess getWrite(string const& name) const;
+	int addGetter(int varId, vector<ZVarTypeId> const& paramTypeIds);
+	int addSetter(int varId, vector<ZVarTypeId> const& paramTypeIds);
 	// Functions
 	vector<int> getLocalFunctionIds(string const& name) const;
 	void getFunctionIds(vector<int>& ids, string const& name) const;
@@ -77,10 +104,20 @@ public:
 	int getLocalTypeId(string const& name) const;
 	using Scope::addType;
 	int addType(string const& name, ZVarTypeId typeId, AST* node);
+	// Classes
+	int getLocalClassId(string const& name) const;
+	int addClass(string const& name, AST* node);
 	// Variables
 	int getLocalVariableId(string const& name) const;
 	using Scope::addVariable;
 	int addVariable(string const& name, ZVarTypeId typeId, AST* node);
+	// Properties
+	int getLocalGetterId(int varId) const;
+	int getLocalSetterId(int varId) const;
+	using Scope::addGetter;
+	int addGetter(int varId, vector<ZVarTypeId> const& paramTypeIds, AST* node);
+	using Scope::addSetter;
+	int addSetter(int varId, vector<ZVarTypeId> const& paramTypeIds, AST* node);
 	// Functions
 	using Scope::getLocalFunctionIds;
 	void getLocalFunctionIds(vector<int>& ids, string const& name) const;
@@ -91,9 +128,19 @@ private:
     Scope* parent;
     map<string, Scope*> children;
 	map<string, int> types;
+	map<string, int> classes;
 	map<string, int> variables;
+	map<int, int> getters;
+	map<int, int> setters;
 	map<string, vector<int> > functionsByName;
 	map<FunctionSignature, int> functionsBySignature;
+};
+
+class ZClass : public BasicScope
+{
+public:
+	ZClass(SymbolTable& table, int id);
+	int const id;
 };
 
 #endif

src/parser/TypeChecker.cpp
@@ -248,67 +248,19 @@ void TypeCheck::caseExprArrow(ASTExprArrow &host)
         }
     }
 
-    ZVarTypeId type = host.getLVal()->getType();
-
-    string name = "get" + host.getName();
-    if (isIndexed) name += "[]";
-
-	int functionId;
-    switch (type)
-    {
-    case ZVARTYPEID_FFC:
-    {
-        functionId = FFCSymbols::getInst().matchFunction(name);
-        break;
-    }
-    case ZVARTYPEID_LINK:
-    {
-        functionId = LinkSymbols::getInst().matchFunction(name);
-        break;
-    }
-    case ZVARTYPEID_SCREEN:
-    {
-        functionId = ScreenSymbols::getInst().matchFunction(name);
-        break;
-    }
-    case ZVARTYPEID_GAME:
-    {
-        functionId = GameSymbols::getInst().matchFunction(name);
-        break;
-    }
-    case ZVARTYPEID_ITEM:
-    {
-        functionId = ItemSymbols::getInst().matchFunction(name);
-        break;
-    }
-    case ZVARTYPEID_ITEMCLASS:
-    {
-        functionId = ItemclassSymbols::getInst().matchFunction(name);
-        break;
-    }
-    case ZVARTYPEID_NPC:
-    {
-        functionId = NPCSymbols::getInst().matchFunction(name);
-        break;
-    }
-    case ZVARTYPEID_LWPN:
-    {
-        functionId = LinkWeaponSymbols::getInst().matchFunction(name);
-        break;
-    }
-    case ZVARTYPEID_EWPN:
-    {
-        functionId = EnemyWeaponSymbols::getInst().matchFunction(name);
-        break;
-    }
-    default:
+    ZVarTypeId typeId = host.getLVal()->getType();
+	LibrarySymbols* lib = LibrarySymbols::getTypeInstance(typeId);
+	if (!lib)
+	{
         failure = true;
         printErrorMsg(&host, ARROWNOTPOINTER);
         return;
-    }
+	}
+
+	int functionId = lib->matchGetter(host.getName());
 
 	vector<ZVarTypeId> functionParams = symbolTable.getFuncParamTypeIds(functionId);
-    if (functionId == -1 || functionParams.size() != (isIndexed ? 2 : 1) || functionParams[0] != type)
+    if (functionId == -1 || functionParams.size() != (isIndexed ? 2 : 1) || functionParams[0] != typeId)
     {
         failure = true;
         printErrorMsg(&host, ARROWNOVAR, host.getName() + (isIndexed ? "[]" : ""));
@@ -478,62 +430,12 @@ void TypeCheck::caseFuncCall(ASTFuncCall &host)
         // Luckily I will here assert that each type's functions MUST be unique
 		ASTExprArrow& arrow = *(ASTExprArrow*)host.getName();
         string name = arrow.getName();
-        ZVarTypeId type = arrow.getLVal()->getType();
+        ZVarTypeId typeId = arrow.getLVal()->getType();
 
-		int functionId;
-		switch(type)
-		{
-		case ZVARTYPEID_FFC:
-		{
-			functionId = FFCSymbols::getInst().matchFunction(name);
-			break;
-		}
-		case ZVARTYPEID_LINK:
-		{
-			functionId = LinkSymbols::getInst().matchFunction(name);
-			break;
-		}
-		case ZVARTYPEID_SCREEN:
-		{
-			functionId = ScreenSymbols::getInst().matchFunction(name);
-			break;
-		}
-		case ZVARTYPEID_GAME:
-		{
-			functionId = GameSymbols::getInst().matchFunction(name);
-			break;
-		}
-		case ZVARTYPEID_ITEM:
-		{
-			functionId = ItemSymbols::getInst().matchFunction(name);
-			break;
-		}
-		case ZVARTYPEID_ITEMCLASS:
-		{
-			functionId = ItemclassSymbols::getInst().matchFunction(name);
-			break;
-		}
-		case ZVARTYPEID_NPC:
-		{
-			functionId = NPCSymbols::getInst().matchFunction(name);
-			break;
-		}
-		case ZVARTYPEID_LWPN:
-		{
-			functionId = LinkWeaponSymbols::getInst().matchFunction(name);
-			break;
-		}
-		case ZVARTYPEID_EWPN:
-		{
-			functionId = EnemyWeaponSymbols::getInst().matchFunction(name);
-			break;
-		}
-		default:
-			assert(false);
-		}
+		LibrarySymbols* lib = LibrarySymbols::getTypeInstance(typeId);
+		assert(lib);
 
-		vector<ZVarTypeId> functionParams = symbolTable.getFuncParamTypeIds(functionId);
-
+		int functionId = lib->matchFunction(name);
         if (functionId == -1)
         {
             failure = true;
@@ -541,6 +443,7 @@ void TypeCheck::caseFuncCall(ASTFuncCall &host)
             return;
         }
 
+		vector<ZVarTypeId> functionParams = symbolTable.getFuncParamTypeIds(functionId);
         if (paramtypes.size() != functionParams.size())
         {
             failure = true;
@@ -1086,62 +989,15 @@ void GetLValType::caseExprArrow(ASTExprArrow &host)
         }
     }
 
-    string name = "set" + host.getName();
-    if (isIndexed) name += "[]";
-
-	int functionId;
-    switch (type)
-    {
-    case ZVARTYPEID_FFC:
-    {
-        functionId = FFCSymbols::getInst().matchFunction(name);
-        break;
-    }
-    case ZVARTYPEID_LINK:
-    {
-        functionId = LinkSymbols::getInst().matchFunction(name);
-        break;
-    }
-    case ZVARTYPEID_SCREEN:
-    {
-        functionId = ScreenSymbols::getInst().matchFunction(name);
-        break;
-    }
-    case ZVARTYPEID_GAME:
-    {
-        functionId = GameSymbols::getInst().matchFunction(name);
-        break;
-    }
-    case ZVARTYPEID_ITEM:
-    {
-        functionId = ItemSymbols::getInst().matchFunction(name);
-        break;
-    }
-    case ZVARTYPEID_ITEMCLASS:
-    {
-        functionId = ItemclassSymbols::getInst().matchFunction(name);
-        break;
-    }
-    case ZVARTYPEID_NPC:
-    {
-        functionId = NPCSymbols::getInst().matchFunction(name);
-        break;
-    }
-    case ZVARTYPEID_LWPN:
-    {
-        functionId = LinkWeaponSymbols::getInst().matchFunction(name);
-        break;
-    }
-    case ZVARTYPEID_EWPN:
-    {
-        functionId = EnemyWeaponSymbols::getInst().matchFunction(name);
-        break;
-    }
-    default:
-        typeCheck.fail();
+	LibrarySymbols* lib = LibrarySymbols::getTypeInstance(type);
+	if (!lib)
+	{
         printErrorMsg(&host, ARROWNOTPOINTER);
+		typeCheck.fail();
         return;
-    }
+	}
+	int functionId = lib->matchSetter(host.getName());
+
 
 	vector<ZVarTypeId> functionParams = typeCheck.symbolTable.getFuncParamTypeIds(functionId);
     if (functionId == -1 || functionParams.size() != (isIndexed ? 3 : 2) || functionParams[0] != type)

src/parser/Types.cpp
@@ -4,18 +4,18 @@
 #include "Scope.h"
 
 // Standard Type definitions.
-ZVarTypeSimple const ZVarType::VOID(ZVARTYPEID_VOID, "void");
-ZVarTypeSimple const ZVarType::FLOAT(ZVARTYPEID_FLOAT, "float");
-ZVarTypeSimple const ZVarType::BOOL(ZVARTYPEID_BOOL, "bool");
-ZVarTypeSimple const ZVarType::FFC(ZVARTYPEID_FFC, "ffc");
-ZVarTypeSimple const ZVarType::ITEM(ZVARTYPEID_ITEM, "item");
-ZVarTypeSimple const ZVarType::ITEMCLASS(ZVARTYPEID_ITEMCLASS, "itemdata");
-ZVarTypeSimple const ZVarType::NPC(ZVARTYPEID_NPC, "npc");
-ZVarTypeSimple const ZVarType::LWPN(ZVARTYPEID_LWPN, "lweapon");
-ZVarTypeSimple const ZVarType::EWPN(ZVARTYPEID_EWPN, "eweapon");
-ZVarTypeSimple const ZVarType::GAME(ZVARTYPEID_GAME, "game");
-ZVarTypeSimple const ZVarType::LINK(ZVARTYPEID_LINK, "link");
-ZVarTypeSimple const ZVarType::SCREEN(ZVARTYPEID_SCREEN, "screen");
+ZVarTypeSimple const ZVarType::VOID(ZVARTYPEID_VOID, "void", "Void");
+ZVarTypeSimple const ZVarType::FLOAT(ZVARTYPEID_FLOAT, "float", "Float");
+ZVarTypeSimple const ZVarType::BOOL(ZVARTYPEID_BOOL, "bool", "Bool");
+ZVarTypeSimple const ZVarType::FFC(ZVARTYPEID_FFC, "ffc", "FFC");
+ZVarTypeSimple const ZVarType::ITEM(ZVARTYPEID_ITEM, "item", "Item");
+ZVarTypeSimple const ZVarType::ITEMCLASS(ZVARTYPEID_ITEMCLASS, "itemdata", "ItemData");
+ZVarTypeSimple const ZVarType::NPC(ZVARTYPEID_NPC, "npc", "NPC");
+ZVarTypeSimple const ZVarType::LWPN(ZVARTYPEID_LWPN, "lweapon", "LWeapon");
+ZVarTypeSimple const ZVarType::EWPN(ZVARTYPEID_EWPN, "eweapon", "EWeapon");
+ZVarTypeSimple const ZVarType::GAME(ZVARTYPEID_GAME, "game", "Game");
+ZVarTypeSimple const ZVarType::LINK(ZVARTYPEID_LINK, "link", "Link");
+ZVarTypeSimple const ZVarType::SCREEN(ZVARTYPEID_SCREEN, "screen", "Screen");
 ZVarTypeConstFloat const ZVarType::CONST_FLOAT;
 
 ////////////////////////////////////////////////////////////////

commit e17c8edeabaac945fe4cedf7c432e5875be496c6

Fix builtin getters/setters.
Bug introduced in "4dc5753 * Added separate scopes for the builtin classes.".

src/parser/GlobalSymbols.cpp
@@ -59,35 +59,36 @@ void LibrarySymbols::addSymbolsToScope(Scope& scope)
         for (int k = 0; entry.params[k] != -1 && k < 20; k++)
             paramTypeIds.push_back(entry.params[k]);
 
+		string varName = name;
 
 		// Strip out the array at the end.
 		bool isArray = name.substr(name.size() - 2) == "[]";
 		if (isArray)
-			name = name.substr(0, name.size() - 2);
+			varName = name.substr(0, name.size() - 2);
 
 		if (entry.setorget == SETTER && name.substr(0, 3) == "set")
 		{
-			name = name.substr(3); // Strip out "set".
-			int varId = scope.getLocalVariableId(name);
+			varName = varName.substr(3); // Strip out "set".
+			int varId = scope.getLocalVariableId(varName);
 			if (varId == -1)
-				varId = scope.addVariable(name, paramTypeIds[1]);
+				varId = scope.addVariable(varName, paramTypeIds[1]);
 			int functionId = scope.addSetter(varId, paramTypeIds);
 			assert(functionId != -1);
 			setters[name] = functionId;
 		}
 		else if (entry.setorget == GETTER && name.substr(0, 3) == "get")
 		{
-			name = name.substr(3); // Strip out "get".
-			int varId = scope.getLocalVariableId(name);
+			varName = varName.substr(3); // Strip out "get".
+			int varId = scope.getLocalVariableId(varName);
 			if (varId == -1)
-				varId = scope.addVariable(name, entry.rettype);
+				varId = scope.addVariable(varName, entry.rettype);
 			int functionId = scope.addGetter(varId, paramTypeIds);
 			assert(functionId != -1);
 			getters[name] = functionId;
 		}
 		else
 		{
-			int functionId = scope.addFunction(name, entry.rettype, paramTypeIds);
+			int functionId = scope.addFunction(varName, entry.rettype, paramTypeIds);
 			assert(functionId != -1);
 			functions[name] = functionId;
 		}
@@ -118,6 +119,18 @@ int LibrarySymbols::matchSetter(string const& name)
 	return -1;
 }
 
+int LibrarySymbols::getFunctionId(string const& name) const
+{
+	map<string, int>::const_iterator it;
+	it = functions.find(name);
+	if (it != functions.end()) {return it->second;}
+	it = getters.find(name);
+	if (it != getters.end()) {return it->second;}
+	it = setters.find(name);
+	if (it != setters.end()) {return it->second;}
+	return 0;
+}
+
 map<int, vector<Opcode *> > LibrarySymbols::addSymbolsCode(LinkTable &lt)
 {
     map<int, vector<Opcode *> > rval;
@@ -127,7 +140,7 @@ map<int, vector<Opcode *> > LibrarySymbols::addSymbolsCode(LinkTable &lt)
         int var = table[i].var;
         string name = table[i].name;
         bool isIndexed = table[i].numindex > 1;
-        int id = functions[name];
+        int id = getFunctionId(name);
         int label = lt.functionToLabel(id);
         
         switch(table[i].setorget)

src/parser/GlobalSymbols.h
@@ -46,6 +46,7 @@ protected:
     map<string, int> functions;
 	map<string, int> getters;
 	map<string, int> setters;
+	int getFunctionId(string const& name) const;
     virtual vector<Opcode *> getVariable(LinkTable &lt, int id, int var);
     virtual vector<Opcode *> setVariable(LinkTable &lt, int id, int var);
     virtual vector<Opcode *> setBoolVariable(LinkTable &lt, int id, int var);