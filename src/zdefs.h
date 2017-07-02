b6e7101ad3793d5c6d7a8dc887f6cbddd0f68f81
ZoriaRPG <magesguildsociety@gmail.com>
2017-07-02 01:04:34 +0100

    chore: Updated build defs.
    changelog: Bumped ZC Build ID to 31.
    Added and corrected some version strings.

--- src/zdefs.h
+++ src/zdefs.h
@@ -95,11 +95,11 @@
 #include "zc_array.h"
 
 #define ZELDA_VERSION       0x0250                          //version of the program
-#define VERSION_BUILD       30                              //build number of this version
-#define ZELDA_VERSION_STR   "2.50.3 RC1"                    //version of the program as presented in text
+#define VERSION_BUILD       31                              //build number of this version
+#define ZELDA_VERSION_STR   "2.53 Beta 1"                    //version of the program as presented in text
 #define IS_BETA             1                               //is this a beta? (1: beta, -1: alpha)
-#define DATE_STR            "December 04, 2016"
-#define COPYRIGHT_YEAR      "2016"                          //shown on title screen and in ending
+#define DATE_STR            "2nd July, 2017"
+#define COPYRIGHT_YEAR      "2017"                          //shown on title screen and in ending
 
 #define MIN_VERSION         0x0184
 
