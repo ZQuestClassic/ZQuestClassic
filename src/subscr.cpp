e20c99b6565488bbc3d29735f5d613d4bf6c65ff
Etienne Vouga <evouga@gmail.com>
2017-09-06 00:11:27 -0500

    Apply Saffith's fix from 2.50.x to master. Fixes crash when printing subscreen item names and Link possesses only the arrows (without bow or any other items).

--- src/subscr.cpp
+++ src/subscr.cpp
@@ -1468,7 +1468,7 @@ void subscreen_object_selecteditemname::show(BITMAP *dest, const subscreen_group
     switch(curQuest->getItemDefinition(itemid).family)
     {
     case itype_arrow:
-        if(Bitem->dummy_bool[0]==true)  //if we also have a bow
+        if(Bitem && Bitem->dummy_bool[0]==true)  //if we also have a bow
         {
             if(curQuest->isValid(current_item_id(itype_bow)))
             {
