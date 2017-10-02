d7ad79420abda98b24be6febee0678ad0d08bf12
ZoriaRPG <magesguildsociety@gmail.com>
2017-10-02 23:08:06 +0100

    Chore: Clean up unimplemented Debug Console dialogue. Changelog: Removed remnants of Debug Console dialogue that was replaced with a jwin alert.

--- src/zc_sys.cpp
+++ src/zc_sys.cpp
@@ -4671,37 +4671,6 @@ int onClickToFreeze()
     return D_O_K;
 }
 
-
-static DIALOG debugconsole_dlg[] =
-{
-    /* (dialog proc)       (x)   (y)   (w)   (h)   (fg)     (bg)     (key)    (flags)    (d1)      (d2)     (dp)     (dp2) (dp3) */
-    { jwin_win_proc,       40,   38,   241,  173,  vc(14),  vc(1),   0,       D_EXIT,    0,        0, (void *) "ZC Debug COnsole", NULL,  NULL },
-    
-    
-    { jwin_text_proc,         52,   70,  140,  8,    vc(0),   vc(11),  0,       0,         0,        0, (void *) "WARNING: Closing the console window, by using the ", NULL,  NULL },
-    { jwin_text_proc,         52,   80,  140,  8,    vc(0),   vc(11),  0,       0,         0,        0, (void *) "close window widget will terminate ZC!", NULL,  NULL },
-    { jwin_text_proc,         52,   90,  140,  8,    vc(0),   vc(11),  0,       0,         0,        0, (void *) "To close the debug console, use the SHOW DEBUG CONSOLE", NULL,  NULL },
-    { jwin_text_proc,         52,   100,  140,  8,    vc(0),   vc(11),  0,       0,         0,        0, (void *) "menu uption again!", NULL,  NULL },
-    
-    { jwin_frame_proc,     47,   65,   227,  115,  vc(15),  vc(1),   0,       0,         FR_DEEP,  0,       NULL, NULL,  NULL },
-    { d_bitmap_proc,       49,   67,   222,  110,  vc(15),  vc(1),   0,       0,         0,        0,       NULL, NULL,  NULL },
-    { jwin_button_proc,    60,  184,  41,   21,   vc(14),  vc(1),   0,       D_EXIT,    0,        0, (void *) "OK", NULL,  NULL },
-    { NULL,                 0,    0,    0,    0,   0,       0,       0,       0,          0,             0,       NULL,                           NULL,  NULL }
-};
-
-//Warning Info Box
-int onDoDebugConsole()
-{
-	
-    debugconsole_dlg[0].dp2=lfont;
-    
-    if(is_large)
-        large_dialog(debugconsole_dlg);
-        
-    zc_popup_dialog(debugconsole_dlg,1);
-    return D_O_K;
-}
-
 int onDebugConsole()
 {
 	if ( !zconsole ) {
@@ -4709,7 +4678,7 @@ int onDebugConsole()
 		//if ( ret == D_O_K )
 		//{
 		char buf[100];
-		jwin_alert("WARNING: Closing the Debug Console", "WARNING: Closing the console window by using the close window widget will TERMINATE ZC!", "To SAFELY close the debug console, use the SHOW DEBUG CONSOLE menu uption again!",
+		jwin_alert("WARNING: Closing the Debug Console", "WARNING: Closing the console window by using the close window widget will TERMINATE ZC!", "To SAFELY close the debug console, use the SHOW DEBUG CONSOLE menu option again!",
 		buf, "O&K", NULL, 'o', 0, lfont);
 		
 		
