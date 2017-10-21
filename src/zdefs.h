9ffd2d98d1a3db3b930e39ed4c4356d5d153414c
ZoriaRPG <magesguildsociety@gmail.com>
2017-10-21 02:32:31 +0100

    Chore: Modified Version Information and Strings

--- src/zdefs.h
+++ src/zdefs.h
@@ -96,11 +96,12 @@
 
 #define ZELDA_VERSION       0x0254                          //version of the program
 #define ZC_VERSION 25400 //Version ID for ZScript Game->Version
-#define VERSION_BUILD       31                              //build number of this version
+#define VERSION_BUILD       40                              //build number of this version
+//31 == 2.53.0 , leaving 32-39 for bugfixes, and jumping to 40. 
 #define ZELDA_VERSION_STR   "2.54 Alpha 1"                    //version of the program as presented in text
 #define IS_BETA             -1                              //is this a beta? (1: beta, -1: alpha)
 #define VERSION_BETA        00010
-#define DATE_STR            "19th October, 2017"
+#define DATE_STR            "21st October, 2017"
 #define COPYRIGHT_YEAR      "2017"                          //shown on title screen and in ending
 
 #define MIN_VERSION         0x0184
