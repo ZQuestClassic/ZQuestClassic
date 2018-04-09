0b567e283c6d61385b3ac067df2899d610342a35
ZoriaRPG <zoriarpg@gmail.com>
2018-04-09 23:27:56 +0100

    Feat: More whistle code. Not sure why it isn`t hurting anything?!

--- src/weapons.cpp
+++ src/weapons.cpp
@@ -4028,7 +4028,7 @@ offscreenCheck:
     case wLitSBomb:
         if(!clipped) dead=1;
         
-    case wWhistle:
+    //case wWhistle:
     case wBomb:
     case wSBomb:
     case wBait:
@@ -4041,7 +4041,11 @@ offscreenCheck:
         if(!clipped) dead=1;
         
         break;
+    case wWhistle:
+	if(!clipped) dead=1;
         
+        break;    
+    
     case ewFlame:
         if(!clipped) dead=1;
         
