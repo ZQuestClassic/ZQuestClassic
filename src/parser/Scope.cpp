b458d0d2e1139fec52cd8f4bfd5cd9dfea2cde22
gwx <grayswx@gmail.com>
2018-08-22 22:41:03 -0400

    fix: Add back in missing tokens to scanner

--- src/parser/Scope.cpp
+++ src/parser/Scope.cpp
@@ -655,7 +655,6 @@ RootScope::RootScope(TypeStore& typeStore)
 	BuiltinConstant::create(*this, DataType::TEXT, "Text", 0);
 	BuiltinConstant::create(*this, DataType::COMBOS, "ComboData", 0);
 	BuiltinConstant::create(*this, DataType::SPRITEDATA, "SpriteData", 0);
-	BuiltinConstant::create(*this, DataType::GRAPHICS, "Graphics", 0);
 	BuiltinConstant::create(*this, DataType::INPUT, "Input", 0);
 	BuiltinConstant::create(*this, DataType::MAPDATA, "MapData", 0);
 	BuiltinConstant::create(*this, DataType::DMAPDATA, "DMapData", 0);
