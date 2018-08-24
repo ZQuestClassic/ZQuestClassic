ea22118a1ebf2d270cb6dc444d13c3259cd0a690
ZoriaRPG <zoriarpg@gmail.com>
2018-08-24 04:53:14 +0100

    Version: Updated build IDs

--- src/zdefs.h
+++ src/zdefs.h
@@ -99,10 +99,10 @@
 #define ZC_VERSION 25400 //Version ID for ZScript Game->Version
 #define VERSION_BUILD       40                              //build number of this version
 //31 == 2.53.0 , leaving 32-39 for bugfixes, and jumping to 40. 
-#define ZELDA_VERSION_STR   "2.54 Alpha 34"                    //version of the program as presented in text
-#define IS_BETA             -34                         //is this a beta? (1: beta, -1: alpha)
-#define VERSION_BETA        34
-#define DATE_STR            "21st August, 2018"
+#define ZELDA_VERSION_STR   "2.54 Alpha 35"                    //version of the program as presented in text
+#define IS_BETA             -35                         //is this a beta? (1: beta, -1: alpha)
+#define VERSION_BETA        35
+#define DATE_STR            "23rd August, 2018"
 #define COPYRIGHT_YEAR      "2018"                          //shown on title screen and in ending
 
 #define MIN_VERSION         0x0184
