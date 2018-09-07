fafe88b2ecaae6d8e408dfaec3f403bed17a0063
ZoriaRPG <zoriarpg@gmail.com>
2018-09-07 07:34:59 +0100

    Fix: Compile on MSVC

--- src/parser/CompilerUtils.cpp
+++ src/parser/CompilerUtils.cpp
@@ -4,6 +4,7 @@
 #include <cstdarg>
 #include <cstdio>
 
+/*
 std::string formatStr(std::string const* format, ...)
 {
 	static char buffer[formatBufferSize];
@@ -15,3 +16,4 @@ std::string formatStr(std::string const* format, ...)
 
 	return std::string(buffer);
 }
+*/
