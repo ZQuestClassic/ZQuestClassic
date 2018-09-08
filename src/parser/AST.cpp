ZQuestClassic/ZQuestClassic

commit 380f1c8f64f2d370d92e622be94e5b638922788c

fix: Properly initialize global arrays
Now global array declarations entirely defer to their initializers,
which properly recognize if they're in global scope.

src/parser/BuildVisitors.cpp
@@ -434,25 +434,8 @@ void BuildOpcodes::buildArrayInit(ASTDataDecl& host, OpcodeContext& context)
 {
 	Datum& manager = *host.manager;
 
-	// Initializer creates the array and loads the array id into EXP1.
+	// Initializer should take care of everything.
 	visit(host.initializer(), &context);
-
-	// Set variable to EXP1.
-	if (optional<int> globalId = manager.getGlobalId())
-	{
-		addOpcode(new OSetRegister(new GlobalArgument(*globalId),
-		                           new VarArgument(EXP1)));
-	}
-	else
-	{
-		int offset = 10000L * *getStackOffset(manager);
-		addOpcode(new OSetRegister(new VarArgument(SFTEMP),
-		                           new VarArgument(SFRAME)));
-		addOpcode(new OAddImmediate(new VarArgument(SFTEMP),
-		                            new LiteralArgument(offset)));
-		addOpcode(new OStoreIndirect(new VarArgument(EXP1),
-		                             new VarArgument(SFTEMP)));
-	}
 }
 
 void BuildOpcodes::buildArrayUninit(
@@ -1106,6 +1089,8 @@ void BuildOpcodes::caseBoolLiteral(ASTBoolLiteral& host, void*)
     addOpcode(new OSetImmediate(new VarArgument(EXP1), new LiteralArgument(*host.getCompileTimeValue(this))));
 }
 
+
+
 // TODO add implicit stackframe to global case for literals.
 void BuildOpcodes::caseStringLiteral(ASTStringLiteral& host, void* param)
 {
@@ -1184,25 +1169,85 @@ void BuildOpcodes::caseStringLiteral(ASTStringLiteral& host, void* param)
 void BuildOpcodes::caseArrayLiteral(ASTArrayLiteral& host, void* param)
 {
 	OpcodeContext& context = *(OpcodeContext*)param;
+	if (host.declaration) arrayLiteralDeclaration(host, context);
+	else arrayLiteralFree(host, context);
+}
+
+void BuildOpcodes::arrayLiteralDeclaration(
+		ASTArrayLiteral& host, OpcodeContext& context)
+{
+	ASTDataDecl& declaration = *host.declaration;
+	Datum& manager = *declaration.manager;
+
+	// Grab the size from the declaration.
+	int size = -1;
+	ASTDataDeclExtraArray& extraArray = *declaration.extraArrays[0];
+	if (optional<int> totalSize = extraArray.getCompileTimeSize(this))
+		size = *totalSize;
+	else if (extraArray.hasSize())
+	{
+		handleError(CompileError::ExprNotConstant, &host);
+		return;
+	}
+
+	// Otherwise, grab the number of elements as the size.
+	if (size == -1) size = host.elements.size();
+
+	// Make sure the chosen size has enough space.
+	if (size < int(host.elements.size()))
+	{
+		handleError(CompileError::ArrayListTooLarge, &host);
+		return;
+	}
+
+	// Create the array and store its id.
+	if (optional<int> globalId = manager.getGlobalId())
+	{
+		addOpcode(new OAllocateGlobalMemImmediate(
+				          new VarArgument(EXP1),
+				          new LiteralArgument(size * 10000L)));
+		addOpcode(new OSetRegister(new GlobalArgument(*globalId),
+		                           new VarArgument(EXP1)));
+	}
+	else
+	{
+		addOpcode(new OAllocateMemImmediate(new VarArgument(EXP1),
+		                                    new LiteralArgument(size * 10000L)));
+		int offset = 10000L * *getStackOffset(manager);
+		addOpcode(new OSetRegister(new VarArgument(SFTEMP), new VarArgument(SFRAME)));
+		addOpcode(new OAddImmediate(new VarArgument(SFTEMP), new LiteralArgument(offset)));
+		addOpcode(new OStoreIndirect(new VarArgument(EXP1), new VarArgument(SFTEMP)));
+		// Register for cleanup.
+		arrayRefs.push_back(offset);
+	}
+
+	// Initialize array.
+	addOpcode(new OSetRegister(new VarArgument(INDEX),
+	                           new VarArgument(EXP1)));
+	long i = 0;
+	vector<ASTExpr*> elements = host.elements;
+	for (vector<ASTExpr*>::const_iterator it = elements.begin();
+		 it != elements.end(); ++it, i += 10000L)
+	{
+		addOpcode(new OPushRegister(new VarArgument(INDEX)));
+		visit(*it, &context);
+		addOpcode(new OPopRegister(new VarArgument(INDEX)));
+		addOpcode(new OSetImmediate(new VarArgument(INDEX2),
+		                            new LiteralArgument(i)));
+		addOpcode(new OSetRegister(new VarArgument(SCRIPTRAM),
+		                           new VarArgument(EXP1)));
+	}
+}
+
+void BuildOpcodes::arrayLiteralFree(
+		ASTArrayLiteral& host, OpcodeContext& context)
+{
 	Literal& manager = *host.manager;
 
 	int size = -1;
 
-	// If this is part of an array declaration, grab the size info from it.
-	if (host.declaration)
-	{
-		ASTDataDeclExtraArray& extraArray = *host.declaration->extraArrays[0];
-		if (optional<int> totalSize = extraArray.getCompileTimeSize(this))
-			size = *totalSize;
-		else if (extraArray.hasSize())
-		{
-			handleError(CompileError::ExprNotConstant, &host);
-			return;
-		}
-	}
-
 	// If there's an explicit size, grab it.
-	else if (host.size)
+	if (host.size)
 	{
 		if (optional<long> s = host.size->getCompileTimeValue(this))
 			size = *s / 10000L;
@@ -1251,7 +1296,7 @@ void BuildOpcodes::caseArrayLiteral(ASTArrayLiteral& host, void* param)
 	{
 		context.initCode.push_back(new OPushRegister(new VarArgument(INDEX)));
 		opcodeTargets.push_back(&context.initCode);
-		visit(*it, param);
+		visit(*it, &context);
 		opcodeTargets.pop_back();
 		context.initCode.push_back(new OPopRegister(new VarArgument(INDEX)));
 		context.initCode.push_back(

src/parser/BuildVisitors.h
@@ -108,6 +108,11 @@ private:
 	void buildArrayInit(ASTDataDecl& host, OpcodeContext& context);
 	// For when ASTDataDecl is an uninitialized array.
 	void buildArrayUninit(ASTDataDecl& host, OpcodeContext& context);
+	// For when ASTArrayLiteral is a declaration initializer.
+	void arrayLiteralDeclaration(
+			ASTArrayLiteral& host, OpcodeContext& context);
+	// For when ASTArrayLiteral is not a declaration initializer.
+	void arrayLiteralFree(ASTArrayLiteral& host, OpcodeContext& context);
 };
 
 class LValBOHelper : public ASTVisitor

commit 7065617e4de04d257381c2fb5c76b50317982aed

fix: Use array init code even if not declared an array
Errors introduced by 380f1c8f64f2d370d92e622be94e5b638922788c.

src/parser/AST.cpp
@@ -1096,6 +1096,15 @@ DataType const* ASTDataDecl::resolveType(ZScript::Scope* scope) const
 	return type;
 }
 
+bool ZScript::hasSize(ASTDataDecl const& decl)
+{
+	for (vector<ASTDataDeclExtraArray*>::const_iterator it =
+		     decl.extraArrays.begin();
+	     it != decl.extraArrays.end(); ++it)
+		if ((*it)->hasSize()) return true;
+	return false;
+}
+
 // ASTDataDeclExtraArray
 
 ASTDataDeclExtraArray::ASTDataDeclExtraArray(LocationData const& location)

src/parser/AST.h
@@ -737,6 +737,8 @@ namespace ZScript
 		ASTExpr* mInitializer;
 	};
 
+	bool hasSize(ASTDataDecl const&);
+
 	// The extra array parameters appended to a data declaration name.
 	class ASTDataDeclExtraArray : public AST
 	{

src/parser/BuildVisitors.cpp
@@ -387,12 +387,15 @@ void BuildOpcodes::caseDataDecl(ASTDataDecl& host, void* param)
 {
     OpcodeContext& context = *(OpcodeContext*)param;
 	Datum& manager = *host.manager;
+	ASTExpr* init = host.initializer();
 
 	// Ignore inlined values.
 	if (manager.getCompileTimeValue()) return;
 
 	// Switch off to the proper helper function.
-	if (manager.type.isArray())
+	if (manager.type.isArray()
+	    || (init && (init->isArrayLiteral()
+	                 || init->isStringLiteral())))
 	{
 		if (host.initializer()) buildArrayInit(host, context);
 		else buildArrayUninit(host, context);
@@ -1239,9 +1242,14 @@ void BuildOpcodes::arrayLiteralDeclaration(
 	ASTDataDecl& declaration = *host.declaration;
 	Datum& manager = *declaration.manager;
 
-	// Grab the size from the declaration.
+	// Find the size.
 	int size = -1;
-	if (declaration.extraArrays.size() == 1)
+	// From this literal?
+	if (host.size)
+		if (optional<long> s = host.size->getCompileTimeValue(this))
+			size = *s / 10000L;
+	// From the declaration?
+	if (size == -1 && declaration.extraArrays.size() == 1)
 	{
 		ASTDataDeclExtraArray& extraArray = *declaration.extraArrays[0];
 		if (optional<int> totalSize = extraArray.getCompileTimeSize(this))
@@ -1252,10 +1260,16 @@ void BuildOpcodes::arrayLiteralDeclaration(
 			return;
 		}
 	}
-
 	// Otherwise, grab the number of elements as the size.
 	if (size == -1) size = host.elements.size();
 
+	// Make sure we have a valid size.
+	if (size < 1)
+	{
+		handleError(CompileError::ArrayTooSmall(&host));
+		return;
+	}
+	
 	// Make sure the chosen size has enough space.
 	if (size < int(host.elements.size()))
 	{

src/parser/BuildVisitors.h
@@ -75,7 +75,7 @@ namespace ZScript
 		virtual void caseArrayLiteral(ASTArrayLiteral& host, void* param);
 		virtual void caseOptionValue(ASTOptionValue& host, void* param);
 		// Types
-		void caseVarType(ASTDataType& host, void* param) {}
+		void caseDataType(ASTDataType& host, void* param) {}
 
 		vector<Opcode *> getResult() const {return result;}
 		int getReturnLabelID() const {return returnlabelid;}

src/parser/CompileError.xtable
@@ -55,7 +55,7 @@ X(	DeprecatedGlobal,		S,	D															)
 X(	VoidArr,			S,	A,	E,	STR,	VOID,	"Array %s cannot have base type void."							)
 X(	RefArr,				S,	A,	E,	STR,	VOID,	"Arrays of type %s are not allowed to be global."					)
 X(	ArrRedef,			S,	D					   	       	   	      	 						)
-X(	ArrayTooSmall,			A,	A,	E,	STR,	VOID,	"Arrays must have a size of at least 1."						)
+X(	ArrayTooSmall,			A,	A,	E,	VOID,	VOID,	"Arrays must have a size of at least 1."						)
 X(	ArrayListTooLarge,		A,	A,	E,	VOID,	VOID,	"The array's size is set to be shorter than its initial contents."          		)
 X(	ArrayListStringTooLarge,	A,	A,	E,	VOID,	VOID,	"The array's size is set to be shorter than its initial contents.\n"
 										"    (strings take up 1 more space than the number of characters in them.)"		)

src/parser/SemanticAnalyzer.cpp
@@ -907,7 +907,7 @@ void SemanticAnalyzer::caseArrayLiteral(ASTArrayLiteral& host, void*)
 	}
 
 	// Don't allow an explicit size if we're part of a declaration.
-	if (host.size && host.declaration)
+	if (host.size && host.declaration && hasSize(*host.declaration))
 	{
 		handleError(CompileError::ArrayLiteralResize(&host));
 		return;