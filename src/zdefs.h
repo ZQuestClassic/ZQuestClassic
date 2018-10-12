17b793f748dbd1c99140a4c8ade07a3198fdf66c
Robert Venezia <rvenezia@cs.hartford.edu>
2018-10-11 22:17:36 -0400

    Chore: Fix typo, Re-order Item QR dialog, update V_ITEMS to 43

--- src/zdefs.h
+++ src/zdefs.h
@@ -180,7 +180,7 @@ enum {ENC_METHOD_192B104=0, ENC_METHOD_192B105, ENC_METHOD_192B185, ENC_METHOD_2
 #define V_MAPS            19
 #define V_DMAPS            11
 #define V_DOORS            1
-#define V_ITEMS           42
+#define V_ITEMS           43
 #define V_WEAPONS          7
 #define V_COLORS           3 //Misc Colours
 #define V_ICONS            10 //Game Icons
