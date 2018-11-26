d1ca978c63beaaa2158291e3859d6962f41eec3f
ZoriaRPG <zoriarpg@gmail.com>
2018-11-26 10:56:35 +0000

    Vers: Update version strings and change Necromancer to AEternal

--- src/zdefs.h
+++ src/zdefs.h
@@ -99,11 +99,11 @@
 #define ZC_VERSION 25500 //Version ID for ZScript Game->Version
 #define VERSION_BUILD       41                              //build number of this version
 //31 == 2.53.0 , leaving 32-39 for bugfixes, and jumping to 40. 
-#define ZELDA_VERSION_STR   "Necromancer (v2.55) Alpha 1"                    //version of the program as presented in text
-#define IS_BETA             -1                         //is this a beta? (1: beta, -1: alpha)
-#define VERSION_BETA        1
-#define DATE_STR            "14th November, 2018"
-#define ZELDA_ABOUT_STR 	    "ZC Player 'Necromancer', Alpha 1"
+#define ZELDA_VERSION_STR   "AEternal (v2.55) Alpha 2"                    //version of the program as presented in text
+#define IS_BETA             -2                         //is this a beta? (1: beta, -1: alpha)
+#define VERSION_BETA        2
+#define DATE_STR            "26th November, 2018"
+#define ZELDA_ABOUT_STR 	    "ZC Player 'AEternal', Alpha 1"
 #define COPYRIGHT_YEAR      "2018"                          //shown on title screen and in ending
 
 #define MIN_VERSION         0x0184
