7ba522a4c32924a51efadf9f2c43b82867e4e9a7
ZoriaRPG <zoriarpg@gmail.com>
2018-12-22 04:06:07 +0000

    Vers: Update to Beta 28

--- src/zdefs.h
+++ src/zdefs.h
@@ -98,9 +98,9 @@
 #define ZC_VERSION 25300 //Version ID for ZScript Game->Version
 #define ABOUT_VERSION 0x0253
 #define VERSION_BUILD       31                              //build number of this version
-#define ZELDA_VERSION_STR   "Omnius, 2.53 Beta 27"                    //version of the program as presented in text
-#define IS_BETA             27                           //is this a beta? (1: beta, -1: alpha)
-#define DATE_STR            "16th December, 2018"
+#define ZELDA_VERSION_STR   "Omnius, 2.53 Beta 28"                    //version of the program as presented in text
+#define IS_BETA             28                           //is this a beta? (1: beta, -1: alpha)
+#define DATE_STR            "22nd December, 2018"
 #define ZELDA_ABOUT_STR 	    "Zelda Classic 'Omnius'"
 #define COPYRIGHT_YEAR      "2018"     
                      //shown on title screen and in ending
