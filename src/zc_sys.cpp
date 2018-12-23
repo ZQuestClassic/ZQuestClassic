105de25b191ed0ede4b232bf1decac03edaca3d0
ZoriaRPG <zoriarpg@gmail.com>
2018-12-23 19:49:08 +0000

    Rename Screenshot Base Filename to zc_quest_screenshot%03d.png

--- src/zc_sys.cpp
+++ src/zc_sys.cpp
@@ -3582,9 +3582,9 @@ int onGUISnapshot()
     do
     {
 #ifdef ALLEGRO_MACOSX
-        sprintf(buf, "../../../zelda%03d.%s", ++num, snapshotformat_str[SnapshotFormat][1]);
+        sprintf(buf, "../../../zc_quest_screenshot3d.%s", ++num, snapshotformat_str[SnapshotFormat][1]);
 #else
-        sprintf(buf, "zelda%03d.%s", ++num, snapshotformat_str[SnapshotFormat][1]);
+        sprintf(buf, "zc_quest_screenshot%03d.%s", ++num, snapshotformat_str[SnapshotFormat][1]);
 #endif
     }
     while(num<999 && exists(buf));
@@ -3612,7 +3612,7 @@ int onNonGUISnapshot()
     
     do
     {
-        sprintf(buf, "zelda%03d.%s", ++num, snapshotformat_str[SnapshotFormat][1]);
+        sprintf(buf, "zc_quest_screenshot%03d.%s", ++num, snapshotformat_str[SnapshotFormat][1]);
     }
     while(num<999 && exists(buf));
     
@@ -3676,9 +3676,9 @@ int onSaveMapPic()
     do
     {
 #ifdef ALLEGRO_MACOSX
-        sprintf(buf, "../../../zelda%03d.png", ++num);
+        sprintf(buf, "../../../zc_quest_screenshot%03d.png", ++num);
 #else
-        sprintf(buf, "zelda%03d.png", ++num);
+        sprintf(buf, "zc_quest_screenshot%03d.png", ++num);
 #endif
     }
     while(num<999 && exists(buf));
