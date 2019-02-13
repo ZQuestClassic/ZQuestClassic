6c255b4d11626b1fe2d2fd3934e337d541a06650
ZoriaRPG <zoriarpg@gmail.com>
2019-02-13 03:31:08 +0000

    Vers: Build is now 32.

--- src/zdefs.h
+++ src/zdefs.h
@@ -97,10 +97,10 @@
 #define ZELDA_VERSION       0x0250  //version of the program
 #define ZC_VERSION 25300 //Version ID for ZScript Game->Version
 #define ABOUT_VERSION 0x0253
-#define VERSION_BUILD       31                              //build number of this version
+#define VERSION_BUILD       32                             //build number of this version
 #define ZELDA_VERSION_STR   "Omnius, 2.53 Gamma 2"               //version of the program as presented in text
 #define IS_BETA             0                        //is this a beta? (1: beta, -1: alpha)
-#define DATE_STR            "12th February, 2019"
+#define DATE_STR            "13th February, 2019"
 #define ZELDA_ABOUT_STR 	    "Zelda Classic 'Omnius' Gamma 2"
 #define COPYRIGHT_YEAR      "2019"     
                      //shown on title screen and in ending
