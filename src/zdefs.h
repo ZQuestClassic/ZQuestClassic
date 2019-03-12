2f9696baa2c3765f5ca445270276e735c72fb644
ZoriaRPG <zoriarpg@gmail.com>
2019-03-12 13:05:49 +0000

    Vers: Updated build date.

--- src/zdefs.h
+++ src/zdefs.h
@@ -102,7 +102,7 @@
 #define ZELDA_VERSION_STR   "AEternal (v2.55) Alpha 15"                    //version of the program as presented in text
 #define IS_BETA             -15                         //is this a beta? (1: beta, -1: alpha)
 #define VERSION_BETA        15
-#define DATE_STR            "11th March, 2019"
+#define DATE_STR            "12th March, 2019"
 #define ZELDA_ABOUT_STR 	    "ZC Player 'AEternal', Alpha 15"
 #define COPYRIGHT_YEAR      "2019"                          //shown on title screen and in ending
 
