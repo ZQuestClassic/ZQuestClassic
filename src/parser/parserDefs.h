2a7ec9d9285af26dcd486590f33e83422cef964d
Robert Venezia <rvenezia@cs.hartford.edu>
2019-03-26 23:30:36 -0400

    Files: Add `parserDefs.h`, as a `zdefs.h` for the parser.

---
+++ src/parser/parserDefs.h
@@ -0,0 +1,11 @@
+#ifndef _PARSERDEFS_H_
+#define _PARSERDEFS_H_
+
+//Option values. NOTE: Result of "lookupOption" must be '/10000.0' -V
+#define OPT_OFF			0
+#define OPT_ON			1
+#define OPT_ERROR		2
+#define OPT_WARN		3
+
+#endif
+
