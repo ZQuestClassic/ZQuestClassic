d5e7878e203c658223778e6b74c6dd424a3a1234
ZoriaRPG <zoriarpg@gmail.com>
2019-04-09 02:54:44 +0100

    Fix: Sprites do not render using offset positions.

--- src/zdefs.h
+++ src/zdefs.h
@@ -104,7 +104,7 @@
 #define ZELDA_VERSION_STR   "AEternal (v2.55) Alpha 17"                    //version of the program as presented in text
 #define IS_BETA             -17                         //is this a beta? (1: beta, -1: alpha)
 #define VERSION_BETA        17
-#define DATE_STR            "3rd April, 2019"
+#define DATE_STR            "8th April, 2019"
 #define ZELDA_ABOUT_STR 	    "ZC Player 'AEternal', Alpha 17"
 #define COPYRIGHT_YEAR      "2019"                          //shown on title screen and in ending
 
