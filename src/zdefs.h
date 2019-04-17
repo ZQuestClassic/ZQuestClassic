fbc2af3e8090bc496f8206cb2a7fafb48f2f308d
ZoriaRPG <zoriarpg@gmail.com>
2019-04-17 21:08:31 +0100

    Update build date.

--- src/zdefs.h
+++ src/zdefs.h
@@ -100,7 +100,7 @@
 #define VERSION_BUILD       33                             //build number of this version
 #define ZELDA_VERSION_STR   "Omnius, 2.53 Release 2"               //version of the program as presented in text
 #define IS_BETA             0                        //is this a beta? (1: beta, -1: alpha)
-#define DATE_STR            "16th April, 2019"
+#define DATE_STR            "17th April, 2019"
 #define ZELDA_ABOUT_STR 	    "Zelda Classic 'Omnius' Release 2"
 #define COPYRIGHT_YEAR      "2019"     
                      //shown on title screen and in ending
