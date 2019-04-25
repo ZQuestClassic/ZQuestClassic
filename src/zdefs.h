606425bd0df9774e3c26346ce7fbfd84aaea2d7b
ZoriaRPG <zoriarpg@gmail.com>
2019-04-25 20:55:14 +0100

    Vers: Updated to Alpha 21 and updated build date.

--- src/zdefs.h
+++ src/zdefs.h
@@ -101,11 +101,11 @@
 #define ZC_VERSION 25500 //Version ID for ZScript Game->Version
 #define VERSION_BUILD       43                              //build number of this version
 //31 == 2.53.0 , leaving 32-39 for bugfixes, and jumping to 40. 
-#define ZELDA_VERSION_STR   "AEternal (v2.55) Alpha 20"                    //version of the program as presented in text
-#define IS_BETA             -20                         //is this a beta? (1: beta, -1: alpha)
-#define VERSION_BETA        20
-#define DATE_STR            "22nd April, 2019"
-#define ZELDA_ABOUT_STR 	    "ZC Player 'AEternal', Alpha 20"
+#define ZELDA_VERSION_STR   "AEternal (v2.55) Alpha 21"                    //version of the program as presented in text
+#define IS_BETA             -21                         //is this a beta? (1: beta, -1: alpha)
+#define VERSION_BETA        21
+#define DATE_STR            "25th April, 2019"
+#define ZELDA_ABOUT_STR 	    "ZC Player 'AEternal', Alpha 21"
 #define COPYRIGHT_YEAR      "2019"                          //shown on title screen and in ending
 
 #define MIN_VERSION         0x0184
