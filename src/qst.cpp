c58b902047877f95a1e9ccb9256b8eaeaba9ad3d
ZoriaRPG <zoriarpg@gmail.com>
2019-06-19 21:03:20 +0100

    Fix: Don't force qr_LINKXY_IS_FLOAT off.

--- src/qst.cpp
+++ src/qst.cpp
@@ -2716,10 +2716,11 @@ int readrules(PACKFILE *f, zquestheader *Header, bool keepdata)
 	set_bit(quest_rules, qr_SCRIPTDRAWSWHENSCROLLING, 0);  	    
 	set_bit(quest_rules, qr_SCRIPTDRAWSINWARPS, 0);  	    
 	set_bit(quest_rules, qr_DYINGENEMYESDONTHURTLINK, 0);  	    
-	set_bit(quest_rules, qr_OUTOFBOUNDSENEMIES, 0);  	    
+	set_bit(quest_rules, qr_OUTOFBOUNDSENEMIES, 0);  
+	set_bit(quest_rules, qr_LINKXY_IS_FLOAT, 0);
     }
     
-    set_bit(quest_rules, qr_LINKXY_IS_FLOAT, 0);
+    
     
     if ( tempheader.zelda_version < 0x255 || (tempheader.zelda_version == 0x255 && tempheader.build < 46) )
     {
