381954f34b0ad227e5dc610a790d24b51b6382ee
ZoriaRPG <zoriarpg@gmail.com>
2019-07-23 17:13:04 +0100

    Alpha 29: Update to A29 and update version date/time.

--- src/zdefs.h
+++ src/zdefs.h
@@ -105,11 +105,11 @@
 #define ZC_VERSION 25500 //Version ID for ZScript Game->Version
 #define VERSION_BUILD       46                              //build number of this version
 //31 == 2.53.0 , leaving 32-39 for bugfixes, and jumping to 40. 
-#define ZELDA_VERSION_STR   "AEternal (v2.55) Alpha 28"                    //version of the program as presented in text
-#define IS_BETA             -28                         //is this a beta? (1: beta, -1: alpha)
-#define VERSION_BETA        28
-#define DATE_STR            "18h July, 2019, 08:43GMT"
-#define ZELDA_ABOUT_STR 	    "ZC Player 'AEternal', Alpha 27"
+#define ZELDA_VERSION_STR   "AEternal (v2.55) Alpha 29"                    //version of the program as presented in text
+#define IS_BETA             -29                         //is this a beta? (1: beta, -1: alpha)
+#define VERSION_BETA        29
+#define DATE_STR            "23rd July, 2019, 17:12GMT"
+#define ZELDA_ABOUT_STR 	    "ZC Player 'AEternal', Alpha 29"
 #define COPYRIGHT_YEAR      "2019"                          //shown on title screen and in ending
 
 #define MIN_VERSION         0x0184
