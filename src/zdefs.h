369e8b1b93a5e9bf8580c2c0447a957fb7822cd7
ZoriaRPG <zoriarpg@gmail.com>
2019-08-27 13:39:25 +0100

    Update 2.55 Master to Alpha 31 Status

--- src/zdefs.h
+++ src/zdefs.h
@@ -106,10 +106,10 @@
 #define VERSION_BUILD       47                              //build number of this version
 //31 == 2.53.0 , leaving 32-39 for bugfixes, and jumping to 40. 
 #define ZELDA_VERSION_STR   "AEternal (v2.55) Alpha 30"                    //version of the program as presented in text
-#define IS_BETA             -30                         //is this a beta? (1: beta, -1: alpha)
-#define VERSION_BETA        30
+#define IS_BETA             -31                         //is this a beta? (1: beta, -1: alpha)
+#define VERSION_BETA        31
 #define DATE_STR            "27th August, 2019, 13:20 PM EST"
-#define ZELDA_ABOUT_STR 	    "ZC Player 'AEternal', Alpha 30"
+#define ZELDA_ABOUT_STR 	    "ZC Player 'AEternal', Alpha 31"
 #define COPYRIGHT_YEAR      "2019"                          //shown on title screen and in ending
 
 #define MIN_VERSION         0x0184
