7d09e06d9cf57c9db15230b57cda893ffa40f3cf
Venrob <gamefanrob@gmail.com>
2019-08-28 04:51:10 -0400

    Update Build Date

--- src/zdefs.h
+++ src/zdefs.h
@@ -108,7 +108,7 @@
 #define ZELDA_VERSION_STR   "AEternal (v2.55) Alpha 30"                    //version of the program as presented in text
 #define IS_BETA             -31                         //is this a beta? (1: beta, -1: alpha)
 #define VERSION_BETA        31
-#define DATE_STR            "27th August, 2019, 13:20 PM EST"
+#define DATE_STR            "28th August, 2019, 4:50 AM EST"
 #define ZELDA_ABOUT_STR 	    "ZC Player 'AEternal', Alpha 31"
 #define COPYRIGHT_YEAR      "2019"                          //shown on title screen and in ending
 
