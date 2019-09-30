42ea9d656e4f2a5fc23ed516948c77e7bec1744d
ZoriaRPG <zoriarpg@gmail.com>
2019-09-30 11:54:21 +0100

    Backport: Compiler Audio SFX from 2.55A32

--- src/zquest.cpp
+++ src/zquest.cpp
@@ -163,6 +163,20 @@ bool show_hitboxes = false;
 extern word quest_header_zelda_version; //2.53 ONLY. In 2.55, we have an array for this in FFCore! -Z
 extern word quest_header_zelda_build; //2.53 ONLY. In 2.55, we have an array for this in FFCore! -Z
 
+/*****************/
+/*****  SFX  *****/
+/*****************/
+
+// array of voices, one for each sfx sample in the data file
+// 0+ = voice #
+// -1 = voice not allocated
+static int sfx_voice[WAV_COUNT];
+
+word compile_success_sample = 0;
+word compile_error_sample = 0;
+word compile_finish_sample = 0;
+word compile_audio_volume = 0;
+
 // Used to find FFC script names
 extern std::map<int, pair<string,string> > ffcmap;
 std::vector<string> asffcscripts;
@@ -19287,7 +19301,46 @@ int onCompileScript()
             gotoless_not_equal = (0 != get_bit(quest_rules, qr_GOTOLESSNOTEQUAL)); // Used by BuildVisitors.cpp
             ScriptsData *result = compile("tmp");
             unlink("tmp");
+	    
+	    if ( result )
+	    {
+		compile_success_sample = get_config_int("Compiler","compile_success_sample",20);
+		compile_audio_volume = get_config_int("Compiler","compile_audio_volume",200);
+		if(sfxdat)
+		sfx_voice[compile_success_sample]=allocate_voice((SAMPLE*)sfxdata[compile_success_sample].dat);
+		else sfx_voice[compile_success_sample]=allocate_voice(&customsfxdata[compile_success_sample]);
+		voice_set_volume(sfx_voice[compile_success_sample], compile_audio_volume);
+		voice_start(sfx_voice[compile_success_sample]);
+			
+	    }
+	    else
+	    {
+		//al_trace("Error, play err sfx.\n");
+		    compile_error_sample = get_config_int("Compiler","compile_error_sample",28);
+		    compile_audio_volume = get_config_int("Compiler","compile_audio_volume",200);
+		//al_trace("Module SFX datafile is %s \n",moduledata.datafiles[sfx_dat]);
+		    if(sfxdat)
+		    sfx_voice[compile_error_sample]=allocate_voice((SAMPLE*)sfxdata[compile_error_sample].dat);
+		    else sfx_voice[compile_error_sample]=allocate_voice(&customsfxdata[compile_error_sample]);
+		    voice_set_volume(sfx_voice[compile_error_sample], compile_audio_volume);
+		//set_volume(255,-1);
+		//kill_sfx();
+		    voice_start(sfx_voice[compile_error_sample]);
+		//sfx(28, 128, false,true);  
+		    
+	    }
+	    
             box_end(true);
+	    if(sfx_voice[compile_success_sample]!=-1)
+	    {
+		deallocate_voice(sfx_voice[compile_success_sample]);
+		sfx_voice[compile_success_sample]=-1;
+	    }
+	    if(sfx_voice[compile_error_sample]!=-1)
+	    {
+		deallocate_voice(sfx_voice[compile_error_sample]);
+		sfx_voice[compile_error_sample]=-1;
+	    }
             refresh(rALL);
             
             if(result == NULL)
@@ -19532,7 +19585,23 @@ int onCompileScript()
                     }
                     
                     unlink("tmp");
+		  
+		//al_trace("Module SFX datafile is %s \n",moduledata.datafiles[sfx_dat]);
+		    compile_finish_sample = get_config_int("Compiler","compile_finish_sample",34);
+		    compile_audio_volume = get_config_int("Compiler","compile_audio_volume",200);
+		    if(sfxdat)
+		    sfx_voice[compile_finish_sample]=allocate_voice((SAMPLE*)sfxdata[compile_finish_sample].dat);
+		    else sfx_voice[compile_finish_sample]=allocate_voice(&customsfxdata[compile_finish_sample]);
+		    voice_set_volume(sfx_voice[compile_finish_sample], compile_audio_volume);
+		//set_volume(255,-1);
+		//kill_sfx();
+		    voice_start(sfx_voice[compile_finish_sample]);
                     jwin_alert("Done!","ZScripts successfully loaded into script slots",NULL,NULL,"O&K",NULL,'k',0,lfont);
+		    if(sfx_voice[compile_finish_sample]!=-1)
+		    {
+			deallocate_voice(sfx_voice[compile_finish_sample]);
+			sfx_voice[compile_finish_sample]=-1;
+		    }
                     build_biffs_list();
                     build_biitems_list();
                     
@@ -19935,14 +20004,7 @@ static DIALOG sfx_edit_dlg[] =
     { NULL,                 0,    0,    0,    0,   0,       0,       0,       0,          0,             0,       NULL,                           NULL,  NULL }
 };
 
-/*****************/
-/*****  SFX  *****/
-/*****************/
 
-// array of voices, one for each sfx sample in the data file
-// 0+ = voice #
-// -1 = voice not allocated
-static int sfx_voice[WAV_COUNT];
 
 void Z_init_sound()
 {
