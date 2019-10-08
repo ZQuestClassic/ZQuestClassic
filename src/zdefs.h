0d11e7c6453ee3cb302a165527e66367c29af469
ZoriaRPG <zoriarpg@gmail.com>
2019-10-08 06:47:31 +0100

    Vers: Update to Alpha 35; updated changelog.txt

--- src/zdefs.h
+++ src/zdefs.h
@@ -105,11 +105,11 @@
 #define ZC_VERSION 25500 //Version ID for ZScript Game->Version
 #define VERSION_BUILD       47                              //build number of this version
 //31 == 2.53.0 , leaving 32-39 for bugfixes, and jumping to 40. 
-#define ZELDA_VERSION_STR   "AEternal (v2.55) Alpha 34"                    //version of the program as presented in text
-#define IS_BETA             -34                        //is this a beta? (1: beta, -1: alpha)
-#define VERSION_BETA        34	
+#define ZELDA_VERSION_STR   "AEternal (v2.55) Alpha 35"                    //version of the program as presented in text
+#define IS_BETA             -35                        //is this a beta? (1: beta, -1: alpha)
+#define VERSION_BETA        35	
 #define DATE_STR            "8th October, 2019, 06:45GMT"
-#define ZELDA_ABOUT_STR 	    "ZC Player 'AEternal', Alpha 34"
+#define ZELDA_ABOUT_STR 	    "ZC Player 'AEternal', Alpha 35"
 #define COPYRIGHT_YEAR      "2019"                          //shown on title screen and in ending
 
 #define MIN_VERSION         0x0184
