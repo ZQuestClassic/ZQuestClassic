590f7b02a26a4aab88744b1363a8d62cb172d10c
ZoriaRPG <zoriarpg@gmail.com>
2019-10-08 10:31:15 +0100

    Added theme files; updated changelog and datestap.

--- src/zdefs.h
+++ src/zdefs.h
@@ -108,7 +108,7 @@
 #define ZELDA_VERSION_STR   "AEternal (v2.55) Alpha 35"                    //version of the program as presented in text
 #define IS_BETA             -35                        //is this a beta? (1: beta, -1: alpha)
 #define VERSION_BETA        35	
-#define DATE_STR            "8th October, 2019, 06:45GMT"
+#define DATE_STR            "8th October, 2019, 09:15GMT"
 #define ZELDA_ABOUT_STR 	    "ZC Player 'AEternal', Alpha 35"
 #define COPYRIGHT_YEAR      "2019"                          //shown on title screen and in ending
 
