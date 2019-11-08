d32002be44a76da7c491a891017ef9978a7dacd1
ZoriaRPG <zoriarpg@gmail.com>
2019-11-08 20:09:09 +0000

    Give eweapon and npc ParentUID Write Access
    Changelog: It is now possible to write to eweapon->ParentUID
    	and to npc->ScriptUID.
    	This does NOT automatically associate the object
    	with the parent, and it exists purely for
    	the semantics of allowing fully-scripted sprites
    	to set the parent UID of child sprites solely
    	within the context of the script.
    	It may be overwritten by the engine animation
    	of the normal parent sprite, particularly if
    	used on a normally segmented enemy type.
    Added new instructions to debug commands list.

--- src/ffscript.cpp
+++ src/ffscript.cpp
@@ -4994,7 +4994,7 @@ case NPCBEHAVIOUR: {
         break;
 	
 	case EWPNPARENTUID:
-        if(0!=(s=checkEWpn(ri->ewpn, "ScriptUID")))
+        if(0!=(s=checkEWpn(ri->ewpn, "ParentUID")))
             ret=(((weapon*)(s))->parent_script_UID); //literal, not *10000
             
         break;
@@ -10947,6 +10947,12 @@ void set_register(const long arg, const long value)
             
         break;
 	
+	
+    case EWPNPARENTUID:
+        if(0!=(s=checkEWpn(ri->ewpn, "ParentUID")))
+        (((weapon*)(s))->parent_script_UID) = value; //literal, not *10000
+	break;
+	
    case EWPNPARENT:
         if(0!=(s=checkEWpn(ri->ewpn, "Parent")))
             (((weapon*)(s))->parentid)= ( (get_bit(quest_rules,qr_OLDEWPNPARENT)) ? value / 10000 : value );
@@ -11340,6 +11346,13 @@ if(GuyH::loadNPC(ri->guyref, str) == SH::_NoError) \
 	}
     }
     break;
+    
+    case NPCPARENTUID:
+        if(GuyH::loadNPC(ri->guyref, "npc->ParentUID") == SH::_NoError)
+	{
+            GuyH::getNPC()->parent_script_UID = value; //literal, not *10000
+	}
+        break;
     
     case NPCHITBY:
     {
@@ -29259,6 +29272,7 @@ script_variable ZASMVars[]=
 	{ "DROPSETCHANCES",		DROPSETCHANCES,        0,             0 },
 	{ "DROPSETNULLCHANCE",		DROPSETNULLCHANCE,        0,             0 },
 	{ "DROPSETCHOOSE",		DROPSETCHOOSE,        0,             0 },
+	{ "NPCPARENTUID",		NPCPARENTUID,        0,             0 },
 	{ " ",                       -1,             0,             0 }
 };
 
