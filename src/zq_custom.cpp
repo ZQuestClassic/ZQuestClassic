ddd69e9f667eabb5ab57aef86c27937a2f6bd210
ZoriaRPG <zoriarpg@gmail.com>
2019-11-19 07:16:36 +0000

    Fix: Don't Display Unimplemented Defences (Item Editor)
    Changelog: The Item Editor 'Default Defence' list no longer displays
    	unimplemented enemy defence resolution types.

--- src/zq_custom.cpp
+++ src/zq_custom.cpp
@@ -845,7 +845,69 @@ const char *counterlist(int index, int *list_size)
 
 static ListData counter_list(counterlist, &pfont);
 
+const char the_enemy_defences[edLAST][255]=
+{
+	"(None)", "1/2 Damage", "1/4 Damage", "Stun", "Stun Or Block", "Stun Or Ignore", "Block If < 1",
+	"Block If < 2", "Block If < 4", "Block If < 6", "Block If < 8", "Block", "Ignore If < 1", "Ignore",
+	"One-Hit-Kill", "Block if Power < 10", "Double Damage", "Triple Damage", "Quadruple Damage",
+	"Enemy Gains HP = Damage", "Trigger Screen Secrets", "-freeze", "-msgnotenabled", "-msgline", 
+	"-lvldamage", "-lvlreduction", "Split", "Transform", "-lvlblock2", "-lvlblock3", "-lvlblock4", 
+	"-lvlblock5", "-shock", "Bomb Explosion", "Superbomb Explosion", "-deadrock", "-breakshoeld",
+	"-restoreshield", "-specialdrop", "-incrcounter", "-reducecounter", "Harmless Explosion",
+	"-killnosplit", "-tribble", "-fireball", "-fireballlarge", "Summon", "-wingame", "-jump",
+	"-eat", "-showmessage"
+};
 
+
+list_data_struct biedefs[edLAST];
+int biedefs_cnt=-1;
+
+void build_biedefs_list()
+{
+    int start=biedefs_cnt=0;
+    
+    for(int i=start; i<41; i++)
+    {
+	//Load enemy names from the module
+        //if(moduledata.enem_type_names[i][0]!='-')
+	if(the_enemy_defences[i][0]!='-')
+	{
+		//load these from the module
+	   // biedefs[biedefs_cnt].s, = (char *)moduledata.enem_type_names[i]); //, (char *)enetype_string[i]);
+	    //biedefs[biedefs_cnt].s = (char *)enetype_string[i];
+	    biedefs[biedefs_cnt].s = (char *)the_enemy_defences[i];
+	    biedefs[biedefs_cnt].i = i;
+	    ++biedefs_cnt;
+	}
+		
+    }
+    
+    // No alphabetic sorting for this list
+    for(int j=start+1; j<biedefs_cnt-1; j++)
+    {
+        if(!strcmp(biedefs[j].s,"(None)"))
+        {
+            for(int i=j; i>0; i--)
+                zc_swap(biedefs[i],biedefs[i-1]);
+                
+            break;
+        }
+    }
+}
+
+const char *edefslist(int index, int *list_size)
+{
+    if(index<0)
+    {
+        *list_size = biedefs_cnt;
+        return NULL;
+    }
+    
+    return biedefs[index].s;
+    
+}
+
+static ListData enemy_defence_list(edefslist, &pfont);
 
 
 //Moved defenselist up here so that it is also available to itemdata. -Z
@@ -1462,7 +1524,7 @@ static DIALOG itemdata_dlg[] =
     { jwin_droplist_proc,     107,     48,     72,      16, jwin_pal[jcTEXTFG],  jwin_pal[jcTEXTBG],           0,       0,           1,    0, (void *) &lweapontype_list,						 NULL,   NULL 				   },
     //295
     { jwin_text_proc,           8,     70,     96,      8,    vc(14),                 vc(1),                   0,       0,           0,    0, (void *) "Default Defense",                  NULL,   NULL                  },
-    { jwin_droplist_proc,     107,     68,     72,      16, jwin_pal[jcTEXTFG],  jwin_pal[jcTEXTBG],           0,       0,           1,    0, (void *) &defense_list,						 NULL,   NULL 				   },
+    { jwin_droplist_proc,     107,     68,     72,      16, jwin_pal[jcTEXTFG],  jwin_pal[jcTEXTBG],           0,       0,           1,    0, (void *) &enemy_defence_list,						 NULL,   NULL 				   },
     //297
     { jwin_text_proc,           8,     90,     96,      8,    vc(14),                 vc(1),                   0,       0,           0,    0, (void *) "Movement Pattern",                  NULL,   NULL                  },
     { jwin_droplist_proc,     107,     88,     72,      16, jwin_pal[jcTEXTFG],  jwin_pal[jcTEXTBG],           0,       0,           1,    0, (void *) &weapon_pattern_llist,						 NULL,   NULL 				   },
@@ -2425,6 +2487,8 @@ void edit_itemdata(int index)
     {
         build_biwt_list();
     }
+    build_biedefs_list();
+    
     build_biitems_list();
     build_biitemsprites_list();
     int script = 0, pickupscript = 0, the_spritescript = 0;
@@ -2460,8 +2524,13 @@ void edit_itemdata(int index)
         if(biwt[j].i == itemsbuf[index].useweapon)
             itemdata_dlg[294].d1 = j;
     }
+    for(int j=0; j<biedefs_cnt; j++)
+    {
+        if(biedefs[j].i == itemsbuf[index].useweapon)
+            itemdata_dlg[296].d1 = j;
+    }
     //itemdata_dlg[294].d1 = itemsbuf[index].useweapon;
-    itemdata_dlg[296].d1 = itemsbuf[index].usedefence;
+    //itemdata_dlg[296].d1 = itemsbuf[index].usedefence;
     itemdata_dlg[298].d1 = itemsbuf[index].weap_pattern[0];
     
     //.dp is correcxt here, and these now work, and are retained. -Z
@@ -2580,12 +2649,13 @@ void edit_itemdata(int index)
 	
 	//! These now store in the editor, but if you change the values, save the quest, and reload, 
 	//! ZQuest crashes on reading items (bad token)
-	test.usedefence =  itemdata_dlg[296].d1; //atoi(wdef);
+	//test.usedefence =  itemdata_dlg[296].d1; //atoi(wdef);
 	test.weaprange = vbound(atoi(wrange),0,214747);
 	test.weapduration = vbound(atoi(wdur),0,214747);
 	//test.useweapon = itemdata_dlg[294].d1; //atoi(wweap);
 	
 	test.useweapon = itemdata_dlg[294].d1 != 0 ? biwt[itemdata_dlg[294].d1].i : wNone;
+	test.usedefence = itemdata_dlg[296].d1 != 0 ? biedefs[itemdata_dlg[296].d1].i : 0;
 	
 	test.weap_pattern[0] = itemdata_dlg[298].d1;; //atoi(wptrn);
 	test.weap_pattern[1] = vbound(atoi(warg1),-214747, 214747);
