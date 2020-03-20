127f1e59185f415417af2905ae15c05381c37895
ZoriaRPG <zoriarpg@gmail.com>
2020-03-20 22:45:50 +0000

    Add void wipe_abc_keypresses() to jwin.h

--- src/jwin.h
+++ src/jwin.h
@@ -132,6 +132,7 @@ int jwin_radiofont_proc(int msg, DIALOG *d, int c);
 int jwin_tab_proc(int msg, DIALOG *d, int c);
 int jwin_hline_proc(int msg, DIALOG *d, int c);
 int jwin_vline_proc(int msg, DIALOG *d, int c);
+void wipe_abc_keypresses();
 
 /* other GUI procedures */
 void jwin_set_dialog_color(DIALOG *dialog);
