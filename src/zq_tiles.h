d05eb8d20eaa82a65053067d5bbccf58eba9ce50
ZoriaRPG <zoriarpg@gmail.com>
2020-04-29 04:48:31 +0100

    Fix Mass Overlay Tiles

--- src/zq_tiles.h
+++ src/zq_tiles.h
@@ -113,6 +113,7 @@ void tile_info_1(int oldtile,int oldflip,int oldcs,int tile,int flip,int cs,int 
 bool copy_tiles(int &tile,int &tile2,int &copy,int &copycnt, bool rect_sel, bool move);
 bool copy_tiles_united(int &tile,int &tile2,int &copy,int &copycnt, bool rect_sel, bool move);
 bool overlay_tiles_united(int &tile,int &tile2,int &copy,int &copycnt, bool rect, bool move, int cs, bool backwards);
+bool overlay_tile_united_mass(int &tile,int &tile2,int &copy,int &copycnt, bool rect, bool move, int cs, bool backwards);
 bool overlay_tiles(int &tile,int &tile2,int &copy,int &copycnt, bool rect_sel, bool move, int cs, bool backwards);
 void copy_combos(int &tile,int &tile2,int &copy,int &copycnt, bool masscopy);
 void move_combos(int &tile,int &tile2,int &copy,int &copycnt);
