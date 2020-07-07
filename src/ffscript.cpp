630b9e8e13ea17d25b4fb8b0bb52c07bf5341d0c
ZoriaRPG <zoriarpg@gmail.com>
2020-07-08 00:00:14 +0100

    Fixed possible crash on invalid gotos from logging args order.

--- src/ffscript.cpp
+++ src/ffscript.cpp
@@ -9985,11 +9985,11 @@ int run_script(const byte type, const word script, const byte i)
 		switch(type)
 		{
 			case SCRIPT_FFC:
-				Z_scripterrlog("%s Script %s attempted o GOTO an invalid instruction (%d).\n", sarg1, script_types[type], ffcmap[i].second.c_str()); break;
+				Z_scripterrlog("%s Script %s attempted to GOTO an invalid instruction (%d).\n", script_types[type], ffcmap[i].second.c_str(), sarg1); break;
 			case SCRIPT_ITEM:
-				Z_scripterrlog("%s Script %s attempted o GOTO an invalid instruction (%d).\n", sarg1, script_types[type], itemmap[i].second.c_str()); break;
+				Z_scripterrlog("%s Script %s attempted to GOTO an invalid instruction (%d).\n", script_types[type], itemmap[i].second.c_str(), sarg1); break;
 			case SCRIPT_GLOBAL:
-				Z_scripterrlog("%s Script %s attempted o GOTO an invalid instruction (%d).\n", sarg1, script_types[type], globalmap[i].second.c_str()); break;
+				Z_scripterrlog("%s Script %s attempted to GOTO an invalid instruction (%d).\n", script_types[type], globalmap[i].second.c_str(), sarg1); break;
 			
 			default: break;
 		}
@@ -10009,11 +10009,11 @@ int run_script(const byte type, const word script, const byte i)
 		switch(type)
 		{
 			case SCRIPT_FFC:
-				Z_scripterrlog("%s Script %s attempted o GOTO an invalid instruction (%d).\n", (get_register(sarg1) / 10000), script_types[type], ffcmap[i].second.c_str()); break;
+				Z_scripterrlog("%s Script %s attempted o GOTO an invalid instruction (%d).\n", script_types[type], ffcmap[i].second.c_str(), (get_register(sarg1) / 10000)); break;
 			case SCRIPT_ITEM:
-				Z_scripterrlog("%s Script %s attempted o GOTO an invalid instruction (%d).\n", (get_register(sarg1) / 10000), script_types[type], itemmap[i].second.c_str()); break;
+				Z_scripterrlog("%s Script %s attempted o GOTO an invalid instruction (%d).\n", script_types[type], itemmap[i].second.c_str(), (get_register(sarg1) / 10000)); break;
 			case SCRIPT_GLOBAL:
-				Z_scripterrlog("%s Script %s attempted o GOTO an invalid instruction (%d).\n", (get_register(sarg1) / 10000), script_types[type], globalmap[i].second.c_str()); break;
+				Z_scripterrlog("%s Script %s attempted o GOTO an invalid instruction (%d).\n", script_types[type], globalmap[i].second.c_str(), (get_register(sarg1) / 10000)); break;
 			
 			default: break;
 		}
@@ -10034,11 +10034,11 @@ int run_script(const byte type, const word script, const byte i)
 			switch(type)
 			{
 				case SCRIPT_FFC:
-					Z_scripterrlog("%s Script %s attempted o GOTO an invalid instruction (%d).\n", sarg1, script_types[type], ffcmap[i].second.c_str()); break;
+					Z_scripterrlog("%s Script %s attempted o GOTO an invalid instruction (%d).\n", script_types[type], ffcmap[i].second.c_str(), sarg1); break;
 				case SCRIPT_ITEM:
-					Z_scripterrlog("%s Script %s attempted o GOTO an invalid instruction (%d).\n", sarg1, script_types[type], itemmap[i].second.c_str()); break;
+					Z_scripterrlog("%s Script %s attempted o GOTO an invalid instruction (%d).\n", script_types[type], itemmap[i].second.c_str(), sarg1); break;
 				case SCRIPT_GLOBAL:
-					Z_scripterrlog("%s Script %s attempted o GOTO an invalid instruction (%d).\n", sarg1, script_types[type], globalmap[i].second.c_str()); break;
+					Z_scripterrlog("%s Script %s attempted o GOTO an invalid instruction (%d).\n", script_types[type], globalmap[i].second.c_str(), sarg1); break;
 				
 				default: break;
 			}
@@ -10060,11 +10060,11 @@ int run_script(const byte type, const word script, const byte i)
 			switch(type)
 			{
 				case SCRIPT_FFC:
-					Z_scripterrlog("%s Script %s attempted o GOTO an invalid instruction (%d).\n", sarg1, script_types[type], ffcmap[i].second.c_str()); break;
+					Z_scripterrlog("%s Script %s attempted o GOTO an invalid instruction (%d).\n", script_types[type], ffcmap[i].second.c_str(), sarg1); break;
 				case SCRIPT_ITEM:
-					Z_scripterrlog("%s Script %s attempted o GOTO an invalid instruction (%d).\n", sarg1, script_types[type], itemmap[i].second.c_str()); break;
+					Z_scripterrlog("%s Script %s attempted o GOTO an invalid instruction (%d).\n", script_types[type], itemmap[i].second.c_str(), sarg1); break;
 				case SCRIPT_GLOBAL:
-					Z_scripterrlog("%s Script %s attempted o GOTO an invalid instruction (%d).\n", sarg1, script_types[type], globalmap[i].second.c_str()); break;
+					Z_scripterrlog("%s Script %s attempted o GOTO an invalid instruction (%d).\n", script_types[type], globalmap[i].second.c_str(), sarg1); break;
 				
 				default: break;
 			}
@@ -10086,11 +10086,11 @@ int run_script(const byte type, const word script, const byte i)
 			switch(type)
 			{
 				case SCRIPT_FFC:
-					Z_scripterrlog("%s Script %s attempted o GOTO an invalid instruction (%d).\n", sarg1, script_types[type], ffcmap[i].second.c_str()); break;
+					Z_scripterrlog("%s Script %s attempted o GOTO an invalid instruction (%d).\n", script_types[type], ffcmap[i].second.c_str(), sarg1); break;
 				case SCRIPT_ITEM:
-					Z_scripterrlog("%s Script %s attempted o GOTO an invalid instruction (%d).\n", sarg1, script_types[type], itemmap[i].second.c_str()); break;
+					Z_scripterrlog("%s Script %s attempted o GOTO an invalid instruction (%d).\n", script_types[type], itemmap[i].second.c_str(), sarg1); break;
 				case SCRIPT_GLOBAL:
-					Z_scripterrlog("%s Script %s attempted o GOTO an invalid instruction (%d).\n", sarg1, script_types[type], globalmap[i].second.c_str()); break;
+					Z_scripterrlog("%s Script %s attempted o GOTO an invalid instruction (%d).\n", script_types[type], globalmap[i].second.c_str(), sarg1); break;
 				
 				default: break;
 			}
@@ -10112,11 +10112,11 @@ int run_script(const byte type, const word script, const byte i)
 			switch(type)
 			{
 				case SCRIPT_FFC:
-					Z_scripterrlog("%s Script %s attempted o GOTO an invalid instruction (%d).\n", sarg1, script_types[type], ffcmap[i].second.c_str()); break;
+					Z_scripterrlog("%s Script %s attempted o GOTO an invalid instruction (%d).\n", script_types[type], ffcmap[i].second.c_str(), sarg1); break;
 				case SCRIPT_ITEM:
-					Z_scripterrlog("%s Script %s attempted o GOTO an invalid instruction (%d).\n", sarg1, script_types[type], itemmap[i].second.c_str()); break;
+					Z_scripterrlog("%s Script %s attempted o GOTO an invalid instruction (%d).\n", script_types[type], itemmap[i].second.c_str(), sarg1); break;
 				case SCRIPT_GLOBAL:
-					Z_scripterrlog("%s Script %s attempted o GOTO an invalid instruction (%d).\n", sarg1, script_types[type], globalmap[i].second.c_str()); break;
+					Z_scripterrlog("%s Script %s attempted o GOTO an invalid instruction (%d).\n", script_types[type], globalmap[i].second.c_str(), sarg1); break;
 				
 				default: break;
 			}
