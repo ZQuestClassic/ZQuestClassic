63b9721715b40128d03d5497ed41c8dc3ff73fd6
Saffith <Saffith@users.noreply.github.com>
2021-09-04 23:11:58 -0400

    Added a fallback showDialog() to produce better error messages.

--- src/gui/showDialog.h
+++ src/gui/showDialog.h
@@ -3,6 +3,7 @@
 
 #include "dialogMessage.h"
 #include "dialogRunner.h"
+#include "helper.h"
 #include <type_traits>
 
 namespace gui
@@ -12,8 +13,10 @@ namespace gui
 // based on which can be called.
 
 template<typename T>
-std::enable_if_t<std::is_invocable_v<
-    decltype(&T::handleMessage), T&, typename T::Message, gui::MessageArg>, void>
+std::enable_if_t<
+    std::is_invocable_v<
+        decltype(&T::handleMessage), T&, typename T::Message, gui::MessageArg
+    >, void>
 showDialog(T& dlg)
 {
     auto dr=DialogRunner();
@@ -21,12 +24,33 @@ showDialog(T& dlg)
 }
 
 template<typename T>
-std::enable_if_t<std::is_invocable_v<
-    decltype(&T::handleMessage), T&, typename T::Message>, void>
+std::enable_if_t<
+    std::is_invocable_v<
+        decltype(&T::handleMessage), T&, typename T::Message
+    >, void>
 showDialog(T& dlg)
 {
     auto dr=DialogRunner();
     dr.runWithoutArg(dlg);
+}
+
+// This one just exists to produce a more helpful error message if neither
+// version is correctly implemented. The enable_if is just to prevent
+// additional errors from ambiguity with the two above.
+template<typename T, bool b=false>
+std::enable_if_t<
+    not std::is_invocable_v<
+        decltype(&T::handleMessage), T&, typename T::Message, gui::MessageArg>
+    and not std::is_invocable_v<
+        decltype(&T::handleMessage), T&, typename T::Message
+    >, void>
+showDialog(T& dlg)
+{
+    ZCGUI_STATIC_ASSERT(b,
+        "No valid handleMessage() implementation found.\n"
+        "You must implement one of the following:\n"
+        "handleMessage([DialogClass]::Message, gui::EventArg)\n"
+        "handleMessage([DialogClass]::Message)");
 }
 
 }
