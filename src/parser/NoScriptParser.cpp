f9e4129c9d0bcea95513c6cd01830f4a52800794
Saffith <Saffith@users.noreply.github.com>
2021-09-12 12:46:51 -0400

    Fix a conflict with windows.h and a type name missing a namespace.

--- src/parser/NoScriptParser.cpp
+++ src/parser/NoScriptParser.cpp
@@ -2,7 +2,7 @@
 #include "../zsys.h"
 #include "Compiler.h"
 
-ScriptsData * compile(const char *filename)
+ZScript::ScriptsData * compile(const char *filename)
 {
     box_out("Cannot compile ZScript: ZQuest was compiled without ZScript support!");
     box_eol();
