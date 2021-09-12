4d62c0fc5f7987f391f7ee7a9928119f9509d936
Saffith <Saffith@users.noreply.github.com>
2021-09-12 13:09:27 -0400

    Fixed no compiler errors properly this time.

--- src/parser/NoScriptParser.cpp
+++ src/parser/NoScriptParser.cpp
@@ -1,11 +1,41 @@
 #include "../precompiled.h" //2.53 Updated to 16th Jan, 2017
 #include "../zsys.h"
 #include "Compiler.h"
+#include <boost/move/unique_ptr.hpp>
 
-ZScript::ScriptsData * compile(const char *filename)
+namespace ZScript
+{
+
+boost::movelib::unique_ptr<ScriptsData> compile(std::string const& filename)
 {
     box_out("Cannot compile ZScript: ZQuest was compiled without ZScript support!");
     box_eol();
 
-    return NULL;
+    return nullptr;
+}
+
+// These still need to be defined somewhere to prevent linking errors.
+
+//ZScript::ScriptType ZScript::ScriptType::
+bool operator==(const ScriptType&, const ScriptType&)
+{
+    assert(false);
+    return false;
+}
+
+ScriptType const ScriptType ::invalid;
+ScriptType const ScriptType::global;
+ScriptType const ScriptType::ffc;
+ScriptType const ScriptType::item;
+ScriptType const ScriptType::npc;
+ScriptType const ScriptType::eweapon;
+ScriptType const ScriptType::lweapon;
+ScriptType const ScriptType::link;
+ScriptType const ScriptType::dmapdata;
+ScriptType const ScriptType::screendata;
+ScriptType const ScriptType::itemsprite;
+ScriptType const ScriptType::untyped;
+ScriptType const ScriptType::subscreendata;
+ScriptType const ScriptType::combodata;
+
 }
