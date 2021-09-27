d69aafa6ee6321959cc1e7e039e400c5fae525f5
EmilyV99 <emilygamergirl99@gmail.com>
2021-09-27 02:09:47 -0400

    Restructure "output/" directory

---
+++ output/modules/classic/qstdat.h
@@ -0,0 +1,8 @@
+/* Allegro datafile object indexes, produced by grabber v4.2.0, MSVC.s */
+/* Datafile: e:\ZC\2.55_Win_Alpha_14_clean\modules\classic\classic_qst.dat */
+/* Date: Thu Mar 07 05:04:26 2019 */
+/* Do not hand edit! */
+
+#define _SIGNATURE                       0        /* DATA */
+#define NESQST_NEW_QST                   1        /* DATA */
+
