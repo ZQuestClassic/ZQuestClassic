c9ad01f4f77be014fa4f5ea8d1bd0e074365d366
DimiPZC <joshuaf@live.ca>
2021-11-05 15:24:57 -0400

    fixed issue with loading audio not letting zc start.

--- src/qst.cpp
+++ src/qst.cpp
@@ -12897,11 +12897,23 @@ int32_t readsfx(PACKFILE *f, zquestheader *Header, bool keepdata)
 				}
 			}
 		}
+		/*
 		else if(i <= Z35)
 		{
 			SAMPLE* datsamp = (SAMPLE*)(sfxdata[i].dat);
 			memcpy(&temp_sample, datsamp, sizeof(SAMPLE));
 			set_bit(tempflag, i-1, 1);
+		}
+		*/
+		else if(i <= Z35)
+		{
+			zprint2("Attempting load sfx '%d' into quest from sfx.dat....\n", i);
+			SAMPLE* datsamp = (SAMPLE*)(sfxdata[i].dat);
+			memcpy(&temp_sample, datsamp, sizeof(SAMPLE));
+			set_bit(tempflag, i-1, 1);
+			int32_t len = (temp_sample.bits==8?1:2)*(temp_sample.stereo==0?1:2)*temp_sample.len;
+			temp_sample.data = calloc(len,1);
+			memcpy(temp_sample.data, datsamp->data, len);
 		}
 		else continue;
 		
