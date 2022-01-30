5006c4c5be23bf5ca6ae4ec092939108744440a6
DimiPZC <joshuaf@live.ca>
2022-01-30 18:30:00 -0500

    Fixed Zoria's ->Jump blunder

--- src/ffscript.cpp
+++ src/ffscript.cpp
@@ -4386,7 +4386,7 @@ long get_register(const long arg)
     case ITEMJUMP:
         if(0!=(s=checkItem(ri->itemref)))
         {
-            ret = long(((item*)(s))->fall) * -100.0;
+	    ret = (long(((item*)(s))->fall / fix(100.0)) * 10000);
         }
         
         break;
@@ -4782,7 +4782,7 @@ long get_register(const long arg)
         if(GuyH::loadNPC(ri->guyref, "npc->Jump") != SH::_NoError)
             ret = -10000;
         else
-            ret = long(GuyH::getNPC()->fall) * -100.0;
+            ret = (long(-GuyH::getNPC()->fall / fix(100.0)) * 10000);
             
         break;
         
@@ -4870,7 +4870,7 @@ long get_register(const long arg)
         
     case LWPNJUMP:
         if(0!=(s=checkLWpn(ri->lwpn,"Jump")))
-            ret = long(((weapon*)(s))->fall) * -100.0;
+	    ret = (long(((weapon*)(s))->fall / fix(100.0)) * 10000);
             
         break;
         
@@ -5095,7 +5095,7 @@ long get_register(const long arg)
         
     case EWPNJUMP:
         if(0!=(s=checkEWpn(ri->ewpn, "Jump")))
-            ret = long(((weapon*)(s))->fall) * -100.0;
+            ret = (long(((weapon*)(s))->fall / fix(100.0)) * 10000);
             
         break;
         
