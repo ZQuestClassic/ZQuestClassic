518bfc4ae977723be0e0690fd59fb5da319e4dc1
Connor Clark <cjamcl@gmail.com>
2022-04-16 01:23:35 -0700

    web: allegro5 patch

--- build_emscripten.sh
+++ build_emscripten.sh
@@ -38,24 +38,6 @@ EMCC_CACHE_DIR="$(dirname $(which emcc))/cache"
 EMCC_CACHE_INCLUDE_DIR="$EMCC_CACHE_DIR/sysroot/include"
 EMCC_CACHE_LIB_DIR="$EMCC_CACHE_DIR/sysroot/lib/wasm32-emscripten"
 
-# Temporary workarounds until various things are fixed upstream.
-
-# Ensure that the SDL source code has been downloaded.
-if [ ! -d "$EMCC_CACHE_DIR/ports/sdl2" ]
-then
-  embuilder build sdl2
-fi
-if [ ! -d "$EMCC_CACHE_DIR/ports/sdl2_mixer" ]
-then
-  embuilder build sdl2_mixer
-fi
-
-# Must manually delete the SDL library to force Emscripten to rebuild it.
-rm -rf "$EMCC_CACHE_LIB_DIR"/libSDL2.a "$EMCC_CACHE_LIB_DIR"/libSDL2-mt.a
-rm -rf "$EMCC_CACHE_LIB_DIR"/libSDL2_mixer_mid-mp3-ogg.a
-
-sh ../../patches/apply.sh
-
 EMCC_FLAGS=(
   -s USE_FREETYPE=1
   -s USE_VORBIS=1
@@ -126,26 +108,22 @@ emcmake cmake ../.. \
   -D CMAKE_EXE_LINKER_FLAGS="${LINKER_FLAGS[*]}" \
   -D CMAKE_EXECUTABLE_SUFFIX_CXX=".html"
 
-# The a5 SDL audio system hardcodes a value for # samples that is too high. Until I can upstream a patch to make this
-# configurable, just manually change it here!
-sed -i -e 's/4096/512/' _deps/allegro5-src/addons/audio/sdl_audio.c
+# Ensure that the SDL source code has been downloaded,
+# otherwise the patches can't be applied.
+if [ ! -d "$EMCC_CACHE_DIR/ports/sdl2" ]
+then
+  embuilder build sdl2
+fi
+if [ ! -d "$EMCC_CACHE_DIR/ports/sdl2_mixer" ]
+then
+  embuilder build sdl2_mixer
+fi
 
-# https://github.com/liballeg/allegro5/pull/1322
-sed -i -e 's/(SDL_INIT_EVERYTHING)/(SDL_INIT_EVERYTHING-SDL_INIT_HAPTIC)/' _deps/allegro5-src/src/sdl/sdl_system.c
+# Manually delete libraries from Emscripten cache to force a rebuild.
+rm -rf "$EMCC_CACHE_LIB_DIR"/libSDL2-mt.a
+rm -rf "$EMCC_CACHE_LIB_DIR"/libSDL2_mixer_mid-mp3-ogg.a
 
-# This emscripten-specific timer code actually really messes up the framerate, making it go way too fast.
-sed -i -e 's/ _al_timer_thread_handle_tick/\/\/_al_timer_thread_handle_tick/' _deps/allegro5-src/src/sdl/sdl_system.c
-
-# Fix allegro's SDL's joysticks for emscripten.
-# See https://github.com/liballeg/allegro5/pull/1326
-sed -i -e 's/if (count <= 0)/if (false)/' _deps/allegro5-src/src/sdl/sdl_joystick.c
-NEEDLE="joysticks = calloc(count, sizeof \* joysticks);"
-sed -i -e "s/$NEEDLE$/joysticks = count > 0 ? calloc(count, sizeof * joysticks) : NULL;/" _deps/allegro5-src/src/sdl/sdl_joystick.c
-
-# Fix allegro's SDL's support for joystick button names.
-# See https://github.com/liballeg/allegro5/pull/1327
-NEEDLE='= "button";'
-sed -i -e "s/$NEEDLE$/= SDL_IsGameController(i) ? SDL_GameControllerGetStringForButton(b) : \"button\";/" _deps/allegro5-src/src/sdl/sdl_joystick.c
+sh ../../patches/apply.sh
 
 TARGETS="${@:-zelda zquest}"
 cmake --build . -t $TARGETS
