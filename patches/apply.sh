881ed9cc0243db278f6dcced8f7577eaf8ac4f65
Connor Clark <cjamcl@gmail.com>
2022-04-16 01:23:35 -0700

    web: allegro5 patch

--- patches/apply.sh
+++ patches/apply.sh
@@ -3,13 +3,13 @@
 set -e
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+EMCC_CACHE_DIR="$(dirname $(which emcc))/cache"
 
 # folder patch
-function apply_patch_to_emcc_port {
+function apply_patch {
   cd "$1"
 
   if [ -d .git ]; then
-    git clean -fd
     git checkout -- .
   else
     git init
@@ -18,8 +18,9 @@ function apply_patch_to_emcc_port {
   fi
 
   patch -p1 < "$2"
+  cd -
 }
 
-EMCC_CACHE_DIR="$(dirname $(which emcc))/cache"
-apply_patch_to_emcc_port "$EMCC_CACHE_DIR/ports/sdl2/SDL-release-2.0.20" "$SCRIPT_DIR/sdl2.patch"
-apply_patch_to_emcc_port "$EMCC_CACHE_DIR/ports/sdl2_mixer/SDL2_mixer-release-2.0.2" "$SCRIPT_DIR/sdl2_mixer.patch"
+apply_patch "$EMCC_CACHE_DIR/ports/sdl2/SDL-release-2.0.20" "$SCRIPT_DIR/sdl2.patch"
+apply_patch "$EMCC_CACHE_DIR/ports/sdl2_mixer/SDL2_mixer-release-2.0.2" "$SCRIPT_DIR/sdl2_mixer.patch"
+apply_patch _deps/allegro5-src "$SCRIPT_DIR/allegro5.patch"
