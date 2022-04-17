39051570affa7d7a5fed7462dbaa692b61db577f
Connor Clark <cjamcl@gmail.com>
2022-04-17 15:16:24 -0700

    web: patch to update to sdl mixer 2.0.4

--- patches/apply.sh
+++ patches/apply.sh
@@ -11,7 +11,8 @@
 set -e
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-EMCC_CACHE_DIR="$(dirname $(which emcc))/cache"
+EMCC_DIR="$(dirname $(which emcc))"
+EMCC_CACHE_DIR="$EMCC_DIR/cache"
 
 NO_GIT_CLEAN=false
 GIT_CLEAN=true
@@ -40,7 +41,32 @@ function apply_patch {
 }
 
 echo "Applying patches ..."
+
+apply_patch "$EMCC_DIR" "$SCRIPT_DIR/emscripten.patch" $NO_GIT_CLEAN
+
+# Ensure that the SDL source code has been downloaded,
+# otherwise the patches can't be applied.
+if [ ! -d "$EMCC_CACHE_DIR/ports/sdl2" ]
+then
+  embuilder build sdl2
+  embuilder clear sdl2
+fi
+if [ ! -d "$EMCC_CACHE_DIR/ports/sdl2_mixer/SDL_mixer-release-2.0.4" ]
+then
+  rm -rf "$EMCC_CACHE_DIR/ports/sdl2_mixer"
+  embuilder build sdl2_mixer
+  embuilder clear sdl2_mixer
+fi
+
+# Manually delete libraries from Emscripten cache to force a rebuild.
+rm -rf "$EMCC_CACHE_LIB_DIR"/libSDL2-mt.a
+rm -rf "$EMCC_CACHE_LIB_DIR"/libSDL2_mixer_mid-mp3-ogg.a
+# This would work except you can't clear port variants.
+# https://github.com/emscripten-core/emscripten/issues/16744
+# embuilder clear sdl2-mt sdl2_mixer_mid_mp3_ogg
+
 apply_patch "$EMCC_CACHE_DIR/ports/sdl2/SDL-release-2.0.20" "$SCRIPT_DIR/sdl2.patch" $GIT_CLEAN
-apply_patch "$EMCC_CACHE_DIR/ports/sdl2_mixer/SDL2_mixer-release-2.0.2" "$SCRIPT_DIR/sdl2_mixer.patch" $GIT_CLEAN
+apply_patch "$EMCC_CACHE_DIR/ports/sdl2_mixer/SDL_mixer-release-2.0.4" "$SCRIPT_DIR/sdl2_mixer.patch" $GIT_CLEAN
 apply_patch _deps/allegro5-src "$SCRIPT_DIR/allegro5.patch" $NO_GIT_CLEAN
+
 echo "Done applying patches!"
