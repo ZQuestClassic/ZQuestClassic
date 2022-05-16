e599839922b282e975bedec02cf41c94fdadbc8e
Connor Clark <cjamcl@gmail.com>
2022-05-15 20:19:49 -0700

    do not busy loop in allegro legacy event queues

--- allegro-legacy/src/a5/a5_joystick.c
+++ allegro-legacy/src/a5/a5_joystick.c
@@ -52,7 +52,8 @@ static void * a5_joystick_thread_proc(ALLEGRO_THREAD * thread, void * data)
     while(!al_get_thread_should_stop(thread))
     {
         al_init_timeout(&timeout, 0.1);
-        if(al_wait_for_event_until(queue, &event, &timeout))
+        al_wait_for_event(queue, &event);
+        if(true)
         {
             switch(event.type)
             {
