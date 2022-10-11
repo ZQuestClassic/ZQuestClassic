df21e25fab77f012a58003a6bf9a49e00065b56b
Connor Clark <cjamcl@gmail.com>
2022-10-11 07:58:26 -0500

    remove boost/optional old code

--- src/parser/Types.h
+++ src/parser/Types.h
@@ -6,6 +6,7 @@
 #include <map>
 #include <string>
 #include <vector>
+#include <boost/type_traits.hpp>
 #include "CompilerUtils.h"
 #include "parserDefs.h"
 
--- src/precompiled.h
+++ src/precompiled.h
@@ -19,7 +19,6 @@
 
 //Boost
 #include <boost/make_shared.hpp>
-#include <boost/optional.hpp>
 #include <boost/smart_ptr.hpp>
 #include <boost/move/make_unique.hpp>
 
