9c2f7c6cf9bedd3cfdbb2a020f6264b552b16f29
Connor Clark <cjamcl@gmail.com>
2022-11-27 23:01:41 -0800

    upload json coverage to codecov

--- cmake/CodeCoverage.cmake
+++ cmake/CodeCoverage.cmake
@@ -9,7 +9,7 @@ function(setup_coverage_test_target target_name output_dir test_runner)
   file(MAKE_DIRECTORY ${output_dir})
   add_custom_target(${target_name}
         COMMAND ${test_runner} ${ARGN}
-        COMMAND ${CODE_COVERAGE_GCOVR} -d --html-details ${output_dir}/index.html -r ${CMAKE_SOURCE_DIR} -f ${CMAKE_SOURCE_DIR}/src --html-theme blue --html-title "Zelda Classic Coverage Report"
+        COMMAND ${CODE_COVERAGE_GCOVR} -d --html-details ${output_dir}/index.html --json ${CMAKE_BINARY_DIR}/coverage.json -r ${CMAKE_SOURCE_DIR} -f ${CMAKE_SOURCE_DIR}/src --html-theme blue --html-title "Zelda Classic Coverage Report"
         COMMAND ${CMAKE_COMMAND} -E echo "Open ${output_dir}/index.html in your browser to view the coverage report."
         WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
         COMMENT "Generating coverage results")
