66c6ada2b2d0c70e27e6945ab376b61b282fa578
Connor Clark <cjamcl@gmail.com>
2022-11-28 22:47:03 -0800

    double replay test timeout for windows ci (better)

--- tests/run_replay_tests.py
+++ tests/run_replay_tests.py
@@ -127,9 +127,9 @@ def run_replay_test(replay_file):
             exe_args.extend(['-frame', str(num_frames_checked)])
             print(f"-frame {num_frames_checked}, only doing {100 * num_frames_checked / num_frames:.2f}% ... ", end='', flush=True)
             estimated_duration = max_duration
-        if args.ci == 'windows':
-            estimated_duration *= 2
         timeout = max(60 + estimated_duration * 1.5, 60 * 3)
+        if args.ci == 'windows':
+            timeout *= 2
     else:
         timeout = None
 
