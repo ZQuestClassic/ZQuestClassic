e55a502583e811aa890d4db8b952f53d4cf607e5
Connor Clark <cjamcl@gmail.com>
2022-12-22 23:25:09 -0800

    include zconsole.exe in windows package

--- scripts/package.py
+++ scripts/package.py
@@ -118,6 +118,7 @@ else:
             binary_file(build_dir / 'zquest'),
             binary_file(build_dir / 'zscript'),
             binary_file(build_dir / 'zlauncher'),
+            binary_file(build_dir / 'zconsole') if system == 'Windows' else None,
             *(glob_files(build_dir, '*.dll') if system == 'Windows' else []),
             *(glob_files(build_dir, '*.so*') if system == 'Linux' else []),
             *(glob_files(build_dir, '*.dylib') if system == 'Darwin' else []),
