ab707a6ae525396507d04ff9e93de176c0b9430f
Connor Clark <cjamcl@gmail.com>
2023-02-20 14:58:23 -0800

    ci: fix signatures not being set for releases
    Regressed in c6f570

--- .github/workflows/configure-signatures.sh
+++ .github/workflows/configure-signatures.sh
@@ -6,8 +6,8 @@ echo '#define DEV_SIGNOFF "Build_Script"' > src/metadata/sigs/devsig.h.sig
 echo '#define __TIMEZONE__ "UTC"' >> src/metadata/sigs/devsig.h.sig
 echo '' >> src/metadata/sigs/devsig.h.sig
 
-# TODO: this arg should be a compiler name, not os.
-if [ "$1" == 'win' ]; then
+echo '' > src/metadata/sigs/compilersig.h.sig
+if [ "$1" == 'msvc' ]; then
   echo '#define V_ZC_COMPILERSIG 1' >> src/metadata/sigs/compilersig.h.sig
   echo '#define COMPILER_V_FIRST 19' >> src/metadata/sigs/compilersig.h.sig
   echo '#define COMPILER_V_SECOND 32' >> src/metadata/sigs/compilersig.h.sig
@@ -15,7 +15,7 @@ if [ "$1" == 'win' ]; then
   echo '#define COMPILER_V_FOURTH 0' >> src/metadata/sigs/compilersig.h.sig
   echo '#define COMPILER_NAME "MSVC"' >> src/metadata/sigs/compilersig.h.sig
   echo '#define COMPILER_VERSION "v17, 2022"' >> src/metadata/sigs/compilersig.h.sig
-elif [ "$1" == 'mac' ]; then
+elif [ "$1" == 'clang' ]; then
   echo '#define V_ZC_COMPILERSIG 1' >> src/metadata/sigs/compilersig.h.sig
   echo '#define COMPILER_V_FIRST __clang_major__' >> src/metadata/sigs/compilersig.h.sig
   echo '#define COMPILER_V_SECOND __clang_minor__' >> src/metadata/sigs/compilersig.h.sig
@@ -23,17 +23,11 @@ elif [ "$1" == 'mac' ]; then
   echo '#define COMPILER_V_FOURTH 0' >> src/metadata/sigs/compilersig.h.sig
   echo '#define COMPILER_NAME "clang"' >> src/metadata/sigs/compilersig.h.sig
   echo '#define COMPILER_VERSION __clang_version__' >> src/metadata/sigs/compilersig.h.sig
-elif [ "$1" == 'ubuntu' ]; then
-  # We also use clang here.
-  echo '#define V_ZC_COMPILERSIG 1' >> src/metadata/sigs/compilersig.h.sig
-  echo '#define COMPILER_V_FIRST __clang_major__' >> src/metadata/sigs/compilersig.h.sig
-  echo '#define COMPILER_V_SECOND __clang_minor__' >> src/metadata/sigs/compilersig.h.sig
-  echo '#define COMPILER_V_THIRD __clang_patchlevel__' >> src/metadata/sigs/compilersig.h.sig
-  echo '#define COMPILER_V_FOURTH 0' >> src/metadata/sigs/compilersig.h.sig
-  echo '#define COMPILER_NAME "clang"' >> src/metadata/sigs/compilersig.h.sig
-  echo '#define COMPILER_VERSION __clang_version__' >> src/metadata/sigs/compilersig.h.sig
+elif [ "$1" == 'gcc' ]; then
+  # TODO: we don't use gcc in CI at the moment.
+  exit 1
 else
-  echo "unknown platform $1"
+  echo "unknown compiler $1"
   exit 1
 fi
 echo '' >> src/metadata/sigs/compilersig.h.sig
