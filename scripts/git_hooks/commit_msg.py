a8f26b2595acf45620e6fb9d59634d05c56c1425
Connor Clark <cjamcl@gmail.com>
2023-06-10 23:07:38 -0700

    misc: add git hooks to validate commit messages

---
+++ scripts/git_hooks/commit_msg.py
@@ -0,0 +1,54 @@
+import sys
+import re
+from pathlib import Path
+from common import valid_types, valid_scopes
+
+commit_msg_path = Path(sys.argv[1])
+commit_msg = commit_msg_path.read_text()
+is_valid = True
+
+log = ''
+
+
+def add_log(*args):
+    global log
+    message = ' '.join(args)
+    log += message
+    log += '\n'
+
+
+# Prefix is required, scope is optional.
+first_line = commit_msg.splitlines()[0]
+match = re.match(r'(\w+)\((\w+)\): (.+)',
+                 first_line) or re.match(r'(\w+):( )(.+)', first_line)
+if not match:
+    add_log('commit message must match expected pattern, using an expected type and optional scope.')
+    add_log('examples:\n\tfix: fix the thing\n\tfix(zc): fix the thing\n')
+    add_log(f'valid types (the first word of the commit) are:',
+            ', '.join(valid_types))
+    add_log(f'valid scopes (the optional text in parentheses) are:',
+            ', '.join(valid_scopes))
+    add_log()
+    is_valid = False
+else:
+    type, scope, oneliner = match.groups()
+    scope = scope.strip()
+
+    is_valid = True
+    if type not in valid_types:
+        add_log(
+            f'invalid type: {type}\nMust be one of: {", ".join(valid_types)}')
+        is_valid = False
+    if scope and scope not in valid_scopes:
+        add_log(
+            f'invalid scope: {scope}\nMust be one of: {", ".join(valid_scopes)}')
+        is_valid = False
+
+if not is_valid:
+    print(commit_msg)
+    print('============== FAILED TO COMMIT ==================')
+    print(log)
+    print('commit message is not valid, please rewrite it')
+    print('to skip this validation, commit again with the --no-verify flag')
+    print('for reference, the commit message is above')
+    exit(1)
---
+++ scripts/git_hooks/common.py
@@ -0,0 +1,20 @@
+valid_types = [
+    'feat',
+    'fix',
+    'docs',
+    'build',
+    'chore',
+    'refactor',
+    'test',
+    'ci',
+    'misc',
+]
+
+valid_scopes = [
+    'zc',
+    'zq',
+    'zscript',
+    'launcher',
+    'zconsole',
+    'web',
+]
