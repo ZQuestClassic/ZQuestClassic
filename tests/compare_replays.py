9a81e9cf487b051f09626bcf5501eb0dca94939c
Connor Clark <cjamcl@gmail.com>
2023-06-30 14:04:52 -0700

    test: refactor compare_replays.py to prepare for re-use

--- tests/compare_replays.py
+++ tests/compare_replays.py
@@ -29,19 +29,6 @@ def dir_path(path):
         raise ArgumentTypeError(f'{path} is not a valid directory')
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--workflow_run', type=int, action='append')
-parser.add_argument('--local', type=dir_path, action='append')
-parser.add_argument('--repo', default='ArmageddonGames/ZQuestClassic')
-parser.add_argument('--token')
-args = parser.parse_args()
-
-
-if not args.workflow_run and not args.local:
-    raise ArgumentTypeError(
-        'must provide at least one --workflow_run or --local')
-
-
 script_dir = os.path.dirname(os.path.realpath(__file__))
 out_dir = Path(f'{script_dir}/compare-report')
 
@@ -56,7 +43,8 @@ def hash_image(filename):
 #  - a list of replays and any snapshots they produced
 # TODO remove older `test_run` concept and use `test_results` directly in the JS instead.
 def collect_test_run_from_dir(directory: Path):
-    test_results_json = json.loads((directory/'test_results.json').read_text('utf-8'))
+    test_results_json = json.loads(
+        (directory/'test_results.json').read_text('utf-8'))
     test_results = ReplayTestResults(**test_results_json)
     test_run = {
         'label': '',
@@ -140,91 +128,117 @@ def collect_test_runs_from_dir(directory: Path):
     return runs_by_platform.values()
 
 
-if out_dir.exists():
-    shutil.rmtree(out_dir)
-out_dir.mkdir(parents=True)
+def collect_test_runs_from_ci(gh: Github, repo: str, workflow_run_id: str):
+    workflow_dir = get_gha_artifacts_with_retry(gh, repo, workflow_run_id)
+    return collect_test_runs_from_dir(workflow_dir)
 
-# TODO: push args.* to same array in argparse so that order is preserved.
-# first should always be baseline. For now, assume it is the workflow option.
 
-all_test_runs = []
-if args.workflow_run:
-    print(f'collecting test runs from CI of {args.repo}')
-    gh = Github(args.token)
-    for run_id in args.workflow_run:
-        print(f'=== collecting test runs from workflow run {run_id}')
-        workflow_dir = get_gha_artifacts_with_retry(gh, args.repo, run_id)
-        test_runs = collect_test_runs_from_dir(workflow_dir)
+def create_compare_report(test_runs):
+    if out_dir.exists():
+        shutil.rmtree(out_dir)
+    out_dir.mkdir(parents=True)
 
-        print('found:')
+    def count_images():
+        hashes = set()
         for test_run in test_runs:
-            print(f' - {test_run["label"]}')
-        all_test_runs.extend(test_runs)
-if args.local:
-    for directory in args.local:
-        print(f'=== collecting test runs from {directory}')
-        test_runs = collect_test_runs_from_dir(directory)
+            for replay_data in test_run['replays']:
+                for snapshot in replay_data['snapshots']:
+                    hashes.add(snapshot['hash'])
+        return len(hashes)
 
-        print('found:')
-        for test_run in test_runs:
-            print(f' - {test_run["label"]}')
-        all_test_runs.extend(test_runs)
+    if 'CI' in os.environ:
+        image_count = count_images()
+        if image_count > 4450:
+            print(
+                f'found {image_count} images, which is too many to upload to surge')
+            baseline_test_run = test_runs[0]
+            for test_run in test_runs[1:]:
+                for replay_data in test_run['replays']:
+                    filtered_snapshots = []
+                    for snapshot in replay_data['snapshots']:
+                        frame = snapshot['frame']
+                        baseline_replay_data = next(
+                            (d for d in baseline_test_run['replays'] if d['name'] == replay_data['name']), None)
+                        if not baseline_replay_data:
+                            continue
 
-def count_images():
-    hashes = set()
-    for test_run in all_test_runs:
+                        baseline_snapshot = next(
+                            (s for s in baseline_replay_data['snapshots'] if s['frame'] == frame), None)
+                        if not baseline_snapshot:
+                            continue
+
+                        filtered_snapshots.append(snapshot)
+
+                    replay_data['snapshots'] = filtered_snapshots
+
+            image_count = count_images()
+            print(f'reduced to {image_count} images')
+
+    snapshots_dir = out_dir / 'snapshots'
+    snapshots_dir.mkdir()
+    for test_run in test_runs:
         for replay_data in test_run['replays']:
             for snapshot in replay_data['snapshots']:
-                hashes.add(snapshot['hash'])
-    return len(hashes)
+                hashsum = snapshot['hash']
+                ext = snapshot['path'].suffix
+                filename = f'{hashsum}{ext}'
+                dest = snapshots_dir / filename
+                if not dest.exists():
+                    shutil.copy2(snapshot['path'].absolute(), dest)
+                snapshot['path'] = str(dest.relative_to(out_dir))
 
-if 'CI' in os.environ:
-    image_count = count_images()
-    if image_count > 4450:
-        print(f'found {image_count} images, which is too many to upload to surge')
-        baseline_test_run = all_test_runs[0]
-        for test_run in all_test_runs[1:]:
-            for replay_data in test_run['replays']:
-                filtered_snapshots = []
-                for snapshot in replay_data['snapshots']:
-                    frame = snapshot['frame']
-                    baseline_replay_data = next((d for d in baseline_test_run['replays'] if d['name'] == replay_data['name']), None)
-                    if not baseline_replay_data:
-                        continue
+    html = Path(
+        f'{script_dir}/compare-resources/compare.html').read_text('utf-8')
+    css = Path(
+        f'{script_dir}/compare-resources/compare.css').read_text('utf-8')
+    js = Path(f'{script_dir}/compare-resources/compare.js').read_text('utf-8')
+    deps = Path(
+        f'{script_dir}/compare-resources/pixelmatch.js').read_text('utf-8')
 
-                    baseline_snapshot = next((s for s in baseline_replay_data['snapshots'] if s['frame'] == frame), None)
-                    if not baseline_snapshot:
-                        continue
+    result = html.replace(
+        '// JAVASCRIPT', f'const testRuns = {json.dumps(test_runs, indent=2)}\n  {js}')
+    result = result.replace('// DEPS', deps)
+    result = result.replace('/* CSS */', css)
+    out_path = Path(f'{out_dir}/index.html')
+    out_path.write_text(result)
+    print(f'report written to {out_path}')
 
-                    filtered_snapshots.append(snapshot)
 
-                replay_data['snapshots'] = filtered_snapshots
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--workflow_run', type=int, action='append')
+    parser.add_argument('--local', type=dir_path, action='append')
+    parser.add_argument('--repo', default='ArmageddonGames/ZQuestClassic')
+    parser.add_argument('--token')
+    args = parser.parse_args()
 
-        image_count = count_images()
-        print(f'reduced to {image_count} images')
+    if not args.workflow_run and not args.local:
+        raise ArgumentTypeError(
+            'must provide at least one --workflow_run or --local')
 
-snapshots_dir = out_dir / 'snapshots'
-snapshots_dir.mkdir()
-for test_run in all_test_runs:
-    for replay_data in test_run['replays']:
-        for snapshot in replay_data['snapshots']:
-            hashsum = snapshot['hash']
-            ext = snapshot['path'].suffix
-            filename = f'{hashsum}{ext}'
-            dest = snapshots_dir / filename
-            if not dest.exists():
-                shutil.copy2(snapshot['path'].absolute(), dest)
-            snapshot['path'] = str(dest.relative_to(out_dir))
+    # TODO: push args.* to same array in argparse so that order is preserved.
+    # first should always be baseline. For now, assume it is the workflow option.
 
-html = Path(f'{script_dir}/compare-resources/compare.html').read_text('utf-8')
-css = Path(f'{script_dir}/compare-resources/compare.css').read_text('utf-8')
-js = Path(f'{script_dir}/compare-resources/compare.js').read_text('utf-8')
-deps = Path(f'{script_dir}/compare-resources/pixelmatch.js').read_text('utf-8')
+    all_test_runs = []
+    if args.workflow_run:
+        print(f'collecting test runs from CI of {args.repo}')
+        gh = Github(args.token)
+        for run_id in args.workflow_run:
+            print(f'=== collecting test runs from workflow run {run_id}')
+            test_runs = collect_test_runs_from_ci(gh, args.repo, run_id)
 
-result = html.replace(
-    '// JAVASCRIPT', f'const testRuns = {json.dumps(all_test_runs, indent=2)}\n  {js}')
-result = result.replace('// DEPS', deps)
-result = result.replace('/* CSS */', css)
-out_path = Path(f'{out_dir}/index.html')
-out_path.write_text(result)
-print(f'report written to {out_path}')
+            print('found:')
+            for test_run in test_runs:
+                print(f' - {test_run["label"]}')
+            all_test_runs.extend(test_runs)
+    if args.local:
+        for directory in args.local:
+            print(f'=== collecting test runs from {directory}')
+            test_runs = collect_test_runs_from_dir(directory)
+
+            print('found:')
+            for test_run in test_runs:
+                print(f' - {test_run["label"]}')
+            all_test_runs.extend(test_runs)
+
+    create_compare_report(all_test_runs)
