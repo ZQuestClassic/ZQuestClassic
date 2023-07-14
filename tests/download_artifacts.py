dca122f8e86fee82d15dbd3755124ff251bba47c
Connor Clark <cjamcl@gmail.com>
2023-07-13 21:58:57 -0700

    test: add download_artifacts.py

---
+++ tests/download_artifacts.py
@@ -0,0 +1,17 @@
+# Downloads artifacts from GitHub Actions.
+
+import argparse
+from github import Github
+from common import get_gha_artifacts
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--repo', default='ArmageddonGames/ZQuestClassic')
+    parser.add_argument('--token', required=True)
+    parser.add_argument('--workflow_run', type=int, required=True)
+
+    args = parser.parse_args()
+    gh = Github(args.token)
+
+    path = get_gha_artifacts(gh, args.repo, args.workflow_run)
+    print(f'saved to {path}')
