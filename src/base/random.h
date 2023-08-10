97acebb06ecba31da71011c991f4f13b8dadef85
EmilyV99 <emilygamergirl99@gmail.com>
2023-08-10 10:43:34 -0400

    test: add (crude) RNG debugging option

--- src/base/random.h
+++ src/base/random.h
@@ -4,13 +4,31 @@
 #include "base/zdefs.h"
 #include <random>
 
-typedef std::mt19937 zc_randgen;
+#ifdef IS_PLAYER
+#define DEBUG_RAND false
+#else
+#define DEBUG_RAND false //Can only work when IS_PLAYER
+#endif
 
+typedef std::mt19937 zc_randgen;
 zc_randgen* zc_get_default_rand();
+
+#if !DEBUG_RAND
 int32_t zc_oldrand(zc_randgen* rng=NULL);
 int32_t zc_rand(zc_randgen* rng=NULL);
 int32_t zc_rand(int32_t upper, int32_t lower=0, zc_randgen* rng=NULL);
 void zc_srand(int32_t seedval, zc_randgen* rng=NULL);
+#else
+
+#define zc_oldrand(...) db_oldrand(__FILE__,__LINE__,__VA_ARGS__)
+int32_t db_oldrand(char const* f, int l,zc_randgen* rng=NULL);
+#define zc_rand(...) db_rand(__FILE__,__LINE__,__VA_ARGS__)
+int32_t db_rand(char const* f, int l,zc_randgen* rng=NULL);
+int32_t db_rand(char const* f, int l,int32_t upper,int32_t lower=0,zc_randgen* rng=NULL);
+#define zc_srand(...) db_srand(__FILE__,__LINE__,__VA_ARGS__)
+void db_srand(char const* f, int l, int32_t seedval, zc_randgen* rng=NULL);
+
+#endif
 
 #endif
 
