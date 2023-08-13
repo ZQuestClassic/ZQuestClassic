7c6430abe8d3e6b4484961ec74a8192c1833378b
Connor Clark <cjamcl@gmail.com>
2023-08-13 13:29:16 -0700

    misc: support squashing and rewriting commit body for changelogs

---
+++ scripts/changelog_overrides/generic_changes.md
@@ -0,0 +1,12 @@
+subject ae823474a24261e92266df611c62a5f6007c33d3 fix(zc): waitdraw not working for ffcs over 32
+subject c8e2c861edc73de3ff851d3cbc9bc8d07037d007 fix(zc): npc scripts not running on screen load
+subject 935757d42efb0f999d7a5a1b0d2e893cac962614 fix(zc): enemies not running scripts on first frame with scriptloadenemies
+subject 95e3288ec29c716b8645a2e01da311a94fded6b5 fix(zc): Test Mode issues when quitting during opening wipe
+subject 9bf02571c6c09c182561bd565fc13fe701d4ad04 misc: ./zelda -load-and-quit `<qstpath>`
+subject 5641cf8c1a9e76ed8fca68e7189f71c5efd20e1e fix(zc): new respawn points with ladder
+subject 9762141dbe1bd7461b5dee03a627f290c6ed322a fix(zc): various newer hero movement
+subject 0da042ba3b238bba1b468c753f24cce71cefc2a4 fix(zscript): Negative Array rule issues
+subject dabe7c091b89fa2e09217dfb63c652184b78f65e feat(vscode): create VS Code extension
+subject 6ecf0775afeac108bb4f806b76f9c1835c0a98d1 feat(vscode): update vscode extension keywords
+subject d82a0d8458b3029283385db04f1d935fd0dbce48 misc: add 'std_zh' scope to changelog generation
+subject f7228536ad55c2fb4bb20d00bba8b8c06d7e67c2 feat(std_zh): add 'GetLevelSwitchState()'/'SetLevelSwitchState()' helper functions
--- scripts/generate_changelog.py
+++ scripts/generate_changelog.py
@@ -2,9 +2,8 @@ import argparse
 import re
 import os
 import subprocess
-import textwrap
 from pathlib import Path
-from dataclasses import dataclass
+from dataclasses import dataclass, field, replace
 from typing import Dict, List
 from git_hooks.common import valid_types, valid_scopes
 
@@ -21,8 +20,49 @@ from_sha = getattr(args, 'from', None)
 
 commit_url_prefix = 'https://github.com/ArmageddonGames/ZQuestClassic/commit'
 
-overrides = (script_dir / 'changelog_overrides.txt').read_text()
-overrides = dict(map(lambda s: s.split(' ', 1), overrides.splitlines()))
+overrides = dict()
+overrides_squashes = dict()
+
+def parse_override_file(file: Path):
+    last_override = None
+    current_squash_list = []
+
+    for line in file.read_text().splitlines():
+        line = line.rstrip()
+
+        if last_override and last_override[0] == 'reword' and not re.match(r'^(reword|subject|squash|pick)', line):
+            if line == '=end':
+                last_override = None
+                continue
+
+            last_override[1] += f'\n{line}'
+            continue
+
+        if not line:
+            continue
+
+        parts = line.split(' ', 2)
+        if len(parts) == 3:
+            type, hash, rest = parts
+        else:
+            type, hash, rest = [*parts, None]
+
+        if len(hash) != 40:
+            raise Exception('expected full hashes')
+        if hash in overrides:
+            raise Exception(f'hash already present: {hash}')
+
+        if type == 'squash':
+            current_squash_list.append(hash)
+        elif last_override and last_override[0] == 'squash':
+            overrides_squashes[hash] = current_squash_list
+            current_squash_list = []
+
+        overrides[hash] = last_override = [type, rest]
+
+    if last_override and last_override[0] == 'squash':
+        raise Exception('squash into what?')
+
 
 @dataclass
 class Commit:
@@ -33,6 +73,7 @@ class Commit:
     subject: str
     oneline: str
     body: str
+    squashed_commits: List['Commit'] = field(default_factory=lambda: [])
 
     def scope_and_oneline(self):
         if self.scope:
@@ -53,6 +94,13 @@ def parse_scope_and_type(subject: str):
 
     type, oneline = match.groups()
     return type, None, oneline
+
+
+def get_type_index(type: str):
+    if type in valid_types:
+        return valid_types.index(type)
+    else:
+        return -1
 
 
 def get_scope_index(scope: str):
@@ -144,7 +192,8 @@ def split_text_into_logical_markdown_chunks(text: str) -> List[str]:
             inside_list_element = True
             continue
 
-        inside_paragraph_block = True
+        if line:
+            inside_paragraph_block = True
         current_paragraph_block += line + ' '
 
     if current_paragraph_block:
@@ -176,8 +225,11 @@ def generate_changelog(commits_by_type: Dict[str, List[Commit]], format: str):
                     label = get_scope_label(scope)
                     lines.append(f'### {label}\n')
                 for c in commits:
-                    link = f'[`{c.short_hash}`]({commit_url_prefix}/{c.hash})'
-                    lines.append(f'- {c.oneline} {link}')
+                    if c.squashed_commits:
+                        lines.append(f'- {c.oneline}')
+                    else:
+                        link = f'[`{c.short_hash}`]({commit_url_prefix}/{c.hash})'
+                        lines.append(f'- {c.oneline} {link}')
                     if c.body:
                         lines.append('   &nbsp;')
                         for l in split_text_into_logical_markdown_chunks(c.body):
@@ -186,6 +238,12 @@ def generate_changelog(commits_by_type: Dict[str, List[Commit]], format: str):
                                     lines.append(f'   >{l2}')
                                 lines.append(f'   >')
 
+                    if c.squashed_commits:
+                        lines.append('   >Relevant changes:')
+                        for squashed in c.squashed_commits:
+                            # TODO: also show body?
+                            link = f'[`{squashed.short_hash}`]({commit_url_prefix}/{squashed.hash})'
+                            lines.append(f'   > - {squashed.subject} {link}')
                 lines.append('')
     elif format == 'plaintext':
         for type, commits in commits_by_type.items():
@@ -204,10 +262,20 @@ def generate_changelog(commits_by_type: Dict[str, List[Commit]], format: str):
                 if c.body:
                     lines.append('  ' + c.body.replace('\n', '\n  ') + '\n')
                     prev_had_body = True
+
+                if c.squashed_commits:
+                    lines.append('\n  Relevant changes:')
+                    for squashed in c.squashed_commits:
+                        # TODO: also show body?
+                        lines.append('    ' + squashed.subject)
             lines.append('')
 
     return '\n'.join(lines)
 
+
+for path in (script_dir / 'changelog_overrides').rglob('*.md'):
+    if path.name != 'README.md':
+        parse_override_file(path)
 
 if from_sha:
     branch = from_sha
@@ -221,8 +289,6 @@ commits_text = subprocess.check_output(
 commits: List[Commit] = []
 for commit_text in commits_text.splitlines():
     short_hash, hash, subject = commit_text.split(' ', 2)
-    if hash in overrides:
-        subject = overrides[hash]
     body = subprocess.check_output(
         f'git log -1 {hash} --format="%b"', shell=True, encoding='utf-8').strip()
     m = re.search(r'end changelog', body, re.IGNORECASE)
@@ -231,11 +297,39 @@ for commit_text in commits_text.splitlines():
     type, scope, oneline = parse_scope_and_type(subject)
     commits.append(Commit(type, scope, short_hash, hash, subject, oneline, body))
 
+# Squash commits.
+squashed_hashes = []
+for commit in commits:
+    if commit.hash not in overrides_squashes:
+        continue
+
+    squash_list = overrides_squashes[commit.hash]
+    squashed_hashes.extend(squash_list)
+    commit.squashed_commits = [c for c in commits if c.hash in squash_list]
+    commit.squashed_commits.insert(0, Commit(**commit.__dict__))
+    commit.squashed_commits.sort(key=lambda c: (get_type_index(c.type), get_scope_index(c.scope)))
+commits = [c for c in commits if c.hash not in squashed_hashes]
+
+# Replace commit messages with overrides.
+for commit in commits:
+    hash = commit.hash
+    if hash in overrides and overrides[hash][0] == 'subject':
+        commit.subject = overrides[hash][1]
+    elif hash in overrides and overrides[hash][0] == 'reword':
+        lines = overrides[hash][1].splitlines()
+        commit.subject = lines[0]
+        commit.body = '\n'.join(lines[1:])
+    else:
+        continue
+
+    type, scope, oneline = parse_scope_and_type(commit.subject)
+    commit.type = type
+    commit.scope = scope
+    commit.oneline = oneline
 
 commits_by_type: Dict[str, List[Commit]] = {}
 for type in valid_types:
     commits_by_type[type] = []
-
 
 for commit in commits:
     by_type = commits_by_type.get(commit.type, [])
