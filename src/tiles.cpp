b6752f122a49ba849e42e43a3f4cf541341c4240
Connor Clark <cjamcl@gmail.com>
2023-08-27 19:21:36 -0700

    refactor: use calloc in reset_tile instead of manually zeroing

--- src/tiles.cpp
+++ src/tiles.cpp
@@ -427,17 +427,12 @@ void reset_tile(tiledata *buf, int32_t t, int32_t format=1)
         free(buf[t].data);
     }
     
-    buf[t].data=(byte *)malloc(tilesize(buf[t].format));
+    buf[t].data=(byte *)calloc(tilesize(buf[t].format), 1);
     
     if(buf[t].data==NULL)
     {
         quit_game();
         Z_error_fatal("Unable to initialize tile #%d.\n", t);
-    }
-    
-    for(int32_t i=0; i<tilesize(buf[t].format); i++)
-    {
-        buf[t].data[i]=0;
     }
 }
 
