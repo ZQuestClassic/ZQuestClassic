8c359cdb394d99e2b2bc587ead44aec4c039d0c4
DimiPZC <joshuaf@live.ca>
2023-09-27 12:13:48 -0400

    fix: replays depending on wrong moveflags
    not sure of a better commit title

--- src/zc/replay.cpp
+++ src/zc/replay.cpp
@@ -26,7 +26,7 @@ struct ReplayStep;
 
 static const int ASSERT_SNAPSHOT_BUFFER = 10;
 static const int ASSERT_FAILED_EXIT_CODE = 120;
-static const int VERSION = 19;
+static const int VERSION = 20;
 
 static const std::string ANNOTATION_MARKER = "«";
 static const char TypeMeta = 'M';
