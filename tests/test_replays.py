c469d89def657eb1840154ad2128e6b4e5a32ca8
Connor Clark <cjamcl@gmail.com>
2023-10-05 18:58:34 -0700

    test!: update more references to classic_1st.qst

--- tests/test_replays.py
+++ tests/test_replays.py
@@ -254,7 +254,7 @@ class TestReplays(unittest.TestCase):
             '-record', replay_path,
             '-replay-debug',
             '-frame', '100',
-            '-test', 'Z1 Recreations/classic_1st.qst', '0', '119',
+            '-test', 'quests/Z1 Recreations/classic_1st.qst', '0', '119',
         ])
         run_target.check_run('zplayer', [
             '-headless',
