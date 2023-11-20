47b273d899b485b4da7aced613e09bf3ba15a7a1
Connor Clark <cjamcl@gmail.com>
2023-11-20 17:21:54 -0600

    feat(launcher): add options for window size, and cap max default window size
    `[ZLAUNCH] window_width, window_height` in zcl.cfg
    
    Cap max default size to 2x

--- src/base/zapp.h
+++ src/base/zapp.h
@@ -18,7 +18,7 @@ bool is_web();
 bool is_ci();
 void set_headless_mode();
 bool is_headless();
-std::pair<int, int> zc_get_default_display_size(int base_width, int base_height, int saved_width, int saved_height);
+std::pair<int, int> zc_get_default_display_size(int base_width, int base_height, int saved_width, int saved_height, int max_scale = 3);
 void zc_process_display_events();
 void zapp_reporting_add_breadcrumb(const char* category, const char* message);
 void zapp_reporting_set_tag(const char* key, const char* value);
--- src/launcher/launcher.cpp
+++ src/launcher/launcher.cpp
@@ -191,8 +191,9 @@ int32_t main(int32_t argc, char* argv[])
 
 	all_disable_threaded_display();
 
-	// TODO: remember window size.
-	auto [w, h] = zc_get_default_display_size(zq_screen_w, zq_screen_h, -1, -1);
+	int window_width = zc_get_config("ZLAUNCH", "window_width", -1);
+	int window_height = zc_get_config("ZLAUNCH", "window_height", -1);
+	auto [w, h] = zc_get_default_display_size(zq_screen_w, zq_screen_h, window_width, window_height, 2);
 	int32_t videofail = set_gfx_mode(GFX_AUTODETECT_WINDOWED,w,h,zq_screen_w, zq_screen_h);
 	
 	if(videofail)
