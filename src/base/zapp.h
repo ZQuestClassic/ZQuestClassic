919bda1567879a5450a41445945da8dda0f766da
Connor Clark <cjamcl@gmail.com>
2023-11-20 17:21:54 -0600

    feat(launcher): add options for window size, and cap max default window size
    `[ZLAUNCH] window_width, window_height` in zcl.cfg
    
    Cap max default size to 2x

--- src/base/zapp.h
+++ src/base/zapp.h
@@ -19,7 +19,7 @@ bool is_ci();
 void set_headless_mode();
 bool is_headless();
 void zapp_setup_icon();
-std::pair<int, int> zc_get_default_display_size(int base_width, int base_height, int saved_width, int saved_height);
+std::pair<int, int> zc_get_default_display_size(int base_width, int base_height, int saved_width, int saved_height, int max_scale = 3);
 void zc_process_display_events();
 void zapp_reporting_add_breadcrumb(const char* category, const char* message);
 void zapp_reporting_set_tag(const char* key, const char* value);
