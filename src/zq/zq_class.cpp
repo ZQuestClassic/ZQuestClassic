b7953178558447d5f96428d254c21e4a3cdc5583
EmilyV99 <emilygamergirl99@gmail.com>
2023-12-02 17:47:17 -0500

    fix(zq): Crash on changing map count

--- src/zq/zq_class.cpp
+++ src/zq/zq_class.cpp
@@ -166,7 +166,12 @@ void zmap::clear()
 {
 	*this = zmap();
 }
-
+void zmap::force_refr_pointer()
+{
+	if(unsigned(currmap) > map_count || (currmap*MAPSCRS > TheMaps.size()))
+		screens = &TheMaps[currmap*MAPSCRS];
+	else screens = nullptr;
+}
 bool zmap::CanUndo()
 {
     return undo_stack.size() > 0;
@@ -6372,6 +6377,7 @@ bool setMapCount2(int32_t c)
     try
     {
         TheMaps.resize(c*MAPSCRS);
+		Map.force_refr_pointer();
 		map_autolayers.resize(c*6);
     }
     catch(...)
