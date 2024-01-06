68fc92a3c957fd8cf5f680ee521efc035ee51a69
EmilyV99 <emilygamergirl99@gmail.com>
2024-01-06 12:12:11 -0500

    fix: placing flags on layers not marking the layer screen as "valid"

--- src/zq/zquest.cpp
+++ src/zq/zquest.cpp
@@ -8738,6 +8738,12 @@ void doflags()
 					int scr = CurrentLayer ? Map.CurrScr()->layerscreen[CurrentLayer-1] : Map.getCurrScr();
 					Map.DoSetFlagCommand(map, scr, c, Flag);
 				}
+				if(!(cur_scr->valid&mVALID))
+				{
+					Map.CurrScr()->valid|=mVALID;
+					cur_scr->valid|=mVALID;
+					Map.setcolor(Color);
+				}
 				Flag = tflag;
 			}
 		}
