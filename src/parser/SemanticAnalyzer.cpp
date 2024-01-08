a65faeff57127e6fba872f39d10b724bbf6c953d
EmilyV99 <emilygamergirl99@gmail.com>
2024-01-08 00:52:35 -0500

    fix(zscript): compiler crash related to invalid array indeces

--- src/parser/SemanticAnalyzer.cpp
+++ src/parser/SemanticAnalyzer.cpp
@@ -1386,6 +1386,7 @@ void SemanticAnalyzer::caseExprIndex(ASTExprIndex& host, void* param)
 	if (host.index)
 	{
 		visit(host.index.get(), paramRead); //only READ the index!
+		if (breakRecursion(host)) return;
 		checkCast(*host.index->getReadType(scope, this), DataType::FLOAT,
 				  host.index.get());
 		if (breakRecursion(host)) return;
