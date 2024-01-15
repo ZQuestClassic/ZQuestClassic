b9cafe07b073b7de07bb2f27207582bc8d435d62
Emily Venezia <emilygamergirl99@gmail.com>
2024-01-15 02:45:30 -0500

    fix!: more lens/enemy stuff missed in prior commit

--- src/zc/guys.cpp
+++ src/zc/guys.cpp
@@ -4025,8 +4025,7 @@ bool enemy::dont_draw()
 	if(flags&lens_only && !lensclk)
 		return true;
 		
-	if(lensclk && (itemsbuf[Hero.getLastLensID()].flags & ITEM_FLAG6) && !(itemsbuf[Hero.getLastLensID()].flags & ITEM_FLAG7) &&
-	!((flags&lens_only) && (get_qr(qr_LENSSEESENEMIES) || (itemsbuf[Hero.getLastLensID()].flags & ITEM_FLAG5))))
+	if(lensclk && !((flags&lens_only) && (get_qr(qr_LENSSEESENEMIES) || (itemsbuf[Hero.getLastLensID()].flags & ITEM_FLAG5))))
 		return true;
 		
 	return false;
@@ -4080,14 +4079,6 @@ void enemy::draw(BITMAP *dest)
 	//Lens check
 	if (lensclk)
 	{
-		if((itemsbuf[Hero.getLastLensID()].flags & ITEM_FLAG6) && !(flags&lens_only))
-		{
-			if (canSee == DRAW_NORMAL) 
-			{
-				if (itemsbuf[Hero.getLastLensID()].flags & ITEM_FLAG7) canSee = DRAW_CLOAKED;
-				else canSee = DRAW_INVIS; //Should never happen cause dont_draw should catch this, but just in case.
-			}
-		}
 		if(flags&lens_only)
 		{
 			if (canSee == DRAW_INVIS) canSee = DRAW_NORMAL;
