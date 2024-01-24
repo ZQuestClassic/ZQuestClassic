ced4dbad8443b3979d244366bd7e0583e8f0d5c3
Connor Clark <cjamcl@gmail.com>
2024-01-23 17:37:31 -0800

    refactor(zc): add get_register_ref_dependency in zasm_table.cpp
    Also run `propagate_values` after the comparison opt passes, as it was
    accidentally skipping some optimizations due to the comparisons passes
    assuming D2 is being compared.

--- src/zasm_table.h
+++ src/zasm_table.h
@@ -1,5 +1,6 @@
 #include "base/zdefs.h"
 #include <initializer_list>
+#include <optional>
 
 struct CommandDependency
 {
@@ -11,3 +12,9 @@ std::initializer_list<CommandDependency> get_command_implicit_dependencies(int c
 // Many registers use the value within data registers when evaluating its value.
 // For example, GLOBALRAM uses both rINDEX and rINDEX2 to index a specific array.
 std::initializer_list<int> get_register_dependencies(int reg);
+
+// Same as get_register_dependencies, but just returns a single register (at most) representing
+// some ref that references a specific game entity.
+std::optional<int> get_register_ref_dependency(int reg);
+
+bool has_register_dependency(int reg);
