4f739c9d7024da29977867f10a7ebf28192b01b8
Connor Clark <cjamcl@gmail.com>
2024-03-03 13:20:36 -0800

    test: fix hash for compare replay image dedupe
    The previous hash did not take into account the image format's color
    palette, so hash collisions prevented some failing frames from showing.

--- tests/compare_replays.py
+++ tests/compare_replays.py
@@ -4,6 +4,7 @@
 
 import argparse
 import hashlib
+import io
 import json
 import os
 import re
@@ -39,7 +40,9 @@ is_ci = 'CI' in os.environ
 
 
 def hash_image(filename):
-    return hashlib.md5(Image.open(filename).tobytes()).hexdigest()
+    byte_io = io.BytesIO()
+    Image.open(filename).save(byte_io, 'bmp')
+    return hashlib.md5(byte_io.getvalue()).hexdigest()
 
 
 # A `test_results` represents an invocation of run_replay_tests.py, including:
