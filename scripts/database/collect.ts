e7e7f3ea9f33555938f7d63cbc1a00577ff0e7c7
Connor Clark <cjamcl@gmail.com>
2024-07-01 16:32:54 -0700

    misc!: add resourceHashes to database manifest

--- scripts/database/collect.ts
+++ scripts/database/collect.ts
@@ -62,9 +62,11 @@ interface QuestManifest {
   releases: Array<{
     name: string;
     date: string;
-    /** Hash of the zip downloaded from PZC. Does not include external music. */
+    /** sha1 hash of the zip downloaded from PZC. Does not include external music. */
     hash: string;
     resources: string[];
+    /** md5 hash of each resource. */
+    resourceHashes: string[];
   }>;
   defaultPath: string;
   /** External music. */
@@ -199,10 +201,10 @@ function download(url: string, outputPath: string) {
     });
 }
 
-function getFileHash(path: string): Promise<string> {
+function getFileHash(path: string, algorithm: string): Promise<string> {
   return new Promise(resolve => {
     const fd = fs.createReadStream(path);
-    const hash = crypto.createHash('sha1');
+    const hash = crypto.createHash(algorithm);
     hash.setEncoding('hex');
     fd.on('end', function() {
       hash.end();
@@ -210,6 +212,11 @@ function getFileHash(path: string): Promise<string> {
     });
     fd.pipe(hash);
   });
+}
+
+async function getMd5Hash(path: string): Promise<string> {
+  const hash = await getFileHash(path, 'md5');
+  return hash.toUpperCase();
 }
 
 async function downloadGoogleDriveUrl(page: puppeteer.Page, url: string, destination: string) {
@@ -519,7 +526,7 @@ async function processId(page: puppeteer.Page, type: EntryType, index: number) {
     fs.rmSync(archivePath, {force: true});
     await download(`https://www.purezc.net/index.php?page=download&section=${capitalize(type)}&id=${index}`, archivePath);
   }
-  const contentHash = await getFileHash(archivePath);
+  const contentHash = await getFileHash(archivePath, 'sha1');
 
   let thisRelease;
   const releases = existingManifestEntry?.releases || [];
@@ -536,6 +543,7 @@ async function processId(page: puppeteer.Page, type: EntryType, index: number) {
       date: dateUpdated || dateAdded || '',
       hash: contentHash,
       resources: [],
+      resourceHashes: [],
     };
     releases.unshift(thisRelease);
   } else {
@@ -619,10 +627,20 @@ async function processId(page: puppeteer.Page, type: EntryType, index: number) {
     }
   }
 
+  if (thisRelease.resources.length === 0) {
+    console.log(`[${id}] WARNING - no resources found, ignoring update`);
+    fs.rmSync(resourcesDir, {recursive: true, force: true});
+    return;
+  }
+
   const qsts = thisRelease.resources.filter(r => r.endsWith('.qst'));
   const qst = qsts[0];
   // TODO: Don't forget previous value.
   const defaultPath = `${id}/${thisRelease.name}/${qst}`;
+
+  thisRelease.resourceHashes = await Promise.all(thisRelease.resources.map(resource => {
+    return getMd5Hash(`${DB}/${id}/${thisRelease.name}/${resource}`);
+  }));
 
   const quest: QuestManifest = {
     id,
