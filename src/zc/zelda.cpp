connorjclark/ZeldaClassic

feat(zc): add opt-in automation for uploading replays
When enabled, once a week replays are uploaded to a server when the
program exits. Only replays of known quests from the database are
uploaded. There is also an option to manually trigger this process
rather than enabling it automatically.

Please consider enabling this, as it greatly helps out with development
by allowing us to prevent bugs. This is important for any new features
we add, but extremely so for the upcoming Z3 scrolling overhaul.

See `docs/replays.md` for more information about the replay system.

src/zc/zelda.cpp
@@ -23,6 +23,7 @@
 
 #include <stdlib.h>
 
+#include "zc/replay_upload.h"
 #include "zc/zasm_optimize.h"
 #include "zc/zasm_utils.h"
 #include "zscriptversion.h"
@@ -1558,8 +1559,8 @@ int32_t load_quest(gamedata *g, bool report, byte printmetadata)
 
 std::string create_replay_path_for_save(const gamedata_header& header)
 {
-	std::filesystem::path replay_file_dir = zc_get_config("zeldadx", "replay_file_dir", "replays/");
-	std::filesystem::create_directory(replay_file_dir);
+	fs::path replay_file_dir = zc_get_config("zeldadx", "replay_file_dir", "replays/");
+	fs::create_directory(replay_file_dir);
 	std::string filename_prefix = fmt::format("{}-{}", header.title, header.name);
 	sanitize(filename_prefix);
 	return create_new_file_path(replay_file_dir, filename_prefix, REPLAY_EXTENSION).string();
@@ -1743,6 +1744,14 @@ int32_t init_game()
 	if (replay_is_active())
 	{
 		replay_set_meta("qst_title", game->header.title);
+
+		if (!replay_has_meta("guid"))
+		{
+			uint8_t bytes[16];
+			for (int i = 0; i < 16; i++)
+				bytes[i] = rand();
+			replay_set_meta("guid", util::make_hex_string(std::begin(bytes), std::end(bytes)));
+		}
 
 		std::optional<std::string> previous_hash;
 		if (replay_has_meta("qst_hash"))
@@ -4283,6 +4292,12 @@ int main(int argc, char **argv)
 		return get_qr(qr_SCRIPTERRLOG) || DEVLEVEL > 0;
 	});
 
+	if (used_switch(argc, argv, "-upload-replays"))
+	{
+		replay_upload();
+		return 0;
+	}
+
 	int only_arg = used_switch(argc, argv, "-only");
 	if (only_arg)
 	{
@@ -5460,6 +5475,10 @@ void quit_game()
 	if(qstpath) free(qstpath);
 
 	FFCore.shutdown();
+
+#ifdef HAS_CURL
+	replay_upload_auto();
+#endif
 }
 
 bool isSideViewGravity(int32_t t)

ZQuestClassic/ZQuestClassic

src/zc/zelda.cpp
@@ -25,6 +25,7 @@
 #include <al5_img.h>
 #include <loadpng.h>
 
+#include "zc/replay_upload.h"
 #include "zscriptversion.h"
 #include "sound/zcmusic.h"
 #include "base/zdefs.h"
@@ -60,6 +61,10 @@
 #include "zc/render.h"
 #include "zinfo.h"
 #include "music_playback.h"
+
+#ifdef HAS_UUID
+#include <uuid.h>
+#endif
 
 using namespace util;
 extern FFScript FFCore; //the core script engine.
@@ -1574,8 +1579,8 @@ int32_t load_quest(gamedata *g, bool report, byte printmetadata)
 
 std::string create_replay_path_for_save(const gamedata_header& header)
 {
-	std::filesystem::path replay_file_dir = zc_get_config("zeldadx", "replay_file_dir", "replays/");
-	std::filesystem::create_directory(replay_file_dir);
+	fs::path replay_file_dir = zc_get_config("zeldadx", "replay_file_dir", "replays/");
+	fs::create_directory(replay_file_dir);
 	std::string filename_prefix = fmt::format("{}-{}", header.title, header.name);
 	sanitize(filename_prefix);
 	return create_new_file_path(replay_file_dir, filename_prefix, REPLAY_EXTENSION).string();
@@ -1714,6 +1719,13 @@ int32_t init_game()
 			replay_set_sync_rng(true);
 			replay_set_meta("qst", relativize_path(game->header.qstpath));
 			replay_set_meta("name", game->get_name());
+#ifdef HAS_UUID
+			if (!replay_has_meta("uuid"))
+			{
+				auto uuid = uuids::uuid_system_generator{}();
+				replay_set_meta("uuid", uuids::to_string(uuid));
+			}
+#endif
 			replay_save();
 		}
 		else if (!firstplay && !game->header.replay_file.empty())
@@ -1761,6 +1773,14 @@ int32_t init_game()
 	{
 		replay_set_meta("qst_title", game->header.title);
 
+#ifdef HAS_UUID
+		if (!replay_has_meta("uuid"))
+		{
+			auto uuid = uuids::uuid_system_generator{}();
+			replay_set_meta("uuid", uuids::to_string(uuid));
+		}
+#endif
+
 		std::optional<std::string> previous_hash;
 		if (replay_has_meta("qst_hash"))
 			previous_hash = replay_get_meta_str("qst_hash");
@@ -1783,11 +1803,13 @@ int32_t init_game()
 		uint8_t md5sum[16];
 		cvs_MD5Final(md5sum, &ctx);
 		std::string hash = util::make_hex_string(std::begin(md5sum), std::end(md5sum));
-		replay_set_meta("qst_hash", hash);
-		delete[] buffer;
+		if (!replay_has_meta("qst_hash"))
+			replay_set_meta("qst_hash", hash);
 
 		if (previous_hash && previous_hash != hash)
 			replay_set_meta_bool("qst_modified", true);
+
+		delete[] buffer;
 	}
 
 	FFCore.init();
@@ -4261,6 +4283,12 @@ int main(int argc, char **argv)
 		return get_qr(qr_SCRIPTERRLOG) || DEVLEVEL > 0;
 	});
 
+	if (used_switch(argc, argv, "-upload-replays"))
+	{
+		replay_upload();
+		return 0;
+	}
+
 	if (used_switch(argc,argv,"-test-zc"))
 	{
 		bool success = true;
@@ -5599,6 +5627,10 @@ void quit_game()
 	if(qstpath) free(qstpath);
 
 	FFCore.shutdown();
+
+#ifdef HAS_CURL
+	replay_upload_auto();
+#endif
 }
 
 bool isSideViewGravity(int32_t t)