64c2e30f772baf7e835a4b2c51937bfab48182a4
Jambu <jambu2024@outlook.com>
2024-07-30 20:42:37 -0600

    refactor: combine misc attributes for guys into array
    enough of this madness...

--- src/base/zdefs.h
+++ src/base/zdefs.h
@@ -1306,7 +1306,8 @@ struct guydata
     int16_t  dp, wdp, weapon;
     
     int16_t  rate, hrate, step, homing, grumble, item_set;
-    int32_t   misc1, misc2, misc3, misc4, misc5, misc6, misc7, misc8, misc9, misc10, misc11, misc12, misc13, misc14, misc15;
+    
+	int32_t attributes[32]; //refactored this into an array, no more madness.
     int16_t  bgsfx, bosspal, extend;
     byte defense[edefLAST255];
     byte  hitsfx, deadsfx;
@@ -1325,9 +1326,6 @@ struct guydata
     int16_t frozenmisc[10];
     //v 34
     int16_t firesfx; //weapon fire (attack) sound
-    //expanded ->Attributes[] to size of 32.
-    int32_t misc16, misc17, misc18, misc19, misc20, misc21, misc22, misc23, 
-	misc24, misc25, misc26, misc27, misc28, misc29, misc30, misc31, misc32;
     int32_t movement[32]; //Reserved for npc movement types and args. 
     int32_t new_weapon[32]; //Reserved for weapon patterns and args.
     int32_t initD[8], initA[2];
