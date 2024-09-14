e2501aec1045f39f35288a9bfdca2b546235e34c
Connor Clark <cjamcl@gmail.com>
2024-09-13 19:22:15 -0700

    build!: link TBB as needed for std::execution

--- src/zc/zasm_utils.cpp
+++ src/zc/zasm_utils.cpp
@@ -10,11 +10,20 @@
 #include <vector>
 #include <sstream>
 
-#ifdef POOLSTL_STD_SUPPLEMENT
-#include <poolstl/poolstl.hpp>
-#else
+// The <execution> header defines compiler-provided execution policies, but is not always present.
+// See: https://github.com/alugowski/poolSTL
+#if __cplusplus >= 201603L || (defined(_MSVC_LANG) && _MSVC_LANG >= 201603L)
+#if __has_include(<execution>)
+#ifndef POOLSTL_STD_SUPPLEMENT_NO_INCLUDE
+// On GCC and Clang simply including <execution> requires linking with TBB.
+// MinGW does not require TBB, but performance may be sequential. To keep parallel performance fallback to poolSTL.
 #include <execution>
 #endif
+#endif
+#endif
+
+#define POOLSTL_STD_SUPPLEMENT
+#include <poolstl/poolstl.hpp>
 
 StructuredZasm zasm_construct_structured(const zasm_script* script)
 {
