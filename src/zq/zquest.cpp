ZQuestClassic/ZQuestClassic

commit c1273e1491df61889ce7dbb14c9604cae9a601aa

refactor: use vector for storing FFCs
Previously, every screen allocated memory to hold the maximum number of
FFCs (2.55 alpha 112 bumped this from 32 to 128), which took ~43 KB per
screen, even if no FFCs were used. Now screens only allocate as many
FFCs as they need.

For a quest that uses no FFCs at all, this saves ~5 MB of memory per
map. For Yuurand, total memory usage on startup went from 1.3 GB to 800
MB.

src/zq/zquest.cpp
@@ -5460,7 +5460,8 @@ void draw_screenunit(int32_t unit, int32_t flags)
 			if(ShowFFCs)
 			{
 				mapscr* ffscr = prv_mode?Map.get_prvscr():Map.CurrScr();
-				for(int32_t i=MAXFFCS-1; i>=0; i--)
+				int c = ffscr->numFFC();
+				for(int32_t i=c-1; i>=0; i--)
 				{
 					ffcdata& ff = ffscr->ffcs[i];
 					if(ff.data !=0 && (CurrentLayer<2 || (ff.flags&ffc_overlay)))
@@ -9924,7 +9925,8 @@ void domouse()
 	{
 		static int mapscr_tooltip_id = ttip_register_id();
 		bool did_ffttip = false;
-		for(int32_t i=MAXFFCS-1; i>=0; i--)
+		int c = Map.CurrScr()->numFFC();
+		for(int32_t i=c-1; i>=0; i--)
 			if(Map.CurrScr()->ffcs[i].data !=0 && (CurrentLayer<2 || (Map.CurrScr()->ffcs[i].flags&ffc_overlay)))
 			{
 				int32_t ffx = Map.CurrScr()->ffcs[i].x.getFloor();
@@ -10499,7 +10501,8 @@ void domouse()
 				}
 				
 				// Move FFCs
-				for(int32_t i=MAXFFCS-1; i>=0; i--)
+				int c = Map.CurrScr()->numFFC();
+				for(int32_t i=c-1; i>=0; i--)
 					if(Map.CurrScr()->ffcs[i].data !=0 && (CurrentLayer<2 || (Map.CurrScr()->ffcs[i].flags&ffc_overlay)))
 					{
 						int32_t ffx = Map.CurrScr()->ffcs[i].x.getFloor();
@@ -10575,11 +10578,12 @@ void domouse()
 				ComboBrushPause=0;
 				
 				bool clickedffc = false;
-				uint32_t earliestfreeffc = MAXFFCS;
 				
 				// FFC right-click menu
 				// This loop also serves to find the free ffc with the smallest slot number.
-				for(int32_t i=MAXFFCS-1; i>=0; i--)
+				int c = Map.CurrScr()->numFFC();
+				uint32_t earliestfreeffc = c;
+				for(int32_t i=c-1; i>=0; i--)
 				{
 					auto data = Map.CurrScr()->ffcs[i].data;
 					if(data==0)
@@ -27781,6 +27785,10 @@ bool replay_is_replaying() {return false;}
 bool replay_version_check(int min, int max) {return false;}
 bool replay_is_debug() {return false;}
 int32_t item::run_script(int32_t mode){return 0;};
+ffcdata* slopes_getFFC(int index)
+{
+	return nullptr;
+}
 
 #ifdef __EMSCRIPTEN__
 extern "C" void open_test_mode()

src/base/mapscr.cpp
@@ -1,4 +1,5 @@
 #include "base/mapscr.h"
+#include "base/general.h"
 #include "base/zsys.h"
 #include "base/qrs.h"
 
@@ -51,13 +52,32 @@ void mapscr::zero_memory()
 	*this = mapscr();
 }
 
+void mapscr::ensureFFC(size_t ind)
+{
+	getFFC(ind);
+}
+
+ffcdata& mapscr::getFFC(size_t ind)
+{
+	assert(ind < MAXFFCS);
+
+	if (ind < ffcs.size())
+		return ffcs[ind];
+
+	// ffc_count_dirty does not need to be set because `data` is still zero, so the count
+	// won't have changed. Only need to mark it dirty when `data`
+	// changes (handled in `screen_ffc_modify_postroutine`).
+	ffcs.resize(ind + 1);
+	return ffcs[ind];
+}
+
 word mapscr::numFFC()
 {
 	if (!ffc_count_dirty)
 		return num_ffcs;
 
 	int lastffc = -1;
-	for (int w = MAXFFCS - 1; w >= 0; --w)
+	for (int w = ffcs.size() - 1; w >= 0; --w)
 	{
 		if (ffcs[w].data)
 		{
@@ -66,7 +86,10 @@ word mapscr::numFFC()
 		}
 	}
 	if (lastffc < 31 && get_qr(qr_OLD_FFC_FUNCTIONALITY))
+	{
 		lastffc = 31;
+		ensureFFC(31);
+	}
 	num_ffcs = lastffc + 1;
 	ffc_count_dirty = false;
 	return num_ffcs;

src/base/mapscr.h
@@ -97,8 +97,16 @@ struct mapscr
 	byte cset[176];
 	
 	byte entry_x, entry_y; //Where Hero entered the screen. Used for pits, and to prevent water walking. -Z
-	
-	ffcdata ffcs[MAXFFCS];
+
+	// This vector never shrinks during gameplay.
+	// Starts empty, and is only filled up to fit `ind` as `getFFC(ind)` is called.
+	// Max size is MAXFFCS.
+	// NOTE: Direct access at index `ind` is only safe to do if:
+	// 1) it has just been checked to be less than `numFFC()` or
+	// 2) `getFFC(ind)` or `ensureFFC(ind)` was just called
+	std::vector<ffcdata> ffcs;
+	void ensureFFC(size_t ind);
+	ffcdata& getFFC(size_t ind);
 	word numFFC();
 	void ffcCountMarkDirty();
 	

src/base/qst.cpp
@@ -16411,11 +16411,11 @@ int32_t readmapscreen_old(PACKFILE *f, zquestheader *Header, mapscr *temp_mapscr
 		float tempfloat;
 		word tempw;
 		temp_mapscr->ffcCountMarkDirty();
-		
+		temp_mapscr->ffcs.clear();
+		temp_mapscr->ffcs.resize(32);
 		for(m=0; m<32; m++)
 		{
 			ffcdata& tempffc = temp_mapscr->ffcs[m];
-			tempffc.clear();
 			if((bits>>m)&1)
 			{
 				if(!p_igetw(&tempw,f))
@@ -16642,11 +16642,11 @@ int32_t readmapscreen_old(PACKFILE *f, zquestheader *Header, mapscr *temp_mapscr
 				}
 			}
 		}
-		for(m = 32; m < MAXFFCS; ++m)
-		{
-			temp_mapscr->ffcs[m].clear();
-		}
 	}
+
+	temp_mapscr->ffcCountMarkDirty();
+	temp_mapscr->ffcs.resize(temp_mapscr->numFFC());
+	temp_mapscr->ffcs.shrink_to_fit();
 	
 	//add in the new whistle flags
 	if(version<13)
@@ -17078,12 +17078,13 @@ int32_t readmapscreen(PACKFILE *f, zquestheader *Header, mapscr *temp_mapscr, wo
 		word tempw;
 		static ffcdata nil_ffc;
 		temp_mapscr->ffcCountMarkDirty();
+		temp_mapscr->ffcs.clear();
+		temp_mapscr->ffcs.resize(std::min(MAXFFCS, (int)numffc));
 		for(word m = 0; m < numffc; ++m)
 		{
 			ffcdata& tempffc = (m < MAXFFCS)
 				? temp_mapscr->ffcs[m]
 				: nil_ffc; //sanity
-			tempffc.clear();
 			if(old_ff && !(bits & (1<<m))) continue;
 			
 			if(!p_igetw(&tempw,f))
@@ -17157,15 +17158,16 @@ int32_t readmapscreen(PACKFILE *f, zquestheader *Header, mapscr *temp_mapscr, wo
 					tempffc.initd[q] = 0;
 			}
 		}
-		for(word m = numffc; m < MAXFFCS; ++m)
-		{
-			temp_mapscr->ffcs[m].clear();
-		}
 		//END FFC
 		if(version > 29)
 			if(!p_getlstr(&temp_mapscr->usr_notes, f))
 				return qe_invalid;
 	}
+
+	temp_mapscr->ffcCountMarkDirty();
+	temp_mapscr->ffcs.resize(temp_mapscr->numFFC());
+	temp_mapscr->ffcs.shrink_to_fit();
+
 	return 0;
 }
 
@@ -22159,6 +22161,7 @@ static int32_t _lq_int(const char *filename, zquestheader *Header, miscQdata *Mi
         {
             for(int32_t j=0; j<MAPSCRS; j++)
             {
+                TheMaps[(i*MAPSCRS)+j].ensureFFC(32);
                 for(int32_t m=0; m<32; m++)
                 {
                     if(combobuf[TheMaps[(i*MAPSCRS)+j].ffcs[m].data].type == cCHANGE)

src/dialog/ffc_editor.cpp
@@ -31,7 +31,7 @@ bool call_ffc_dialog(int32_t ffcombo, ffdata const& init, mapscr* scr)
 	return edited;
 }
 
-ffdata::ffdata(mapscr const* scr, int32_t ind)
+ffdata::ffdata(mapscr* scr, int32_t ind)
 {
 	clear();
 	load(scr, ind);
@@ -47,29 +47,31 @@ void ffdata::clear()
 	memset(this, 0, sizeof(ffdata));
 	fwid = fhei = 15;
 }
-void ffdata::load(mapscr const* scr, int32_t ind)
+void ffdata::load(mapscr* scr, int32_t ind)
 {
 	if(unsigned(ind)>MAXFFCS-1) return;
-	x = scr->ffcs[ind].x.getZLong();
-	y = scr->ffcs[ind].y.getZLong();
-	dx = scr->ffcs[ind].vx.getZLong();
-	dy = scr->ffcs[ind].vy.getZLong();
-	ax = scr->ffcs[ind].ax.getZLong();
-	ay = scr->ffcs[ind].ay.getZLong();
-	data = scr->ffcs[ind].data;
-	cset = scr->ffcs[ind].cset;
-	delay = scr->ffcs[ind].delay;
-	flags = scr->ffcs[ind].flags;
-	link = scr->ffcs[ind].link;
+
+	ffcdata& ffc = scr->getFFC(ind);
+	x = ffc.x.getZLong();
+	y = ffc.y.getZLong();
+	dx = ffc.vx.getZLong();
+	dy = ffc.vy.getZLong();
+	ax = ffc.ax.getZLong();
+	ay = ffc.ay.getZLong();
+	data = ffc.data;
+	cset = ffc.cset;
+	delay = ffc.delay;
+	flags = ffc.flags;
+	link = ffc.link;
 	twid = scr->ffTileWidth(ind)-1;
 	thei = scr->ffTileHeight(ind)-1;
 	fwid = scr->ffEffectWidth(ind)-1;
 	fhei = scr->ffEffectHeight(ind)-1;
-	script = scr->ffcs[ind].script;
+	script = ffc.script;
 	for(auto q = 0; q < 2; ++q)
-		inita[q] = scr->ffcs[ind].inita[q];
+		inita[q] = ffc.inita[q];
 	for(auto q = 0; q < 8; ++q)
-		initd[q] = scr->ffcs[ind].initd[q];
+		initd[q] = ffc.initd[q];
 }
 void ffdata::save(mapscr* scr, int32_t ind)
 {

src/dialog/ffc_editor.h
@@ -36,9 +36,9 @@ struct ffdata
 	std::array<int, 2> inita;
 	
 	ffdata();
-	ffdata(mapscr const* scr, int32_t ind);
+	ffdata(mapscr* scr, int32_t ind);
 	void clear();
-	void load(mapscr const* scr, int32_t ind);
+	void load(mapscr* scr, int32_t ind);
 	void save(mapscr* scr, int32_t ind);
 	ffdata& operator=(ffdata const& other);
 };

src/ffc.cpp
@@ -133,7 +133,8 @@ void ffcdata::doContactDamage(int32_t hdir)
 		int ffnum = -1;
 		if(loaded)
 		{
-			for (word i = 0; i < MAXFFCS; i++)
+			int c = tmpscr->numFFC();
+			for (word i = 0; i < c; i++)
 			{
 				if (this == &tmpscr->ffcs[i])
 				{

src/slopes.cpp
@@ -1,6 +1,7 @@
 #include "slopes.h"
 #include "base/combo.h"
 #include "base/zc_math.h"
+#include "base/mapscr.h"
 #include "ffc.h"
 #include "sprite.h"
 #include "zc/maps.h"
@@ -84,16 +85,37 @@ void draw_slopes_a5(int32_t x, int32_t y, ALLEGRO_COLOR col)
 		p.second.get_info().draw_a5(x,y,col);
 }
 
+ffcdata* slopes_getFFC(int index);
+
 slope_info slope_object::get_info() const
 {
-	bool ff = ffc && ffc->getLoaded();
-	word const* id = ff ? &ffc->data : cmbid;
-	if(!id) return slope_info();
+	extern mapscr tmpscr[2];
+
+	word const* id;
+	zfix x, y;
+
+	if (ffc_id != -1 && slopes_getFFC(ffc_id)->getLoaded())
+	{
+		ffcdata* ffc = slopes_getFFC(ffc_id);
+		id = &ffc->data; // TODO: is this an issue? what if ffcs resizes.
+		x = ffc->x;
+		y = ffc->y;
+	}
+	else
+	{
+		id = cmbid;
+		x = xoffs;
+		y = yoffs;
+	}
+
+	if (!id)
+		return slope_info();
+
 	newcombo const& cmb = combobuf[*id];
 	if(cmb.type != cSLOPE)
 		return slope_info();
-	
-	return slope_info(cmb, ff ? ffc->x : xoffs, ff ? ffc->y : yoffs);
+
+	return slope_info(cmb, x, y);
 }
 
 void slope_object::updateslope()
@@ -105,8 +127,8 @@ void slope_object::updateslope()
 	oy2 = inf.y2;
 }
 
-slope_object::slope_object(word* cid, ffcdata* ff, int32_t id, word cpos)
-	: ffc(ff), cmbid(cid), id(id)
+slope_object::slope_object(word* cid, ffcdata* ffc, int32_t ffc_id, int32_t id, word cpos)
+	: cmbid(cid), id(id), ffc_id(ffc_id)
 {
 	if(ffc)
 	{

commit 036cea2b5b590bc467c35be61010c10389b120fa

fix(zq)!: fix shadowed variable, which broke rclick Select Combo
Regressed in c1273e14

src/zq/zquest.cpp
@@ -5460,8 +5460,8 @@ void draw_screenunit(int32_t unit, int32_t flags)
 			if(ShowFFCs)
 			{
 				mapscr* ffscr = prv_mode?Map.get_prvscr():Map.CurrScr();
-				int c = ffscr->numFFC();
-				for(int32_t i=c-1; i>=0; i--)
+				int num_ffcs = ffscr->numFFC();
+				for(int32_t i=num_ffcs-1; i>=0; i--)
 				{
 					ffcdata& ff = ffscr->ffcs[i];
 					if(ff.data !=0 && (CurrentLayer<2 || (ff.flags&ffc_overlay)))
@@ -6574,8 +6574,8 @@ void refresh(int32_t flags, bool update)
 		
 		if(ShowFFScripts && !prv_mode)
 		{
-			word c = Map.CurrScr()->numFFC();
-			for(word i=0; i< c; i++)
+			word num_ffcs = Map.CurrScr()->numFFC();
+			for(word i=0; i< num_ffcs; i++)
 			{
 				if(ypos+showfont_h-1 > map_page_bar[0].y)
 					break;
@@ -6842,8 +6842,8 @@ void refresh(int32_t flags, bool update)
 		
 		bool undercombo = false, warpa = false, warpb = false, warpc = false, warpd = false, warpr = false;
 		
-		word maxffc = Map.CurrScr()->numFFC();
-		for(int32_t c=0; c<176+128+1+maxffc; ++c)
+		word num_ffcs = Map.CurrScr()->numFFC();
+		for(int32_t c=0; c<176+128+1+num_ffcs; ++c)
 		{
 			// Checks both combos, secret combos, undercombos and FFCs
 			//Fixme:
@@ -9925,8 +9925,8 @@ void domouse()
 	{
 		static int mapscr_tooltip_id = ttip_register_id();
 		bool did_ffttip = false;
-		int c = Map.CurrScr()->numFFC();
-		for(int32_t i=c-1; i>=0; i--)
+		int num_ffcs = Map.CurrScr()->numFFC();
+		for(int32_t i=num_ffcs-1; i>=0; i--)
 			if(Map.CurrScr()->ffcs[i].data !=0 && (CurrentLayer<2 || (Map.CurrScr()->ffcs[i].flags&ffc_overlay)))
 			{
 				int32_t ffx = Map.CurrScr()->ffcs[i].x.getFloor();
@@ -9973,14 +9973,14 @@ void domouse()
 			}
 			
 			mapscr* draw_mapscr = Map.AbsoluteScr(drawmap, drawscr);
-			if(unsigned(c) < 176 && draw_mapscr && !gui_mouse_b())
+			if(unsigned(num_ffcs) < 176 && draw_mapscr && !gui_mouse_b())
 			{
-				int cid = draw_mapscr->data[c];
+				int cid = draw_mapscr->data[num_ffcs];
 				newcombo const& cmb = combobuf[cid];
 				std::ostringstream oss;
-				int cs = draw_mapscr->cset[c];
-				int sflag = draw_mapscr->sflag[c];
-				oss << "Pos: " << c
+				int cs = draw_mapscr->cset[num_ffcs];
+				int sflag = draw_mapscr->sflag[num_ffcs];
+				oss << "Pos: " << num_ffcs
 					<< "\nCombo: " << cid
 					<< "\nCSet: " << cs;
 				if(sflag || cmb.flag)
@@ -10501,8 +10501,8 @@ void domouse()
 				}
 				
 				// Move FFCs
-				int c = Map.CurrScr()->numFFC();
-				for(int32_t i=c-1; i>=0; i--)
+				int num_ffcs = Map.CurrScr()->numFFC();
+				for(int32_t i=num_ffcs-1; i>=0; i--)
 					if(Map.CurrScr()->ffcs[i].data !=0 && (CurrentLayer<2 || (Map.CurrScr()->ffcs[i].flags&ffc_overlay)))
 					{
 						int32_t ffx = Map.CurrScr()->ffcs[i].x.getFloor();
@@ -10530,13 +10530,13 @@ void domouse()
 					}
 					mapscr* draw_mapscr = Map.AbsoluteScr(drawmap, drawscr);
 					if(!draw_mapscr) return;
-					Combo=draw_mapscr->data[c];
+					Combo=draw_mapscr->data[num_ffcs];
 					if(AutoBrush)
 						BrushWidth = BrushHeight = 1;
 					if(key[KEY_LSHIFT]||key[KEY_RSHIFT])
-						CSet=draw_mapscr->cset[c];
+						CSet=draw_mapscr->cset[num_ffcs];
 					if(CHECK_CTRL_CMD)
-						First[current_combolist]=scrollto_cmb(draw_mapscr->data[c]);
+						First[current_combolist]=scrollto_cmb(draw_mapscr->data[num_ffcs]);
 				}
 				else if(CHECK_CTRL_CMD)
 				{
@@ -10581,9 +10581,9 @@ void domouse()
 				
 				// FFC right-click menu
 				// This loop also serves to find the free ffc with the smallest slot number.
-				int c = Map.CurrScr()->numFFC();
+				int num_ffcs = Map.CurrScr()->numFFC();
 				uint32_t earliestfreeffc = c;
-				for(int32_t i=c-1; i>=0; i--)
+				for(int32_t i=num_ffcs-1; i>=0; i--)
 				{
 					auto data = Map.CurrScr()->ffcs[i].data;
 					if(data==0)