ZQuestClassic/ZQuestClassic

commit 036cea2b5b590bc467c35be61010c10389b120fa

fix(zq)!: fix shadowed variable, which broke rclick Select Combo
Regressed in c1273e14

src/zq/zquest.cpp
@@ -5460,8 +5460,8 @@ void draw_screenunit(int32_t unit, int32_t flags)
 			if(ShowFFCs)
 			{
 				mapscr* ffscr = prv_mode?Map.get_prvscr():Map.CurrScr();
-				int c = ffscr->numFFC();
-				for(int32_t i=c-1; i>=0; i--)
+				int num_ffcs = ffscr->numFFC();
+				for(int32_t i=num_ffcs-1; i>=0; i--)
 				{
 					ffcdata& ff = ffscr->ffcs[i];
 					if(ff.data !=0 && (CurrentLayer<2 || (ff.flags&ffc_overlay)))
@@ -6574,8 +6574,8 @@ void refresh(int32_t flags, bool update)
 		
 		if(ShowFFScripts && !prv_mode)
 		{
-			word c = Map.CurrScr()->numFFC();
-			for(word i=0; i< c; i++)
+			word num_ffcs = Map.CurrScr()->numFFC();
+			for(word i=0; i< num_ffcs; i++)
 			{
 				if(ypos+showfont_h-1 > map_page_bar[0].y)
 					break;
@@ -6842,8 +6842,8 @@ void refresh(int32_t flags, bool update)
 		
 		bool undercombo = false, warpa = false, warpb = false, warpc = false, warpd = false, warpr = false;
 		
-		word maxffc = Map.CurrScr()->numFFC();
-		for(int32_t c=0; c<176+128+1+maxffc; ++c)
+		word num_ffcs = Map.CurrScr()->numFFC();
+		for(int32_t c=0; c<176+128+1+num_ffcs; ++c)
 		{
 			// Checks both combos, secret combos, undercombos and FFCs
 			//Fixme:
@@ -9925,8 +9925,8 @@ void domouse()
 	{
 		static int mapscr_tooltip_id = ttip_register_id();
 		bool did_ffttip = false;
-		int c = Map.CurrScr()->numFFC();
-		for(int32_t i=c-1; i>=0; i--)
+		int num_ffcs = Map.CurrScr()->numFFC();
+		for(int32_t i=num_ffcs-1; i>=0; i--)
 			if(Map.CurrScr()->ffcs[i].data !=0 && (CurrentLayer<2 || (Map.CurrScr()->ffcs[i].flags&ffc_overlay)))
 			{
 				int32_t ffx = Map.CurrScr()->ffcs[i].x.getFloor();
@@ -9973,14 +9973,14 @@ void domouse()
 			}
 			
 			mapscr* draw_mapscr = Map.AbsoluteScr(drawmap, drawscr);
-			if(unsigned(c) < 176 && draw_mapscr && !gui_mouse_b())
+			if(unsigned(num_ffcs) < 176 && draw_mapscr && !gui_mouse_b())
 			{
-				int cid = draw_mapscr->data[c];
+				int cid = draw_mapscr->data[num_ffcs];
 				newcombo const& cmb = combobuf[cid];
 				std::ostringstream oss;
-				int cs = draw_mapscr->cset[c];
-				int sflag = draw_mapscr->sflag[c];
-				oss << "Pos: " << c
+				int cs = draw_mapscr->cset[num_ffcs];
+				int sflag = draw_mapscr->sflag[num_ffcs];
+				oss << "Pos: " << num_ffcs
 					<< "\nCombo: " << cid
 					<< "\nCSet: " << cs;
 				if(sflag || cmb.flag)
@@ -10501,8 +10501,8 @@ void domouse()
 				}
 				
 				// Move FFCs
-				int c = Map.CurrScr()->numFFC();
-				for(int32_t i=c-1; i>=0; i--)
+				int num_ffcs = Map.CurrScr()->numFFC();
+				for(int32_t i=num_ffcs-1; i>=0; i--)
 					if(Map.CurrScr()->ffcs[i].data !=0 && (CurrentLayer<2 || (Map.CurrScr()->ffcs[i].flags&ffc_overlay)))
 					{
 						int32_t ffx = Map.CurrScr()->ffcs[i].x.getFloor();
@@ -10530,13 +10530,13 @@ void domouse()
 					}
 					mapscr* draw_mapscr = Map.AbsoluteScr(drawmap, drawscr);
 					if(!draw_mapscr) return;
-					Combo=draw_mapscr->data[c];
+					Combo=draw_mapscr->data[num_ffcs];
 					if(AutoBrush)
 						BrushWidth = BrushHeight = 1;
 					if(key[KEY_LSHIFT]||key[KEY_RSHIFT])
-						CSet=draw_mapscr->cset[c];
+						CSet=draw_mapscr->cset[num_ffcs];
 					if(CHECK_CTRL_CMD)
-						First[current_combolist]=scrollto_cmb(draw_mapscr->data[c]);
+						First[current_combolist]=scrollto_cmb(draw_mapscr->data[num_ffcs]);
 				}
 				else if(CHECK_CTRL_CMD)
 				{
@@ -10581,9 +10581,9 @@ void domouse()
 				
 				// FFC right-click menu
 				// This loop also serves to find the free ffc with the smallest slot number.
-				int c = Map.CurrScr()->numFFC();
+				int num_ffcs = Map.CurrScr()->numFFC();
 				uint32_t earliestfreeffc = c;
-				for(int32_t i=c-1; i>=0; i--)
+				for(int32_t i=num_ffcs-1; i>=0; i--)
 				{
 					auto data = Map.CurrScr()->ffcs[i].data;
 					if(data==0)

commit e1ef1fa76b60efa08bff54bf9b5f87d3d339f851

fix(zq)!: fix combo tooltip being broken
Regressed in 036cea2

src/zq/questReport.cpp
@@ -2176,8 +2176,8 @@ void scriptLocationReport()
             sc=m*MAPSCRS+s;
             ts=&TheMaps[sc];
             
-			word c = ts->numFFC();
-            for(word i=0; i<c; i++)
+			word num_ffcs = ts->numFFC();
+            for(word i=0; i<num_ffcs; i++)
             {
                 int32_t script = ts->ffcs[i].script;
                 

src/zq/zq_class.cpp
@@ -2682,8 +2682,8 @@ void zmap::draw(BITMAP* dest,int32_t x,int32_t y,int32_t flags,int32_t map,int32
 	
 	_zmap_drawlayer(dest, x, y, layers[1], antiflags, basescr->layeropacity[1-1]!=255, true, HL_LAYER(1));
 	
-	int c = basescr->numFFC();
-	for(int32_t i=c-1; i>=0; i--)
+	int num_ffcs = basescr->numFFC();
+	for(int32_t i=num_ffcs-1; i>=0; i--)
 	{
 		if(basescr->ffcs[i].data)
 		{
@@ -2882,7 +2882,7 @@ void zmap::draw(BITMAP* dest,int32_t x,int32_t y,int32_t flags,int32_t map,int32
 	}
 	_zmap_drawlayer(dest, x, y, layers[5], antiflags, basescr->layeropacity[5-1]!=255, true, HL_LAYER(5));
 	
-	for(int32_t i=c-1; i>=0; i--)
+	for(int32_t i=num_ffcs-1; i>=0; i--)
 	{
 		if(basescr->ffcs[i].data)
 		{
@@ -2910,7 +2910,7 @@ void zmap::draw(BITMAP* dest,int32_t x,int32_t y,int32_t flags,int32_t map,int32
 	
 	_zmap_drawlayer(dest, x, y, layers[6], antiflags, basescr->layeropacity[6-1]!=255, true, HL_LAYER(6));
 	
-	for(int32_t i=c-1; i>=0; i--)
+	for(int32_t i=num_ffcs-1; i>=0; i--)
 		if(basescr->ffcs[i].data)
 			if(basescr->ffcs[i].flags&ffc_changer)
 				putpixel(dest,(basescr->ffcs[i].x.getInt())+x,(basescr->ffcs[i].y.getInt())+y,vc(zc_oldrand()%16));
@@ -2927,7 +2927,7 @@ void zmap::draw(BITMAP* dest,int32_t x,int32_t y,int32_t flags,int32_t map,int32
 				for(int32_t i=0; i<176; i++)
 					put_walkflags(dest,((i&15)<<4)+x,(i&0xF0)+y,layers[k+1]->data[i], 0);
 		}
-		for(int32_t i=c-1; i>=0; i--)
+		for(int32_t i=num_ffcs-1; i>=0; i--)
 		{
 			if(auto data = basescr->ffcs[i].data)
 			{
@@ -14416,8 +14416,8 @@ void zmap::prv_secrets(bool high16only)
     }
     
     //FFCs
-	word c = s->numFFC();
-    for(word i=0; i<c; ++i)
+	word num_ffcs = s->numFFC();
+    for(word i=0; i<num_ffcs; ++i)
     {
         bool putit;
         

src/zq/zquest.cpp
@@ -9973,14 +9973,14 @@ void domouse()
 			}
 			
 			mapscr* draw_mapscr = Map.AbsoluteScr(drawmap, drawscr);
-			if(unsigned(num_ffcs) < 176 && draw_mapscr && !gui_mouse_b())
+			if(unsigned(c) < 176 && draw_mapscr && !gui_mouse_b())
 			{
-				int cid = draw_mapscr->data[num_ffcs];
+				int cid = draw_mapscr->data[c];
 				newcombo const& cmb = combobuf[cid];
 				std::ostringstream oss;
-				int cs = draw_mapscr->cset[num_ffcs];
-				int sflag = draw_mapscr->sflag[num_ffcs];
-				oss << "Pos: " << num_ffcs
+				int cs = draw_mapscr->cset[c];
+				int sflag = draw_mapscr->sflag[c];
+				oss << "Pos: " << c
 					<< "\nCombo: " << cid
 					<< "\nCSet: " << cs;
 				if(sflag || cmb.flag)