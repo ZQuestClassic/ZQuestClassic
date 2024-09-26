ZQuestClassic/ZQuestClassic

commit d97e0673ddda00f39ab34bd00d56594e7afbc30e

fix: improve key input latency by simplifying d_vsync_proc

src/gui/jwin.cpp
@@ -9094,49 +9094,12 @@ void draw_checkerboard(BITMAP* dest, int x, int y, int sz, optional<int> cb_sz, 
 
 int32_t d_vsync_proc(int32_t msg,DIALOG *d,int32_t c)
 {
-	static std::chrono::steady_clock::time_point tics;
-	auto now = std::chrono::steady_clock::now();
-
+	// This use to be what kept 60fps, but now render_timer_wait handles that.
     switch(msg)
     {
-		case MSG_START:
-			tics = std::chrono::steady_clock::now();
-			break;
-			
 		case MSG_IDLE:
 		{
-			int num_vsyncs = 0;
-			while (now - tics >= std::chrono::milliseconds(1000/60))
-			{
-				tics += std::chrono::milliseconds(1000/60);
-				broadcast_dialog_message(MSG_VSYNC, c);
-				if(d->dp)
-				{
-					int32_t ret = (*(std::function<int32_t()>*)d->dp)();
-					switch(ret)
-					{
-						case ONTICK_EXIT:
-							if(d->flags&D_NEW_GUI)
-								close_new_gui_dlg(d);
-							return D_EXIT;
-						case ONTICK_CLOSE:
-							if(d->flags&D_NEW_GUI)
-							{
-								//Simulate a GUI_EVENT for the window proc
-								DIALOG* window = d-1;
-								while(window->proc != jwin_win_proc) --window;
-								int32_t ret = new_gui_event(window-1, geCLOSE);
-								if(ret >= 0)
-									return ret;
-							}
-							return D_EXIT;
-						case ONTICK_REDRAW:
-							return D_REDRAW;
-					}
-				}
-
-				if (++num_vsyncs == 3) break;
-			}
+			broadcast_dialog_message(MSG_VSYNC, c);
 			break;
 		}
     }

src/base/gui.cpp
@@ -39,8 +39,6 @@ void broadcast_dialog_message(DIALOG* dialog, int32_t msg, int32_t c)
 static int run_zq_dialog(DIALOG *dlg, int focus_obj, bool checkexit)
 {
 	DIALOG_PLAYER *player = init_dialog(dlg,focus_obj);
-	bool should_draw = true;
-	int num_idle_frames = 0;
 	while(update_dialog(player))
 	{
 		if(checkexit)
@@ -52,27 +50,7 @@ static int run_zq_dialog(DIALOG *dlg, int focus_obj, bool checkexit)
 				return -1;
 			}
 		}
-		if (player->res & D_REDRAWME)
-		{
-			player->res &= ~D_REDRAWME;
-			should_draw = true;
-		}
-		if (should_draw)
-		{
-			should_draw = false;
-			num_idle_frames = 0;
-			update_hw_screen();
-			al_rest(1. / 60);
-			continue;
-		}
-
-		// Not perfect, but beats using 100% of CPU.
-		// The above may miss things, so draw at least a few times a second.
-		if (num_idle_frames++ == 15)
-		{
-			should_draw = 1;
-		}
-		al_rest(1. / 60);
+		update_hw_screen();
 	}
 	return shutdown_dialog(player);
 }

src/zq/gui/tileanim_frame.cpp
@@ -35,10 +35,6 @@ int32_t tile_anim_proc(int32_t msg,DIALOG *d,int32_t c)
 				d->flags |= D_DIRTY; //mark for redraw
 			else if(clk < -data[TileFrame::tfr_delay])
 				clk = -data[TileFrame::tfr_delay]; //Handle delay modification
-			break;
-		}
-		case MSG_DRAW:
-		{
 			if(clk >= data[TileFrame::tfr_speed])
 			{
 				clk %= data[TileFrame::tfr_speed];
@@ -47,12 +43,18 @@ int32_t tile_anim_proc(int32_t msg,DIALOG *d,int32_t c)
 					frm %= data[TileFrame::tfr_frames];
 					clk = -data[TileFrame::tfr_delay];
 				}
+				d->flags |= D_DIRTY;
 			}
 			else if(frm >= data[TileFrame::tfr_frames])
 			{ //Incase frames was changed
 				frm %= data[TileFrame::tfr_frames];
 				clk = -data[TileFrame::tfr_delay];
+				d->flags |= D_DIRTY;
 			}
+			break;
+		}
+		case MSG_DRAW:
+		{
 			int32_t tileToDraw = data[TileFrame::tfr_tile];
 			tileToDraw += frm; //the frame count
 			tileToDraw += (frm*data[TileFrame::tfr_skipx]); //xskip

commit dd0bf95ce3a58bec04c89e6ffa14b6d424659b22

fix!: actually call new dialog system's onTick callback
Regressed in d97e067

src/gui/jwin.cpp
@@ -9178,6 +9178,30 @@ int32_t d_vsync_proc(int32_t msg,DIALOG *d,int32_t c)
 		case MSG_IDLE:
 		{
 			broadcast_dialog_message(MSG_VSYNC, c);
+			if(d->dp)
+			{
+				int32_t ret = (*(std::function<int32_t()>*)d->dp)();
+				switch(ret)
+				{
+					case ONTICK_EXIT:
+						if(d->flags&D_NEW_GUI)
+							close_new_gui_dlg(d);
+						return D_EXIT;
+					case ONTICK_CLOSE:
+						if(d->flags&D_NEW_GUI)
+						{
+							//Simulate a GUI_EVENT for the window proc
+							DIALOG* window = d-1;
+							while(window->proc != jwin_win_proc) --window;
+							int32_t ret = new_gui_event(window-1, geCLOSE);
+							if(ret >= 0)
+								return ret;
+						}
+						return D_EXIT;
+					case ONTICK_REDRAW:
+						return D_REDRAW;
+				}
+			}
 			break;
 		}
     }