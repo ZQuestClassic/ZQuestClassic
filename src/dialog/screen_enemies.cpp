ZQuestClassic/ZQuestClassic

commit 54498fae33160767f620db8afee625d7ad14906a

refactor(zq): upgrade screen enemy selector to new gui (#989)

src/dialog/screen_enemies.cpp
@@ -1,0 +1,275 @@
+#include "screen_enemies.h"
+#include "info_lister.h"
+#include "screen_data.h"
+#include "enemypattern.h"
+#include "gui/key.h"
+#include "alert.h"
+#include "info.h"
+#include "base/zsys.h"
+#include "gui/builder.h"
+#include "zc_list_data.h"
+#include "zq/zq_class.h"
+#include <fmt/format.h>
+#include "zq/zquest.h"
+
+extern bool saved;
+extern char* guy_string[eMAXGUYS];
+
+using std::string;
+
+void call_screenenemies_dialog()
+{
+	ScreenEnemiesDialog().show();
+}
+
+ScreenEnemiesDialog::ScreenEnemiesDialog() :
+	thescr(Map.CurrScr()),
+	last_enemy(0),
+	copied_enemy_id(-1),
+	list_patterns(GUI::ZCListData::patterntypes())
+{
+	memcpy(oldenemy, Map.CurrScr()->enemy, sizeof(oldenemy));
+}
+
+void ScreenEnemiesDialog::RebuildList()
+{
+	list_scr_enemies.clear();
+
+	for (int q=0; q < 10; ++q)
+	{
+		char const* npcname = guy_string[thescr->enemy[q]];
+		std::string name;
+		if (thescr->enemy[q] <= 0)
+			name = "(None)";
+		else
+			name = npcname;
+		list_scr_enemies.add(name, thescr->enemy[q]);
+	}
+}
+
+void ScreenEnemiesDialog::UpdatePreview()
+{
+	std::string copied_name = "";
+	int32_t enemyID = thescr->enemy[scr_enemies->getSelectedIndex()];
+	if (copied_enemy_id >= 0)
+	{
+		guydata const& copied_enemy = guysbuf[copied_enemy_id];
+		copied_name = fmt::format("{}", guy_string[copied_enemy_id]);
+	}
+	if (enemyID >= 0)
+	{
+		guydata const& enemy = guysbuf[enemyID];
+		widgInfo->setText(fmt::format(
+			"#{}\nTile: {}\nsTile: {}"
+			"\neTile: {}\nHP: {}\nDamage: {}\nW. Damage: {}\nFamily: {}\nDrop: {}\nScript: {}\nW Script: {}"
+			"\n\nCopied:\n{}",
+			enemyID, enemy.tile, enemy.s_tile, enemy.e_tile, enemy.hp, enemy.dp,
+			enemy.wdp, enemy.family, enemy.item_set, enemy.script, enemy.weaponscript, copied_name));
+		if (enemyID > 0)
+		{
+			widgPrev->setDisabled(false);
+			if (get_qr(qr_NEWENEMYTILES))
+				widgPrev->setTile(enemy.e_tile + efrontfacingtile(enemyID));
+			else
+				widgPrev->setTile(enemy.tile + efrontfacingtile(enemyID));
+			widgPrev->setCSet(enemy.cset & 0xF);
+			widgPrev->setFrames(0);
+			widgPrev->setSpeed(0);
+			widgPrev->setDelay(0);
+			widgPrev->setSkipX((enemy.SIZEflags & OVERRIDE_TILE_WIDTH)
+				? enemy.txsz - 1 : 0);
+			widgPrev->setSkipY((enemy.SIZEflags & OVERRIDE_TILE_HEIGHT)
+				? enemy.tysz - 1 : 0);
+		}
+		else
+		{
+			widgPrev->setDisabled(true);
+			widgPrev->setTile(0);
+			widgPrev->setCSet(0);
+			widgPrev->setFrames(0);
+			widgPrev->setSpeed(0);
+			widgPrev->setDelay(0);
+			widgPrev->setSkipX(0);
+			widgPrev->setSkipY(0);
+		}
+	}
+	widgPrev->setVisible(true);
+	widgPrev->setDoSized(true);
+	widgPrev->overrideWidth(Size::pixels(48 + 4));
+	widgPrev->overrideHeight(Size::pixels(48 + 4));
+}
+
+std::shared_ptr<GUI::Widget> ScreenEnemiesDialog::view()
+{
+	using namespace GUI::Builder;
+	using namespace GUI::Props;
+	using namespace GUI::Key;
+
+	window = Window(
+		use_vsync = true,
+		title = "Select Enemies",
+		info = "Click the ten spaces to the left to select different enemies to spawn on this screen."
+		"\nYou can also access Enemy Flags and set the Spawning Pattern.",
+		shortcuts = {
+			E = message::PASTEFROMSCREEN,
+			GUI::Key::F = message::FLAGS,
+			P = message::PATTERN,
+			K = message::OK,
+			C = message::COPY,
+			V = message::PASTE,
+			Enter = message::EDIT,
+			Del = message::CLEAR,
+			Backspace = message::CLEAR,
+		},
+		onClose = message::CANCEL,
+		Row(
+			Column(
+				scr_enemies = List(fitParent = true, maxwidth = 400_px, vAlign = 0.0,
+					selectedIndex = last_enemy,
+					data = list_scr_enemies,
+					fitParent = true,
+					onSelectFunc = [&](int32_t val)
+					{
+						//selectedValue = scr_enemies->getSelectedValue();
+						//selectedIndex = scr_enemies->getSelectedIndex();
+						message::SELECT;
+					},
+					onDClick = message::EDIT
+				),
+				Label(text = fmt::format("Pattern: {}", list_patterns.getText(thescr->pattern)), minwidth = 400_px),
+				Row(hAlign = 1.0,vAlign = 1.0,topPadding = 0.5_em,spacing = 1_em,
+					Button(
+						text = Map.CanPaste() ? fmt::format("Past&e (from {}x{:02x})", (Map.CopyScr() >> 8) + 1, Map.CopyScr() & 255) : "Past&e from screen",
+						colSpan=2, fitParent=true,
+						minwidth = 90_px,
+						onClick = message::OK,
+						disabled = !Map.CanPaste()
+					),
+					Button(
+						text = "&Flags",
+						colSpan = 2, fitParent = true,
+						minwidth = 90_px,
+						onClick = message::FLAGS
+					),
+					Button(
+						text = "&Pattern",
+						colSpan = 2, fitParent = true,
+						minwidth = 90_px,
+						onClick = message::PATTERN
+					)
+				),
+				Row(
+					vAlign = 1.0,
+					topPadding = 0.5_em,
+					spacing = 1_em,
+					Button(
+						text = "O&K",
+						fitParent = true,
+						minwidth = 90_px,
+						onClick = message::OK
+					),
+					Button(
+						text = "Cancel",
+						fitParent = true,
+						minwidth = 90_px,
+						onClick = message::CANCEL
+					)
+				)
+			),
+			Column(
+				widgPrev = TileFrame(visible = false),
+				widgInfo = Label(text = "", minwidth=250_px, maxwidth = 250_px, fitParent=true)
+			)
+		)
+	);	
+	RebuildList();
+	UpdatePreview();
+	return window;
+}
+
+bool ScreenEnemiesDialog::handleMessage(const GUI::DialogMessage<message>& msg)
+{
+	bool refresh = false;
+	switch (msg.message)
+	{
+	case message::COPY:
+		copied_enemy_id = thescr->enemy[scr_enemies->getSelectedIndex()];
+		[[fallthrough]];
+	case message::SELECT:
+		UpdatePreview();
+		refresh = true;
+		break;
+	case message::PASTE:
+		if (copied_enemy_id > -1 && copied_enemy_id < eMAXGUYS)
+			thescr->enemy[scr_enemies->getSelectedIndex()] = copied_enemy_id;
+		RebuildList();
+		UpdatePreview();
+		refresh = true;
+		break;
+	case message::EDIT:
+	{
+		int32_t current_enemy = thescr->enemy[scr_enemies->getSelectedIndex()];
+		EnemyListerDialog(current_enemy, true).show();
+		if (lister_sel_val != current_enemy)
+		{
+			thescr->enemy[scr_enemies->getSelectedIndex()] = lister_sel_val;
+			RebuildList();
+			UpdatePreview();
+		}
+		refresh = true;
+		break;
+	}
+	case message::CLEAR:
+		thescr->enemy[scr_enemies->getSelectedIndex()] = 0;
+		RebuildList();
+		UpdatePreview();
+		refresh = true;
+		break;
+	case message::PASTEFROMSCREEN:
+		Map.DoPasteScreenCommand(PasteCommandType::ScreenEnemies);
+		RebuildList();
+		UpdatePreview();
+		refresh = true;
+		break;
+	case message::FLAGS:
+		call_screendata_dialog(3);
+		refresh = true;
+		break;
+	case message::PATTERN:
+		call_enemypattern_dialog();
+		refresh = true;
+		break;
+	case message::OK:
+	{
+		bool end = false;
+		bool confirm = true;
+		for (int32_t i = 0; i < 10; i++)
+		{
+			if (thescr->enemy[i] == 0)
+				end = true;
+			else if (end)
+			{
+				AlertDialog("Inactive Enemies!",
+					"Enemies won't appear if preceded by '(None)' in the list! Continue?",
+					[&](bool ret, bool)
+					{
+						confirm = ret;
+					}).show();
+				break;
+			}
+		}
+		if (!confirm)
+		{
+			rerun_dlg = true;
+			last_enemy = scr_enemies->getSelectedIndex();
+		}
+		return true;
+	}
+	case message::CANCEL:
+		memcpy(thescr->enemy, oldenemy, sizeof(thescr->enemy));
+		return true;
+	}
+	if(refresh) rerun_dlg = true;
+	last_enemy = scr_enemies->getSelectedIndex();
+	return refresh;
+}

src/dialog/enemypattern.cpp
@@ -1,0 +1,79 @@
+#include "enemypattern.h"
+#include "gui/key.h"
+#include "info.h"
+#include "base/zsys.h"
+#include "gui/builder.h"
+#include "zc_list_data.h"
+#include "zq/zq_class.h"
+
+extern bool saved;
+
+void call_enemypattern_dialog()
+{
+	EnemyPatternDialog(Map.CurrScr()).show();
+}
+
+EnemyPatternDialog::EnemyPatternDialog(mapscr* scr) :
+	thescr(scr),
+	last_pattern(int32_t(scr->pattern))
+{}
+
+std::shared_ptr<GUI::Widget> EnemyPatternDialog::view()
+{
+	using namespace GUI::Builder;
+	using namespace GUI::Props;
+	using namespace GUI::Key;
+
+	window = Window(
+		title = "Select Pattern",
+		info = "These will change how the screen enemies will spawn.",
+		shortcuts = {
+			K = message::OK
+		},
+		onClose = message::CANCEL,
+		Column(hAlign=1.0,
+			Label(text="Classic: Pseudorandom locations near"
+				"\nthe middle of the screen."),
+			Label(text="Random: Any available location"
+				"\nat a sufficient distance from the player."),
+			scr_pattern = List(fitParent = true,
+				selectedIndex = last_pattern,
+				data = GUI::ZCListData::patterntypes(),
+				onSelectFunc = [&](int32_t val)
+				{
+					last_pattern = val;
+				}),
+			Row(
+				topPadding = 0.5_em,
+				vAlign = 1.0,
+				spacing = 2_em,
+				Button(
+					text = "O&K",
+					minwidth = 90_px,
+					onClick = message::OK
+				),
+				Button(
+					text = "Cancel",
+					minwidth = 90_px,
+					onClick = message::CANCEL
+				)
+			)
+		)
+	);
+
+	return window;
+}
+
+bool EnemyPatternDialog::handleMessage(const GUI::DialogMessage<message>& msg)
+{
+	switch (msg.message)
+	{
+	case message::OK:
+		thescr->pattern = byte(last_pattern);
+		saved = false;
+		[[fallthrough]];
+	case message::CANCEL:
+	default:
+		return true;
+	}
+}

src/dialog/enemypattern.h
@@ -1,0 +1,36 @@
+#ifndef ZC_DIALOG_ENEMY_PATTERN_H_
+#define ZC_DIALOG_ENEMY_PATTERN_H_
+
+#include <gui/dialog.h>
+#include <gui/button.h>
+#include <gui/label.h>
+#include <gui/window.h>
+#include <gui/list_data.h>
+#include <gui/list.h>
+
+void call_enemypattern_dialog();
+
+class EnemyPatternDialog : public GUI::Dialog<EnemyPatternDialog>
+{
+public:
+    enum class message
+    {
+        REFR_INFO, OK, CANCEL
+    };
+
+    std::shared_ptr<GUI::Widget> view() override;
+    bool handleMessage(const GUI::DialogMessage<message>& msg);
+private:
+    EnemyPatternDialog(mapscr* scr);
+
+    std::shared_ptr<GUI::Window> window;
+    std::shared_ptr<GUI::List> scr_pattern;
+
+    int32_t last_pattern;
+    mapscr* thescr;
+    GUI::ListData list_patterns;
+
+    friend void call_enemypattern_dialog();
+};
+
+#endif

src/dialog/screen_enemies.h
@@ -1,0 +1,47 @@
+#ifndef ZC_DIALOG_SCREEN_ENEMIES_H_
+#define ZC_DIALOG_SCREEN_ENEMIES_H_
+
+#include <gui/dialog.h>
+#include <gui/button.h>
+#include <gui/label.h>
+#include <gui/list.h>
+#include <gui/list_data.h>
+#include <gui/window.h>
+#include <zq/gui/tileanim_frame.h>
+#include <initializer_list>
+#include <string>
+
+
+void call_screenenemies_dialog();
+
+class ScreenEnemiesDialog : public GUI::Dialog<ScreenEnemiesDialog>
+{
+public:
+    enum class message
+    {
+        REFR_INFO, COPY, PASTE, SELECT, EDIT, CLEAR, PASTEFROMSCREEN, FLAGS, PATTERN, OK, CANCEL
+    };
+
+    void RebuildList();
+    void UpdatePreview();
+
+    std::shared_ptr<GUI::Widget> view() override;
+    bool handleMessage(const GUI::DialogMessage<message>& msg);
+
+private:
+    ScreenEnemiesDialog();
+    std::shared_ptr<GUI::Window> window;
+    std::shared_ptr<GUI::List> scr_enemies;
+    std::shared_ptr<GUI::Label> widgInfo;
+    std::shared_ptr<GUI::TileFrame> widgPrev;
+
+    int32_t copied_enemy_id=0;
+    int32_t last_enemy;
+    mapscr* thescr;
+    word oldenemy[10];
+    GUI::ListData list_scr_enemies, list_patterns;
+    
+
+    friend void call_screenenemies_dialog();
+};
+#endif

src/zc_list_data.cpp
@@ -1224,3 +1224,19 @@ GUI::ListData const& GUI::ZCListData::scripttypes()
 {
 	return script_types;
 };
+
+static const GUI::ListData pattern_types
+{
+	{ "Spawn (Classic)", 0},
+	{ "Enter from Sides (Consecutive)", 1},
+	{ "Enter from Sides (Random)", 2},
+	{ "Fall From Ceiling (Classic)", 3},
+	{ "Fall From Ceiling (Random)", 4},
+	{ "Spawn (Random)", 5},
+	{ "Spawn No Enemies", 6}
+};
+
+GUI::ListData const& GUI::ZCListData::patterntypes()
+{
+	return pattern_types;
+};

src/zc_list_data.h
@@ -65,6 +65,7 @@ namespace GUI::ZCListData
 	GUI::ListData const& buttons();
 	GUI::ListData const& autocombotypes();
 	GUI::ListData const& scripttypes();
+	GUI::ListData const& patterntypes();
 }
 
 #endif

src/zq/zq_custom.cpp
@@ -199,7 +199,7 @@ void large_dialog(DIALOG *d, float RESIZE_AMT)
 		// Bigger font
 		bool bigfontproc = (d[i].proc != jwin_droplist_proc && d[i].proc != jwin_abclist_proc && d[i].proc != d_wlist_proc && d[i].proc != jwin_list_proc && d[i].proc != d_dmaplist_proc
 							&& d[i].proc != d_dropdmaplist_proc && d[i].proc != d_warplist_proc && d[i].proc != d_warplist_proc && d[i].proc != d_wclist_proc && d[i].proc != d_ndroplist_proc
-							&& d[i].proc != d_idroplist_proc && d[i].proc != d_nidroplist_proc && d[i].proc != jwin_as_droplist_proc && d[i].proc != d_ffcombolist_proc && d[i].proc != d_enelist_proc && d[i].proc != sstype_drop_proc
+							&& d[i].proc != d_idroplist_proc && d[i].proc != d_nidroplist_proc && d[i].proc != jwin_as_droplist_proc && d[i].proc != d_ffcombolist_proc && d[i].proc != sstype_drop_proc
 							&& d[i].proc != d_comboalist_proc);
 							
 		if(bigfontproc && !d[i].dp2)
@@ -2028,7 +2028,6 @@ void showEnemyScriptMetaHelp(const guydata& test, int32_t i)
 	}
 }
 
-extern DIALOG elist_dlg[];
 static int32_t copiedGuy;
 
 int32_t readonenpc(PACKFILE *f, int32_t index)
@@ -2802,83 +2801,6 @@ int32_t writeonenpc(PACKFILE *f, int32_t i)
 	return 1;
 }
 
-
-
-
-
-
-void paste_enemy(int32_t index)
-{
-	if(index < 0) index = elist_dlg[2].d1;
-	if(index==0)
-		return;
-	if(copiedGuy<0) //Nothing copied
-		return;
-	guysbuf[bie[index].i]=guysbuf[copiedGuy];
-	elist_dlg[2].flags|=D_DIRTY;
-	saved=false;
-}
-void copy_enemy(int32_t index)
-{
-	if(index < 0) index = elist_dlg[2].d1;
-	if(index==0)
-		return;
-	copiedGuy=bie[index].i;
-}
-void save_enemy(int32_t index)
-{
-	if(index < 0) index = elist_dlg[2].d1;
-	if(index==0)
-		return;
-	if(!prompt_for_new_file_compat("Save NPC(.znpc)", "znpc", NULL,datapath,false))
-		return;
-	int32_t iid = bie[index].i;
-	
-	PACKFILE *f=pack_fopen_password(temppath,F_WRITE, "");
-	if(!f) return;
-	if (!writeonenpc(f,iid))
-	{
-		Z_error("Could not write to .znpc packfile %s\n", temppath);
-		InfoDialog("ZNPC Error", "Could not save the specified enemy.").show();
-	}
-	pack_fclose(f);
-}
-void load_enemy(int32_t index)
-{
-	if(index < 0) index = elist_dlg[2].d1;
-	if(index==0)
-		return;
-	if(!prompt_for_existing_file_compat("Load NPC(.znpc)", "znpc", NULL,datapath,false))
-		return;
-	PACKFILE *f=pack_fopen_password(temppath,F_READ, "");
-	if(!f) return;
-	
-	if (!readonenpc(f,index))
-	{
-		al_trace("Could not read from .znpc packfile %s\n", temppath);
-		InfoDialog("ZNPC Error", "Could not load the specified enemy.").show();
-	}
-	
-	pack_fclose(f);
-	elist_dlg[2].flags|=D_DIRTY;
-	saved=false;
-}
-
-
-void elist_rclick_func(int32_t index, int32_t x, int32_t y)
-{
-	if(index == 0)
-		return;
-	
-	NewMenu rcmenu {
-		{ "&Copy", [&](){copy_enemy(index);} },
-		{ "Paste", "&v", [&](){paste_enemy(index);}, 0, copiedGuy <= 0 },
-		{ "&Save", [&](){save_enemy(index);} },
-		{ "&Load", [&](){load_enemy(index);} },
-	};
-	rcmenu.pop(x, y);
-}
-
 int32_t onCustomEnemies()
 {
 	EnemyListerDialog().show();
@@ -2890,7 +2812,6 @@ void edit_enemydata(int32_t index)
 {
 	call_enemy_editor(index);
 }
-
 
 int32_t onCustomGuys()
 {

commit a4d9202197c1618bbc9f0a5714395545da943bea

fix(zq)!: fix pasting enemies from other screen in screen enemy dialog
This regressed recently in 54498fa

src/dialog/screen_enemies.cpp
@@ -142,7 +142,7 @@ std::shared_ptr<GUI::Widget> ScreenEnemiesDialog::view()
 						text = Map.CanPaste() ? fmt::format("Past&e (from {}x{:02x})", (Map.CopyScr() >> 8) + 1, Map.CopyScr() & 255) : "Past&e from screen",
 						colSpan=2, fitParent=true,
 						minwidth = 90_px,
-						onClick = message::OK,
+						onClick = message::PASTEFROMSCREEN,
 						disabled = !Map.CanPaste()
 					),
 					Button(