ZQuestClassic/ZQuestClassic

commit 7f249ee2ec123fefdbc69b968d8f99fe71bf5c82

fix(zscript): fix broken array literals when declared as array pointers
In ZScript, there are array declarations and array pointers. Array
pointers are used as array parameters, and declarations are (typically)
used to declare arrays (either with an initializer list, or as an empty
array).

Array declarations register their array to be deallocated at the end of
the scope. Array pointers can take an array, and won't deallocate it
when it goes out of scope. That's why pointers are used in parameters.

This syntax is easy to mix up, so one might declare an array using the
pointer syntax. The compiler would not complain, but it would actually
produce invalid code.

```
item items1[] = {item}; // array declaration syntax, worked
item[] items1 = {item}; // array pointer syntax, was broken
```

This is a recent-ish regression in the 3.0 branch, and did not happpen
in 2.55. This has now been resolved.

end changelog

Some key implementation changes to note:

- `BuildOpcodes::caseDataDecl` now only used `buildVariable`, even for
  arrays
- local arrays are now registered for dealloc via
  `registerLocalArrayDealloc`, and that usually gets called right after
  `OAllocateMemImmediate` is emitted. The exception is arrayLiteralFree
  and stringLiteralFree, since they dealloc their temporary values
  sooner, and in a different way
- buildArrayUninit now pushes its value as EXP1, to be used by
  buildVariable.

src/parser/BuildVisitors.cpp
@@ -283,6 +283,13 @@ void BuildOpcodes::addOpcodes(Container const& container)
 		addOpcode(ptr);
 }
 
+// Register local arrays for cleanup (but just array declarations that create literals, not "pointers").
+// This should go away when arrays are managed by GC.
+void BuildOpcodes::registerLocalArrayDealloc(int stackoffset)
+{
+	arrayRefs.push_back(stackoffset);
+}
+
 void BuildOpcodes::deallocateArrayRef(int32_t arrayRef)
 {
 	addOpcode(new OLoad(new VarArgument(EXP2), new LiteralArgument(arrayRef)));
@@ -1612,26 +1619,17 @@ void BuildOpcodes::caseDataDecl(ASTDataDecl& host, void* param)
 {
 	if(parsing_user_class == puc_vars) return;
 	OpcodeContext& context = *(OpcodeContext*)param;
-	ASTExpr* init = host.getInitializer();
 	Datum& manager = *host.manager;
 	if(manager.is_erased()) //var unused, optimized away
 	{
-		sidefx_visit(init, param);
+		sidefx_visit(host.getInitializer(), param);
 		return;
 	}
 
 	// Ignore inlined values.
 	if (manager.getCompileTimeValue()) return;
 
-	// Switch off to the proper helper function.
-	if (!host.extraArrays.empty()
-		|| (init && (init->isArrayLiteral()
-					 || init->isStringLiteral())))
-	{
-		if (init) buildArrayInit(host, context);
-		else buildArrayUninit(host, context);
-	}
-	else buildVariable(host, context);
+	buildVariable(host, context);
 }
 
 void BuildOpcodes::buildVariable(ASTDataDecl& host, OpcodeContext& context)
@@ -1643,25 +1641,36 @@ void BuildOpcodes::buildVariable(ASTDataDecl& host, OpcodeContext& context)
 	
 	// Load initializer, if present.
 	auto init = host.getInitializer();
-	if(!init && host.getFlag(ASTDataDecl::FL_SKIP_EMPTY_INIT))
-		return;
-	auto val = init ? init->getCompileTimeValue(this, scope) : optional<int>(0);
-	if(!val)
-		visit(init, &context);
+
+	std::optional<int> comptime_val;
+	if (init)
+		comptime_val = init->getCompileTimeValue(this, scope);
+	if (!comptime_val)
+	{
+		if (init)
+			visit(init, &context);
+		else if (!host.extraArrays.empty())
+			buildArrayUninit(host, context);
+		else if (host.getFlag(ASTDataDecl::FL_SKIP_EMPTY_INIT))
+			return;
+		else
+			comptime_val = 0;
+	}
 
 	auto writeType = &host.resolveType(scope, this);
 	bool is_object = writeType && writeType->isObject();
+	bool is_array = writeType && writeType->isArray();
 	bool holds_object = writeType && writeType->canHoldObject();
 
-	// Set variable to EXP1 or val, depending on the initializer.
+	// Set variable to EXP1 or comptime_val, depending on the initializer.
 	if (auto globalId = manager.getGlobalId())
 	{
 		if (holds_object)
 			addOpcode(new OMarkTypeRegister(new GlobalArgument(*globalId), new LiteralArgument((int)writeType->getScriptObjectTypeId())));
 
-		if (val)
-			addOpcode(new OSetImmediate(new GlobalArgument(*globalId), new LiteralArgument(*val)));
-		else if (is_object)
+		if (comptime_val)
+			addOpcode(new OSetImmediate(new GlobalArgument(*globalId), new LiteralArgument(*comptime_val)));
+		else if (is_object && !is_array)
 			addOpcode(new OSetObject(new GlobalArgument(*globalId), new VarArgument(EXP1)));
 		else
 			addOpcode(new OSetRegister(new GlobalArgument(*globalId), new VarArgument(EXP1)));
@@ -1669,12 +1678,12 @@ void BuildOpcodes::buildVariable(ASTDataDecl& host, OpcodeContext& context)
 	else
 	{
 		int32_t offset = manager.getStackOffset(false);
-		if (val)
+		if (comptime_val)
 		{
 			// I tried to optimize this away in some circumstances, it lead to only problems -Em
-			addOpcode(new OStoreV(new LiteralArgument(*val), new LiteralArgument(offset)));
+			addOpcode(new OStoreV(new LiteralArgument(*comptime_val), new LiteralArgument(offset)));
 		}
-		else if (is_object)
+		else if (is_object && !is_array)
 		{
 			// This command decrements the reference of the object currently stored at the position before
 			// setting the new object and incrementing its reference. Since we set the initial value for
@@ -1685,20 +1694,9 @@ void BuildOpcodes::buildVariable(ASTDataDecl& host, OpcodeContext& context)
 	}
 }
 
-void BuildOpcodes::buildArrayInit(ASTDataDecl& host, OpcodeContext& context)
+void BuildOpcodes::buildArrayUninit(ASTDataDecl& host, OpcodeContext& context)
 {
 	Datum& manager = *host.manager;
-
-	// Initializer should take care of everything.
-	visit(host.getInitializer(), &context);
-}
-
-void BuildOpcodes::buildArrayUninit(
-		ASTDataDecl& host, OpcodeContext& context)
-{
-	Datum& manager = *host.manager;
-	if(manager.is_erased()) //var unused, optimized away
-		return;
 
 	// Right now, don't support nested arrays.
 	if (host.extraArrays.size() != 1)
@@ -1728,16 +1726,11 @@ void BuildOpcodes::buildArrayUninit(
 						  new VarArgument(EXP1),
 						  new LiteralArgument(totalSize),
 						  new LiteralArgument(script_object_type_id)));
-		addOpcode(new OSetRegister(new GlobalArgument(*globalId),
-								   new VarArgument(EXP1)));
 	}
 	else
 	{
 		addOpcode(new OAllocateMemImmediate(new VarArgument(EXP1), new LiteralArgument(totalSize), new LiteralArgument(script_object_type_id)));
-		int32_t offset = manager.getStackOffset(false);
-		addOpcode(new OStore(new VarArgument(EXP1), new LiteralArgument(offset)));
-		// Register for cleanup.
-		arrayRefs.push_back(offset);
+		registerLocalArrayDealloc(manager.getStackOffset(false));
 	}
 }
 
@@ -3736,18 +3729,13 @@ void BuildOpcodes::stringLiteralDeclaration(
 						  new VarArgument(EXP1),
 						  new LiteralArgument(size * 10000L),
 						  new LiteralArgument(0)));
-		addOpcode(new OSetRegister(new GlobalArgument(*globalId),
-								   new VarArgument(EXP1)));
 	}
 	else
 	{
 		addOpcode(new OAllocateMemImmediate(new VarArgument(EXP1),
 											new LiteralArgument(size * 10000L),
 											new LiteralArgument(0)));
-		int32_t offset = manager.getStackOffset(false);
-		addOpcode(new OStore(new VarArgument(EXP1), new LiteralArgument(offset)));
-		// Register for cleanup.
-		arrayRefs.push_back(offset);
+		registerLocalArrayDealloc(manager.getStackOffset(false));
 	}
 
 	addOpcode(new OWritePODString(new VarArgument(EXP1), new StringArgument(data)));
@@ -3766,6 +3754,7 @@ void BuildOpcodes::stringLiteralDeclaration(
 					  // new LiteralArgument(0)));
 }
 
+// "free" means it is not attached to a variable declaration (so it's either a param value, or used in an assignment)
 void BuildOpcodes::stringLiteralFree(
 		ASTStringLiteral& host, OpcodeContext& context)
 {
@@ -3784,6 +3773,7 @@ void BuildOpcodes::stringLiteralFree(
 						   new VarArgument(EXP1),
 						   new LiteralArgument(size * 10000L),
 						   new LiteralArgument(0)));
+	// Gets deallocated at end of this usage, see bottom of this method.
 	addOpcode2(init, new OStore(new VarArgument(EXP1),
 									  new LiteralArgument(offset)));
 
@@ -3812,8 +3802,8 @@ void BuildOpcodes::stringLiteralFree(
 	////////////////////////////////////////////////////////////////
 	// Register for cleanup.
 	
+	// Store the dealloc code here.
 	deallocateArrayRef(offset, dealloc);
-	//arrayRefs.push_back(offset); //Replaced by deallocCode
 }
 
 void BuildOpcodes::caseArrayLiteral(ASTArrayLiteral& host, void* param)
@@ -3882,19 +3872,16 @@ void BuildOpcodes::arrayLiteralDeclaration(
 						  new VarArgument(EXP1),
 						  new LiteralArgument(size * 10000L),
 						  new LiteralArgument(script_object_type_id)));
-		addOpcode(new OSetRegister(new GlobalArgument(*globalId),
-								   new VarArgument(EXP1)));
 	}
 	else
 	{
 		addOpcode(new OAllocateMemImmediate(new VarArgument(EXP1),
 											new LiteralArgument(size * 10000L),
 											new LiteralArgument(script_object_type_id)));
-		int32_t offset = manager.getStackOffset(false);
-		addOpcode(new OStore(new VarArgument(EXP1), new LiteralArgument(offset)));
-		// Register for cleanup.
-		arrayRefs.push_back(offset);
+		registerLocalArrayDealloc(manager.getStackOffset(false));
 	}
+
+	addOpcode(new OPushRegister(new VarArgument(EXP1)));
 
 	// Initialize array.
 	std::vector<int32_t> constelements;
@@ -3932,6 +3919,8 @@ void BuildOpcodes::arrayLiteralDeclaration(
 			}
 		}
 	}
+
+	addOpcode(new OPopRegister(new VarArgument(EXP1)));
 }
 
 void BuildOpcodes::arrayLiteralFree(
@@ -3974,6 +3963,7 @@ void BuildOpcodes::arrayLiteralFree(
 			new OAllocateMemImmediate(new VarArgument(EXP1),
 									  new LiteralArgument(size * 10000L),
 									  new LiteralArgument(0)));
+	// Gets deallocated at end of this usage, see bottom of this method.
 	addOpcode2(context.initCode,
 			new OStore(new VarArgument(EXP1),
 							   new LiteralArgument(offset)));
@@ -4027,8 +4017,8 @@ void BuildOpcodes::arrayLiteralFree(
 	////////////////////////////////////////////////////////////////
 	// Register for cleanup.
 
+	// Store the dealloc code here.
 	deallocateArrayRef(offset, context.deallocCode);
-	//arrayRefs.push_back(offset); //Replaced by deallocCode
 }
 
 void BuildOpcodes::caseOptionValue(ASTOptionValue& host, void*)

tests/replays/playground/playground.qst
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:52580f2d78f6fb158100817db6fcfe55f70247c50c25f059552d5247fdb8c48c
-size 3162085
+oid sha256:4c79ff906b9e0f9537f09c903380f5ffd3e6cc26ddbdfb6d6345ddf90df30cfd
+size 3163166

src/parser/AST.cpp
@@ -1433,13 +1433,13 @@ void ASTDataDecl::execute(ASTVisitor& visitor, void* param)
 
 void ASTDataDecl::setInitializer(ASTExpr* initializer)
 {
-	if(ASTExprVarInitializer* init = dynamic_cast<ASTExprVarInitializer*>(initializer))
+	if (initializer->isConstant())
 	{
-		initializer_ = init;
+		initializer_ = new ASTExprVarInitializer(initializer, initializer->location);
 	}
 	else
 	{
-		initializer_ = new ASTExprVarInitializer(initializer, initializer->location);
+		initializer_ = initializer;
 	}
 
 	// Give a string or array literal a reference back to this object so it

src/parser/AST.h
@@ -1083,8 +1083,8 @@ namespace ZScript
 
 		Type getDeclarationType() const /*override*/ {return TYPE_DATA;}
 
-		ASTExprVarInitializer* getInitializer() {return initializer_.get();}
-		ASTExprVarInitializer const* getInitializer() const {return initializer_.get();}
+		ASTExpr* getInitializer() {return initializer_.get();}
+		ASTExpr const* getInitializer() const {return initializer_.get();}
 		void setInitializer(ASTExpr* initializer);
 
 		// Resolves the type, using either the list's or this node's own base type
@@ -1129,7 +1129,7 @@ namespace ZScript
 
 	private:
 		// The initialization expression. Optional.
-		owning_ptr<ASTExprVarInitializer> initializer_;
+		owning_ptr<ASTExpr> initializer_;
 	};
 
 	bool hasSize(ASTDataDecl const&);

src/parser/BuildVisitors.h
@@ -124,6 +124,7 @@ namespace ZScript
 		template <class Container>
 		void addOpcodes(Container const& container);
 	
+		void registerLocalArrayDealloc(int stackoffset);
 		void deallocateArrayRef(int32_t arrayRef);
 		void deallocateArrayRef(int32_t arrayRef, std::vector<std::shared_ptr<Opcode>>& code);
 		void deallocateRefsUntilCount(int32_t count);
@@ -224,8 +225,6 @@ namespace ZScript
 		void pop_params(uint count);
 		// For when ASTDataDecl is for a single variable.
 		void buildVariable(ASTDataDecl& host, OpcodeContext& context);
-		// For when ASTDataDecl is an initialized array.
-		void buildArrayInit(ASTDataDecl& host, OpcodeContext& context);
 		// For when ASTDataDecl is an uninitialized array.
 		void buildArrayUninit(ASTDataDecl& host, OpcodeContext& context);
 		// For when ASTStringLiteral is a declaration initializer.

src/parser/ZScript.cpp
@@ -377,8 +377,10 @@ std::optional<int32_t> Variable::getCompileTimeValue(bool getinitvalue) const
 {
 	if(getinitvalue)
 	{
-		ASTExprVarInitializer* init = node.getInitializer();
-		return init ? init->value : std::nullopt;
+		ASTExpr* init = node.getInitializer();
+		if (auto expr_const = dynamic_cast<ASTExprVarInitializer*>(init))
+			return expr_const->value;
+		return std::nullopt;
 	}
 	return std::nullopt;
 }

tests/replays/playground/auto_arrays.zplay
@@ -1,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:aee03afaaec1805c1d2a6e598a1a6b26284b8c165a2e4384fc0baac2b99a05ad
+size 1257

commit 2ea0c00b8d8289ff44e389fda2fe6d1cb5cfb52b

fix(zscript)!: don't mark global arrays as objects via MARK_TYPE_REG
This broke the garbage collection system, as arrays aren't really
objects (yet).

Regressed in 7f249ee

src/parser/BuildVisitors.cpp
@@ -1665,7 +1665,7 @@ void BuildOpcodes::buildVariable(ASTDataDecl& host, OpcodeContext& context)
 	// Set variable to EXP1 or comptime_val, depending on the initializer.
 	if (auto globalId = manager.getGlobalId())
 	{
-		if (holds_object)
+		if (holds_object && !is_array)
 			addOpcode(new OMarkTypeRegister(new GlobalArgument(*globalId), new LiteralArgument((int)writeType->getScriptObjectTypeId())));
 
 		if (comptime_val)

tests/replays/playground/auto_garbage_collection.zplay
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cbe389a4ff9a31885eb9db1cbd1e65afe5df8aeb179d6fff0410dbfd46016836
-size 1964
+oid sha256:b061b06bd242f0b374743abaff767d0f45fa3534c49a651294d2824b9cf250dd
+size 1984

tests/replays/playground/playground.qst
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:041b0dcae432493578150dd2c4db42d277a76dda15a111e2e3b8afb12f40c81c
-size 3165095
+oid sha256:ef765246923d22446d637f02c4657c6b5bd61ae9394043e410b7bd6bb3c9146b
+size 3165090

tests/scripts/playground/auto/garbage_collection_expected.txt
@@ -2828,7 +2828,6 @@ SETV GD3 0
 MARK_TYPE_REG GD5 0.0008
 SETV GD5 0
 ALLOCATEGMEMV D2 10 0.0008
-MARK_TYPE_REG GD4 0.0008
 SETR GD4 D2
 SETV GD2 0
 QUIT; void run() Body End

tests/scripts/playground/auto/test_runner.zs
@@ -16,6 +16,16 @@ namespace Test
 		}
 	}
 
+	void AssertEqual<T>(T actual, T expected, char32[] error)
+	{
+		if (expected != actual)
+		{
+			printf("[Test] failed assertion: expected %d but got %d\n", expected, actual);
+			printf("[Test] %s\n", error);
+			Game->Exit();
+		}
+	}
+
 	void Assert<T>(T actual)
 	{
 		if (!actual)