ZQuestClassic/ZQuestClassic

commit 7908ad4b85394898e1e28aa30ba2e21b186ad16c

refactor(zscript)!: allow template argument lists, better template compilation

src/parser/ScriptParser.cpp
@@ -455,11 +455,17 @@ unique_ptr<IntermediateData> ScriptParser::generateOCode(FunctionData& fdata)
 	vector<Function*> funs = program.getUserFunctions();
 	appendElements(funs, program.getUserClassConstructors());
 	appendElements(funs, program.getUserClassDestructors());
-	for (vector<Function*>::iterator it = funs.begin();
-	     it != funs.end(); ++it)
+	for (vector<Function*>::iterator it = funs.begin(); it != funs.end(); ++it)
 	{
 		Function& function = **it;
-		if(function.is_aliased()) continue;
+		if(function.is_aliased())
+			continue;
+		if(function.isTemplateSkip())
+		{
+			for(auto& fun : function.get_applied_funcs())
+				funs.push_back(fun.get());
+			continue;
+		}
 		bool classfunc = function.getFlag(FUNCFLAG_CLASSFUNC) && !function.getFlag(FUNCFLAG_STATIC);
 		int puc = 0;
 		if(classfunc)
@@ -870,6 +876,12 @@ vector<shared_ptr<Opcode>> ScriptParser::assembleOne(Program& program,
 		Function& function = **it;
 		if(function.is_aliased())
 			continue;
+		if(function.isTemplateSkip())
+		{
+			for(auto& applied : function.get_applied_funcs())
+				allFunctions.push_back(applied.get());
+			continue;
+		}
 		functionsByLabel[function.getLabel()] = &function;
 		if(function.getFlag(FUNCFLAG_CONSTRUCTOR))
 			functionsByLabel[function.getAltLabel()] = &function;

resources/include/bindings/global.zh
@@ -293,7 +293,7 @@ internal void ClearTile(int tile);
 // @zasm
 //   POP D2
 //   ARRAYSIZE D2
-internal int SizeOfArray(T[] arr);
+internal int SizeOfArray<T>(T[] arr);
 
 // Resizes the array 'arr' to size 'size'. If 'size' is <1, the array becomes
 // size 1 instead.
@@ -302,21 +302,21 @@ internal int SizeOfArray(T[] arr);
 //   POP D3
 //   POP D2
 //   RESIZEARRAYR D2 D3
-internal void ResizeArray(T[] arr, int size);
+internal void ResizeArray<T>(T[] arr, int size);
 
 // Grants ownership of the target array to the currently running script.
 //
 // @zasm
 //   POP D2
 //   OWNARRAYR D2
-internal void OwnArray(T[] arr);
+internal void OwnArray<T>(T[] arr);
 
 // If [arr] is a local array, destroys it immediately. Otherwise does nothing.
 //
 // @zasm
 //   POP D2
 //   DESTROYARRAYR D2
-internal void DestroyArray(T[] arr);
+internal void DestroyArray<T>(T[] arr);
 
 // Takes an Object parameter. Grants ownership of the object to the currently
 // running script.
@@ -347,35 +347,35 @@ internal void OverlayTile(int src, int dest);
 // @zasm
 //   POP D2
 //   FLOOR D2
-internal constexpr T Floor(T val);
+internal constexpr T Floor<T>(T val);
 
 // Returns [val] rounded up to the next higher integer.
 //
 // @zasm
 //   POP D2
 //   CEILING D2
-internal constexpr T Ceiling(T val);
+internal constexpr T Ceiling<T>(T val);
 
 // Truncates [n] to the nearest integer, rounding towards 0.
 //
 // @zasm
 //   POP D2
 //   TRUNCATE D2
-internal constexpr T Truncate(T n);
+internal constexpr T Truncate<T>(T n);
 
 // Truncates [n] to the nearest integer.
 //
 // @zasm
 //   POP D2
 //   ROUND D2
-internal constexpr T Round(T n);
+internal constexpr T Round<T>(T n);
 
 // Truncates [n] to the nearest integer, rounding away from 0.
 //
 // @zasm
 //   POP D2
 //   ROUNDAWAY D2
-internal constexpr T RoundAway(T n);
+internal constexpr T RoundAway<T>(T n);
 
 // Returns information from the real-time clock of the system.
 //
@@ -488,7 +488,7 @@ internal void itoacat(char32[] dest, int val);
 //   POP D3
 //   POP D2
 //   ARRAYCOPY D2 D3
-internal void ArrayCopy(T[] dest, T[] src);
+internal void ArrayCopy<T>(T[] dest, T[] src);
 
 // Returns the length of the string in characters.
 //
@@ -635,7 +635,7 @@ internal int strrchr(char32[] str, int c);
 // @zasm
 //   POP D2
 //   ISVALIDARRAY D2
-internal bool IsValidArray(T[] arr);
+internal bool IsValidArray<T>(T[] arr);
 
 // Converts Degrees to Radians. More accurate than the old std_functions
 // DegtoRad.
@@ -715,7 +715,7 @@ internal int sprintf(char32[] buf, char32[] format);
 // @zasm
 //   PRINTFA
 //   POPARGS D5 0.0002
-internal void printfa(char32[] format, T[] args);
+internal void printfa<T>(char32[] format, T[] args);
 
 // Acts exactly as output console, it is instead placed in the [dest] buffer. If
 // the [dest] buffer is too small, it will be resized to fit the contents.
@@ -723,28 +723,28 @@ internal void printfa(char32[] format, T[] args);
 // @zasm
 //   SPRINTFA
 //   POPARGS D5 0.0003
-internal int sprintfa(char32[] buf, char32[] format, T[] args);
+internal int sprintfa<T>(char32[] buf, char32[] format, T[] args);
 
 // Returns the largest parameter.
 //
 // @vargs 2
 // @zasm
 //   MAXVARG
-internal constexpr T Max(T val1, T val2);
+internal constexpr T Max<T>(T val1, T val2);
 
 // Returns the smallest parameter.
 //
 // @vargs 2
 // @zasm
 //   MINVARG
-internal constexpr T Min(T val1, T val2);
+internal constexpr T Min<T>(T val1, T val2);
 
 // Returns a random parameter, using the global RandGen.
 //
 // @vargs 1
 // @zasm
 //   CHOOSEVARG
-internal T Choose(T arg1);
+internal T Choose<T>(T arg1);
 
 // Increases the size of the array by 1, inserting [val] at the
 // back of the array.
@@ -753,7 +753,7 @@ internal T Choose(T arg1);
 //   PUSHV -1
 //   ARRAYPUSH
 //   POPARGS D5 0.0003
-internal bool ArrayPushBack(T[] arr, T val);
+internal bool ArrayPushBack<T>(T[] arr, T val);
 
 // Increases the size of the array by 1, inserting [val] at the
 // front of the array.
@@ -762,7 +762,7 @@ internal bool ArrayPushBack(T[] arr, T val);
 //   PUSHV 0
 //   ARRAYPUSH
 //   POPARGS D5 0.0003
-internal bool ArrayPushFront(T[] arr, T val);
+internal bool ArrayPushFront<T>(T[] arr, T val);
 
 // Increases the size of the array by 1, inserting [val] at the
 // specified index of the array. If an invalid index is passed, the
@@ -771,7 +771,7 @@ internal bool ArrayPushFront(T[] arr, T val);
 // @zasm
 //   ARRAYPUSH
 //   POPARGS D5 0.0003
-internal bool ArrayPushAt(T[] arr, T val, int indx);
+internal bool ArrayPushAt<T>(T[] arr, T val, int indx);
 
 // Decreases the size of the array by 1, removing and returning the element at
 // the back of the array.
@@ -780,7 +780,7 @@ internal bool ArrayPushAt(T[] arr, T val, int indx);
 //   PUSHV -1
 //   ARRAYPOP
 //   POPARGS D5 0.0002
-internal T ArrayPopBack(T[] arr);
+internal T ArrayPopBack<T>(T[] arr);
 
 // Decreases the size of the array by 1, removing and returning the element at
 // the front of the array.
@@ -789,7 +789,7 @@ internal T ArrayPopBack(T[] arr);
 //   PUSHV 0
 //   ARRAYPOP
 //   POPARGS D5 0.0002
-internal T ArrayPopFront(T[] arr);
+internal T ArrayPopFront<T>(T[] arr);
 
 // Decreases the size of the array by 1, removing and returning the element at
 // the specified index of the array. If an invalid index is passed,
@@ -798,7 +798,7 @@ internal T ArrayPopFront(T[] arr);
 // @zasm
 //   ARRAYPOP
 //   POPARGS D5 0.0002
-internal T ArrayPopAt(T[] arr, int indx);
+internal T ArrayPopAt<T>(T[] arr, int indx);
 
 // @zasm
 //   POP D3

src/parser/AST.cpp
@@ -669,6 +669,21 @@ void ASTString::execute(ASTVisitor& visitor, void* param)
 	visitor.caseString(*this, param);
 }
 
+// ASTStringList
+
+ASTStringList::ASTStringList(LocationData const& location)
+	: AST(location) {}
+ASTStringList::ASTStringList(std::string comment, LocationData const& location)
+	: AST(location)
+{
+	doc_comment = comment;
+}
+void ASTStringList::execute(ASTVisitor& visitor, void* param)
+{
+	for(auto& ptr : strings)
+		ptr->execute(visitor,param);
+}
+
 // ASTAnnotation
 
 ASTAnnotation::ASTAnnotation(ASTString* key, ASTString* strval, LocationData const& location)

src/parser/AST.h
@@ -50,6 +50,7 @@ namespace ZScript
 	class ASTFile;
 	class ASTFloat;
 	class ASTString;
+	class ASTStringList;
 	class ASTAnnotation;
 	class ASTAnnotationList;
 	class ASTSetOption;
@@ -388,6 +389,19 @@ namespace ZScript
 		const std::string& getValue() const {return str;}
 	private:
 		std::string str;
+	};
+	
+	class ASTStringList : public AST
+	{
+	public:
+		ASTStringList(LocationData const& location = LOC_NONE);
+		ASTStringList(std::string comment,
+		          LocationData const& location);
+		ASTStringList* clone() const {return new ASTStringList(*this);}
+	
+		void execute(ASTVisitor& visitor, void* param = NULL);
+		
+		owning_vector<ASTString> strings;
 	};
 
 	class ASTAnnotation : public AST
@@ -989,9 +1003,12 @@ namespace ZScript
 		owning_ptr<ASTExprIdentifier> identifier;
 		owning_ptr<ASTDataType> returnType;
 		owning_vector<ASTDataDecl> parameters;
+		owning_vector<ASTDataDecl> param_template;
 		owning_vector<ASTExprConst> optparams;
+		owning_vector<ASTString> templates;
 		std::vector<int32_t> optvals;
 		owning_ptr<ASTBlock> block;
+		std::vector<std::shared_ptr<DataTypeTemplate>> template_types;
 		std::string invalidMsg;
 		Function* func;
 		Scope* parentScope;

src/parser/ASTVisitors.cpp
@@ -139,19 +139,32 @@ void RecursiveVisitor::visit(AST* node, void* param)
 	if (node) visit(*node, param);
 }
 
-void RecursiveVisitor::visitFunctionInternals(ZScript::Program& program)
+void RecursiveVisitor::_visit_internals(Function& func)
 {
-	vector<Function*> functions = program.getUserGlobalFunctions();
-
-	for (vector<Function*>::iterator it = functions.begin();
-	     it != functions.end(); ++it)
+	if(func.isTemplateSkip())
 	{
-		Function& func = **it;
+		auto& vec = func.get_applied_funcs();
+		auto& sz = template_function_data[&func];
+		if(sz >= vec.size())
+			return;
+		for(size_t q = sz; q < vec.size(); ++q)
+			template_function_queue.insert(vec[q].get());
+		sz = vec.size();
+	}
+	else
+	{
 		Scope* oldscope = scope;
 		scope = func.getInternalScope();
 		analyzeFunctionInternals(func);
 		scope = oldscope;
 	}
+}
+void RecursiveVisitor::visitFunctionInternals(ZScript::Program& program)
+{
+	vector<Function*> functions = program.getUserGlobalFunctions();
+	
+	for (auto& func : functions)
+		_visit_internals(*func);
 	
 	for (vector<Script*>::iterator it = program.scripts.begin();
 		 it != program.scripts.end(); ++it)
@@ -159,15 +172,8 @@ void RecursiveVisitor::visitFunctionInternals(ZScript::Program& program)
 		Script& script = **it;
 		scope = &script.getScope();
 		functions = scope->getLocalFunctions();
-		for (vector<Function*>::iterator it = functions.begin();
-		     it != functions.end(); ++it)
-		{
-			Function& func = **it;
-			Scope* oldscope = scope;
-			scope = func.getInternalScope();
-			analyzeFunctionInternals(func);
-			scope = oldscope;
-		}
+		for (auto& func : functions)
+			_visit_internals(*func);
 		scope = scope->getParent();
 	}
 	
@@ -177,15 +183,8 @@ void RecursiveVisitor::visitFunctionInternals(ZScript::Program& program)
 		Namespace& namesp = **it;
 		scope = &namesp.getScope();
 		functions = scope->getLocalFunctions();
-		for (vector<Function*>::iterator it = functions.begin();
-		     it != functions.end(); ++it)
-		{
-			Function& func = **it;
-			Scope* oldscope = scope;
-			scope = func.getInternalScope();
-			analyzeFunctionInternals(func);
-			scope = oldscope;
-		}
+		for (auto& func : functions)
+			_visit_internals(*func);
 		scope = scope->getParent();
 	}
 	
@@ -201,45 +200,41 @@ void RecursiveVisitor::visitFunctionInternals(ZScript::Program& program)
 
 		functions = cscope->getConstructors();
 		parsing_user_class = puc_construct;
-		for (vector<Function*>::iterator it = functions.begin();
-		     it != functions.end(); ++it)
-		{
-			Function& func = **it;
-			Scope* oldscope = scope;
-			scope = func.getInternalScope();
-			analyzeFunctionInternals(func);
-			scope = oldscope;
-		}
+		for (auto& func : functions)
+			_visit_internals(*func);
 		
 		functions = scope->getLocalFunctions();
-		for (vector<Function*>::iterator it = functions.begin();
-		     it != functions.end(); ++it)
+		for (auto& func : functions)
 		{
-			Function& func = **it;
-			if(func.getFlag(FUNCFLAG_STATIC))
+			if(func->getFlag(FUNCFLAG_STATIC))
 				parsing_user_class = puc_none;
 			else
 				parsing_user_class = puc_funcs;
-			Scope* oldscope = scope;
-			scope = func.getInternalScope();
-			analyzeFunctionInternals(func);
-			scope = oldscope;
+			_visit_internals(*func);
 		}
 		
 		functions = cscope->getDestructor();
 		parsing_user_class = puc_destruct;
-		for (vector<Function*>::iterator it = functions.begin();
-		     it != functions.end(); ++it)
-		{
-			Function& func = **it;
-			Scope* oldscope = scope;
-			scope = func.getInternalScope();
-			analyzeFunctionInternals(func);
-			scope = oldscope;
-		}
+		for (auto& func : functions)
+			_visit_internals(*func);
 		parsing_user_class = puc_none;
 		
 		scope = scope->getParent();
+	}
+
+
+	for (auto& pair : template_function_data)
+		_visit_internals(*pair.first);
+	while(!template_function_queue.empty())
+	{
+		while(!template_function_queue.empty())
+		{
+			Function* func = *template_function_queue.begin();
+			_visit_internals(*func);
+			template_function_queue.erase(template_function_queue.begin());
+		}
+		for (auto& pair : template_function_data)
+			_visit_internals(*pair.first);
 	}
 }
 

src/parser/ASTVisitors.h
@@ -390,6 +390,19 @@ namespace ZScript
 		
 		// Set to true if a hard error occurs (Halting)
 		bool failure_halt;
+	private:
+		void _visit_internals(Function& func);
+		map<Function*,size_t> template_function_data;
+		std::set<Function*> template_function_queue;
+	};
+	
+	struct ScopeReverter
+	{
+		ScopeReverter(Scope** ptr) : ptr(ptr), val(*ptr) {}
+		~ScopeReverter() {*ptr = val;}
+	private:
+		Scope** ptr;
+		Scope* val;
 	};
 }
 

src/parser/LibrarySymbols.cpp
@@ -358,9 +358,9 @@ bool setConstExprForBinding(Function* fn)
 {
 	std::map<std::string, CONSTEXPR_CBACK_TY> *impl_map = nullptr;
 
-	if (fn->getInternalScope()->getParent()->isClass())
+	if (fn->getExternalScope()->getParent()->isClass())
 	{
-		std::string class_name = fn->getInternalScope()->getParent()->getClass()->getName().value_or("");
+		std::string class_name = fn->getExternalScope()->getParent()->getClass()->getName().value_or("");
 		if (class_name == "screendata")
 			impl_map = &const_expr_screen_impl;
 		else

src/parser/MetadataVisitor.cpp
@@ -430,7 +430,12 @@ void MetadataVisitor::caseFuncDecl(ASTFuncDecl& host, void* param)
 {
 	if (host.prototype)
 		return;
-
+	if (host.func->isTemplateSkip())
+	{
+		for(auto& applied : host.func->get_applied_funcs())
+			visit(applied->getNode(), param);
+		return;
+	}
 	auto prev_active = active;
 	auto kind = host.getFlag(FUNCFLAG_CONSTRUCTOR) ? SymbolKind::Constructor : SymbolKind::Function;
 	appendDocSymbol(kind, host);

commit cd6acf2e683dfc558d7c6e05881d20df572a0470

fix(zscript)!: prevent crash from invalid iterator during script assembly
Regressed 6 months ago in 7908ad4

src/parser/ScriptParser.cpp
@@ -923,10 +923,9 @@ vector<shared_ptr<Opcode>> ScriptParser::assembleOne(Program& program,
 	appendElements(allFunctions, program.getUserClassConstructors());
 	appendElements(allFunctions, program.getUserClassDestructors());
 	map<int32_t, Function*> functionsByLabel;
-	for (vector<Function*>::iterator it = allFunctions.begin();
-	     it != allFunctions.end(); ++it)
+	for (size_t i = 0; i < allFunctions.size(); i++)
 	{
-		Function& function = **it;
+		Function& function = *allFunctions[i];
 		if(function.is_aliased())
 			continue;
 		if(function.isTemplateSkip())