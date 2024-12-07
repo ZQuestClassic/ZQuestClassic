52b70261f51359612f3017ed5935882fa140ad48
Emily Venezia <emilygamergirl99@gmail.com>
2024-12-07 02:13:06 -0500

    fix(zq): `Mouse Scroll` setting not working on 4th combo column

--- src/zq/zquest.cpp
+++ src/zq/zquest.cpp
@@ -9774,7 +9774,7 @@ void domouse()
 	{
 		int32_t test_list=0;
 		
-		for(test_list=0; test_list<3; ++test_list)
+		for(test_list=0; test_list<num_combo_cols; ++test_list)
 		{
 			if((x>=combolist[test_list].x) && (x<combolist[test_list].x+(combolist[test_list].xscale*combolist[test_list].w)))
 			{
@@ -9782,7 +9782,7 @@ void domouse()
 			}
 		}
 		
-		if(test_list<3)
+		if(test_list<num_combo_cols)
 		{
 			if(y>=combolist[test_list].y-mouse_scroll_h && y<=combolist[test_list].y && First[test_list])
 			{
