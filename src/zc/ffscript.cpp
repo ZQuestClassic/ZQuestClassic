4e80d6e3e3d6d185e5f9b20162f9a0bd93ed218f
Connor Clark <cjamcl@gmail.com>
2024-12-07 17:38:30 -0800

    fix(zscript)!: correctly handle GAMEEXIT for jit

--- src/zc/ffscript.cpp
+++ src/zc/ffscript.cpp
@@ -42839,6 +42839,7 @@ bool command_could_return_not_ok(int command)
 	case EWPNDEL:
 	case GAMECONTINUE:
 	case GAMEEND:
+	case GAMEEXIT:
 	case GAMERELOAD:
 	case GAMESAVECONTINUE:
 	case GAMESAVEQUIT:
