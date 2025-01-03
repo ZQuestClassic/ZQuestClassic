4028184ffd2dfdab5546f66d986a42c3d363e800
Emily Venezia <emilygamergirl99@gmail.com>
2025-01-02 23:04:16 -0500

    feat(zq): 'Drag Squares From Center' option, for FFCs/warp squares/etc

--- src/zq/zquest.h
+++ src/zq/zquest.h
@@ -153,7 +153,8 @@ extern bool zoomed_minimap;
 extern int32_t alignment_arrow_timer;
 extern int32_t  Flip,Combo,CSet;
 extern int32_t  Flags,Flag,menutype;
-extern int32_t MouseScroll, SavePaths, CycleOn, NoScreenPreview,WarnOnInitChanged,DisableLPalShortcuts,DisableCompileConsole,skipLayerWarning,numericalFlags;
+extern int32_t MouseScroll, SavePaths, CycleOn, NoScreenPreview,WarnOnInitChanged,
+	DisableLPalShortcuts,DisableCompileConsole,skipLayerWarning,numericalFlags,DragCenterOfSquares;
 extern uint8_t InvalidBG;
 extern int32_t Frameskip, RequestedFPS, zqColorDepth, zqUseWin32Proc;
 extern bool ShowFPS, SaveDragResize, DragAspect, SaveWinPos;
