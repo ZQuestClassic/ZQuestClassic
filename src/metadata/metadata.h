d1d4fc3a9befb21b0870b9c5a319d601100722c6
Connor Clark <cjamcl@gmail.com>
2025-02-27 12:52:44 -0800

    build!: fix windows build re version macros in rc files

--- src/metadata/metadata.h
+++ src/metadata/metadata.h
@@ -5,3 +5,9 @@
 #define COMPILER_V_FOURTH 0
 #define COMPILER_NAME "Unknown"
 #define COMPILER_VERSION "Unknown"
+
+// Do NOT use or edit these. These are only used for .rc files. They get updated automatically in build.yml.
+#define V_ZC_FIRST 3
+#define V_ZC_SECOND 0
+#define V_ZC_THIRD 0
+#define V_ZC_FOURTH 0
