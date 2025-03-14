ZQuestClassic/ZQuestClassic

commit d2dd3eebf57ea30251fa3dd059baa8ed7dd5dbd3

feat(zscript): support expression in default parameters
The default for optional parameters no longer need to be constant. For
example, this is now valid:

```
void debug(int num, int x = Player->X, int y = Player->Y - 10) {
	// draw num at x, y ...
}
```

src/parser/SemanticAnalyzer.cpp
@@ -981,11 +981,8 @@ void SemanticAnalyzer::caseFuncDecl(ASTFuncDecl& host, void* param)
 	{
 		DataType const* getType = (*it)->getReadType(scope, this);
 		if(getType)
-			checkCast(*getType, *paramTypes[parcnt], &host);
+			checkCast(*getType, *paramTypes[parcnt], *it);
 		if(breakRecursion(host)) return;
-		std::optional<int32_t> optVal = (*it)->getCompileTimeValue(this, scope);
-		assert(optVal);
-		host.optvals.push_back(*optVal);
 	}
 	if(breakRecursion(host)) return;
 
@@ -1763,6 +1760,25 @@ void SemanticAnalyzer::caseExprCall(ASTExprCall& host, void* param)
 	deprecWarn(host.binding, &host, "Function", host.binding->getUnaliasedSignature().asString());
 	if(host.binding->getFlag(FUNCFLAG_READ_ONLY))
 		handleError(CompileError::ReadOnly(&host, host.binding->getUnaliasedSignature().asString()));
+
+	auto parcnt = host.binding->paramTypes.size() - host.binding->numOptionalParams;
+	auto& optparams = host.binding->node->optparams;
+
+	for (int i = host.parameters.size(); i < host.binding->paramTypes.size(); i++)
+	{
+		auto* p = optparams[i - parcnt];
+		DataType const* getType = p->getReadType(scope, this);
+		if(getType)
+			checkCast(*getType, *host.binding->paramTypes[parcnt], &host);
+		if(breakRecursion(host)) return;
+
+		auto* new_node = p->clone();
+		if (auto p = dynamic_cast<ASTExprCall*>(new_node))
+			p->binding = nullptr;
+		host.parameters.push_back(new_node);
+		visit(new_node, param);
+		if(breakRecursion(host)) return;
+	}
 }
 
 void SemanticAnalyzer::caseExprNegate(ASTExprNegate& host, void*)

src/parser/AST.h
@@ -1009,9 +1009,8 @@ namespace ZScript
 		owning_ptr<ASTDataType> returnType;
 		owning_vector<ASTDataDecl> parameters;
 		owning_vector<ASTDataDecl> param_template;
-		owning_vector<ASTExprConst> optparams;
+		owning_vector<ASTExpr> optparams;
 		owning_vector<ASTString> templates;
-		std::vector<int32_t> optvals;
 		owning_ptr<ASTBlock> block;
 		std::vector<std::shared_ptr<DataTypeTemplate>> template_types;
 		std::string invalidMsg;
@@ -1884,9 +1883,11 @@ namespace ZScript
 		{
 			auto leftType = left->getReadType(scope, errorHandler);
 			auto rightType = right->getReadType(scope, errorHandler);
-			if (leftType->isBitflagsEnum() || rightType->isBitflagsEnum())
+			if (!leftType)
+				left->getReadType(scope, errorHandler);
+			if ((leftType && leftType->isBitflagsEnum()) || rightType->isBitflagsEnum())
 				return leftType;
-			if (leftType->isLong() || rightType->isLong())
+			if ((leftType && leftType->isLong()) || rightType->isLong())
 				return &DataType::LONG;
 			return &DataType::FLOAT;
 		}

src/parser/BuildVisitors.cpp
@@ -2017,13 +2017,10 @@ void BuildOpcodes::caseExprCall(ASTExprCall& host, void* param)
 				addOpcode(new OPushRegister(new VarArgument(EXP1)));
 			}
 		}
-		
-		bool vargs = func.getFlag(FUNCFLAG_VARARGS);
+
 		int v = num_used_params-num_actual_params;
-		
-		size_t vargcount = 0;
-		size_t used_opt_params = 0;
-		(v>0 ? vargcount : used_opt_params) = abs(v);
+
+		size_t vargcount = v;
 		//push the parameters, in forward order
 		size_t param_indx = 0;
 		for (; param_indx < host.parameters.size()-vargcount; ++param_indx)
@@ -2060,16 +2057,6 @@ void BuildOpcodes::caseExprCall(ASTExprCall& host, void* param)
 					VISIT_USEVAL(arg, INITCTX);
 					push_param(true);
 				}
-			}
-		}
-		else if(used_opt_params)
-		{
-			auto opt_param_count = func.opt_vals.size();
-			auto skipped_optional_params = opt_param_count - used_opt_params;
-			//push any optional parameter values
-			for(auto q = skipped_optional_params; q < opt_param_count; ++q)
-			{
-				addOpcode(new OPushImmediate(new LiteralArgument(func.opt_vals[q])));
 			}
 		}
 
@@ -2149,10 +2136,8 @@ void BuildOpcodes::caseExprCall(ASTExprCall& host, void* param)
 		bool vargs = func.getFlag(FUNCFLAG_VARARGS);
 		auto num_used_params = host.parameters.size();
 		auto num_actual_params = func.paramTypes.size() - (vargs ? 1 : 0);
-		size_t vargcount = 0;
-		size_t used_opt_params = 0;
 		int v = num_used_params-num_actual_params;
-		(v>0 ? vargcount : used_opt_params) = abs(v);
+		size_t vargcount = v > 0 ? v : 0;
 		size_t pushcount = 0;
 		if(vargs)
 		{
@@ -2216,18 +2201,7 @@ void BuildOpcodes::caseExprCall(ASTExprCall& host, void* param)
 			commentBack("Peek the Vargs array pointer");
 			addOpcode(new OPushRegister(new VarArgument(EXP1)));
 		}
-		else if(used_opt_params)
-		{
-			//push any optional parameter values
-			auto num_actual_params = func.paramTypes.size();
-			auto used_opt_params = num_actual_params - host.parameters.size();
-			auto opt_param_count = func.opt_vals.size();
-			auto skipped_optional_params = opt_param_count - used_opt_params;
-			for(auto q = skipped_optional_params; q < opt_param_count; ++q)
-			{
-				addOpcode(new OPushImmediate(new LiteralArgument(func.opt_vals[q])));
-			}
-		}
+
 		if (host.left->isTypeArrow())
 		{
 			//load the value of the left-hand of the arrow into EXP1
@@ -2278,15 +2252,15 @@ void BuildOpcodes::caseExprCall(ASTExprCall& host, void* param)
 		bool vargs = func.getFlag(FUNCFLAG_VARARGS);
 		bool user_vargs = vargs && !func.isInternal();
 		auto num_used_params = host.parameters.size();
-		auto num_actual_params = func.paramTypes.size() - (user_vargs ? 1 : 0);
+		auto num_actual_params = host.parameters.size() - (user_vargs ? 1 : 0);
 		if(host.left->isTypeArrow())
 			--num_actual_params; //Don't count the arrow param!
 		size_t vargcount = 0;
 		size_t used_opt_params = 0;
 		int v = num_used_params-num_actual_params;
 		(v>0 ? vargcount : used_opt_params) = abs(v);
-		size_t pushcount = 0;
-		
+		size_t pushcount = num_used_params;
+
 		if(user_vargs)
 		{
 			//push the vargs, in forward order
@@ -2332,7 +2306,7 @@ void BuildOpcodes::caseExprCall(ASTExprCall& host, void* param)
 		for (; param_indx < num_used_params-vargcount; ++param_indx)
 		{
 			auto& arg = host.parameters.at(param_indx);
-			bool unused = !func.isInternal() && !func.getFlag(FUNCFLAG_INTERNAL) && func.paramDatum[param_indx]->is_erased();
+			bool unused = !func.isInternal() && !func.getFlag(FUNCFLAG_INTERNAL) && func.paramDatum.size() > param_indx && func.paramDatum[param_indx]->is_erased();
 			//Compile-time constants can be optimized slightly...
 			if(auto val = arg->getCompileTimeValue(this, scope))
 			{
@@ -2349,7 +2323,8 @@ void BuildOpcodes::caseExprCall(ASTExprCall& host, void* param)
 				push_param();
 			}
 		}
-		pushcount += (num_used_params-vargcount);
+		
+		// pushcount += (num_used_params-vargcount);
 		if(vargs && (vargcount || user_vargs))
 		{
 			if(user_vargs)
@@ -2377,16 +2352,7 @@ void BuildOpcodes::caseExprCall(ASTExprCall& host, void* param)
 				}
 			}
 		}
-		else if(used_opt_params)
-		{
-			//push any optional parameter values
-			auto opt_param_count = func.opt_vals.size();
-			auto skipped_optional_params = opt_param_count - used_opt_params;
-			for(auto q = skipped_optional_params; q < opt_param_count; ++q)
-			{
-				addOpcode(new OPushImmediate(new LiteralArgument(func.opt_vals[q])));
-			}
-		}
+
 		commentStartEnd(targ_sz, fmt::format("{}{} Params",comment_pref,func_comment));
 		//goto
 		addOpcode(new OCallFunc(new LabelArgument(funclabel, true)));

src/parser/DocVisitor.cpp
@@ -240,10 +240,10 @@ void DocVisitor::caseFuncDecl(ASTFuncDecl& host, void* param)
 		};
 		(*symbol)["parameters"].push_back(std::move(j_param));
 	}
-	for (int i = 0; i < host.optvals.size(); i++)
+	for (int i = 0; i < host.optparams.size(); i++)
 	{
 		std::string snippet = getSourceCodeSnippet(host.optparams[i]->location);
-		(*symbol)["parameters"][host.parameters.size() - host.optvals.size() + i]["default"] = (*symbol)["value"] = snippet;
+		(*symbol)["parameters"][host.parameters.size() - host.optparams.size() + i]["default"] = (*symbol)["value"] = snippet;
 	}
 	if (host.getFlag(FUNCFLAG_VARARGS))
 		(*symbol)["varargs"] = host.func->extra_vargs;

src/parser/RegistrationVisitor.cpp
@@ -1118,18 +1118,9 @@ void RegistrationVisitor::caseFuncDecl(ASTFuncDecl& host, void* param)
 	for(auto it = host.optparams.begin(); it != host.optparams.end() && parcnt < paramTypes.size(); ++it, ++parcnt)
 	{
 		DataType const* getType = (*it)->getReadType(scope, this);
-		if(!getType) return;
-		checkCast(*getType, *paramTypes[parcnt], &host);
+		if(getType)
+			checkCast(*getType, *paramTypes[parcnt], *it);
 		if(breakRecursion(host)) return;
-		std::optional<int32_t> optVal = (*it)->getCompileTimeValue(this, scope);
-		if (!optVal)
-		{
-			handleError(CompileError::Error(*it, fmt::format("Function '{}' has an optional parameter whose default value is not a constant expression", host.getName())));
-			doRegister(host);
-			return;
-		}
-
-		host.optvals.push_back(*optVal);
 	}
 	if(breakRecursion(host)) return;
 	
@@ -1533,9 +1524,7 @@ void RegistrationVisitor::caseExprCall(ASTExprCall& host, void* param)
 	deprecWarn(host.binding, &host, "Function", host.binding->getUnaliasedSignature().asString());
 	if(host.binding->getFlag(FUNCFLAG_READ_ONLY))
 		handleError(CompileError::ReadOnly(&host, host.binding->getUnaliasedSignature().asString()));
-	
-	if(!host.binding->get_constexpr())
-		handleError(CompileError::GlobalVarFuncCall(&host));
+
 	doRegister(host);
 }
 

src/parser/Scope.cpp
@@ -925,7 +925,7 @@ inline void ZScript::trimBadFunctions(std::vector<Function*>& functions, std::ve
 		
 		auto targetSize = parameterTypes.size();
 		auto maxSize = function->paramTypes.size() - (user_vargs ? 1 : 0);
-		auto minSize = maxSize - function->opt_vals.size();
+		auto minSize = maxSize - function->numOptionalParams;
 		// Match against parameter count, including optional params.
 		if (minSize > targetSize || (!vargs && maxSize < targetSize))
 		{
@@ -1667,10 +1667,7 @@ Function* BasicScope::addFunction(
 	fun->setInternalScope(subscope->makeFunctionChild(*fun));
 	if(node)
 	{
-		for(auto it = node->optvals.begin(); it != node->optvals.end(); ++it)
-		{
-			fun->opt_vals.push_back(*it);
-		}
+		fun->numOptionalParams = node->optparams.size();
 	}
 	if (flags&FUNCFLAG_INTERNAL)
 		initFunctionBinding(fun, handler);
@@ -2451,10 +2448,7 @@ Function* ClassScope::addFunction(
 	fun->setInternalScope(subscope->makeFunctionChild(*fun));
 	if(node)
 	{
-		for(auto it = node->optvals.begin(); it != node->optvals.end(); ++it)
-		{
-			fun->opt_vals.push_back(*it);
-		}
+		fun->numOptionalParams = node->optparams.size();
 	}
 	if (flags&FUNCFLAG_INTERNAL)
 	{

src/parser/ZScript.cpp
@@ -571,7 +571,7 @@ Function::Function(DataType const* returnType, string const& name,
 				   vector<DataType const*> paramTypes, vector<shared_ptr<const string>> paramNames, int32_t id,
 				   int32_t flags, int32_t internal_flags, bool prototype, optional<int32_t> defaultReturn)
 	: returnType(returnType), name(name), hasPrefixType(false), isFromTypeTemplate(false),
-	  extra_vargs(0), paramTypes(paramTypes), paramNames(paramNames), opt_vals(), id(id),
+	  extra_vargs(0), paramTypes(paramTypes), paramNames(paramNames), numOptionalParams(), id(id),
 	  node(NULL), data_decl_source_node(NULL), internalScope(NULL), externalScope(NULL), thisVar(NULL),
 	  internal_flags(internal_flags), prototype(prototype),
 	  defaultReturn(defaultReturn), label(std::nullopt), flags(flags),

src/parser/ZScript.h
@@ -463,7 +463,7 @@ namespace ZScript
 		std::vector<Datum*> paramDatum;
 		std::vector<DataType const*> templ_bound_ts;
 		
-		std::vector<int32_t> opt_vals;
+		int32_t numOptionalParams;
 		int32_t id;
 
 		ASTFuncDecl* node;

commit 6176466b18f3d0e5818432be66c4bc633209c475

fix(zscript)!: bad type checking for default parameters
This regressed recently in d2dd3ee.

src/parser/SemanticAnalyzer.cpp
@@ -1769,7 +1769,7 @@ void SemanticAnalyzer::caseExprCall(ASTExprCall& host, void* param)
 		auto* p = optparams[i - parcnt];
 		DataType const* getType = p->getReadType(scope, this);
 		if(getType)
-			checkCast(*getType, *host.binding->paramTypes[parcnt], &host);
+			checkCast(*getType, *host.binding->paramTypes[i], &host);
 		if(breakRecursion(host)) return;
 
 		auto* new_node = p->clone();