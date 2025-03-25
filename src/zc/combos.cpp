ZQuestClassic/ZQuestClassic

commit 3967f0ebfc80c010c5df8b2bd11368ba672e97a1

fix(zc): apply switch state when checking bordering screens
For example, the smart scrolling feature which prevents scrolling when
detecting solid combos on the next screen was not considering the state
of level or global switches.

src/zc/maps.cpp
@@ -1115,16 +1115,18 @@ int32_t MAPCOMBO2(int32_t layer, int32_t x, int32_t y)
 
 static void apply_state_changes_to_screen(mapscr& scr, int32_t map, int32_t screen, int32_t flags, bool secrets_do_replay_comment)
 {
+	auto screen_handles = create_screen_handles_one(&scr);
+
 	if ((flags & mSECRET) && canPermSecret(cur_dmap, screen))
 	{
 		reveal_hidden_stairs(&scr, screen, false);
 		bool do_layers = false;
 		bool from_active_screen = false;
-		trigger_secrets_for_screen_internal(create_screen_handles_one(&scr), do_layers, from_active_screen, -3, secrets_do_replay_comment);
+		trigger_secrets_for_screen_internal(screen_handles, do_layers, from_active_screen, -3, secrets_do_replay_comment);
 	}
 	if (flags & mLIGHTBEAM)
 	{
-		for_every_rpos_in_screen(&scr, [&](const rpos_handle_t& rpos_handle) {
+		for_every_rpos_in_screen(screen_handles, [&](const rpos_handle_t& rpos_handle) {
 			if (rpos_handle.ctype() == cLIGHTTARGET)
 			{
 				if (!(rpos_handle.combo().usrflags&cflag1)) //Unlit version
@@ -1133,7 +1135,9 @@ static void apply_state_changes_to_screen(mapscr& scr, int32_t map, int32_t scre
 		});
 	}
 
-	auto screen_handles = create_screen_handles_one(&scr);
+	int lvl = DMaps[cur_dmap].level;
+	toggle_switches(game->lvlswitches[lvl], true, screen_handles);
+	toggle_gswitches_load(screen_handles);
 
 	if(flags&mLOCKBLOCK)              // if special stuff done before
 	{
@@ -2738,7 +2742,7 @@ void trigger_secrets_for_screen_internal(const screen_handles_t& screen_handles,
 
 	if (from_active_screen)
 	{
-		for_every_combo_in_screen(scr, [&](const auto& handle) {
+		for_every_combo_in_screen(screen_handles, [&](const auto& handle) {
 			if (handle.combo().triggerflags[2] & combotriggerSECRETSTR)
 				do_trigger_combo(handle, ctrigSECRETS);
 		});
@@ -5613,7 +5617,7 @@ void openshutters(mapscr* scr)
 		}
 	
 	auto& combo_cache = combo_caches::shutter;
-	for_every_combo_in_screen(scr, [&](const auto& handle) {
+	for_every_combo_in_screen(create_screen_handles(scr), [&](const auto& handle) {
 		auto& cmb = combo_cache.minis[handle.data()];
 		if (cmb.shutter)
 			do_trigger_combo(handle);
@@ -5796,15 +5800,15 @@ static void load_a_screen_and_layers_post(int dmap, int screen, int ldir)
 		}
 	}
 
+	auto screen_handles = create_screen_handles(base_scr);
+
 	int destlvl = DMaps[dmap].level;
-	toggle_switches(game->lvlswitches[destlvl], true, base_scr);
-	toggle_gswitches_load(base_scr);
+	toggle_switches(game->lvlswitches[destlvl], true, screen_handles);
+	toggle_gswitches_load(screen_handles);
 
 	bool should_check_for_state_things = (screen < 0x80);
 	if (should_check_for_state_things)
 	{
-		auto screen_handles = create_screen_handles(base_scr);
-
 		if (game->maps[mi]&mLOCKBLOCK)
 			remove_lockblocks(screen_handles);
 		if (game->maps[mi]&mBOSSLOCKBLOCK)
@@ -6231,12 +6235,12 @@ void loadscr_old(int32_t destdmap, int32_t screen,int32_t ldir,bool overlay)
 		}
 	}
 	
-	toggle_switches(game->lvlswitches[destlvl], true, special_warp_return_scr);
-	toggle_gswitches_load(special_warp_return_scr);
-
 	screen_handles_t screen_handles;
 	for (int i = 0; i <= 6; i++)
 		screen_handles[i] = {scr, special_warp_return_scrs[i].is_valid() ? &special_warp_return_scrs[i] : nullptr, screen, i};
+
+	toggle_switches(game->lvlswitches[destlvl], true, screen_handles);
+	toggle_gswitches_load(screen_handles);
 
 	if(game->maps[mi]&mLOCKBLOCK)			  // if special stuff done before
 	{
@@ -7051,17 +7055,18 @@ void toggle_switches(dword flags, bool entry)
 	if(!flags) return; //No flags to toggle
 
 	for_every_base_screen_in_region([&](mapscr* scr, unsigned int region_scr_x, unsigned int region_scr_y) {
-		toggle_switches(flags, entry, scr);
+		toggle_switches(flags, entry, create_screen_handles(scr));
 	});
 }
-void toggle_switches(dword flags, bool entry, mapscr* m)
+void toggle_switches(dword flags, bool entry, const screen_handles_t& screen_handles)
 {
 	if(!flags) return; //No flags to toggle
 
+	mapscr* m = screen_handles[0].base_scr;
 	int screen = m->screen;
 	bool is_active_screen = is_in_current_region(m);
 
-	for_every_rpos_in_screen(m, [&](const rpos_handle_t& rpos_handle) {
+	for_every_rpos_in_screen(screen_handles, [&](const rpos_handle_t& rpos_handle) {
 		byte togglegrid[176] = {0};
 		mapscr* scr = rpos_handle.scr;
 		int lyr = rpos_handle.layer;
@@ -7200,26 +7205,27 @@ void toggle_switches(dword flags, bool entry, mapscr* m)
 void toggle_gswitches(int32_t state, bool entry)
 {
 	for_every_base_screen_in_region([&](mapscr* scr, unsigned int region_scr_x, unsigned int region_scr_y) {
-		toggle_gswitches(state, entry, scr);
+		toggle_gswitches(state, entry, create_screen_handles(scr));
 	});
 }
-void toggle_gswitches(int32_t state, bool entry, mapscr* base_scr)
+void toggle_gswitches(int32_t state, bool entry, const screen_handles_t& screen_handles)
 {
 	bool states[256] = {false};
 	states[state] = true;
-	toggle_gswitches(states, entry, base_scr);
+	toggle_gswitches(states, entry, screen_handles);
 }
-void toggle_gswitches(bool* states, bool entry, mapscr* base_scr)
+void toggle_gswitches(bool* states, bool entry, const screen_handles_t& screen_handles)
 {
 	if(!states) return;
 
 	auto& combo_cache = combo_caches::gswitch;
+	mapscr* base_scr = screen_handles[0].base_scr;
 	int screen = base_scr->screen;
-	bool iscurscr = is_in_current_region(base_scr);
+	bool is_active_screen = is_in_current_region(base_scr);
 	byte togglegrid[176] = {0};
 	for(int32_t lyr = 0; lyr <= 6; ++lyr)
 	{
-		mapscr* scr = lyr == 0 ? base_scr : get_scr_layer_valid(screen, lyr);
+		mapscr* scr = screen_handles[lyr].scr;
 		if (!scr)
 			continue;
 
@@ -7228,7 +7234,7 @@ void toggle_gswitches(bool* states, bool entry, mapscr* base_scr)
 			int cid = scr->data[pos];
 			auto& mini_cmb = combo_cache.minis[cid];
 
-			if (iscurscr)
+			if (is_active_screen)
 			{
 				if (mini_cmb.trigger_global_state)
 					if (states[combobuf[cid].trig_gstate])
@@ -7348,7 +7354,7 @@ void toggle_gswitches(bool* states, bool entry, mapscr* base_scr)
 		}
 	}
 	
-	if(iscurscr)
+	if(is_active_screen)
 	{
 		int screen_index_offset = get_region_screen_offset(screen);
 		word c = base_scr->numFFC();
@@ -7363,14 +7369,14 @@ void toggle_gswitches(bool* states, bool entry, mapscr* base_scr)
 		}
 	}
 }
-void toggle_gswitches_load(mapscr* base_scr)
+void toggle_gswitches_load(const screen_handles_t& screen_handles)
 {
 	bool states[256];
 	for(auto q = 0; q < 256; ++q)
 	{
 		states[q] = game->gswitch_timers[q] != 0;
 	}
-	toggle_gswitches(states, true, base_scr);
+	toggle_gswitches(states, true, screen_handles);
 }
 void run_gswitch_timers()
 {
@@ -7388,7 +7394,7 @@ void run_gswitch_timers()
 		++it;
 	}
 	for_every_base_screen_in_region([&](mapscr* scr, unsigned int region_scr_x, unsigned int region_scr_y) {
-		toggle_gswitches(states, false, scr);
+		toggle_gswitches(states, false, create_screen_handles(scr));
 	});
 }
 void onload_gswitch_timers() //Reset all timers that were counting down, no trigger necessary

src/iter.h
@@ -189,11 +189,12 @@ ZC_FORCE_INLINE void for_every_combo(T&& fn, bool include_ffcs = !get_qr(qr_OLD_
 
 template<typename T>
 requires std::is_invocable_v<T, const rpos_handle_t&> && std::is_invocable_v<T, const ffc_handle_t&>
-ZC_FORCE_INLINE void for_every_combo_in_screen(mapscr* scr, T&& fn, bool include_ffcs = !get_qr(qr_OLD_FFC_FUNCTIONALITY))
+ZC_FORCE_INLINE void for_every_combo_in_screen(const screen_handles_t& screen_handles, T&& fn, bool include_ffcs = !get_qr(qr_OLD_FFC_FUNCTIONALITY))
 {
-	for_every_rpos_in_screen(scr, fn);
-	if (include_ffcs)
-		for_every_ffc_in_screen(scr, fn);
+	mapscr* base_scr = screen_handles[0].base_scr;
+	for_every_rpos_in_screen(screen_handles, fn);
+	if (include_ffcs && is_in_current_region(base_scr))
+		for_every_ffc_in_screen(base_scr, fn);
 }
 
 // Iterates over every ffc in the current region, until execution is requested to stop.
@@ -259,35 +260,42 @@ ZC_FORCE_INLINE std::optional<ffc_handle_t> find_ffc(T&& fn)
 // Callback function: void fn(const rpos_handle_t& rpos_handle_t)
 template<typename T>
 requires std::is_invocable_v<T, const rpos_handle_t&>
-ZC_FORCE_INLINE void for_every_rpos_in_screen(mapscr* scr, T&& fn)
+ZC_FORCE_INLINE void for_every_rpos_in_screen(const screen_handles_t& screen_handles, T&& fn)
 {
-	auto [handles, count] = get_current_region_handles(scr);
+	mapscr* base_scr = screen_handles[0].base_scr;
 
-	if (handles != nullptr)
+	if (is_in_current_region(base_scr))
 	{
-		for (int i = 0; i < count; i++)
-		{
-			rpos_handle_t rpos_handle = handles[i];
-			for (int j = 0; j < 176; j++)
-			{
-				fn(rpos_handle);
-				rpos_handle.rpos = (rpos_t)((int)rpos_handle.rpos + 1);
-				rpos_handle.pos += 1;
-			}
-		}
+		auto [handles, count] = get_current_region_handles(base_scr);
 
-		return;
+		if (handles != nullptr)
+		{
+			for (int i = 0; i < count; i++)
+			{
+				rpos_handle_t rpos_handle = handles[i];
+				for (int j = 0; j < 176; j++)
+				{
+					fn(rpos_handle);
+					rpos_handle.rpos = (rpos_t)((int)rpos_handle.rpos + 1);
+					rpos_handle.pos += 1;
+				}
+			}
+
+			return;
+		}
 	}
 
 	// Not in current region.
 
 	rpos_handle_t rpos_handle;
-	int map = scr->map;
-	int screen = scr->screen;
+	int screen = base_scr->screen;
 	rpos_handle.screen = screen;
 	for (int lyr = 0; lyr <= 6; ++lyr)
 	{
-		rpos_handle.scr = lyr == 0 ? scr : get_scr_layer(map, screen, lyr);
+		if (!screen_handles[lyr].scr)
+			continue;
+
+		rpos_handle.scr = screen_handles[lyr].scr;
 		rpos_handle.layer = lyr;
 		for (int pos = 0; pos < 176; ++pos)
 		{

src/zc/guys.cpp
@@ -18423,9 +18423,8 @@ static void activate_fireball_statues(mapscr* scr)
 		return;
 	}
 
-	for_every_rpos_in_screen(scr, [&](const rpos_handle_t& rpos_handle) {
-		if (rpos_handle.layer == 0)
-			activate_fireball_statue(rpos_handle);
+	for_every_rpos_in_screen_layer0(scr, [&](const rpos_handle_t& rpos_handle) {
+		activate_fireball_statue(rpos_handle);
 	});
 }
 

src/zc/hero.cpp
@@ -23740,7 +23740,7 @@ void HeroClass::checkspecial()
 			// Enemies have been defeated.
 
 			// generic 'Enemies->' trigger
-			for_every_combo_in_screen(scr, [&](const auto& handle) {
+			for_every_combo_in_screen(create_screen_handles(scr), [&](const auto& handle) {
 				auto& cmb = handle.combo();	
 				if (cmb.triggerflags[2] & combotriggerENEMIESKILLED)
 				{

src/zc/maps.h
@@ -425,11 +425,11 @@ bool hit_walkflag(int32_t x,int32_t y,int32_t cnt);
 bool solpush_walkflag(int32_t x, int32_t y, int32_t cnt, solid_object const* ign = nullptr);
 void map_bkgsfx(bool on);
 void toggle_switches(dword flags, bool entry);
-void toggle_switches(dword flags, bool entry, mapscr* m);
+void toggle_switches(dword flags, bool entry, const screen_handles_t& screen_handles);
 void toggle_gswitches(int32_t state, bool entry);
-void toggle_gswitches(int32_t state, bool entry, mapscr* base_scr);
-void toggle_gswitches(bool* states, bool entry, mapscr* base_scr);
-void toggle_gswitches_load(mapscr* base_scr);
+void toggle_gswitches(int32_t state, bool entry, const screen_handles_t& screen_handles);
+void toggle_gswitches(bool* states, bool entry, const screen_handles_t& screen_handles);
+void toggle_gswitches_load(const screen_handles_t& screen_handles);
 void run_gswitch_timers();
 void onload_gswitch_timers();
 

tests/replays/playground/playground.qst
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ddd2d5a2293447e34e5abe192ed2afb18b789a6ab06b327598d4bfff6b9b69ee
-size 3175420
+oid sha256:8abae02eb3e0e08a5d321923f32f559f77886e9dde1a0d13f76f868af6aacd84
+size 3175628

tests/replays/playground/switches_smart_scrolling.zplay
@@ -1,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c8465a90b7de14595a35d25919c7851d2648612617e723de363d4da44c46ccef
+size 21025

commit 8dc3f5e7c0756326f8c93c7332f1c8155d21e2ff

fix(zc)!: consider non-active screens in xstatecombos/xdoors
Regressed in 3967f0eb

src/zc/combos.cpp
@@ -2372,7 +2372,7 @@ void do_ex_trigger(const rpos_handle_t& rpos_handle)
 		combo_caches::drawing.refresh(rpos_handle.data());
 	}
 	
-	if(cmb.trigcopycat) //has a copycat set
+	if(cmb.trigcopycat && is_in_current_region(rpos_handle.base_scr())) //has a copycat set
 		trig_copycat(cid, rpos_handle);
 }
 

src/zc/maps.cpp
@@ -2372,46 +2372,32 @@ bool remove_xstatecombos_mi(const screen_handles_t& screen_handles, int32_t mi, 
 
 	mapscr* s = screen_handles[0].scr;
 	int screen = s->screen;
+	bool is_active_screen = is_in_current_region(s);
 
-	rpos_handle_t rpos_handle;
-	rpos_handle.screen = screen;
-	rpos_handle.layer = 0;
-	for (int j = 0; j <= 6; j++)
-	{
-		mapscr* scr = screen_handles[0].scr;
-		if (!scr->is_valid()) continue;
-
-		rpos_handle.scr = scr;
-		rpos_handle.layer = j;
-		
-		for (int32_t i=0; i<176; i++)
+	for_every_rpos_in_screen(screen_handles, [&](const rpos_handle_t& rpos_handle) {
+		if(triggers && force_ex_trigger(rpos_handle, xflag))
+			didit = true;
+		else switch (rpos_handle.ctype())
 		{
-			rpos_handle.rpos = POS_TO_RPOS(i, screen);
-			rpos_handle.pos = i;
-			if(triggers && force_ex_trigger(rpos_handle, xflag))
-				didit = true;
-			else switch (rpos_handle.ctype())
+			case cLOCKBLOCK: case cLOCKBLOCK2:
+			case cBOSSLOCKBLOCK: case cBOSSLOCKBLOCK2:
+			case cCHEST: case cCHEST2:
+			case cLOCKEDCHEST: case cLOCKEDCHEST2:
+			case cBOSSCHEST: case cBOSSCHEST2:
 			{
-				case cLOCKBLOCK: case cLOCKBLOCK2:
-				case cBOSSLOCKBLOCK: case cBOSSLOCKBLOCK2:
-				case cCHEST: case cCHEST2:
-				case cLOCKEDCHEST: case cLOCKEDCHEST2:
-				case cBOSSCHEST: case cBOSSCHEST2:
+				auto& cmb = rpos_handle.combo();
+				if(!(cmb.usrflags&cflag16)) return; //custom state instead of normal state
+				if(cmb.attribytes[5] == xflag)
 				{
-					auto& cmb = rpos_handle.combo();
-					if(!(cmb.usrflags&cflag16)) continue; //custom state instead of normal state
-					if(cmb.attribytes[5] == xflag)
-					{
-						rpos_handle.increment_data();
-						didit=true;
-					}
-					break;
+					rpos_handle.increment_data();
+					didit=true;
 				}
+				break;
 			}
 		}
-	}
+	});
 
-	if (!get_qr(qr_OLD_FFC_FUNCTIONALITY))
+	if (is_active_screen && !get_qr(qr_OLD_FFC_FUNCTIONALITY))
 	{
 		word c = s->numFFC();
 		int screen_index_offset = get_region_screen_offset(screen);
@@ -2472,30 +2458,15 @@ bool remove_xdoors_mi(const screen_handles_t& screen_handles, int32_t mi, uint d
 
 	mapscr* scr = screen_handles[0].scr;
 	int screen = scr->screen;
+	bool is_active_screen = is_in_current_region(scr);
 
-	rpos_handle_t rpos_handle;
-	rpos_handle.screen = screen;
-	rpos_handle.layer = 0;
-	for (int j = 0; j <= 6; j++)
-	{
-		mapscr* scr_2 = screen_handles[j].scr;
-		if (!scr_2) continue;
+	for_every_rpos_in_screen(screen_handles, [&](const rpos_handle_t& rpos_handle) {
+		if (triggers && force_ex_door_trigger(rpos_handle, dir, ind))
+			didit = true;
+		else; //future door combo types?
+	});
 
-		rpos_handle.scr = scr_2;
-		rpos_handle.screen = screen;
-		rpos_handle.layer = j;
-		
-		for (int32_t i=0; i<176; i++)
-		{
-			rpos_handle.rpos = POS_TO_RPOS(i, screen);
-			rpos_handle.pos = i;
-			if(triggers && force_ex_door_trigger(rpos_handle, dir, ind))
-				didit = true;
-			else; //future door combo types?
-		}
-	}
-
-	if (!get_qr(qr_OLD_FFC_FUNCTIONALITY))
+	if (!get_qr(qr_OLD_FFC_FUNCTIONALITY) && is_active_screen)
 	{
 		word c = scr->numFFC();
 		int screen_index_offset = get_region_screen_offset(screen);