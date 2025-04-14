ZQuestClassic/ZQuestClassic

commit aab56590d1eb98857f25c37b8074ddd8e1ea43d6

fix(zq)!: enemy editor defense tab disables when it should, has correct defenses aligned with each label

src/dialog/enemyeditor.cpp
@@ -42,7 +42,7 @@ EnemyEditorDialog::EnemyEditorDialog(guydata const& ref, int32_t index) :
 	//pointer crap
 
 	index(index), local_guyref(ref), enemy_name(guy_string[index]),
-	spawn_type(ref.flags & guy_fade_instant ? 2 : ref.flags & guy_fade_flicker ? 1 : 0),
+	spawn_type(ref.flags& guy_fade_instant ? 2 : ref.flags & guy_fade_flicker ? 1 : 0),
 	death_type(0), //unimplemented for now
 	list_enemies(GUI::ZCListData::enemies(false, true)),
 	list_families(GUI::ZCListData::efamilies()),
@@ -54,20 +54,14 @@ EnemyEditorDialog::EnemyEditorDialog(guydata const& ref, int32_t index) :
 	list_dropsets(GUI::ZCListData::dropsets(false)),
 	list_sprites(GUI::ZCListData::miscsprites()),
 	list_eweaptype(GUI::ZCListData::eweaptypes()),
-	list_defenses(GUI::ZCListData::defenses(wSword, wRefFire2, true).filter(
+	list_defenses(GUI::ZCListData::defenses(edefBRANG, edefLAST255, true).filter(
 		[&](GUI::ListItem& itm)
 		{
-			if (itm.value == -1) return false;
-			if ((itm.value < edefSCRIPT || itm.value > edefSONIC) && itm.value < edefLAST255) return true;
+			if ((itm.value < edefSCRIPT || itm.value > edefSONIC)) return true;
 			else return false;
-		}).alphabetize() + GUI::ZCListData::defenses(wScript1, wScript10, true)),
+		}) + GUI::ZCListData::defenses(edefSCRIPT01, edefSCRIPT10 + 1, true)),
 	list_deftypes(GUI::ZCListData::deftypes())
-{
-	for (auto q = 0; q < edefLAST255; ++q)
-	{
-		local_defense[q] = local_guyref.defense[q];
-	}
-}
+{}
 
 EnemyEditorDialog::EnemyEditorDialog(int32_t index) :
 	EnemyEditorDialog(guysbuf[index], index) 
@@ -746,22 +740,50 @@ inline bool EnemyEditorDialog::NoDefenses()
 	return local_guyref.family == eeROCK || local_guyref.family == eeTRAP || local_guyref.family == eeDONGO || local_guyref.family == eeGANON;
 }
 
-std::shared_ptr<GUI::Widget> EnemyEditorDialog::DefenseField(auto* field, GUI::ListData const& ls)
+std::shared_ptr<GUI::Widget> EnemyEditorDialog::DefenseField(auto* indexs, bool _dobutton)
 {
 	using namespace GUI::Builder;
 	using namespace GUI::Props;
 
-	return DropDownList(
-		disabled = NoDefenses(),
-		data = ls,
-		hAlign = 0.0,
-		selectedValue = *field,
-		fitParent = true,
-		onSelectFunc = [field](int32_t val)
+	std::shared_ptr<GUI::Grid> grid = Rows<3>();
+	for (int32_t q = 0; q < sizeof(indexs)+1; q++)
+	{
+		int32_t index = indexs[q];
+		if (index == -1)
 		{
-			*field = val;
+			grid->add(_d);
+			grid->add(_d);
+			grid->add(_d);
+			return grid;
 		}
-	);
+		GUI::ListItem& li = list_defenses.accessItem(index);
+		grid->add(Label(
+			text = li.text,
+			hAlign = 1.0,
+			rightPadding = 0_px,
+			disabled = NoDefenses()));
+		grid->add(DropDownList(
+			disabled = NoDefenses(),
+			data = list_deftypes,
+			hAlign = 0.0,
+			selectedValue = local_guyref.defense[index],
+			fitParent = true,
+			onSelectFunc = [&, index](int32_t val)
+			{
+				local_guyref.defense[index] = val;
+			}));
+		if (q == 0 && _dobutton)
+			grid->add(Button(
+				fitParent = true,
+				disabled = NoDefenses(),
+				text = "Set All",
+				minwidth = 40_px,
+				height = 1.5_em,
+				onClick = message::SETALLDEFENSE
+			));
+		else grid->add(_d);
+	}
+	return grid;
 }
 
 //Flags Tab
@@ -1309,35 +1331,38 @@ std::shared_ptr<GUI::Widget> EnemyEditorDialog::view()
 		ptr = &guy_tabs[3],
 		TabRef(name = "Attributes", TabPanel(
 			ptr = &guy_tabs[4],
-			TabRef(name = "Attributes 1", Row(
+			TabRef(name = "1", Row(
 				attributes1_tab
 			)),
-			TabRef(name = "Attributes 2", Row(
+			TabRef(name = "2", Row(
 				attributes2_tab
 			)),
-			TabRef(name = "Attributes 3", Row(
+			TabRef(name = "3", Row(
 				attributes3_tab
 			))
 		))
 	);
+	//sanity check required these are legit the defenses we have gaps in the defenses and they 10 per page.
+	int32_t defensearray1[10] = { 0,1,2,3,4,5,6,7,8,9 };
+	int32_t defensearray2[10] = { 10,11,12,13,14,15,16,35,36,-1 };
+	int32_t defensearray3[10] = { 37,38,39,40,-1,-1,-1,-1,-1,-1 };
+	int32_t defensearray4[10] = { 20,21,22,23,24,25,26,27,28,29 };
 	auto defenses_tab = TabPanel(
 		ptr = &guy_tabs[5],
-		TabRef(name = "Defenses", Row(
-			DDPanel(
-				padding = 3_px,
-				values = local_defense,
-				count = 20,
-				ddlist = list_deftypes,
-				data = list_defenses
-			),
-			Button(
-				text = "Set All To First",
-				minwidth = 40_px,
-				height = 1.5_em,
-				vAlign = 0.1,
-				vPadding = 0_px,
-				onClick = message::SETALLDEFENSE
-			)
+		TabRef(name = "Defenses", TabPanel(
+			ptr = &guy_tabs[6],
+			TabRef(name = "1", Column(
+				DefenseField(defensearray1, true)
+			)),
+			TabRef(name = "2", Column(
+				DefenseField(defensearray2)
+			)),
+			TabRef(name = "3", Column(
+				DefenseField(defensearray3)
+			)),
+			TabRef(name = "Script", Column(
+				DefenseField(defensearray4)
+			))
 		))
 	);
 	auto flags_tab = TabPanel(
@@ -1775,8 +1800,6 @@ void EnemyEditorDialog::apply_enemy()
 	else if (spawn_type==2)
 		local_guyref.flags = (local_guyref.flags & ~(guy_fade_flicker)) | guy_fade_instant;
 
-	for (auto q = 0; q < edefLAST255; ++q)
-		local_guyref.defense[q] = byte(local_defense[q]);
 	guysbuf[index] = local_guyref;
 	strncpy(guy_string[index], enemy_name.c_str(), 63);
 
@@ -1798,7 +1821,7 @@ bool EnemyEditorDialog::handleMessage(const GUI::DialogMessage<message>& msg)
 	case message::SETALLDEFENSE:
 	{
 		for (int q = 0; q < edefLAST255; ++q)
-			local_defense[q] = local_defense[0];
+			local_guyref.defense[q] = local_guyref.defense[0];
 		loadEnemyType();
 		rerun_dlg = true;
 		return true;

src/dialog/enemyeditor.h
@@ -61,7 +61,6 @@ private:
 
 	int32_t index;
 	guydata local_guyref;
-	int32_t local_defense[edefLAST255+1];
 	string enemy_name;
 	int32_t spawn_type,death_type;
 
@@ -77,7 +76,7 @@ private:
 
 	bool NoDefenses();
 
-	std::shared_ptr<GUI::Widget> DefenseField(auto* field, GUI::ListData const& ls);
+	std::shared_ptr<GUI::Widget> DefenseField(auto* indexs, bool _dobutton=false);
 
 	std::shared_ptr<GUI::Widget> SizeFlag(int32_t index);
 

src/zc_list_data.cpp
@@ -633,69 +633,64 @@ GUI::ListData GUI::ZCListData::weaptypes(bool numbered)
 	return ls;
 }
 
+//problably not the best name
+static string DefenseToWeaponName(byte def)
+{
+	switch (def)
+	{
+	case edefBRANG: return ZI.getWeapName(wBrang); break;
+	case edefBOMB: return ZI.getWeapName(wLitBomb); break;
+	case edefSBOMB: return ZI.getWeapName(wLitSBomb); break;
+	case edefARROW: return ZI.getWeapName(wArrow); break;
+	case edefFIRE: return ZI.getWeapName(wFire); break;
+	case edefWAND: return "Wand"; break;
+	case edefMAGIC: return ZI.getWeapName(wMagic); break;
+	case edefHOOKSHOT: return ZI.getWeapName(wHookshot); break;
+	case edefHAMMER: return ZI.getWeapName(wHammer); break;
+	case edefSWORD: return ZI.getWeapName(wSword); break;
+	case edefBEAM: return ZI.getWeapName(wBeam); break;
+	case edefREFBEAM: return ZI.getWeapName(wRefBeam); break;
+	case edefREFMAGIC: return ZI.getWeapName(wRefMagic); break;
+	case edefREFBALL: return ZI.getWeapName(wRefFireball); break;
+	case edefREFROCK: return ZI.getWeapName(wRefRock); break;
+	case edefSTOMP: return ZI.getWeapName(wStomp); break;
+	case edefBYRNA: return ZI.getWeapName(wCByrna); break;
+	case edefSCRIPT01: return ZI.getWeapName(wScript1); break;
+	case edefSCRIPT02: return ZI.getWeapName(wScript2); break;
+	case edefSCRIPT03: return ZI.getWeapName(wScript3); break;
+	case edefSCRIPT04: return ZI.getWeapName(wScript4); break;
+	case edefSCRIPT05: return ZI.getWeapName(wScript5); break;
+	case edefSCRIPT06: return ZI.getWeapName(wScript6); break;
+	case edefSCRIPT07: return ZI.getWeapName(wScript7); break;
+	case edefSCRIPT08: return ZI.getWeapName(wScript8); break;
+	case edefSCRIPT09: return ZI.getWeapName(wScript9); break;
+	case edefSCRIPT10: return ZI.getWeapName(wScript10); break;
+	case edefWhistle: return ZI.getWeapName(wWhistle); break;
+	case edefSwitchHook: return "SwitchHook"; break;
+	case edefTHROWN: return ZI.getWeapName(wThrown); break;
+	case edefREFARROW: return ZI.getWeapName(wRefArrow); break;
+	case edefREFFIRE: return ZI.getWeapName(wRefFire); break;
+	case edefREFFIRE2: return ZI.getWeapName(wRefFire2); break;
+	//???
+	case edefICE: return ZI.getWeapName(wIce); break;
+	case edefBAIT: return ZI.getWeapName(wBait); break;
+	case edefWIND: return ZI.getWeapName(wWind); break;
+	case edefSPARKLE: return ZI.getWeapName(wFSparkle); break;
+	case edefSONIC: return ZI.getWeapName(wSound); break;
+	default: return "";
+	}
+}
+
 GUI::ListData GUI::ZCListData::defenses(byte first, byte last, bool enemy)
 {
-	std::map<std::string, int32_t> vals;
+	map<std::string, int32_t> vals;
 
 	GUI::ListData ls;
-	//ls.add("(None)", 0);
-	if (first == 0) first = 1;
-	if (last <= first || last >= wMax) last = wMax - 1;
-	for (int32_t q = first; q < last + 1; ++q)
+	for (int32_t q = first; q < last; ++q)
 	{
-		if (!ZI.isUsableWeap(q))
-			continue; //Hidden
-		char const* defense_str = ZI.getWeapName(q);
-		byte defense = q;
-		if (enemy)
-		{
-			switch (defense)
-			{
-			case wSword: defense = edefSWORD; break;
-			case wBeam: defense = edefBEAM; break;
-			case wBrang: defense = edefBRANG; break;
-			case wBomb: defense = edefBOMB; break;
-			case wSBomb: defense = edefSBOMB; break;
-			case wLitBomb: defense = -1; break;
-			case wLitSBomb: defense = -1; break;
-			case wArrow: defense = edefARROW; break;
-			case wFire: defense = edefFIRE; break;
-			case wWhistle: defense = edefWhistle; break;
-			case wBait: defense = edefBAIT; break;
-			case wWand: defense = edefWAND; break;
-			case wMagic: defense = edefMAGIC; break;
-			case wWind: defense = edefWIND; break;
-			case wRefMagic: defense = edefREFMAGIC; break;
-			case wRefFireball: defense = edefREFBALL; break;
-			case wRefRock: defense = edefREFROCK; break;
-			case wHammer: defense = edefHAMMER; break;
-			case wHookshot: defense = edefHOOKSHOT; break;
-			case wSSparkle: defense = -1; break;
-			case wFSparkle:  defense = -1; break;
-			case wCByrna: defense = edefBYRNA; break;
-			case wRefBeam: defense = edefREFBEAM; break;
-			case wStomp: defense = edefSTOMP; break;
-			case wScript1: defense = edefSCRIPT01; break;
-			case wScript2: defense = edefSCRIPT02; break;
-			case wScript3: defense = edefSCRIPT03; break;
-			case wScript4: defense = edefSCRIPT04; break;
-			case wScript5: defense = edefSCRIPT05; break;
-			case wScript6: defense = edefSCRIPT06; break;
-			case wScript7: defense = edefSCRIPT07; break;
-			case wScript8: defense = edefSCRIPT08; break;
-			case wScript9: defense = edefSCRIPT09; break;
-			case wScript10: defense = edefSCRIPT10; break;
-			case wIce: defense = -1; break;
-			case wSound: defense = -1; break;
-			case wThrown: defense = edefTHROWN; break;
-			case wRefArrow: defense = edefREFARROW; break;
-			case wRefFire: defense = edefREFFIRE; break;
-			case wRefFire2: defense = edefREFFIRE2; break;
-			}
-		}
-		ls.add(fmt::format("{} Defense:", defense_str), defense);
+		std::string name = DefenseToWeaponName(q);
+		ls.add(name, q);
 	}
-
 	return ls;
 }
 

commit d757a32753927b2bc341bfafb468a5597f1630ef

fix(zq)!: ensure Sword defense shows in enemy editor
Regressed recently in aab5659 (enemy editor refactor)

src/dialog/enemyeditor.cpp
@@ -1018,22 +1018,15 @@ inline bool EnemyEditorDialog::NoDefenses()
 	return local_guyref.family == eeROCK || local_guyref.family == eeTRAP || local_guyref.family == eeDONGO || local_guyref.family == eeGANON;
 }
 
-std::shared_ptr<GUI::Widget> EnemyEditorDialog::DefenseField(auto* indexs, bool _dobutton)
+std::shared_ptr<GUI::Widget> EnemyEditorDialog::DefenseField(const std::vector<int>& indices, bool _dobutton)
 {
 	using namespace GUI::Builder;
 	using namespace GUI::Props;
 
 	std::shared_ptr<GUI::Grid> grid = Rows<3>();
-	for (int32_t q = 0; q < sizeof(indexs)+1; q++)
+	for (int32_t q = 0; q < indices.size(); q++)
 	{
-		int32_t index = indexs[q];
-		if (index == -1)
-		{
-			grid->add(_d);
-			grid->add(_d);
-			grid->add(_d);
-			return grid;
-		}
+		int32_t index = indices[q];
 		GUI::ListItem& li = list_defenses.accessItem(index);
 		ddl_defenses[index] = DropDownList(
 			disabled = NoDefenses(),
@@ -1482,10 +1475,10 @@ std::shared_ptr<GUI::Widget> EnemyEditorDialog::view()
 			))
 		))
 	);
-	int32_t defensearray1[10] = { edefBRANG,edefBOMB,edefSBOMB,edefARROW,edefFIRE,edefWAND,edefMAGIC,edefHOOKSHOT,edefHAMMER,edefSWORD };
-	int32_t defensearray2[10] = { edefBEAM,edefREFBEAM,edefREFMAGIC,edefREFBALL,edefREFROCK,edefSTOMP,edefBYRNA,edefWhistle,edefSwitchHook,-1};
-	int32_t defensearray3[10] = { edefTHROWN,edefREFARROW,edefREFFIRE,edefREFFIRE2,-1,-1,-1,-1,-1,-1 };
-	int32_t defensearray4[10] = { edefSCRIPT01, edefSCRIPT02, edefSCRIPT03, edefSCRIPT04, edefSCRIPT05, edefSCRIPT06, edefSCRIPT07, edefSCRIPT08, edefSCRIPT09, edefSCRIPT10 };
+	std::vector<int> defensearray1 = { edefBRANG,edefBOMB,edefSBOMB,edefARROW,edefFIRE,edefWAND,edefMAGIC,edefHOOKSHOT,edefHAMMER,edefSWORD };
+	std::vector<int> defensearray2 = { edefBEAM,edefREFBEAM,edefREFMAGIC,edefREFBALL,edefREFROCK,edefSTOMP,edefBYRNA,edefWhistle,edefSwitchHook};
+	std::vector<int> defensearray3 = { edefTHROWN,edefREFARROW,edefREFFIRE,edefREFFIRE2 };
+	std::vector<int> defensearray4 = { edefSCRIPT01, edefSCRIPT02, edefSCRIPT03, edefSCRIPT04, edefSCRIPT05, edefSCRIPT06, edefSCRIPT07, edefSCRIPT08, edefSCRIPT09, edefSCRIPT10 };
 	auto defenses_tab = TabPanel(
 		ptr = &guy_tabs[5],
 		TabRef(name = "Defenses", TabPanel(

src/dialog/enemyeditor.h
@@ -14,6 +14,7 @@
 #include <functional>
 #include <string_view>
 #include <map>
+#include <vector>
 
 bool call_enemy_editor(int32_t index);
 
@@ -80,7 +81,7 @@ private:
 
 	bool NoDefenses();
 
-	std::shared_ptr<GUI::Widget> DefenseField(auto* indexs, bool _dobutton=false);
+	std::shared_ptr<GUI::Widget> DefenseField(const std::vector<int>& indices, bool _dobutton=false);
 
 	std::shared_ptr<GUI::Widget> SizeFlag(int32_t index);
 