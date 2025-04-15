ZQuestClassic/ZQuestClassic

commit 433581fe7360c39e051a3b39a4b32920bcb97bc3

misc(zscript): treat empty functions like prototype functions
This can be a heavy optimization for empty constructors/destructors.

src/parser/BuildVisitors.cpp
@@ -1493,7 +1493,7 @@ void BuildOpcodes::caseExprCall(ASTExprCall& host, void* param)
 	const string func_comment = fmt::format("Func[{}]",func.getUnaliasedSignature(true).asString());
 	auto targ_sz = commentTarget();
 	bool never_ret = func.getFlag(FUNCFLAG_NEVER_RETURN);
-	if(func.getFlag(FUNCFLAG_NIL) || func.prototype) //Prototype/Nil function
+	if(func.isNil()) //Prototype/Nil function
 	{
 		//Visit each parameter, in case there are side-effects; but don't push the results, as they are unneeded.
 		for (auto it = host.parameters.begin();
@@ -1510,7 +1510,7 @@ void BuildOpcodes::caseExprCall(ASTExprCall& host, void* param)
 			UserClass& user_class = cscope->user_class;
 			vector<Function*> destr = cscope->getDestructor();
 			Function* destructor = destr.size() == 1 ? destr.at(0) : nullptr;
-			if(destructor && !destructor->prototype)
+			if(destructor && !destructor->isNil())
 			{
 				Function* destructor = destr[0];
 				addOpcode(new OSetImmediate(new VarArgument(EXP1),

src/parser/ReturnVisitor.cpp
@@ -114,12 +114,12 @@ void ReturnVisitor::analyzeFunctionInternals(Function& function)
 {
 	ASTFuncDecl* node = function.node;
 	ASTBlock* block = node->block.get();
-	if(!node) return;
+	if(!node || node->isDisabled()) return;
 	if(!block) return;
 	auto& stmts = block->statements;
 	if(extra_pass) //already parsed through at least once
 	{
-		if(function.getFlag(FUNCFLAG_NEVER_RETURN))
+		if(function.getFlag(FUNCFLAG_NEVER_RETURN|FUNCFLAG_NIL))
 			return; //nothing more to possibly do here
 		ResetVisitor resetter;
 		resetter.visit(*block); //reset the 'reachable' state
@@ -129,37 +129,43 @@ void ReturnVisitor::analyzeFunctionInternals(Function& function)
 	markReachable(*node);
 	markReachable(*block);
 	failure_temp = failure_halt = false;
-	//
-	size_t indx = 0;
-	bool earlyterm = false;
-	VisitNode rootnode(node);
-	in_func_body = true;
-	for(auto it = stmts.begin(); it != stmts.end(); ++it)
-	{
-		ASTStmt& stmt = **it;
-		visit(stmt, (void*)&rootnode);
-		earlyterm = rootnode.check_terminate(indx);
-		indx = rootnode.child_index();
-		if(earlyterm)
-			break;
-	}
-	in_func_body = false;
-	
 	bool no_ret = false;
-	
-	if(earlyterm)
+	if(stmts.empty()) //Function is completely empty; optimize
 	{
-		if(!rootnode.get_flag(VNODE_FLAG_HASRETURN))
-		{
-			no_ret = true;
-			//Terminates without return; infinite loop, or script quit?
-			function.setFlag(FUNCFLAG_NEVER_RETURN);
-			marked_never_ret = true;
-		}
+		function.setFlag(FUNCFLAG_NIL);
+		no_ret = true; //still a missing return error if not void/constructor
 	}
-	else no_ret = true;
+	else //Visit the function's statements
+	{
+		size_t indx = 0;
+		bool earlyterm = false;
+		VisitNode rootnode(node);
+		in_func_body = true;
+		for(auto it = stmts.begin(); it != stmts.end(); ++it)
+		{
+			ASTStmt& stmt = **it;
+			visit(stmt, (void*)&rootnode);
+			earlyterm = rootnode.check_terminate(indx);
+			indx = rootnode.child_index();
+			if(earlyterm)
+				break;
+		}
+		in_func_body = false;
+		
+		if(earlyterm)
+		{
+			if(!rootnode.get_flag(VNODE_FLAG_HASRETURN))
+			{
+				no_ret = true;
+				//Terminates without return; infinite loop, or script quit?
+				function.setFlag(FUNCFLAG_NEVER_RETURN);
+				marked_never_ret = true;
+			}
+		}
+		else no_ret = true;
+	}
 	
-	//Void functions can miss out on returns
+	//Void functions && constructors can miss out on returns
 	if(!(function.returnType->isVoid() || function.getFlag(FUNCFLAG_CONSTRUCTOR)) && no_ret)
 	{
 		switch(*ZScript::lookupOption(*scope, CompileOption::OPT_ON_MISSING_RETURN)/10000)
@@ -171,11 +177,12 @@ void ReturnVisitor::analyzeFunctionInternals(Function& function)
 				break;
 			default: //Error
 				handleError(CompileError::MissingReturnError(node, node->name));
+				node->disable();
 				missing_ret = true;
 				break;
 		}
 	}
-	//
+	
 	scope = oldscope;
 }
 

src/parser/ScriptParser.cpp
@@ -490,7 +490,7 @@ unique_ptr<IntermediateData> ScriptParser::generateOCode(FunctionData& fdata)
 				vector<Function*> destr = user_class.getScope().getDestructor();
 				std::shared_ptr<Opcode> first;
 				Function* destructor = destr.size() == 1 ? destr.at(0) : nullptr;
-				if(destructor && !destructor->prototype)
+				if(destructor && !destructor->isNil())
 				{
 					Function* destructor = destr[0];
 					first.reset(new OSetImmediate(new VarArgument(EXP1),

src/parser/ZScript.h
@@ -521,7 +521,8 @@ namespace ZScript
 			return info;
 		}
 		
-		bool isInternal() const {return !node;};
+		bool isInternal() const {return !node;}
+		bool isNil() const {return prototype || getFlag(FUNCFLAG_NIL);}
 		
 		// If this is a tracing function (disabled by `#option LOGGING false`)
 		bool isTracing() const;

commit 39afdb2a97c1c7c723a6091c41082b48190a077f

fix(zscript)!: empty function optimization for constructors
Classes with explicitly empty constructors were not allocating their
arrays when constructed.

Regressed in 433581f

src/parser/BuildVisitors.cpp
@@ -1951,7 +1951,7 @@ void BuildOpcodes::caseExprCall(ASTExprCall& host, void* param)
 		commentStartEnd(targ_sz, fmt::format("Proto{} Visit Params",func_comment));
 		
 		//Set the return to the default value
-		if(classfunc && func.getFlag(FUNCFLAG_CONSTRUCTOR) && parsing_user_class <= puc_vars)
+		if(classfunc && func.getFlag(FUNCFLAG_CONSTRUCTOR))
 		{
 			ClassScope* cscope = func.getInternalScope()->getClass();
 			UserClass& user_class = cscope->user_class;