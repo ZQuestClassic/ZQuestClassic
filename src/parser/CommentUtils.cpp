connorjclark/ZeldaClassic

refactor(zscript)!: preserve order of comment annotations; add ParsedComment class

src/parser/CommentUtils.cpp
@@ -2,11 +2,119 @@
 #include "parser/AST.h"
 #include "parser/Scope.h"
 #include "parser/ZScript.h"
+#include <optional>
 #include <regex>
 #include <string>
 #include <vector>
 
 using namespace ZScript;
+
+ParsedComment::ParsedComment(const std::string& comment)
+{
+	if (comment.empty())
+		return;
+
+	std::vector<std::string> desc_lines;
+
+	std::string current_key;
+	std::vector<std::string> lines;
+	util::split(comment, lines, '\n');
+	for (auto& line : lines)
+	{
+		bool starts_with_at = false;
+		for (char& c : line)
+		{
+			if (c == ' ' || c == '\t') continue;
+			if (c == '@') starts_with_at = true;
+			break;
+		}
+
+		if (starts_with_at)
+		{
+			util::trimstr(line);
+
+			size_t index = line.find_first_of(' ');
+			if (index == -1)
+			{
+				current_key = line.substr(1);
+				line = "";
+			}
+			else
+			{
+				current_key = line.substr(1, index - 1);
+				line = line.substr(index + 1);
+			}
+		}
+
+		if (current_key.empty())
+		{
+			desc_lines.push_back(line);
+			continue;
+		}
+
+		util::trimstr(line);
+
+		if (!starts_with_at)
+		{
+			if (!line.empty())
+				annotations.back().second += "\n" + line;
+			continue;
+		}
+
+		annotations.emplace_back(current_key, line);
+	}
+
+	bool in_list = false;
+	for (auto& line : desc_lines)
+	{
+		util::trimstr(line);
+
+		if (in_list)
+		{
+			if (line.empty())
+			{
+				in_list = false;
+				continue;
+			}
+			if (line[0] == '-') continue;
+			line = "  " + line;
+		}
+		else
+		{
+			if (line.empty()) continue;
+			if (line[0] == '-')
+			{
+				in_list = true;
+				line = "\n" + line; // Helps rst generation.
+				continue;
+			}
+		}
+	}
+
+	description = fmt::format("{}", fmt::join(desc_lines, "\n"));
+}
+
+bool ParsedComment::contains_annotation(const std::string& key) const
+{
+	for (auto& a : annotations)
+		if (a.first == key) return true;
+	return false;
+}
+
+std::optional<std::string> ParsedComment::get_annotation(const std::string& key) const
+{
+	for (auto& [k, v] : annotations)
+		if (k == key) return v;
+	return std::nullopt;
+}
+
+std::vector<std::string> ParsedComment::get_multi_annotation(const std::string& key) const
+{
+	std::vector<std::string> result;
+	for (auto& [k, v] : annotations)
+		if (k == key) result.push_back(v);
+	return result;
+}
 
 static std::map<const AST*, uint16_t> symbolMap;
 static uint16_t nextSymbolId = 1;
@@ -53,7 +161,7 @@ static ASTExprIdentifier parseExprIdentifier(const std::string& str)
 	return ident;
 }
 
-static void parseForSymbolLinks(Scope* scope, const AST* node, bool check_params, std::vector<ParseCommentResult>& matches, std::string& comment, int start_index, int end_index)
+static void parseForSymbolLinks(Scope* scope, const AST* node, bool check_params, std::vector<CommentSymbolParseResult>& matches, std::string& comment, int start_index, int end_index)
 {
 	// identifier, followed by an optional and non-captured "[]" or "()"
 	static std::string p_ident = "(#?[a-zA-Z_][->:a-zA-Z0-9_]*)(?:\\[\\]|\\(\\))?";
@@ -83,7 +191,7 @@ static void parseForSymbolLinks(Scope* scope, const AST* node, bool check_params
 			if((*it)[2].matched)
 				link_text = (*it)[2].str();
 		}
-		ParseCommentResult match{symbol_name, (int)pos, (int)len, link_text, nullptr};
+		CommentSymbolParseResult match{symbol_name, (int)pos, (int)len, link_text, nullptr};
 		matches.emplace_back(match);
 		it++;
 	}
@@ -234,13 +342,13 @@ static void parseForSymbolLinks(Scope* scope, const AST* node, bool check_params
 	}
 }
 
-std::vector<ParseCommentResult> parseForSymbolLinks(std::string comment, const AST* node, bool check_params)
+std::vector<CommentSymbolParseResult> parseForSymbolLinks(std::string comment, const AST* node, bool check_params)
 {
 	Scope* scope = scope = node->getScope();
 	if (!scope)
 		return {};
 
-	std::vector<ParseCommentResult> matches;
+	std::vector<CommentSymbolParseResult> matches;
 
 	// Text within back ticks should not be parsed.
 	bool in_backticks = false;

src/parser/CommentUtils.h
@@ -1,17 +1,39 @@
+#ifndef COMMENT_UTILS_H_
+#define COMMENT_UTILS_H_
+
+#include <optional>
 #include <string>
+#include <utility>
 #include <vector>
-#include "parser/ZScript.h"
 
-using namespace ZScript;
+namespace ZScript {
+	class AST;
+}
 
-uint16_t getSymbolId(const AST* node);
+// Parses into a map keyed by annotations (ex: @alias).
+// Duplicate annotations have multiple entries.
+// `description` contains all non-annotation text.
+struct ParsedComment {
+	ParsedComment(const std::string& comment);
 
-struct ParseCommentResult {
+	bool contains_annotation(const std::string& key) const;
+	std::optional<std::string> get_annotation(const std::string& key) const;
+	std::vector<std::string> get_multi_annotation(const std::string& key) const;
+
+	std::vector<std::pair<std::string, std::string>> annotations;
+	std::string description;
+};
+
+uint16_t getSymbolId(const ZScript::AST* node);
+
+struct CommentSymbolParseResult {
 	std::string symbol_name;
 	int pos;
 	int len;
 	std::string link_text;
-	const AST* symbol_node;
+	const ZScript::AST* symbol_node;
 };
 
-std::vector<ParseCommentResult> parseForSymbolLinks(std::string comment, const AST* node, bool check_params);
+std::vector<CommentSymbolParseResult> parseForSymbolLinks(std::string comment, const ZScript::AST* node, bool check_params);
+
+#endif

src/parser/RegistrationVisitor.cpp
@@ -221,7 +221,7 @@ void RegistrationVisitor::caseScript(ASTScript& host, void* param)
 
 void RegistrationVisitor::initInternalVar(ASTDataDeclList* var)
 {
-	auto parsed_comment = var->getParsedDocComment();
+	auto parsed_comment = var->getParsedComment();
 
 	int refvar = NUL;
 	UserClass* user_class = nullptr;
@@ -233,7 +233,7 @@ void RegistrationVisitor::initInternalVar(ASTDataDeclList* var)
 
 	for (auto decl : var->getDeclarations())
 	{
-		if (parsed_comment.contains("zasm_var") && parsed_comment.contains("zasm_internal_array"))
+		if (parsed_comment.contains_annotation("zasm_var") && parsed_comment.contains_annotation("zasm_internal_array"))
 		{
 			handleError(CompileError::BadInternal(decl, "Only one of @zasm_var, @zasm_internal_array is allowed"));
 			continue;
@@ -241,7 +241,7 @@ void RegistrationVisitor::initInternalVar(ASTDataDeclList* var)
 
 		// Internal variables in classes must have a zasm_var/zasm_internal_array. Currently, global internal variables
 		// may not have one, in which case it defaults to a constant zero.
-		if (user_class && !parsed_comment.contains("zasm_var") && !parsed_comment.contains("zasm_internal_array"))
+		if (user_class && !parsed_comment.contains_annotation("zasm_var") && !parsed_comment.contains_annotation("zasm_internal_array"))
 		{
 			handleError(CompileError::BadInternal(decl, "Expected one of @zasm_var, @zasm_internal_array"));
 			continue;
@@ -249,24 +249,24 @@ void RegistrationVisitor::initInternalVar(ASTDataDeclList* var)
 
 		bool is_constant_zero = false;
 		int fn_value;
-		if (parsed_comment.contains("zasm_var"))
+		if (auto zasm_var = parsed_comment.get_annotation("zasm_var"))
 		{
-			if (auto sv = get_script_variable(parsed_comment["zasm_var"]))
+			if (auto sv = get_script_variable(*zasm_var))
 			{
 				fn_value = *sv;
 			}
 			else
 			{
-				handleError(CompileError::BadInternal(decl, fmt::format("Invalid ZASM register: {}", parsed_comment["zasm_var"])));
+				handleError(CompileError::BadInternal(decl, fmt::format("Invalid ZASM register: {}", *zasm_var)));
 				continue;
 			}
 		}
-		else if (parsed_comment.contains("zasm_internal_array"))
+		else if (auto zasm_internal_arr = parsed_comment.get_annotation("zasm_internal_array"))
 		{
 			try {
-				fn_value = std::stoi(parsed_comment["zasm_internal_array"]);
+				fn_value = std::stoi(*zasm_internal_arr);
 			} catch (std::exception ex) {
-				handleError(CompileError::BadInternal(decl, fmt::format("Invalid internal array: {} (must be an integer)", parsed_comment["zasm_internal_array"])));
+				handleError(CompileError::BadInternal(decl, fmt::format("Invalid internal array: {} (must be an integer)", *zasm_internal_arr)));
 				continue;
 			}
 
@@ -279,10 +279,10 @@ void RegistrationVisitor::initInternalVar(ASTDataDeclList* var)
 		}
 
 		auto& ty = decl->manager->type;
-		bool is_internal_arr = parsed_comment.contains("zasm_internal_array");
+		bool is_internal_arr = parsed_comment.contains_annotation("zasm_internal_array");
 		bool is_arr = ty.isArray();
 		auto var_type = is_internal_arr ? &ty : ty.baseType(*scope, nullptr);
-		bool deprecated = parsed_comment.contains("deprecated");
+		auto deprecated = parsed_comment.get_annotation("deprecated");
 
 		// Add a getter.
 		{
@@ -296,7 +296,7 @@ void RegistrationVisitor::initInternalVar(ASTDataDeclList* var)
 			if (deprecated)
 			{
 				fn->setFlag(FUNCFLAG_DEPRECATED);
-				fn->setInfo(parsed_comment["deprecated"]);
+				fn->setInfo(*deprecated);
 			}
 			if (is_internal_arr)
 				fn->setFlag(FUNCFLAG_INTARRAY);
@@ -310,7 +310,7 @@ void RegistrationVisitor::initInternalVar(ASTDataDeclList* var)
 		}
 
 		// Add deprecated getters.
-		if (parsed_comment.contains("deprecated_getter"))
+		if (auto deprecated_getter = parsed_comment.get_annotation("deprecated_getter"))
 		{
 			if (is_arr || is_internal_arr)
 			{
@@ -318,7 +318,7 @@ void RegistrationVisitor::initInternalVar(ASTDataDeclList* var)
 				continue;
 			}
 
-			std::string getter_name = parsed_comment["deprecated_getter"];
+			std::string getter_name = *deprecated_getter;
 			std::vector<const DataType*> params;
 			if (refvar != NUL)
 				params.push_back(user_class->getType());
@@ -346,7 +346,7 @@ void RegistrationVisitor::initInternalVar(ASTDataDeclList* var)
 			if (deprecated)
 			{
 				fn->setFlag(FUNCFLAG_DEPRECATED);
-				fn->setInfo(parsed_comment["deprecated"]);
+				fn->setInfo(*deprecated);
 			}
 			if (var->readonly)
 				fn->setFlag(FUNCFLAG_READ_ONLY);
@@ -363,13 +363,12 @@ void RegistrationVisitor::initInternalVar(ASTDataDeclList* var)
 
 void RegistrationVisitor::caseClass(ASTClass& host, void* param)
 {
-	auto parsed_comment = host.getParsedDocComment();
+	auto parsed_comment = host.getParsedComment();
 	UserClass* parent_class = nullptr;
-	if (parsed_comment.contains("extends"))
+	if (auto parent_class_name = parsed_comment.get_annotation("extends"))
 	{
-		std::string parent_class_name = parsed_comment["extends"];
 		ASTExprIdentifier ident{};
-		ident.components.push_back(parent_class_name);
+		ident.components.push_back(*parent_class_name);
 		if (auto type = lookupDataType(*scope, ident, nullptr))
 		{
 			if (auto custom_type = dynamic_cast<const DataTypeCustom*>(type); custom_type)
@@ -377,7 +376,7 @@ void RegistrationVisitor::caseClass(ASTClass& host, void* param)
 		}
 		else
 		{
-			handleError(CompileError::BadInternal(&host, fmt::format("Unknown class \"{}\" in @extends", parent_class_name)));
+			handleError(CompileError::BadInternal(&host, fmt::format("Unknown class \"{}\" in @extends", *parent_class_name)));
 		}
 	}
 
@@ -393,9 +392,9 @@ void RegistrationVisitor::caseClass(ASTClass& host, void* param)
 
 	UserClass& user_class = *host.user_class;
 
-	if (parsed_comment.contains("zasm_ref"))
+	if (auto zasm_ref = parsed_comment.get_annotation("zasm_ref"))
 	{
-		user_class.internalRefVarString = parsed_comment["zasm_ref"];
+		user_class.internalRefVarString = *zasm_ref;
 		user_class.internalRefVar = user_class.internalRefVarString.empty() ? NUL : StringToVar(user_class.internalRefVarString);
 	}
 	else

src/parser/Scope.cpp
@@ -14,13 +14,11 @@
 #include "ZScript.h"
 #include "zasm/table.h"
 #include "zasm/serialize.h"
-#include <sstream>
 
 using namespace ZScript;
 using namespace util;
 using std::set;
 using std::shared_ptr;
-using std::unique_ptr;
 
 ////////////////////////////////////////////////////////////////
 // Scope
@@ -45,8 +43,8 @@ void Scope::invalidateStackSize()
 
 void Scope::initFunctionBinding(Function* fn, CompileErrorHandler* handler)
 {
-	auto parsed_comment = fn->getNode()->getParsedDocComment();
-	if (parsed_comment.contains("delete"))
+	auto parsed_comment = fn->getNode()->getParsedComment();
+	if (parsed_comment.contains_annotation("delete"))
 	{
 		fn->setFlag(FUNCFLAG_NIL);
 		return;
@@ -55,58 +53,43 @@ void Scope::initFunctionBinding(Function* fn, CompileErrorHandler* handler)
 	// All internal binding functions are inline.
 	fn->setFlag(FUNCFLAG_INLINE);
 
-	if (parsed_comment.contains("alias"))
+	for (auto alias : parsed_comment.get_multi_annotation("alias"))
 	{
-		std::vector<std::string> aliases;
-		util::split(parsed_comment["alias"], aliases, '\x1f');
-		for (auto& alias : aliases)
-		{
-			auto alias_fn = new Function();
-			alias_fn->name = alias;
-			alias_fn->alias(fn);
-			addAlias(alias_fn, handler);
-		}
+		auto alias_fn = new Function();
+		alias_fn->name = alias;
+		alias_fn->alias(fn);
+		addAlias(alias_fn, handler);
 	}
 
-	if (parsed_comment.contains("deprecated_alias"))
+	for (auto alias : parsed_comment.get_multi_annotation("deprecated_alias"))
 	{
-		std::vector<std::string> aliases;
-		util::split(parsed_comment["deprecated_alias"], aliases, '\x1f');
-		for (auto& alias : aliases)
-		{
-			auto alias_fn = new Function();
-			alias_fn->name = alias;
-			alias_fn->alias(fn);
-			alias_fn->setFlag(FUNCFLAG_DEPRECATED);
-			alias_fn->setInfo(fmt::format("Use {} instead", fn->name));
-			addAlias(alias_fn, handler);
-		}
+		auto alias_fn = new Function();
+		alias_fn->name = alias;
+		alias_fn->alias(fn);
+		alias_fn->setFlag(FUNCFLAG_DEPRECATED);
+		alias_fn->setInfo(fmt::format("Use {} instead", fn->name));
+		addAlias(alias_fn, handler);
 	}
 
-	if (parsed_comment.contains("exit"))
+	if (parsed_comment.contains_annotation("exit"))
 		fn->setFlag(FUNCFLAG_EXITS|FUNCFLAG_NEVER_RETURN);
-	if (parsed_comment.contains("deprecated"))
+	if (auto depr_message = parsed_comment.get_annotation("deprecated"))
 	{
 		fn->setFlag(FUNCFLAG_DEPRECATED);
-		fn->setInfo(parsed_comment["deprecated"]);
+		fn->setInfo(*depr_message);
 	}
-	if (parsed_comment.contains("reassign_ptr"))
+	if (parsed_comment.contains_annotation("reassign_ptr"))
 		fn->setIntFlag(IFUNCFLAG_REASSIGNPTR);
 
-	if (parsed_comment.contains("vargs"))
+	if (auto vargs = parsed_comment.get_annotation("vargs"))
 	{
-		fn->extra_vargs = std::stoi(parsed_comment["vargs"]);
+		fn->extra_vargs = std::stoi(*vargs);
 		fn->setFlag(FUNCFLAG_VARARGS);
 	}
 
-	auto it = parsed_comment.find("zasm");
 	std::vector<std::string> zasm_lines;
-	if (it != parsed_comment.cend())
-	{
-		const auto& zasm_str = it->second;
-		std::vector<std::string> zasm;
-		util::split(zasm_str, zasm_lines, '\n');
-	}
+	if (auto zasm = parsed_comment.get_annotation("zasm"))
+		util::split(*zasm, zasm_lines, '\n');
 
 	int stack_change = fn->numParams();
 	if (fn->getClass() && !fn->getClass()->internalRefVarString.empty() && !fn->getFlag(FUNCFLAG_CONSTRUCTOR) && !fn->getFlag(FUNCFLAG_DESTRUCTOR))

ZQuestClassic/ZQuestClassic

src/parser/CommentUtils.cpp
@@ -2,11 +2,119 @@
 #include "parser/AST.h"
 #include "parser/Scope.h"
 #include "parser/ZScript.h"
+#include <optional>
 #include <regex>
 #include <string>
 #include <vector>
 
 using namespace ZScript;
+
+ParsedComment::ParsedComment(const std::string& comment)
+{
+	if (comment.empty())
+		return;
+
+	std::vector<std::string> desc_lines;
+
+	std::string current_key;
+	std::vector<std::string> lines;
+	util::split(comment, lines, '\n');
+	for (auto& line : lines)
+	{
+		bool starts_with_at = false;
+		for (char& c : line)
+		{
+			if (c == ' ' || c == '\t') continue;
+			if (c == '@') starts_with_at = true;
+			break;
+		}
+
+		if (starts_with_at)
+		{
+			util::trimstr(line);
+
+			size_t index = line.find_first_of(' ');
+			if (index == -1)
+			{
+				current_key = line.substr(1);
+				line = "";
+			}
+			else
+			{
+				current_key = line.substr(1, index - 1);
+				line = line.substr(index + 1);
+			}
+		}
+
+		if (current_key.empty())
+		{
+			desc_lines.push_back(line);
+			continue;
+		}
+
+		util::trimstr(line);
+
+		if (!starts_with_at)
+		{
+			if (!line.empty())
+				tags.back().second += "\n" + line;
+			continue;
+		}
+
+		tags.emplace_back(current_key, line);
+	}
+
+	bool in_list = false;
+	for (auto& line : desc_lines)
+	{
+		util::trimstr(line);
+
+		if (in_list)
+		{
+			if (line.empty())
+			{
+				in_list = false;
+				continue;
+			}
+			if (line[0] == '-') continue;
+			line = "  " + line;
+		}
+		else
+		{
+			if (line.empty()) continue;
+			if (line[0] == '-')
+			{
+				in_list = true;
+				line = "\n" + line; // Helps rst generation.
+				continue;
+			}
+		}
+	}
+
+	description = fmt::format("{}", fmt::join(desc_lines, "\n"));
+}
+
+bool ParsedComment::contains_tag(const std::string& key) const
+{
+	for (auto& a : tags)
+		if (a.first == key) return true;
+	return false;
+}
+
+std::optional<std::string> ParsedComment::get_tag(const std::string& key) const
+{
+	for (auto& [k, v] : tags)
+		if (k == key) return v;
+	return std::nullopt;
+}
+
+std::vector<std::string> ParsedComment::get_multi_tag(const std::string& key) const
+{
+	std::vector<std::string> result;
+	for (auto& [k, v] : tags)
+		if (k == key) result.push_back(v);
+	return result;
+}
 
 static std::map<const AST*, uint16_t> symbolMap;
 static uint16_t nextSymbolId = 1;
@@ -53,7 +161,7 @@ static ASTExprIdentifier parseExprIdentifier(const std::string& str)
 	return ident;
 }
 
-static void parseForSymbolLinks(Scope* scope, const AST* node, bool check_params, std::vector<ParseCommentResult>& matches, std::string& comment, int start_index, int end_index)
+static void parseForSymbolLinks(Scope* scope, const AST* node, bool check_params, std::vector<CommentSymbolParseResult>& matches, std::string& comment, int start_index, int end_index)
 {
 	// identifier, followed by an optional and non-captured "[]" or "()"
 	static std::string p_ident = "(#?[a-zA-Z_][->:a-zA-Z0-9_]*)(?:\\[\\]|\\(\\))?";
@@ -83,7 +191,7 @@ static void parseForSymbolLinks(Scope* scope, const AST* node, bool check_params
 			if((*it)[2].matched)
 				link_text = (*it)[2].str();
 		}
-		ParseCommentResult match{symbol_name, (int)pos, (int)len, link_text, nullptr};
+		CommentSymbolParseResult match{symbol_name, (int)pos, (int)len, link_text, nullptr};
 		matches.emplace_back(match);
 		it++;
 	}
@@ -234,13 +342,13 @@ static void parseForSymbolLinks(Scope* scope, const AST* node, bool check_params
 	}
 }
 
-std::vector<ParseCommentResult> parseForSymbolLinks(std::string comment, const AST* node, bool check_params)
+std::vector<CommentSymbolParseResult> parseForSymbolLinks(std::string comment, const AST* node, bool check_params)
 {
 	Scope* scope = scope = node->getScope();
 	if (!scope)
 		return {};
 
-	std::vector<ParseCommentResult> matches;
+	std::vector<CommentSymbolParseResult> matches;
 
 	// Text within back ticks should not be parsed.
 	bool in_backticks = false;

src/parser/CommentUtils.h
@@ -1,17 +1,39 @@
+#ifndef COMMENT_UTILS_H_
+#define COMMENT_UTILS_H_
+
+#include <optional>
 #include <string>
+#include <utility>
 #include <vector>
-#include "parser/ZScript.h"
 
-using namespace ZScript;
+namespace ZScript {
+	class AST;
+}
 
-uint16_t getSymbolId(const AST* node);
+// Parses a doc comment into a collections of tags (ex: @alias).
+// Duplicate tags have multiple entries.
+// `description` contains all non-tag text.
+struct ParsedComment {
+	ParsedComment(const std::string& comment);
 
-struct ParseCommentResult {
+	bool contains_tag(const std::string& key) const;
+	std::optional<std::string> get_tag(const std::string& key) const;
+	std::vector<std::string> get_multi_tag(const std::string& key) const;
+
+	std::vector<std::pair<std::string, std::string>> tags;
+	std::string description;
+};
+
+uint16_t getSymbolId(const ZScript::AST* node);
+
+struct CommentSymbolParseResult {
 	std::string symbol_name;
 	int pos;
 	int len;
 	std::string link_text;
-	const AST* symbol_node;
+	const ZScript::AST* symbol_node;
 };
 
-std::vector<ParseCommentResult> parseForSymbolLinks(std::string comment, const AST* node, bool check_params);
+std::vector<CommentSymbolParseResult> parseForSymbolLinks(std::string comment, const ZScript::AST* node, bool check_params);
+
+#endif

src/parser/RegistrationVisitor.cpp
@@ -221,7 +221,7 @@ void RegistrationVisitor::caseScript(ASTScript& host, void* param)
 
 void RegistrationVisitor::initInternalVar(ASTDataDeclList* var)
 {
-	auto parsed_comment = var->getParsedDocComment();
+	auto parsed_comment = var->getParsedComment();
 
 	int refvar = NUL;
 	UserClass* user_class = nullptr;
@@ -233,7 +233,7 @@ void RegistrationVisitor::initInternalVar(ASTDataDeclList* var)
 
 	for (auto decl : var->getDeclarations())
 	{
-		if (parsed_comment.contains("zasm_var") && parsed_comment.contains("zasm_internal_array"))
+		if (parsed_comment.contains_tag("zasm_var") && parsed_comment.contains_tag("zasm_internal_array"))
 		{
 			handleError(CompileError::BadInternal(decl, "Only one of @zasm_var, @zasm_internal_array is allowed"));
 			continue;
@@ -241,7 +241,7 @@ void RegistrationVisitor::initInternalVar(ASTDataDeclList* var)
 
 		// Internal variables in classes must have a zasm_var/zasm_internal_array. Currently, global internal variables
 		// may not have one, in which case it defaults to a constant zero.
-		if (user_class && !parsed_comment.contains("zasm_var") && !parsed_comment.contains("zasm_internal_array"))
+		if (user_class && !parsed_comment.contains_tag("zasm_var") && !parsed_comment.contains_tag("zasm_internal_array"))
 		{
 			handleError(CompileError::BadInternal(decl, "Expected one of @zasm_var, @zasm_internal_array"));
 			continue;
@@ -249,24 +249,24 @@ void RegistrationVisitor::initInternalVar(ASTDataDeclList* var)
 
 		bool is_constant_zero = false;
 		int fn_value;
-		if (parsed_comment.contains("zasm_var"))
+		if (auto zasm_var = parsed_comment.get_tag("zasm_var"))
 		{
-			if (auto sv = get_script_variable(parsed_comment["zasm_var"]))
+			if (auto sv = get_script_variable(*zasm_var))
 			{
 				fn_value = *sv;
 			}
 			else
 			{
-				handleError(CompileError::BadInternal(decl, fmt::format("Invalid ZASM register: {}", parsed_comment["zasm_var"])));
+				handleError(CompileError::BadInternal(decl, fmt::format("Invalid ZASM register: {}", *zasm_var)));
 				continue;
 			}
 		}
-		else if (parsed_comment.contains("zasm_internal_array"))
+		else if (auto zasm_internal_arr = parsed_comment.get_tag("zasm_internal_array"))
 		{
 			try {
-				fn_value = std::stoi(parsed_comment["zasm_internal_array"]);
+				fn_value = std::stoi(*zasm_internal_arr);
 			} catch (std::exception ex) {
-				handleError(CompileError::BadInternal(decl, fmt::format("Invalid internal array: {} (must be an integer)", parsed_comment["zasm_internal_array"])));
+				handleError(CompileError::BadInternal(decl, fmt::format("Invalid internal array: {} (must be an integer)", *zasm_internal_arr)));
 				continue;
 			}
 
@@ -279,10 +279,10 @@ void RegistrationVisitor::initInternalVar(ASTDataDeclList* var)
 		}
 
 		auto& ty = decl->manager->type;
-		bool is_internal_arr = parsed_comment.contains("zasm_internal_array");
+		bool is_internal_arr = parsed_comment.contains_tag("zasm_internal_array");
 		bool is_arr = ty.isArray();
 		auto var_type = is_internal_arr ? &ty : ty.baseType(*scope, nullptr);
-		bool deprecated = parsed_comment.contains("deprecated");
+		auto deprecated = parsed_comment.get_tag("deprecated");
 
 		// Add a getter.
 		{
@@ -296,7 +296,7 @@ void RegistrationVisitor::initInternalVar(ASTDataDeclList* var)
 			if (deprecated)
 			{
 				fn->setFlag(FUNCFLAG_DEPRECATED);
-				fn->setInfo(parsed_comment["deprecated"]);
+				fn->setInfo(*deprecated);
 			}
 			if (is_internal_arr)
 				fn->setFlag(FUNCFLAG_INTARRAY);
@@ -310,7 +310,7 @@ void RegistrationVisitor::initInternalVar(ASTDataDeclList* var)
 		}
 
 		// Add deprecated getters.
-		if (parsed_comment.contains("deprecated_getter"))
+		if (auto deprecated_getter = parsed_comment.get_tag("deprecated_getter"))
 		{
 			if (is_arr || is_internal_arr)
 			{
@@ -318,7 +318,7 @@ void RegistrationVisitor::initInternalVar(ASTDataDeclList* var)
 				continue;
 			}
 
-			std::string getter_name = parsed_comment["deprecated_getter"];
+			std::string getter_name = *deprecated_getter;
 			std::vector<const DataType*> params;
 			if (refvar != NUL)
 				params.push_back(user_class->getType());
@@ -346,7 +346,7 @@ void RegistrationVisitor::initInternalVar(ASTDataDeclList* var)
 			if (deprecated)
 			{
 				fn->setFlag(FUNCFLAG_DEPRECATED);
-				fn->setInfo(parsed_comment["deprecated"]);
+				fn->setInfo(*deprecated);
 			}
 			if (var->readonly)
 				fn->setFlag(FUNCFLAG_READ_ONLY);
@@ -363,13 +363,12 @@ void RegistrationVisitor::initInternalVar(ASTDataDeclList* var)
 
 void RegistrationVisitor::caseClass(ASTClass& host, void* param)
 {
-	auto parsed_comment = host.getParsedDocComment();
+	auto parsed_comment = host.getParsedComment();
 	UserClass* parent_class = nullptr;
-	if (parsed_comment.contains("extends"))
+	if (auto parent_class_name = parsed_comment.get_tag("extends"))
 	{
-		std::string parent_class_name = parsed_comment["extends"];
 		ASTExprIdentifier ident{};
-		ident.components.push_back(parent_class_name);
+		ident.components.push_back(*parent_class_name);
 		if (auto type = lookupDataType(*scope, ident, nullptr))
 		{
 			if (auto custom_type = dynamic_cast<const DataTypeCustom*>(type); custom_type)
@@ -377,7 +376,7 @@ void RegistrationVisitor::caseClass(ASTClass& host, void* param)
 		}
 		else
 		{
-			handleError(CompileError::BadInternal(&host, fmt::format("Unknown class \"{}\" in @extends", parent_class_name)));
+			handleError(CompileError::BadInternal(&host, fmt::format("Unknown class \"{}\" in @extends", *parent_class_name)));
 		}
 	}
 
@@ -393,9 +392,9 @@ void RegistrationVisitor::caseClass(ASTClass& host, void* param)
 
 	UserClass& user_class = *host.user_class;
 
-	if (parsed_comment.contains("zasm_ref"))
+	if (auto zasm_ref = parsed_comment.get_tag("zasm_ref"))
 	{
-		user_class.internalRefVarString = parsed_comment["zasm_ref"];
+		user_class.internalRefVarString = *zasm_ref;
 		user_class.internalRefVar = user_class.internalRefVarString.empty() ? NUL : StringToVar(user_class.internalRefVarString);
 	}
 	else

src/parser/Scope.cpp
@@ -14,13 +14,11 @@
 #include "ZScript.h"
 #include "zasm/table.h"
 #include "zasm/serialize.h"
-#include <sstream>
 
 using namespace ZScript;
 using namespace util;
 using std::set;
 using std::shared_ptr;
-using std::unique_ptr;
 
 ////////////////////////////////////////////////////////////////
 // Scope
@@ -45,8 +43,8 @@ void Scope::invalidateStackSize()
 
 void Scope::initFunctionBinding(Function* fn, CompileErrorHandler* handler)
 {
-	auto parsed_comment = fn->getNode()->getParsedDocComment();
-	if (parsed_comment.contains("delete"))
+	auto parsed_comment = fn->getNode()->getParsedComment();
+	if (parsed_comment.contains_tag("delete"))
 	{
 		fn->setFlag(FUNCFLAG_NIL);
 		return;
@@ -55,58 +53,43 @@ void Scope::initFunctionBinding(Function* fn, CompileErrorHandler* handler)
 	// All internal binding functions are inline.
 	fn->setFlag(FUNCFLAG_INLINE);
 
-	if (parsed_comment.contains("alias"))
+	for (auto alias : parsed_comment.get_multi_tag("alias"))
 	{
-		std::vector<std::string> aliases;
-		util::split(parsed_comment["alias"], aliases, '\x1f');
-		for (auto& alias : aliases)
-		{
-			auto alias_fn = new Function();
-			alias_fn->name = alias;
-			alias_fn->alias(fn);
-			addAlias(alias_fn, handler);
-		}
+		auto alias_fn = new Function();
+		alias_fn->name = alias;
+		alias_fn->alias(fn);
+		addAlias(alias_fn, handler);
 	}
 
-	if (parsed_comment.contains("deprecated_alias"))
+	for (auto alias : parsed_comment.get_multi_tag("deprecated_alias"))
 	{
-		std::vector<std::string> aliases;
-		util::split(parsed_comment["deprecated_alias"], aliases, '\x1f');
-		for (auto& alias : aliases)
-		{
-			auto alias_fn = new Function();
-			alias_fn->name = alias;
-			alias_fn->alias(fn);
-			alias_fn->setFlag(FUNCFLAG_DEPRECATED);
-			alias_fn->setInfo(fmt::format("Use {} instead", fn->name));
-			addAlias(alias_fn, handler);
-		}
+		auto alias_fn = new Function();
+		alias_fn->name = alias;
+		alias_fn->alias(fn);
+		alias_fn->setFlag(FUNCFLAG_DEPRECATED);
+		alias_fn->setInfo(fmt::format("Use {} instead", fn->name));
+		addAlias(alias_fn, handler);
 	}
 
-	if (parsed_comment.contains("exit"))
+	if (parsed_comment.contains_tag("exit"))
 		fn->setFlag(FUNCFLAG_EXITS|FUNCFLAG_NEVER_RETURN);
-	if (parsed_comment.contains("deprecated"))
+	if (auto depr_message = parsed_comment.get_tag("deprecated"))
 	{
 		fn->setFlag(FUNCFLAG_DEPRECATED);
-		fn->setInfo(parsed_comment["deprecated"]);
+		fn->setInfo(*depr_message);
 	}
-	if (parsed_comment.contains("reassign_ptr"))
+	if (parsed_comment.contains_tag("reassign_ptr"))
 		fn->setIntFlag(IFUNCFLAG_REASSIGNPTR);
 
-	if (parsed_comment.contains("vargs"))
+	if (auto vargs = parsed_comment.get_tag("vargs"))
 	{
-		fn->extra_vargs = std::stoi(parsed_comment["vargs"]);
+		fn->extra_vargs = std::stoi(*vargs);
 		fn->setFlag(FUNCFLAG_VARARGS);
 	}
 
-	auto it = parsed_comment.find("zasm");
 	std::vector<std::string> zasm_lines;
-	if (it != parsed_comment.cend())
-	{
-		const auto& zasm_str = it->second;
-		std::vector<std::string> zasm;
-		util::split(zasm_str, zasm_lines, '\n');
-	}
+	if (auto zasm = parsed_comment.get_tag("zasm"))
+		util::split(*zasm, zasm_lines, '\n');
 
 	int stack_change = fn->numParams();
 	if (fn->getClass() && !fn->getClass()->internalRefVarString.empty() && !fn->getFlag(FUNCFLAG_CONSTRUCTOR) && !fn->getFlag(FUNCFLAG_DESTRUCTOR))