ZQuestClassic/ZQuestClassic

commit c6f3f00bda49ca8461b7d5242850186775ecfee7

refactor(zscript)!: handle array type ownership

src/parser/AST.cpp
@@ -1423,8 +1423,6 @@ void ASTDataDecl::setInitializer(ASTExpr* initializer)
 
 DataType const& ASTDataDecl::resolveType(ZScript::Scope* scope, CompileErrorHandler* errorHandler)
 {
-	TypeStore& typeStore = scope->getTypeStore();
-
 	// First resolve the base type.
 	ASTDataType* baseTypeNode = list ? list->baseType.get() : baseType.get();
 	DataType const* type = &baseTypeNode->resolve(*scope, errorHandler);
@@ -1437,12 +1435,8 @@ DataType const& ASTDataDecl::resolveType(ZScript::Scope* scope, CompileErrorHand
 		return *type;
 	
 	// If we have any arrays, tack them onto the base type.
-	for (vector<ASTDataDeclExtraArray*>::const_iterator it = extraArrays.begin();
-		 it != extraArrays.end(); ++it)
-	{
-		DataTypeArray arrayType(*type);
-		type = typeStore.getCanonicalType(arrayType);
-	}
+	for (int q = 0; q < extraArrays.size(); ++q)
+		type = DataTypeArray::create(*type);
 
 	return *type;
 }
@@ -2918,37 +2912,33 @@ DataType const& ASTDataType::resolve(ZScript::Scope& scope, CompileErrorHandler*
 {
 	if(!wasResolved_)
 	{
-		DataType const* resolved = type->resolve(scope, errorHandler);
-		if(resolved && constant_)
+		DataType const* ty = type->resolve(scope, errorHandler);
+		if (ty && ty->isResolved())
 		{
-			string name = resolved->getName();
-			if(constant_>1 || resolved->isConstant())
+			if (constant_)
 			{
-				errorHandler->handleError(CompileError::ConstAlreadyConstant(this, name));
-				return DataType::ZVOID;
-			}
-			resolved = resolved->getConstType();
-		}
-		if(resolved)
-		{
-			DataType* result = nullptr;
-			if(becomeArray)
-			{
-				auto* basety = resolved->baseType(scope, errorHandler);
-				if(basety)
+				string name = ty->getName();
+				if (constant_ > 1 || ty->isConstant())
 				{
-					result = new DataTypeArray(*basety);
-					for(uint q = 1; result && q < becomeArray; ++q)
-					{
-						extra_types.push_back(result); //don't leak the memory
-						result = new DataTypeArray(*result);
-					}
+					errorHandler->handleError(CompileError::ConstAlreadyConstant(this, name));
+					return DataType::ZVOID;
 				}
+				ty = ty->getConstType();
 			}
-			else result = resolved->clone();
-			if(result)
-				type.reset(result);
-			wasResolved_ = result && result->isResolved();
+			if (becomeArray && ty->isResolved())
+			{
+				uint q = 0;
+				if (ty == type.get()) //need to manage the element type ownership
+				{
+					++q;
+					ty = DataTypeArray::create_owning(type.release());
+				}
+				for (; q < becomeArray; ++q)
+					ty = DataTypeArray::create(*ty);
+			}
+			if (ty != type.get())
+				type.reset(ty->clone());
+			wasResolved_ = type && type->isResolved();
 		}
 	}
 	return *type;

src/parser/AST.h
@@ -2300,7 +2300,6 @@ namespace ZScript
 		uint becomeArray;
 	private:
 		bool wasResolved_;
-		owning_vector<DataType> extra_types; //nested arrays would leak pointers if not for this
 	};
 }
 

src/parser/RegistrationVisitor.cpp
@@ -337,9 +337,10 @@ void RegistrationVisitor::caseClass(ASTClass& host, void* param)
 				fn_value = (INTARR_OFFS + fn_value) * 10000;
 			}
 
+			auto& ty = decl->manager->type;
 			bool is_internal_arr = parsed_comment.contains("zasm_internal_array");
-			bool is_arr = decl->manager->type.isArray();
-			auto var_type = decl->manager->type.baseType(*scope, nullptr);
+			bool is_arr = ty.isArray();
+			auto var_type = is_internal_arr ? &ty : ty.baseType(*scope, nullptr);
 			bool deprecated = parsed_comment.contains("deprecated");
 
 			// Add a getter.

src/parser/Scope.cpp
@@ -796,12 +796,8 @@ static int applyTemplateTypes(
 			}
 			if(!varg_basety)
 				return APPLY_TEMPLATE_RET_UNSATISFIABLE;
-			DataType* owned_ty = nullptr;
-			for(int q = 0; q < target_depth+1; ++q) //ensure proper ownership of the nested types //+1 because vargs
-			{
-				varg_basety = owned_ty =
-					owned_ty ? new DataTypeArray(*varg_basety, owned_ty) : new DataTypeArray(*varg_basety);
-			}
+			for(int q = 0; q < target_depth+1; ++q) //+1 because vargs adds a layer of array around the params
+				varg_basety = DataTypeArray::create(*varg_basety);
 			resolved_params.back() = varg_basety;
 		}
 	}
@@ -820,12 +816,8 @@ static int applyTemplateTypes(
 
 		auto target_depth = ret_type->getArrayDepth();
 		ret_type = bound_t;
-		DataType* owned_ty = nullptr;
-		for(int q = 0; q < target_depth; ++q) //ensure proper ownership of the nested types
-		{
-			ret_type = owned_ty =
-				owned_ty ? new DataTypeArray(*ret_type, owned_ty) : new DataTypeArray(*ret_type);
-		}
+		for(int q = 0; q < target_depth; ++q)
+			ret_type = DataTypeArray::create(*ret_type);
 	}
 
 	//Returns an existing matching function, or creates a new one, memory managed -Em

src/parser/ScriptParser.cpp
@@ -514,7 +514,7 @@ unique_ptr<IntermediateData> ScriptParser::generateOCode(FunctionData& fdata)
 				for (auto&& member : user_class.getScope().getClassData())
 				{
 					auto& type = member.second->getNode()->resolveType(scope, nullptr);
-					if (type.isObject())
+					if (type.canHoldObject())
 					{
 						object_indices.push_back(member.second->getIndex());
 						object_indices.push_back((int)type.getScriptObjectTypeId());

src/parser/SemanticAnalyzer.cpp
@@ -1981,9 +1981,7 @@ void SemanticAnalyzer::caseArrayLiteral(ASTArrayLiteral& host, void*)
 		}
 
 		// Convert to array type.
-		host.setReadType(
-				program.getTypeStore().getCanonicalType(
-						DataTypeArray(elementType)));
+		host.setReadType(DataTypeArray::create(elementType));
 	}
 	else
 	{
@@ -2017,9 +2015,7 @@ void SemanticAnalyzer::caseArrayLiteral(ASTArrayLiteral& host, void*)
 		if (!type)
 			type = &DataType::UNTYPED;
 
-		host.setReadType(
-				program.getTypeStore().getCanonicalType(
-						DataTypeArray(*type)));
+		host.setReadType(DataTypeArray::create(*type));
 	}
 
 	// If initialized, check that each element can be cast to type.

src/parser/Types.cpp
@@ -9,6 +9,7 @@
 
 using namespace ZScript;
 using std::string;
+using std::shared_ptr;
 
 ////////////////////////////////////////////////////////////////
 // TypeStore
@@ -233,10 +234,10 @@ DataTypeUnresolved* DataTypeUnresolved::clone() const
 	return copy;
 }
 
-DataType* DataTypeUnresolved::resolve(Scope& scope, CompileErrorHandler* errorHandler)
+DataType const* DataTypeUnresolved::resolve(Scope& scope, CompileErrorHandler* errorHandler)
 {
 	if (DataType const* type = lookupDataType(scope, *iden, errorHandler))
-		return type->clone();
+		return type;
 	return this;
 }
 DataType const* DataTypeUnresolved::baseType(Scope& scope, CompileErrorHandler* errorHandler) const
@@ -352,6 +353,24 @@ DataType const* DataTypeArray::baseType(Scope& scope, CompileErrorHandler* error
 DataType const& DataTypeArray::getBaseType() const
 {
 	return ZScript::getBaseType(elementType);
+}
+
+std::vector<std::unique_ptr<DataTypeArray>> DataTypeArray::created_arr_types;
+DataTypeArray const* DataTypeArray::create(DataType const& elementType)
+{
+	for(auto& ty : created_arr_types)
+		if(ty->elementType == elementType)
+			return ty.get();
+	return created_arr_types.emplace_back(new DataTypeArray(elementType)).get();
+}
+DataTypeArray const* DataTypeArray::create_owning(DataType* elementType)
+{
+	for(auto& ty : created_arr_types)
+		if(ty->elementType == *elementType)
+			return ty.get();
+	auto& ty = created_arr_types.emplace_back(new DataTypeArray(*elementType));
+	ty->owned_type.reset(elementType);
+	return ty.get();
 }
 
 ////////////////////////////////////////////////////////////////

src/parser/Types.h
@@ -213,7 +213,7 @@ namespace ZScript
 
 		// Resolution.
 		virtual bool isResolved() const {return true;}
-		virtual DataType* resolve(ZScript::Scope& scope, CompileErrorHandler* errorHandler) {return this;}
+		virtual DataType const* resolve(ZScript::Scope& scope, CompileErrorHandler* errorHandler) {return this;}
 		virtual DataType const& getBaseType() const {return *this;}
 		virtual DataType const* baseType(ZScript::Scope& scope, CompileErrorHandler* errorHandler) const = 0;
 		// Basics
@@ -303,7 +303,7 @@ namespace ZScript
 		DataTypeUnresolved* clone() const;
 		
 		virtual bool isResolved() const {return false;}
-		virtual DataType* resolve(ZScript::Scope& scope, CompileErrorHandler* errorHandler);
+		virtual DataType const* resolve(ZScript::Scope& scope, CompileErrorHandler* errorHandler);
 		virtual DataType const* baseType(ZScript::Scope& scope, CompileErrorHandler* errorHandler) const;
 		
 		virtual std::string getName() const;
@@ -360,10 +360,10 @@ namespace ZScript
 	class DataTypeArray : public DataType
 	{
 	public:
+		static DataTypeArray const* create(DataType const& elementType);
+		static DataTypeArray const* create_owning(DataType* elementType);
 		DataTypeArray(DataType const& elementType)
 			: DataType(NULL), elementType(elementType), owned_type() {}
-		DataTypeArray(DataType const& elementType, DataType* ownedType)
-			: DataType(NULL), elementType(elementType), owned_type(ownedType) {}
 		DataTypeArray* clone() const {return new DataTypeArray(*this);}
 
 		virtual std::string getName() const {
@@ -386,8 +386,9 @@ namespace ZScript
 	private:
 		DataType const& elementType;
 		std::shared_ptr<DataType> owned_type;
-
+		
 		int32_t selfCompare(DataType const& other) const;
+		static std::vector<std::unique_ptr<DataTypeArray>> created_arr_types;
 	};
 	
 	class DataTypeCustom : public DataType

commit 29b4ea0c40991b91631310811621d544b09bedae

fix(zscript)!: incorrect type being used when accessing some internal arrays
Regressed in c6f3f00

https://discord.com/channels/876899628556091432/1295149938031722547

src/parser/AST.cpp
@@ -1854,7 +1854,7 @@ bool ASTExprIndex::isConstant() const
 DataType const* ASTExprIndex::getReadType(Scope* scope, CompileErrorHandler* errorHandler)
 {
 	DataType const* type = array->getReadType(scope, errorHandler);
-	if (type && type->isArray() && !array->isTypeArrowNonUsrClass())
+	if (type && type->isArray())
 	{
 		DataTypeArray const* atype = static_cast<DataTypeArray const*>(type);
 		type = &atype->getElementType();
@@ -1865,7 +1865,7 @@ DataType const* ASTExprIndex::getReadType(Scope* scope, CompileErrorHandler* err
 DataType const* ASTExprIndex::getWriteType(Scope* scope, CompileErrorHandler* errorHandler)
 {
 	DataType const* type = array->getWriteType(scope, errorHandler);
-	if (type && type->isArray() && !array->isTypeArrowNonUsrClass())
+	if (type && type->isArray())
 	{
 		DataTypeArray const* atype = static_cast<DataTypeArray const*>(type);
 		type = &atype->getElementType();

tests/scripts/errors/arrays.zs
@@ -52,6 +52,10 @@ void main()
 
 	SizeOfArray(arr); // Exact match.
 	SizeOfArray(arr2); // int casts to int[] (no warning)
+
+	// OK.
+	// https://discord.com/channels/876899628556091432/1295149938031722547
+	ffc f = Screen->FFCs[0];
 }
 
 void fnOverloaded(int[] arr)

tests/scripts/errors/arrays_expected.txt
@@ -5,19 +5,19 @@ Pass 1: Parsing
 Pass 2: Preprocessing
 Pass 3: Registration
 Pass 4: Analyzing Code
-arrays.zs Line 69 @ Columns 37-43 - Warning S108: Arrays should be explicitly typed - change `int` to `int[]`
+arrays.zs Line 73 @ Columns 37-43 - Warning S108: Arrays should be explicitly typed - change `int` to `int[]`
 
-67    void fnOldStyleArray(int arr)
-68    {
-69        printf("fnOldStyleArray(int arr)", arr[0]);
+71    void fnOldStyleArray(int arr)
+72    {
+73        printf("fnOldStyleArray(int arr)", arr[0]);
                                              ^~~~~~
 
 
-arrays.zs Line 80 @ Columns 3-9 - Error S109: Arrays should be explicitly typed - change `int` to `int[]`
+arrays.zs Line 84 @ Columns 3-9 - Error S109: Arrays should be explicitly typed - change `int` to `int[]`
 
-78            #option LEGACY_ARRAYS off
-79    
-80            arr[0];
+82            #option LEGACY_ARRAYS off
+83    
+84            arr[0];
               ^~~~~~
 
 
@@ -97,11 +97,11 @@ stdout:
     {
       "range": {
         "start": {
-          "line": 68,
+          "line": 72,
           "character": 36
         },
         "end": {
-          "line": 68,
+          "line": 72,
           "character": 42
         }
       },
@@ -111,11 +111,11 @@ stdout:
     {
       "range": {
         "start": {
-          "line": 79,
+          "line": 83,
           "character": 2
         },
         "end": {
-          "line": 79,
+          "line": 83,
           "character": 8
         }
       },
@@ -236,9 +236,9 @@ stdout:
     }
   ],
   "metadata": {
-    "currentFileSymbols": 18,
-    "symbols": 24,
-    "identifiers": 63,
+    "currentFileSymbols": 19,
+    "symbols": 28,
+    "identifiers": 67,
     "elided": true
   }
 }