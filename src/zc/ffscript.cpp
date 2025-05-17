93aeaa1b2c114c38b07a2bf85c3209f0aec9c8c7
Connor Clark <cjamcl@gmail.com>
2025-05-16 19:44:59 -0700

    fix(zscript)!: implement setter for `mapdata::Flag`
    Overlooked recently in 74e5a2e
    
    (cherry picked from commit c7f19b2dc0d34b6c53a455c21ef8a74a8e6aead1)

--- src/zc/ffscript.cpp
+++ src/zc/ffscript.cpp
@@ -24018,6 +24018,23 @@ void set_register(int32_t arg, int32_t value)
 			break;
 		}
 
+		case MAPDATA_FLAG:
+		{
+			if (mapscr *scr = GetMapscr(ri->mapsref))
+			{
+				int32_t index = ri->d[rINDEX] / 10000;
+				if (BC::checkBounds(index, 0, 8*11 - 1, "mapdata::Flag") != SH::_NoError)
+				{
+					break;
+				}
+	
+				byte& flag = (&scr->flags)[index/8];
+				bool v = value;
+				SETFLAG(flag, 1 << (index%8), v);
+			} else Z_scripterrlog("mapdata::Flag[] pointer (%d) is either invalid or uninitialised.\n", ri->mapsref);
+			break;
+		}
+
 		
 	///----------------------------------------------------------------------------------------------------//
 	//shopdata sd-> Variables
