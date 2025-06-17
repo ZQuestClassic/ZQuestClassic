ZQuestClassic/ZQuestClassic

commit 71b272aee91b7e3c14e45efabc75e0796b38eb69

refactor: rewrite string message parsing and renderer
* Completely rewrite the string message parser / renderer - this will
  make it possible to add more advanced SCC features in the future.
* Executing a SCC no longer takes a frame per command.
* Use the same code for parsing a message string in the player and in
  the editor - this fixes a handful of differences w/ word wrapping that
  the player handled poorly.

https://discord.com/channels/876899628556091432/1277878877057978460

src/base/scc.cpp
@@ -1,0 +1,189 @@
+#include "base/scc.h"
+#include <map>
+#include <optional>
+
+static std::map<int, int> legacy_scc_num_args = {
+	{MSGC_COLOUR, 2}, // (cset,swatch)
+	{MSGC_SPEED, 1}, //  (speed)
+	{MSGC_GOTOIFGLOBAL, 3}, // (register, val, newtring)
+	{MSGC_GOTOIFRAND, 2}, // (factor, newstring)
+	{MSGC_GOTOIF, 2}, // (itemid, newstring)
+	{MSGC_GOTOIFCTR, 3}, // (counter, val, newstring)
+	{MSGC_GOTOIFCTRPC, 3}, // (counter, val, newstring)
+	{MSGC_GOTOIFTRI, 2}, // (level, newstring)
+	{MSGC_GOTOIFTRICOUNT, 2}, // (tricount, newstring)
+	{MSGC_CTRUP, 2}, // (counter, val)
+	{MSGC_CTRDN, 2}, // (counter, val)
+	{MSGC_CTRSET, 2}, // (counter, val)
+	{MSGC_CTRUPPC, 2}, // (counter, val)
+	{MSGC_CTRDNPC, 2}, // (counter, val)
+	{MSGC_CTRSETPC, 2}, // (counter, val)
+	{MSGC_GIVEITEM, 1}, //  (itemid)
+	{MSGC_TAKEITEM, 1}, //  (itemid)
+	{MSGC_WARP, 6}, // (dmap, screen, x, y, effect, sound
+	{MSGC_SETSCREEND, 4}, // (dmap, screen, reg, value)
+	{MSGC_SFX, 1}, //  (sfx)
+	{MSGC_MIDI, 1}, //  (midi)
+	{MSGC_NAME, 0},
+	{MSGC_GOTOIFCREEND, 5}, // (dmap, screen, reg, val, newstring)
+	{MSGC_CHANGEPORTRAIT, 0}, // not implemented
+	{MSGC_NEWLINE, 0},
+	{MSGC_SHDCOLOR, 2}, // (cset,swatch)
+	{MSGC_SHDTYPE, 1}, //  (type)
+	{MSGC_DRAWTILE, 5}, // (tile, cset, wid, hei, flip)
+	{MSGC_ENDSTRING, 0},
+	{MSGC_WAIT_ADVANCE, 0},
+	{MSGC_SETUPMENU, 5}, // (tile, cset, wid, hei, flip)
+	{MSGC_MENUCHOICE, 5}, // (pos, upos, dpos, lpos, rpos)
+	{MSGC_RUNMENU, 0},
+	{MSGC_GOTOMENUCHOICE, 2}, // (pos, newstring)
+	{MSGC_TRIGSECRETS, 1}, // (perm)
+	{MSGC_SETSCREENSTATE, 2}, // (ind, state)
+	{MSGC_SETSCREENSTATER, 4}, // (map, screen, ind, state)
+	{MSGC_FONT, 1}, // (font)
+	{MSGC_RUN_FRZ_GENSCR, 2}, // (script num, force_redraw)
+	{MSGC_TRIG_CMB_COPYCAT, 1}, // (copycat id)
+};
+
+// Increments i and returns the control code argument pointed at.
+static std::optional<word> parse_legacy_binary_argument(const std::string& str, size_t& i)
+{
+	if (i + 1 >= str.size()) return std::nullopt;
+
+	byte val = str[++i] - 1;
+	word ret = val;
+
+	// If an argument is succeeded by 255, then it's a three-byte argument -
+	// between 254 and 65535 (or whatever the maximum actually is)
+	if (i + 2 < str.size() && uint8_t(str[i + 1]) == 255)
+	{
+		val = str[i + 2];
+		word next = val;
+		ret += 254 * next;
+		i += 2;
+	}
+
+	return ret;
+}
+
+std::optional<int> get_scc_command_num_args(int code)
+{
+	auto it = legacy_scc_num_args.find(code);
+	if (it == legacy_scc_num_args.end())
+		return std::nullopt;
+	return it->second;
+}
+
+// Parses a "legacy" encoding of a msg str mixed with plain ascii and binary-encoded string commands.
+// A msg str is a mix of string literals and commands. The string literals are limited to the 32-126
+// ascii values.
+// Each string command is an encoded code of 1-30 or 127+, followed by a fixed number of
+// arguments.
+// The command code is written as +1 the actual value of the code.
+// The number of arguments is determined by legacy_scc_num_args.
+// Each argument is 1 or 3 bytes long. If the first byte is followed by a 255 byte, then it is a
+// 3-byte encoded value. See parse_legacy_binary_argument.
+value_and_warnings<ParsedMsgStr> parse_legacy_msg_str(const std::string& str)
+{
+	value_and_warnings<ParsedMsgStr> result;
+	auto& [parsed_msg_str, warnings] = result;
+
+	size_t prev = 0;
+	size_t cur = 0;
+
+	// The index after the last non-whitespace character.
+	size_t last = 0;
+	if (!str.empty())
+	{
+		for (int i = str.size() - 1; i >= 0; i--)
+		{
+			if (str[i] != ' ')
+			{
+				last = i + 1;
+				break;
+			}
+		}
+	}
+
+	while (cur < last)
+	{
+		byte c = str[cur];
+		if (c >= 32 && c <= 126)
+		{
+			cur++;
+			continue;
+		}
+
+		auto expected_args_opt = get_scc_command_num_args(c - 1);
+		if (!expected_args_opt)
+		{
+			warnings.push_back(fmt::format("Unknown command {}", (int)c - 1));
+			cur++;
+			continue;
+		}
+
+		if (prev != cur)
+		{
+			parsed_msg_str.literals.emplace_back(str.begin() + prev, str.begin() + cur);
+			parsed_msg_str.segment_types.push_back(0);
+		}
+		
+		auto& command = parsed_msg_str.commands.emplace_back();
+		command.code = c - 1;
+		parsed_msg_str.segment_types.push_back(1);
+
+		int expected_args = *expected_args_opt;
+		int found_num_args;
+		for (found_num_args = 0; found_num_args < expected_args; found_num_args++)
+		{
+			if (auto val = parse_legacy_binary_argument(str, cur))
+				command.args[found_num_args] = *val;
+			else break;
+		}
+		command.num_args = found_num_args;
+
+		if (found_num_args != expected_args)
+			warnings.push_back(fmt::format("For command {}, expected {} args but got {}",
+				(int)command.code, expected_args, found_num_args));
+
+		cur++;
+		prev = cur;
+	}
+
+	if (prev != last)
+	{
+		parsed_msg_str.literals.emplace_back(str.begin() + prev, str.begin() + last);
+		parsed_msg_str.segment_types.push_back(0);
+	}
+
+	return result;
+}
+
+// Generates a string with human-readable string commands. Each command starts with `\`, is followed
+// by the command code, then each successive argument separated by `\`. For example, this is a color
+// command: `\26\1\3`.
+// This is only used to present a human-readable/editable string in the editor. For saving back to
+// the legacy encoding format, see parse_to_legacy_msg_str_encoding.
+std::string ParsedMsgStr::serialize() const
+{
+	std::string result;
+
+	size_t literal_index = 0;
+	size_t command_index = 0;
+	for (int i = 0; i < segment_types.size(); i++)
+	{
+		if (segment_types[i] == 0)
+		{
+			result += literals[literal_index++];
+		}
+		else
+		{
+			auto& command = commands[command_index++];
+			result += fmt::format("\\{}", command.code);
+			for (int j = 0; j < command.num_args; j++)
+				result += fmt::format("\\{}", command.args[j]);
+		}
+	}
+
+	return result;
+}

CMakeLists.txt
@@ -594,6 +594,7 @@ set(zcbase_sources
 	src/base/qst.cpp
 	src/base/random.cpp
 	src/base/render.cpp
+	src/base/scc.cpp
 	src/base/sin1.cpp
 	src/base/util.cpp
 	src/base/version.cpp

src/base/general.h
@@ -4,6 +4,13 @@
 #include "base/ints.h"
 #include "base/zfix.h"
 
+#include <string>
+#include <utility>
+#include <vector>
+
+template <typename T>
+using value_and_warnings = std::pair<T, std::vector<std::string>>;
+
 int32_t get_bit(byte const* bitstr,int32_t bit);
 void set_bit(byte *bitstr,int32_t bit,bool val);
 bool toggle_bit(byte *bitstr,int32_t bit);

src/base/msgstr.cpp
@@ -1,4 +1,5 @@
 #include "msgstr.h"
+#include "allegro/debug.h"
 
 MsgStr *MsgStrings;
 
@@ -16,6 +17,7 @@ void MsgStr::copyText(MsgStr const& other)
 	s.resize(other.s.size());
 	s.assign(other.s.c_str());
 	nextstring=other.nextstring;
+	parse();
 }
 
 // Copy style data - everything except s, nextstring, and listpos
@@ -58,7 +60,10 @@ void MsgStr::copyAll(MsgStr const& other)
 void MsgStr::advpaste(MsgStr const& other, bitstring const& flags)
 {
 	if(flags.get(STR_ADVP_TEXT))
+	{
 		s = other.s;
+		parsed_msg_str = {};
+	}
 	if(flags.get(STR_ADVP_NEXTSTR))
 		nextstring = other.nextstring;
 	if(flags.get(STR_ADVP_POSSZ))
@@ -109,6 +114,7 @@ void MsgStr::clear()
 {
 	s = "";
 	s.shrink_to_fit();
+	parsed_msg_str = {};
 	nextstring = 0;
 	tile=0;
 	cset=0;
@@ -138,8 +144,169 @@ void MsgStr::clear()
 	drawlayer=6;
 }
 
-bool valid_str(char const* ptr, char cancel)
+void MsgStr::setFromLegacyEncoding(std::string text)
 {
-	return ptr && ptr[0] && ptr[0] != cancel;
+	s = text;
+	parse();
 }
 
+void MsgStr::parse() const
+{
+	auto [parsed_msg_str, warnings] = parse_legacy_msg_str(s);
+	if (warnings.size())
+	{
+		al_trace("Warning: found message string with SCC warnings: %s\n", parsed_msg_str.serialize().c_str());
+		for (auto& error : warnings)
+			al_trace("\t%s\n", error.c_str());
+	}
+
+	this->parsed_msg_str = std::move(parsed_msg_str);
+}
+
+std::string MsgStr::serialize() const
+{
+	if (parsed_msg_str.literals.empty())
+		parse();
+	return parsed_msg_str.serialize();
+}
+
+MsgStr::iterator MsgStr::create_iterator() const
+{
+	if (parsed_msg_str.literals.empty())
+		parse();
+
+	MsgStr::iterator it{this};
+	return it;
+}
+
+bool MsgStr::iterator::next()
+{
+	return next_character();
+}
+
+void MsgStr::iterator::set_buffer(std::string text)
+{
+	buffer = text;
+	state = text.empty() ? IDLE : CHARACTER;
+	word = "";
+	j = 0;
+	k = 0;
+}
+
+bool MsgStr::iterator::done() const
+{
+	return state == DONE;
+}
+
+std::string MsgStr::iterator::peek(byte n) const
+{
+	if (k + n < word.length())
+		return word.substr(k + n, 1);
+
+	size_t i = j + n - (word.length() - k + 1);
+	if (i < buffer.length())
+		return buffer.substr(i, 1);
+
+	return "";
+}
+
+const char* MsgStr::iterator::remaining_word() const
+{
+	return word.c_str() + k - 1;
+}
+
+bool MsgStr::iterator::next_segment()
+{
+	buffer = "";
+	j = 0;
+	k = 0;
+
+	if (str->parsed_msg_str.segment_types.size() <= segment_index)
+	{
+		state = DONE;
+		return true;
+	}
+
+	if (str->parsed_msg_str.segment_types[segment_index++] == 0)
+	{
+		state = CHARACTER;
+		buffer = str->parsed_msg_str.literals[literal_index++];
+	}
+	else
+	{
+		state = COMMAND;
+		command = str->parsed_msg_str.commands[command_index++];
+	}
+
+	return false;
+}
+
+bool MsgStr::iterator::next_word()
+{
+	k = 0;
+	word = "";
+
+	if (j >= buffer.size())
+	{
+		if (post_segment_delay)
+		{
+			state = IDLE;
+			post_segment_delay--;
+			return false;
+		}
+
+		if (next_segment()) return true;
+	}
+
+	// Buffer is empty here only when processing a command.
+	if (buffer.empty())
+		return false;
+
+	// Set `text` to the "word" currently being processed.
+	// If string wrapping is enabled, this is the entire word leading up to the next space (or end of the strings).
+	// Note that `s` may only be from a string liteal, so commands in-between literals also
+	// counts as a word boundrary for wrapping purposes.
+	if (str->stringflags & STRINGFLAG_WRAP)
+	{
+		// Spaces are like one-character words.
+		if (buffer[j] == ' ')
+		{
+			word = buffer[j++];
+		}
+		else
+		{
+			// Complete words finish at spaces.
+			int k = j;
+			while (j < buffer.size() && buffer[j] != ' ')
+				j++;
+			word = buffer.substr(k, j - k);
+		}
+	}
+	else
+	{
+		word = buffer[j++];
+	}
+
+	return false;
+}
+
+bool MsgStr::iterator::next_character()
+{
+	character = "";
+
+	if (state == 0 || k == word.size())
+		if (next_word()) return true;
+
+	if (state == COMMAND)
+		return false;
+
+	if (k < word.size())
+	{
+		state = CHARACTER;
+		character = word[k++];
+		return false;
+	}
+
+	state = IDLE;
+	return false;
+}

src/base/msgstr.h
@@ -5,55 +5,7 @@
 #include "base/ints.h"
 #include "base/headers.h"
 #include "base/containers.h"
-
-#define MAX_SCC_ARG 65023
-
-//SCCs
-/* Note: Printable ASCII begins at 32 and ends at 126, inclusive. */
-#define MSGC_COLOUR                1    // 2 args (cset,swatch)
-#define MSGC_SPEED                 2    // 1 arg  (speed)
-#define MSGC_GOTOIFGLOBAL          3    // 3 args (register, val, newtring)
-#define MSGC_GOTOIFRAND            4    // 2 args (factor, newstring)
-#define MSGC_GOTOIF                5    // 2 args (itemid, newstring)
-#define MSGC_GOTOIFCTR             6    // 3 args (counter, val, newstring)
-#define MSGC_GOTOIFCTRPC           7    // 3 args (counter, val, newstring)
-#define MSGC_GOTOIFTRI             8    // 2 args (level, newstring)
-#define MSGC_GOTOIFTRICOUNT        9    // 2 args (tricount, newstring)
-#define MSGC_CTRUP                 10   // 2 args (counter, val)
-#define MSGC_CTRDN                 11   // 2 args (counter, val)
-#define MSGC_CTRSET                12   // 2 args (counter, val)
-#define MSGC_CTRUPPC               13   // 2 args (counter, val)
-#define MSGC_CTRDNPC               14   // 2 args (counter, val)
-#define MSGC_CTRSETPC              15   // 2 args (counter, val)
-#define MSGC_GIVEITEM              16   // 1 arg  (itemid)
-#define MSGC_TAKEITEM              17   // 1 arg  (itemid)
-#define MSGC_WARP                  18   // 6 args (dmap, screen, x, y, effect, sound
-#define MSGC_SETSCREEND            19   // 4 args (dmap, screen, reg, value)
-#define MSGC_SFX                   20   // 1 arg  (sfx)
-#define MSGC_MIDI                  21   // 1 arg  (midi)
-#define MSGC_NAME                  22   // 0 args
-#define MSGC_GOTOIFCREEND          23   // 5 args (dmap, screen, reg, val, newstring)
-#define MSGC_CHANGEPORTRAIT        24   // not implemented
-#define MSGC_NEWLINE               25   // 0 args
-#define MSGC_SHDCOLOR              26   // 2 args (cset,swatch)
-#define MSGC_SHDTYPE               27   // 1 arg  (type)
-#define MSGC_DRAWTILE              28   // 5 args (tile, cset, wid, hei, flip)
-#define MSGC_ENDSTRING             29   // 0 args
-#define MSGC_WAIT_ADVANCE          30   // 0 args
-//31
-//32
-//33-127 are ascii chars, unusable
-#define MSGC_SETUPMENU             128  // 5 args (tile, cset, wid, hei, flip)
-#define MSGC_MENUCHOICE            129  // 5 args (pos, upos, dpos, lpos, rpos)
-#define MSGC_RUNMENU               130  // 0 args
-#define MSGC_GOTOMENUCHOICE        131  // 2 args (pos, newstring)
-#define MSGC_TRIGSECRETS           132  // 1 arg (perm)
-#define MSGC_SETSCREENSTATE        133  // 2 args (ind, state)
-#define MSGC_SETSCREENSTATER       134  // 4 args (map, screen, ind, state)
-#define MSGC_FONT                  135  // 1 arg (font)
-#define MSGC_RUN_FRZ_GENSCR        136  // 2 args (script num, force_redraw)
-#define MSGC_TRIG_CMB_COPYCAT      137  // 1 arg (copycat id)
-//138+
+#include "base/scc.h"
 
 #define MSG_NEW_SIZE 8192
 #define MSGBUF_SIZE (MSG_NEW_SIZE*8)
@@ -84,7 +36,10 @@ enum
 
 struct MsgStr
 {
+	// This is the underlying, raw data. SCC commands are encoded in a special way.
+	// Use `serialize` instead to get a human readable string.
 	std::string s;
+	mutable ParsedMsgStr parsed_msg_str;
 	word nextstring;
 	int32_t tile;
 	byte cset;
@@ -119,6 +74,68 @@ struct MsgStr
 	void copyAll(MsgStr const& other);
 	void advpaste(MsgStr const& other, bitstring const& pasteflags);
 	void clear();
+	void setFromLegacyEncoding(std::string text);
+	void parse() const;
+	// A human-readable encoding of the string.
+	std::string serialize() const;
+
+	struct iterator
+	{
+		enum state {
+			NOT_STARTED,
+			CHARACTER,
+			COMMAND,
+			IDLE,
+			DONE,
+		};
+
+		iterator(const MsgStr* str) : str(str) {}
+
+		// Do one iteration of processing the msg str, and sets `state` as appropriate.
+		// Returns true if done.
+		bool next();
+		void set_buffer(std::string text);
+		// Returns true if done.
+		bool done() const;
+		// Returns then nth next character (where 0 is the next one).
+		// Only looks as far as the current buffer - the start of the next segment is a breakpoint.
+		std::string peek(byte n) const;
+		// Returns the rest of the current word.
+		const char* remaining_word() const;
+
+		state state = NOT_STARTED;
+		// A single character. This is a string because eventually this may contain a unicode
+		// character, which is larger than one byte.
+		// This is only valid if `state` is CHARACTER.
+		std::string character;
+		// This is only valid if `state` is COMMAND.
+		StringCommand command;
+		// How many frames to idle after a segment finishes.
+		int post_segment_delay = 0;
+
+	private:
+		bool next_segment();
+		bool next_word();
+		bool next_character();
+
+		const MsgStr* str;
+		// The text from the current segment.
+		std::string buffer;
+		// The text from buffer for the current "word".
+		std::string word;
+		// The next index to use for `str->parsed_msg_str.segment_types`.
+		int segment_index = 0;
+		// The next index to use for `str->parsed_msg_str.literals`.
+		int literal_index = 0;
+		// The next index to use for `str->parsed_msg_str.commands`.
+		int command_index = 0;
+		// The index into `buffer`, where the next `word` will begin.
+		int j = 0;
+		// The index into `word` - `character` will be set based on this when `next()` is called.
+		int k = 0;
+	};
+
+	iterator create_iterator() const;
 };
 
 extern MsgStr* MsgStrings;

src/base/qst.cpp
@@ -3515,8 +3515,7 @@ int32_t readrules(PACKFILE *f, zquestheader *Header)
 
 void init_msgstr(MsgStr *str)
 {
-	str->s = "";
-	str->s.shrink_to_fit();
+	str->setFromLegacyEncoding("");
 	str->nextstring=0;
     str->tile=0;
     str->cset=0;
@@ -3559,7 +3558,7 @@ void init_msgstrings(int32_t start, int32_t end)
     
     if(start==0)
     {
-		MsgStrings[0].s = "(None)";
+		MsgStrings[0].setFromLegacyEncoding("(None)");
         MsgStrings[0].listpos = 0;
     }
 }
@@ -3640,7 +3639,7 @@ int32_t readstrings(PACKFILE *f, zquestheader *Header)
 			}
 			
 			buf[74] = '\0';
-			tempMsgString.s = buf;
+			tempMsgString.setFromLegacyEncoding(buf);
 				
 			if(!p_getc(&tempbyte,f))
 			{
@@ -3676,7 +3675,9 @@ int32_t readstrings(PACKFILE *f, zquestheader *Header)
 			}
 			
 			if (!should_skip)
+			{
 				MsgStrings[x] = tempMsgString;
+			}
 		}
 	}
 	else
@@ -3709,12 +3710,10 @@ int32_t readstrings(PACKFILE *f, zquestheader *Header)
 			return qe_invalid;
 		}
 		
-		if(temp_msg_count >= msg_strings_size)
+		if(temp_msg_count >= msg_strings_size && !should_skip)
 		{
 			Z_message("Reallocating string buffer...\n");
 			
-			// if((MsgStrings=(MsgStr*)_al_sane_realloc(MsgStrings,sizeof(MsgStr)*MAXMSGS))==NULL)
-				// return qe_nomem;
 			delete[] MsgStrings;
 			MsgStrings = new MsgStr[MAXMSGS];
 			msg_strings_size = MAXMSGS;
@@ -3722,13 +3721,13 @@ int32_t readstrings(PACKFILE *f, zquestheader *Header)
 			{
 				MsgStrings[q].clear();
 			}
-			//memset(MsgStrings, 0, sizeof(MsgStr)*MAXMSGS);
 		}
 		
 		//reset the message strings
 		if(s_version < 7)
 			set_qr(qr_OLD_STRING_EDITOR_MARGINS,true);
-		init_msgstrings(0,msg_strings_size);
+		if (!should_skip)
+			init_msgstrings(0,msg_strings_size);
 		
 		int32_t string_length=(s_version<2)?73:145;
 		
@@ -3769,7 +3768,7 @@ int32_t readstrings(PACKFILE *f, zquestheader *Header)
 			if(s_version<2)
 			{
 				buf[72] = '\0';
-				tempMsgString.s = buf;
+				tempMsgString.setFromLegacyEncoding(buf);
 			}
 			else
 			{
@@ -3784,7 +3783,7 @@ int32_t readstrings(PACKFILE *f, zquestheader *Header)
 				}
 				if(string_length > 8192) string_length = 8192;
 				buf[string_length]='\0'; //Force-terminate
-				tempMsgString.s = buf;
+				tempMsgString.setFromLegacyEncoding(buf);
 				
 				if ( s_version >= 6 )
 				{
@@ -3940,8 +3939,11 @@ int32_t readstrings(PACKFILE *f, zquestheader *Header)
 					}
 				}
 			}
-			
-			MsgStrings[i].copyAll(tempMsgString);
+
+			if (!should_skip)
+			{
+				MsgStrings[i].copyAll(tempMsgString);
+			}
 		}
 	}
 	

src/base/scc.h
@@ -1,0 +1,78 @@
+#ifndef ZC_SCC_H_
+#define ZC_SCC_H_
+
+#include "base/general.h"
+
+#include <optional>
+#include <vector>
+#include <string_view>
+
+#define MAX_SCC_ARG 65023
+
+/* Note: Printable ASCII begins at 32 and ends at 126, inclusive. */
+#define MSGC_COLOUR                1    // 2 args (cset,swatch)
+#define MSGC_SPEED                 2    // 1 arg  (speed)
+#define MSGC_GOTOIFGLOBAL          3    // 3 args (register, val, newtring)
+#define MSGC_GOTOIFRAND            4    // 2 args (factor, newstring)
+#define MSGC_GOTOIF                5    // 2 args (itemid, newstring)
+#define MSGC_GOTOIFCTR             6    // 3 args (counter, val, newstring)
+#define MSGC_GOTOIFCTRPC           7    // 3 args (counter, val, newstring)
+#define MSGC_GOTOIFTRI             8    // 2 args (level, newstring)
+#define MSGC_GOTOIFTRICOUNT        9    // 2 args (tricount, newstring)
+#define MSGC_CTRUP                 10   // 2 args (counter, val)
+#define MSGC_CTRDN                 11   // 2 args (counter, val)
+#define MSGC_CTRSET                12   // 2 args (counter, val)
+#define MSGC_CTRUPPC               13   // 2 args (counter, val)
+#define MSGC_CTRDNPC               14   // 2 args (counter, val)
+#define MSGC_CTRSETPC              15   // 2 args (counter, val)
+#define MSGC_GIVEITEM              16   // 1 arg  (itemid)
+#define MSGC_TAKEITEM              17   // 1 arg  (itemid)
+#define MSGC_WARP                  18   // 6 args (dmap, screen, x, y, effect, sound
+#define MSGC_SETSCREEND            19   // 4 args (dmap, screen, reg, value)
+#define MSGC_SFX                   20   // 1 arg  (sfx)
+#define MSGC_MIDI                  21   // 1 arg  (midi)
+#define MSGC_NAME                  22   // 0 args
+#define MSGC_GOTOIFCREEND          23   // 5 args (dmap, screen, reg, val, newstring)
+#define MSGC_CHANGEPORTRAIT        24   // not implemented
+#define MSGC_NEWLINE               25   // 0 args
+#define MSGC_SHDCOLOR              26   // 2 args (cset,swatch)
+#define MSGC_SHDTYPE               27   // 1 arg  (type)
+#define MSGC_DRAWTILE              28   // 5 args (tile, cset, wid, hei, flip)
+#define MSGC_ENDSTRING             29   // 0 args
+#define MSGC_WAIT_ADVANCE          30   // 0 args
+//32-126 are ascii chars, so cannot use 31 for SCC code (since 31 + 1 = space).
+//127 unused
+#define MSGC_SETUPMENU             128  // 5 args (tile, cset, wid, hei, flip)
+#define MSGC_MENUCHOICE            129  // 5 args (pos, upos, dpos, lpos, rpos)
+#define MSGC_RUNMENU               130  // 0 args
+#define MSGC_GOTOMENUCHOICE        131  // 2 args (pos, newstring)
+#define MSGC_TRIGSECRETS           132  // 1 arg (perm)
+#define MSGC_SETSCREENSTATE        133  // 2 args (ind, state)
+#define MSGC_SETSCREENSTATER       134  // 4 args (map, screen, ind, state)
+#define MSGC_FONT                  135  // 1 arg (font)
+#define MSGC_RUN_FRZ_GENSCR        136  // 2 args (script num, force_redraw)
+#define MSGC_TRIG_CMB_COPYCAT      137  // 1 arg (copycat id)
+//138+
+
+struct StringCommand
+{
+	byte code;
+	byte num_args;
+	word args[6];
+};
+
+struct ParsedMsgStr
+{
+	std::vector<std::string_view> literals;
+	std::vector<StringCommand> commands;
+	// 0 = literal
+	// 1 = command
+	std::vector<byte> segment_types;
+
+	std::string serialize() const;
+};
+
+std::optional<int> get_scc_command_num_args(int code);
+value_and_warnings<ParsedMsgStr> parse_legacy_msg_str(const std::string& str);
+
+#endif

src/base/zdefs.cpp
@@ -1579,3 +1579,8 @@ const char* ScriptTypeToString(ScriptType type)
 	};
 	return script_types[(int)type];
 }
+
+bool valid_str(char const* ptr, char cancel)
+{
+	return ptr && ptr[0] && ptr[0] != cancel;
+}

commit cedbfc6b8c36503139f7133e55f1d835647072e1

fix!: crash from executing some message strings
If the last command has an argument of value 32, the new string parser
was messing up and causing a crash when executing the string.

Regressed recently in 71b272a.

src/base/scc.cpp
@@ -150,7 +150,11 @@ value_and_warnings<ParsedMsgStr> parse_legacy_msg_str(const std::string& str)
 		prev = cur;
 	}
 
-	if (prev != last)
+	// prev can be larger than last here, since the calculation for last unfortunately may also
+	// include space (ascii 32) characters being used as the last arg of the last command.
+	// An example of that is message 48 of The Slipstream:
+	// .tmp/replay_uploads/50C00A5197148F3D1587A16509DCC89F/50C00A5197148F3D1587A16509DCC89F.qst
+	if (prev < last)
 	{
 		parsed_msg_str.literals.emplace_back(str.begin() + prev, str.begin() + last);
 		parsed_msg_str.segment_types.push_back(0);