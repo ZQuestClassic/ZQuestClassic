b4fd3580b29b43d7e19c88435267af9226a6c6d9
Connor Clark <cjamcl@gmail.com>
2025-06-28 17:04:43 -0700

    refactor(zscript)!: change read_array default to 0
    (cherry picked from commit f67e729b096136269c09001a95baaab5eada53e8)

--- src/zc/ffscript.cpp
+++ src/zc/ffscript.cpp
@@ -3204,7 +3204,7 @@ template <typename T, size_t N>
 static int read_array(const T(&arr)[N], int index, const char* context)
 {
 	if (BC::checkBounds(index, 0, N - 1, context) != SH::_NoError)
-		return -1;
+		return 0;
 
 	return arr[index];
 }
