ZQuestClassic/ZQuestClassic

commit df3ce2079c6c7ad845d0676e529a60c439ec338f

refactor(zscript): use common abstraction for arrays
Built-in arrays (such as `Screen->State[]`) are now implemented using a
new shared abstraction.

- improves maintainability of arrays
- consistent application of index/value bounds (this was often wrong
  and many issues are now resolved)
- enables new features in the future (such as iterating built-in arrays)

https://discord.com/channels/876899628556091432/1382495697907093574

src/zc/scripting/types/ffc.cpp
@@ -1,0 +1,29 @@
+#include "zc/scripting/arrays.h"
+
+// ffc arrays.
+
+static ArrayRegistrar FFFLAGSD_registrar(FFFLAGSD, []{
+	static ScriptingArray_ObjectMemberBitwiseFlags<ffcdata, &ffcdata::flags, 32> impl;
+	impl.setDefaultValue(0);
+	impl.setSideEffect([](auto ffc, int index, int value){
+		if (index == ffc_solid || index == ffc_changer)
+			ffc->updateSolid();
+	});
+	return &impl;
+}());
+
+static ArrayRegistrar FFINITDD_registrar(FFINITDD, []{
+	static ScriptingArray_ObjectMemberCArray<ffcdata, &ffcdata::initd> impl;
+	impl.setDefaultValue(-10000);
+	impl.setMul10000(false);
+	impl.boundSetterIndex();
+	return &impl;
+}());
+
+static ArrayRegistrar FFMISCD_registrar(FFMISCD, []{
+	static ScriptingArray_ObjectMemberCArray<ffcdata, &ffcdata::miscellaneous> impl;
+	impl.setDefaultValue(-10000);
+	impl.setMul10000(false);
+	impl.boundSetterIndex();
+	return &impl;
+}());

commit d9ed447d57aea60d8914ec0ef695646df042a371

fix(zscript)!: correctly update ffc solidity when changing flag
Regressed recently in df3ce20.

src/zc/scripting/types/ffc.cpp
@@ -6,7 +6,8 @@ static ArrayRegistrar FFFLAGSD_registrar(FFFLAGSD, []{
 	static ScriptingArray_ObjectMemberBitwiseFlags<ffcdata, &ffcdata::flags, 32> impl;
 	impl.setDefaultValue(0);
 	impl.setSideEffect([](auto ffc, int index, int value){
-		if (index == ffc_solid || index == ffc_changer)
+		auto flag = F(index);
+		if (flag == ffc_solid || flag == ffc_changer)
 			ffc->updateSolid();
 	});
 	return &impl;