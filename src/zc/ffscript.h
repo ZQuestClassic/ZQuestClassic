ZQuestClassic/ZQuestClassic

commit df3ce2079c6c7ad845d0676e529a60c439ec338f

refactor(zscript): use common abstraction for arrays
Built-in arrays (such as `Screen->State[]`) are now implemented using a
new shared abstraction.

- improves maintainability of arrays
- consistent application of index/value bounds (this was often wrong
  and many issues are now resolved)
- enables new features in the future (such as iterating built-in arrays)

https://discord.com/channels/876899628556091432/1382495697907093574

src/zc/ffscript.h
@@ -1,8 +1,10 @@
 #ifndef FFSCRIPT_H_
 #define FFSCRIPT_H_
 
+#include "base/dmap.h"
 #include "base/general.h"
 #include "base/mapscr.h"
+#include "base/misctypes.h"
 #include "base/zdefs.h"
 #include "base/initdata.h"
 #include "parser/parserDefs.h"
@@ -628,6 +630,78 @@ void load_genscript(const gamedata& gd);
 void load_genscript(const zinitdata& gd);
 void save_genscript(gamedata& gd);
 
+enum class mapdata_type
+{
+	None,
+	CanonicalScreen,
+	TemporaryCurrentScreen,
+	TemporaryCurrentRegion,
+	TemporaryScrollingScreen,
+	TemporaryScrollingRegion,
+};
+
+struct mapdata {
+	mapdata_type type;
+	mapscr* scr;
+	int screen;
+	int layer;
+
+	bool temporary() const
+	{
+		return type != mapdata_type::None && type != mapdata_type::CanonicalScreen;
+	}
+
+	bool canonical() const
+	{
+		return type == mapdata_type::CanonicalScreen;
+	}
+
+	bool current() const
+	{
+		return type == mapdata_type::TemporaryCurrentRegion || type == mapdata_type::TemporaryCurrentScreen;
+	}
+
+	bool scrolling() const
+	{
+		return type == mapdata_type::TemporaryScrollingRegion || type == mapdata_type::TemporaryScrollingScreen;
+	}
+
+	int max_pos();
+	rpos_handle_t resolve_pos(int pos);
+	ffc_handle_t resolve_ffc_handle(int index);
+	ffcdata* resolve_ffc(int index);
+};
+
+newcombo* checkCombo(int32_t ref, bool skipError = false);
+newcombo* checkComboFromTriggerRef(dword ref);
+dmap* checkDmap(int32_t ref);
+ffcdata* checkFFC(int32_t ref);
+enemy* checkNPC(int32_t ref);
+guydata* checkNPCData(int32_t ref);
+item* checkItem(int32_t ref);
+itemdata* checkItemData(int32_t ref);
+mapdata* checkMapData(int32_t ref);
+mapscr* checkMapDataScr(int32_t ref);
+screendata* checkScreen(int32_t ref);
+user_paldata* checkPalData(int32_t ref, bool skipError = false);
+weapon* checkWpn(int32_t uid);
+weapon* checkLWpn(int32_t uid);
+weapon* checkEWpn(int32_t uid);
+bottletype* checkBottleData(int32_t ref, bool skipError = false);
+bottleshoptype *checkBottleShopData(int32_t ref, bool skipError = false);
+item_drop_object *checkDropSetData(int32_t ref);
+wpndata *checkSpriteData(int32_t ref);
+MsgStr *checkMessageData(int32_t ref);
+user_genscript *checkGenericScr(int32_t ref);
+SubscrWidget *checkSubWidg(int32_t ref, int req_widg_ty = -1, int req_sub_ty = -1);
+ZCSubscreen *checkSubData(int32_t ref, int req_ty = -1);
+SubscrPage *checkSubPage(int32_t ref, int req_ty = -1);
+combo_trigger* checkComboTrigger(dword ref);
+combo_trigger* get_first_combo_trigger();
+
+std::tuple<byte,int8_t,byte,word> from_subref(dword ref);
+dword get_subref(int sub, byte ty, byte pg = 0, word ind = 0);
+
 class FFScript
 {
 	
@@ -866,8 +940,8 @@ void do_paldata_getcolor();
 void do_paldata_setcolor();
 void do_paldata_clearcolor();
 void do_paldata_clearcset();
-int32_t do_paldata_getrgb(int32_t v);
-void do_paldata_setrgb(int32_t v, int32_t val);
+int32_t do_paldata_getrgb(user_paldata* pd, int32_t index, int32_t c);
+void do_paldata_setrgb(user_paldata* pd, int32_t index, int32_t val, int32_t c);
 void do_paldata_mix();
 void do_paldata_mixcset();
 void do_paldata_copy();
@@ -911,7 +985,7 @@ void do_warp_ex(const bool v);
 int32_t quest_format[versiontypesLAST];
 byte skip_ending_credits; //checked in ending.cpp. If > 0, then we skip the game credits, but not Hero's Win script. -Z
 
-byte system_suspend[susptLAST];
+bool system_suspend[susptLAST];
 
 int32_t coreflags;
 int32_t usr_midi_volume, usr_digi_volume, usr_sfx_volume, usr_music_volume, usr_panstyle;
@@ -1402,6 +1476,7 @@ void clearScriptHelperData();
 
 int32_t get_screenflags(mapscr *m, int32_t flagset);
 int32_t get_screeneflags(mapscr *m, int32_t flagset);
+int32_t get_ref_map_index(int32_t ref);
 
 sprite* ResolveBaseSprite(int32_t uid);
 item* ResolveItemSprite(int32_t uid);
@@ -1802,6 +1877,8 @@ struct ScriptEngineData {
 extern std::map<std::pair<ScriptType, int>, ScriptEngineData> scriptEngineDatas;
 
 void on_reassign_script_engine_data(ScriptType type, int index);
+ScriptEngineData& get_ffc_script_engine_data(int index);
+ScriptEngineData& get_item_script_engine_data(int index);
 
 extern FFScript FFCore;
 extern byte flagpos;
@@ -1811,6 +1888,7 @@ void ornextflag(bool flag);
 
 int32_t get_mi();
 int32_t get_total_mi();
+int get_mouse_state(int index);
 
 bool pc_overflow(dword pc, bool print_err = true);
 

src/zc/scripting/arrays.h
@@ -1,0 +1,626 @@
+#ifndef ZC_SCRIPTING_ARRAYS_H_
+#define ZC_SCRIPTING_ARRAYS_H_
+
+// See arrays.cpp for how to define scripting arrays.
+
+#include "base/combo.h"
+#include "base/general.h"
+#include "new_subscr.h"
+#include "zc/ffscript.h"
+#include <cstddef>
+#include <functional>
+#include <type_traits>
+#include <utility>
+
+int zasm_array_size(int zasm_var);
+int zasm_array_get(int zasm_var, int ref, int index);
+bool zasm_array_set(int zasm_var, int ref, int index, int value);
+bool zasm_array_supports(int zasm_var);
+
+template <typename T, size_t N>
+static constexpr size_t comptime_array_size(T (&)[N])
+{
+	return N;
+}
+
+template <auto Fp>
+struct field_type;
+
+template<typename R, typename T, R (T::*FP)>
+struct field_type<FP>
+{
+    using type = R;
+};
+
+template<typename R, typename T, typename...Args, R (T::*FP)(Args...)>
+struct field_type<FP>
+{
+    using type = R;
+}; 
+
+template<auto FP>
+using field_type_t = typename field_type<FP>::type;
+
+namespace transforms
+{
+	template<int min_inclusive, int max_exclusive>
+	static std::optional<int> validate(int value)
+	{
+		if (value < min_inclusive || value >= max_exclusive)
+			return std::nullopt;
+
+		return value;
+	}
+
+	template<int max_exclusive>
+	static std::optional<int> validate(int value)
+	{
+		if (value < 0 || value >= max_exclusive)
+			return std::nullopt;
+
+		return value;
+	}
+
+	template<int low, int high>
+	static std::optional<int> vbound(int value)
+	{
+		return ::vbound(value, low, high);
+	}
+
+	static int vboundByte(int value)
+	{
+		return ::vbound(value, 0, 255);
+	}
+
+	static int vboundWord(int value)
+	{
+		return ::vbound(value, 0, 65535);
+	}
+
+	static int vboundInt(int value)
+	{
+		return ::vbound(value, -214747, 214747);
+	}
+}
+
+// Helper struct to make the static_assert dependent on the template parameter
+template<typename>
+struct always_false : std::false_type {};
+
+template<typename T>
+static T* resolveScriptingObject(int ref)
+{
+	if constexpr (std::is_same<T, user_genscript>::value)
+		return checkGenericScr(ref);
+	else if constexpr (std::is_same<T, sprite>::value)
+		return ResolveBaseSprite(ref);
+	else if constexpr (std::is_same<T, ffcdata>::value)
+		return checkFFC(ref);
+	else if constexpr (std::is_same<T, enemy>::value)
+		return checkNPC(ref);
+	else if constexpr (std::is_same<T, guydata>::value)
+		return checkNPCData(ref);
+	else if constexpr (std::is_same<T, item>::value)
+		return checkItem(ref);
+	else if constexpr (std::is_same<T, itemdata>::value)
+		return checkItemData(ref);
+	else if constexpr (std::is_same<T, newcombo>::value)
+		return checkCombo(ref);
+	else if constexpr (std::is_same<T, combo_trigger>::value)
+		return checkComboTrigger(ref);
+	else if constexpr (std::is_same<T, weapon>::value)
+		return checkWpn(ref);
+	else if constexpr (std::is_same<T, dmap>::value)
+		return checkDmap(ref);
+	else if constexpr (std::is_same<T, mapdata>::value)
+		return checkMapData(ref);
+	else if constexpr (std::is_same<T, mapscr>::value)
+		return checkMapDataScr(ref);
+	else if constexpr (std::is_same<T, user_paldata>::value)
+		return checkPalData(ref);
+	else if constexpr (std::is_same<T, screendata>::value)
+		return checkScreen(ref);
+	else if constexpr (std::is_same<T, ZCSubscreen>::value)
+		return checkSubData(ref);
+	else if constexpr (std::is_same<T, ZCSubscreenActive>::value)
+		return (ZCSubscreenActive*)checkSubData(ref, sstACTIVE);
+	else if constexpr (std::is_same<T, SubscrWidget>::value)
+		return checkSubWidg(ref);
+	else if constexpr (std::is_same<T, SubscrWidgetActive>::value)
+		return (SubscrWidgetActive*)checkSubWidg(ref, sstACTIVE);
+	else if constexpr (std::is_same<T, bottletype>::value)
+		return checkBottleData(ref);
+	else if constexpr (std::is_same<T, bottleshoptype>::value)
+		return checkBottleShopData(ref);
+	else if constexpr (std::is_same<T, item_drop_object>::value)
+		return checkDropSetData(ref);
+	else if constexpr (std::is_same<T, wpndata>::value)
+		return checkSpriteData(ref);
+	else if constexpr (std::is_same<T, MsgStr>::value)
+		return checkMessageData(ref);
+	else if constexpr (std::is_same<T, gamedata>::value)
+		return game;
+	else if constexpr (std::is_same<T, HeroClass>::value)
+		return &Hero;
+	else
+		static_assert(always_false<T>::value, "Unhandled type in 'resolveScriptingObject' function");
+}
+
+class IScriptingArray
+{
+public:
+	virtual ~IScriptingArray() = default;
+	virtual size_t getSize(int ref) const = 0;
+	virtual int getElement(int ref, int index) const = 0;
+	virtual bool setElement(int ref, int index, int value) = 0;
+
+	void setDefaultValue(int defaultValue) { m_defaultValue = defaultValue; }
+	int getDefaultValue() const { return m_defaultValue; }
+
+	void setMul10000(bool v) { m_mul10000 = v; }
+
+	void setReadOnly() { m_readOnly = true; }
+	bool readOnly() { return m_readOnly; }
+
+	void boundIndex() { m_boundGetterIndex = m_boundSetterIndex = true; }
+	void boundGetterIndex() { m_boundGetterIndex = true; }
+	void boundSetterIndex() { m_boundSetterIndex = true; }
+
+	void setValueTransform(std::function<std::optional<int>(int)> valueTransform) { m_valueTransform = std::move(valueTransform); }
+
+protected:
+	int m_defaultValue;
+	bool m_mul10000 = true;
+	bool m_readOnly;
+	bool m_boundGetterIndex;
+	bool m_boundSetterIndex;
+	std::function<std::optional<int>(int)> m_valueTransform;
+
+	template <typename T_Element>
+	std::optional<int> transformValue(int value) const
+	{
+		if constexpr (!std::is_same<T_Element, bool>::value)
+			value = m_mul10000 ? value / 10000 : value;
+		return m_valueTransform ? m_valueTransform(value) : value;
+	}
+};
+
+template <typename T>
+static int convert_value(int value, bool mul10000)
+{
+	if constexpr (std::is_same<T, bool>::value)
+		return value;
+	else
+		return mul10000 ? value / 10000 : value;
+}
+
+static int bound_index(int index, int low, int high)
+{
+	if (index >= low && index <= high)
+		return index;
+
+	scripting_log_error_with_context("Using invalid index {}, bounding to valid range between {} and {}", index, low, high);
+	return index < low ? low : high;
+}
+
+template<typename T_Element>
+class ScriptingArray_GlobalCArray : public IScriptingArray {
+public:
+	ScriptingArray_GlobalCArray(T_Element* data, size_t size) : m_data(data), m_size(size) {}
+
+	size_t getSize(int) const override { return m_size; }
+
+	int getElement(int, int index) const override
+	{
+		if (m_boundGetterIndex)
+			index = bound_index(index, 0, m_size - 1);
+		else if (BC::checkIndex2(index, m_size) != SH::_NoError)
+			return m_defaultValue;
+
+		return m_data[index] * (m_mul10000 ? 10000 : 1);
+	}
+
+	bool setElement(int, int index, int value) override
+	{
+		if (m_boundSetterIndex)
+			index = bound_index(index, 0, m_size - 1);
+		else if (BC::checkIndex2(index, m_size) != SH::_NoError)
+			return false;
+
+		if (auto val = transformValue<T_Element>(value))
+		{
+			m_data[index] = val.value();
+
+			if (m_writeSideEffect)
+				m_writeSideEffect(index);
+			return true;
+		}
+
+		return false;
+	}
+
+	void setSideEffect(std::function<void(int)> writeSideEffect) { m_writeSideEffect = std::move(writeSideEffect); }
+
+private:
+	T_Element* m_data;
+	const size_t m_size;
+	std::function<void(int)> m_writeSideEffect;
+};
+
+template<typename T_Element>
+class ScriptingArray_GlobalComputed : public IScriptingArray
+{
+public:
+	using SizeFunc = std::function<size_t(int ref)>;
+	using GetFunc = std::function<T_Element(int ref, int index)>;
+	using SetFunc = std::function<bool(int ref, int index, T_Element value)>;
+
+	template<typename T_GetFunc, typename T_SetFunc>
+	ScriptingArray_GlobalComputed(SizeFunc sf, T_GetFunc gf, T_SetFunc setf)
+		: m_sizeFunc(std::move(sf)), m_getFunc(std::move(gf)), m_setFunc(std::move(setf)), m_skipIndexCheck(false)
+	{
+		// Define the exact function pointer type we require.
+		using RequiredGetFun = T_Element(*)(int, int);
+		using RequiredSetFun = bool(*)(int, int, T_Element);
+
+		// Perform a compile-time check to ensure the provided lambda's signature matches.
+		// This check will fail if the lambda's parameters do not match exactly.
+		static_assert(
+			std::is_convertible_v<T_GetFunc, RequiredGetFun>,
+			"The provided lambda's signature does not match the required 'T_Element(int, int)'. "
+			"Ensure the value parameter's type is exactly T_Element."
+		);
+		static_assert(
+			std::is_convertible_v<T_SetFunc, RequiredSetFun>,
+			"The provided lambda's signature does not match the required 'bool(int, int, T_Element)'. "
+			"Ensure the value parameter's type is exactly T_Element."
+		);
+	}
+
+	size_t getSize(int ref) const override { return m_sizeFunc(ref); }
+
+	int getElement(int ref, int index) const override
+	{
+		if (!m_skipIndexCheck)
+		{
+			size_t sz = getSize(ref);
+			if (m_boundGetterIndex)
+				index = bound_index(index, 0, sz - 1);
+			else if (BC::checkIndex2(index, sz) != SH::_NoError)
+				return m_defaultValue;
+		}
+
+		return m_getFunc(ref, index) * (m_mul10000 ? 10000 : 1);
+	}
+
+	bool setElement(int ref, int index, int value) override
+	{
+		if (!m_skipIndexCheck)
+		{
+			size_t sz = getSize(ref);
+			if (m_boundSetterIndex)
+				index = bound_index(index, 0, sz - 1);
+			else if (BC::checkIndex2(index, sz) != SH::_NoError)
+				return false;
+		}
+
+		if (auto val = transformValue<T_Element>(value))
+			return m_setFunc(ref, index, val.value());
+
+		return false;
+	}
+
+	void skipIndexCheck() { m_skipIndexCheck = true; }
+
+private:
+	SizeFunc m_sizeFunc;
+	GetFunc m_getFunc;
+	SetFunc m_setFunc;
+	bool m_skipIndexCheck;
+};
+
+// Never use this for new ZASM.
+class ScriptingArray_GlobalComputedOneIndexed : public IScriptingArray
+{
+public:
+	using SizeFunc = std::function<size_t(int ref)>;
+	using GetFunc = std::function<int(int ref, int index)>;
+	using SetFunc = std::function<bool(int ref, int index, int value)>;
+
+	ScriptingArray_GlobalComputedOneIndexed(SizeFunc sf, GetFunc gf, SetFunc setf)
+		: m_sizeFunc(std::move(sf)), m_getFunc(std::move(gf)), m_setFunc(std::move(setf)) {}
+
+	size_t getSize(int ref) const override { return m_sizeFunc(ref); }
+
+	int getElement(int ref, int index) const override
+	{
+		size_t sz = getSize(ref);
+		if (m_boundGetterIndex)
+			index = bound_index(index, 1, sz - 1);
+		else if (BC::checkIndex2(index, sz) != SH::_NoError)
+			return m_defaultValue;
+
+		return m_getFunc(ref, index) * (m_mul10000 ? 10000 : 1);
+	}
+
+	bool setElement(int ref, int index, int value) override
+	{
+		size_t sz = getSize(ref);
+		if (m_boundSetterIndex)
+			index = bound_index(index, 1, sz - 1);
+		else if (BC::checkIndex2(index, sz) != SH::_NoError)
+			return false;
+
+		if (auto val = transformValue<int>(value))
+			return m_setFunc(ref, index, val.value());
+
+		return false;
+	}
+
+private:
+	SizeFunc m_sizeFunc;
+	GetFunc m_getFunc;
+	SetFunc m_setFunc;
+};
+
+template<typename T_Object, auto T_MemberPtr>
+class ScriptingArray_ObjectMemberCArray : public IScriptingArray
+{
+public:
+	using MemberType = field_type<T_MemberPtr>::type;
+	using ElementType = std::remove_extent<MemberType>::type;
+	using N = std::extent<MemberType>;
+
+	static_assert(std::is_array<MemberType>(), "Can only use ScriptingArray_ObjectMemberCArray for array types");
+
+	size_t getSize(int) const override { return N::value; }
+
+	int getElement(int ref, int index) const override
+	{
+		auto* obj = resolveScriptingObject<T_Object>(ref);
+		if (!obj)
+			return 0;
+
+		if (m_boundGetterIndex)
+			index = bound_index(index, 0, N::value - 1);
+		else if (BC::checkIndex2(index, N::value) != SH::_NoError)
+			return m_defaultValue;
+
+		return (obj->*T_MemberPtr)[index] * (m_mul10000 ? 10000 : 1);
+	}
+
+	bool setElement(int ref, int index, int value) override
+	{
+		auto* obj = resolveScriptingObject<T_Object>(ref);
+		if (!obj)
+			return false;
+
+		if (m_boundSetterIndex)
+			index = bound_index(index, 0, N::value - 1);
+		else if (BC::checkIndex2(index, N::value) != SH::_NoError)
+			return false;
+
+		if (auto val = transformValue<ElementType>(value))
+		{
+			(obj->*T_MemberPtr)[index] = val.value();
+
+			if (m_writeSideEffect)
+				m_writeSideEffect(obj, index, val.value());
+			return true;
+		}
+
+		return false;
+	}
+
+	void setSideEffect(std::function<void(T_Object*, int, ElementType)> writeSideEffect) { m_writeSideEffect = std::move(writeSideEffect); }
+
+private:
+	std::function<void(T_Object*, int, ElementType)> m_writeSideEffect;
+};
+
+template<typename T_Object, typename T_Element>
+class ScriptingArray_ObjectComputed : public IScriptingArray
+{
+public:
+	using SizeFunc = std::function<size_t(T_Object* obj)>;
+	using GetFunc = std::function<T_Element(T_Object* obj, int index)>;
+	using SetFunc = std::function<void(T_Object* obj, int index, T_Element value)>;
+
+	template<typename T_GetFunc, typename T_SetFunc>
+	ScriptingArray_ObjectComputed(SizeFunc sf, T_GetFunc gf, T_SetFunc setf)
+		: m_sizeFunc(std::move(sf)), m_getFunc(std::move(gf)), m_setFunc(std::move(setf)), m_skipIndexCheck(false)
+	{
+		// Define the exact function pointer type we require.
+		using RequiredGetFun = T_Element(*)(T_Object*, int);
+		using RequiredSetFun = void(*)(T_Object*, int, T_Element);
+
+		// Perform a compile-time check to ensure the provided lambda's signature matches.
+		// This check will fail if the lambda's parameters do not match exactly.
+		static_assert(
+			std::is_convertible_v<T_GetFunc, RequiredGetFun>,
+			"The provided lambda's signature does not match the required 'T_Element(T_Object*, int)'. "
+			"Ensure the value parameter's type is exactly T_Element."
+		);
+		static_assert(
+			std::is_convertible_v<T_SetFunc, RequiredSetFun>,
+			"The provided lambda's signature does not match the required 'void(T_Object*, int, T_Element)'. "
+			"Ensure the value parameter's type is exactly T_Element."
+		);
+	}
+
+	size_t getSize(int ref) const override
+	{
+		auto* obj = resolveScriptingObject<T_Object>(ref);
+		if (!obj)
+			return 0;
+
+		return m_sizeFunc(obj);
+	}
+
+	int getElement(int ref, int index) const override
+	{
+		auto* obj = resolveScriptingObject<T_Object>(ref);
+		if (!obj)
+			return m_defaultValue;
+
+		if (!m_skipIndexCheck)
+		{
+			size_t sz = m_sizeFunc(obj);
+			if (m_boundGetterIndex)
+				index = bound_index(index, 0, sz - 1);
+			else if (BC::checkIndex2(index, sz) != SH::_NoError)
+				return m_defaultValue;
+		}
+
+		T_Element result = m_getFunc(obj, index);
+		
+		if constexpr (std::is_same<T_Element, bool>::value)
+			return result ? (m_mul10000 ? 10000 : 1) : 0;
+		else
+			return result * (m_mul10000 ? 10000 : 1);
+	}
+
+	bool setElement(int ref, int index, int value) override
+	{
+		auto* obj = resolveScriptingObject<T_Object>(ref);
+		if (!obj)
+			return false;
+
+		if (!m_skipIndexCheck)
+		{
+			size_t sz = m_sizeFunc(obj);
+			if (m_boundSetterIndex)
+				index = bound_index(index, 0, sz - 1);
+			else if (BC::checkIndex2(index, sz) != SH::_NoError)
+				return false;
+		}
+
+		if (auto val = transformValue<T_Element>(value))
+		{
+			m_setFunc(obj, index, val.value());
+			return true;
+		}
+
+		return false;
+	}
+
+	void skipIndexCheck() { m_skipIndexCheck = true; }
+
+private:
+	SizeFunc m_sizeFunc;
+	GetFunc m_getFunc;
+	SetFunc m_setFunc;
+	bool m_skipIndexCheck;
+};
+
+template<typename T_Object, auto T_MemberPtr, size_t N>
+class ScriptingArray_ObjectMemberBitwiseFlags : public IScriptingArray
+{
+public:
+	using MemberType = field_type<T_MemberPtr>::type;
+
+	size_t getSize(int) const override { return N; }
+
+	int getElement(int ref, int index) const override
+	{
+		auto* obj = resolveScriptingObject<T_Object>(ref);
+		if (!obj)
+			return 0;
+
+		if (m_boundGetterIndex)
+			index = bound_index(index, 0, N - 1);
+		else if (BC::checkIndex2(index, N) != SH::_NoError)
+			return m_defaultValue;
+
+		return ((obj->*T_MemberPtr >> index) & 1 ? 1 : 0) * (m_mul10000 ? 10000 : 1);
+	}
+
+	bool setElement(int ref, int index, int value) override
+	{
+		auto* obj = resolveScriptingObject<T_Object>(ref);
+		if (!obj)
+			return false;
+
+		if (m_boundSetterIndex)
+			index = bound_index(index, 0, N - 1);
+		else if (BC::checkIndex2(index, N) != SH::_NoError)
+			return false;
+
+		if (auto val = transformValue<bool>(value))
+		{
+			auto flag = (MemberType)(1<<index);
+			SETFLAG(obj->*T_MemberPtr, flag, val.value());
+
+			if (m_writeSideEffect)
+				m_writeSideEffect(obj, index, val.value());
+			return true;
+		}
+
+		return false;
+	}
+
+	void setSideEffect(std::function<void(T_Object*, int, int)> writeSideEffect) { m_writeSideEffect = std::move(writeSideEffect); }
+
+private:
+	std::function<void(T_Object*, int, int)> m_writeSideEffect;
+};
+
+template<typename T_Object, auto T_MemberPtr>
+class ScriptingArray_ObjectMemberContainer : public IScriptingArray
+{
+public:
+	using MemberType = field_type<T_MemberPtr>::type;
+	using ElementType = MemberType::obj_type;
+
+	size_t getSize(int ref) const override
+	{
+		auto* obj = resolveScriptingObject<T_Object>(ref);
+		if (!obj) return 0;
+
+		return (obj->*T_MemberPtr).size();
+	}
+
+	int getElement(int ref, int index) const override
+	{
+		auto* obj = resolveScriptingObject<T_Object>(ref);
+		if (!obj) return m_defaultValue;
+
+		const auto& container = obj->*T_MemberPtr;
+
+		if (m_boundGetterIndex)
+			index = bound_index(index, 0, container.size() - 1);
+		else if (BC::checkIndex2(index, container.size()) != SH::_NoError)
+			return m_defaultValue;
+
+		return container[index] * (m_mul10000 ? 10000 : 1);
+	}
+
+	bool setElement(int ref, int index, int value) override
+	{
+		auto* obj = resolveScriptingObject<T_Object>(ref);
+		if (!obj) return false;
+
+		auto& container = (obj->*T_MemberPtr);
+
+		if (m_boundSetterIndex)
+			index = bound_index(index, 0, container.size() - 1);
+		else if (BC::checkIndex2(index, container.size()) != SH::_NoError)
+			return false;
+
+		if (auto val = transformValue<ElementType>(value))
+		{
+			container[index] = val.value();
+			return true;
+		}
+		return false;
+	}
+};
+
+struct ArrayRegistrar
+{
+	ArrayRegistrar(int zasm_var, IScriptingArray* arrayImpl);
+};
+
+void zasm_array_register(int zasm_var, IScriptingArray* impl);
+
+#endif

commit fde17f26532db9dcb9a0cee27a68533ce1afebae

fix(zscript)!: correct bounds for 1-indexed arrays (like `Screen->LayerScreen[]`)
These arrays were incorrectly returning garbage for index 0, and
returning a default value for the last index.

Regressed recently in df3ce20.

src/zc/ffscript.h
@@ -1832,6 +1832,17 @@ public:
 
 		return _NoError;
 	}
+
+	static INLINE int32_t checkIndex2OneIndex(int32_t n, int32_t len)
+	{
+		if(n <= 0 || n > len)
+		{
+			scripting_log_error_with_context("Invalid index: {} - must be > 0 and <= {}", n, len);
+			return _OutOfBounds;
+		}
+
+		return _NoError;
+	}
 	
 	static INLINE int32_t checkBoundsPos(const int32_t n, const int32_t boundlow, const int32_t boundup)
 	{

src/zc/scripting/arrays.h
@@ -342,8 +342,8 @@ public:
 	{
 		size_t sz = getSize(ref);
 		if (m_boundGetterIndex)
-			index = bound_index(index, 1, sz - 1);
-		else if (BC::checkIndex2(index, sz) != SH::_NoError)
+			index = bound_index(index, 1, sz);
+		else if (BC::checkIndex2OneIndex(index, sz) != SH::_NoError)
 			return m_defaultValue;
 
 		return m_getFunc(ref, index) * (m_mul10000 ? 10000 : 1);
@@ -353,8 +353,8 @@ public:
 	{
 		size_t sz = getSize(ref);
 		if (m_boundSetterIndex)
-			index = bound_index(index, 1, sz - 1);
-		else if (BC::checkIndex2(index, sz) != SH::_NoError)
+			index = bound_index(index, 1, sz);
+		else if (BC::checkIndex2OneIndex(index, sz) != SH::_NoError)
 			return false;
 
 		if (auto val = transformValue<int>(value))