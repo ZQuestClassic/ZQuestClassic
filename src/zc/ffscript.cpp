ZQuestClassic/ZQuestClassic

commit 004f602b674a19965b1f1ddee083a78de01beb8d

refactor(zscript)!: call clear_ref in script_exit_cleanup

src/zc/ffscript.cpp
@@ -24368,6 +24368,7 @@ static void script_exit_cleanup(bool no_dealloc)
 				ffc->script = 0;
 			auto& data = get_script_engine_data(type, i);
 			data.doscript = false;
+			data.clear_ref();
 		}
 		break;
 
@@ -24384,6 +24385,7 @@ static void script_exit_cleanup(bool no_dealloc)
 		{
 			auto& data = get_script_engine_data(type, i);
 			data.doscript = false;
+			data.clear_ref();
 		}
 		break;
 		case ScriptType::Ewpn:
@@ -24393,7 +24395,7 @@ static void script_exit_cleanup(bool no_dealloc)
 		{
 			auto& data = get_script_engine_data(type, i);
 			data.doscript = false;
-			data.initialized = false;
+			data.clear_ref();
 			if (auto spr = sprite::getByUID(i))
 				spr->script = 0;
 		}
@@ -24408,6 +24410,7 @@ static void script_exit_cleanup(bool no_dealloc)
 			// TODO use `i`?
 			auto& data = get_script_engine_data(type, gen_frozen_index-1);
 			data.doscript = false;
+			data.clear_ref();
 			break;
 		}
 
@@ -24418,15 +24421,9 @@ static void script_exit_cleanup(bool no_dealloc)
 			if ( !collect )
 			{
 				if ( (itemsbuf[i].flags&item_passive_script) && game->item[i] ) itemsbuf[i].script = 0; //Quit perpetual scripts, too.
-				data.doscript = 0;
-				data.clear_ref();
 			}
-			else
-			{
-				data.doscript = 0;
-				data.clear_ref();
-			}
-			data.initialized = false;
+			data.doscript = 0;
+			data.clear_ref();
 			break;
 		}
 	}

commit ac29eec66f325da9068afb3546241e5d6c3a9ac1

fix(zscript)!: prevent rare crash when jitted script ends
Recently regressed in 004f602.

src/zc/ffscript.cpp
@@ -1399,11 +1399,10 @@ bool& FFScript::waitdraw(ScriptType type, int index)
 	return get_script_engine_data(type, index).waitdraw;
 }
 
-static void set_current_script_engine_data(ScriptType type, int script, int index)
+static void set_current_script_engine_data(ScriptEngineData& data, ScriptType type, int script, int index)
 {
 	bool got_initialized = false;
 
-	auto& data = get_script_engine_data(type, index);
 	ri = &data.ref;
 	stack = &data.stack;
 	ret_stack = &data.ret_stack;
@@ -24464,36 +24463,8 @@ int32_t run_script(ScriptType type, word script, int32_t i)
 		return RUNSCRIPT_ERROR;
 	}
 
-	switch(type)
-	{
-		case ScriptType::FFC:
-		case ScriptType::Global:
-		case ScriptType::Hero:
-		case ScriptType::DMap:
-		case ScriptType::OnMap:
-		case ScriptType::ScriptedActiveSubscreen:
-		case ScriptType::ScriptedPassiveSubscreen:
-		case ScriptType::EngineSubscreen:
-		case ScriptType::Screen:
-		case ScriptType::Combo:
-		case ScriptType::Item:
-		case ScriptType::NPC:
-		case ScriptType::Lwpn:
-		case ScriptType::Ewpn:
-		case ScriptType::ItemSprite:
-		case ScriptType::Generic:
-		case ScriptType::GenericFrozen:
-		{
-			set_current_script_engine_data(type, script, i);
-		}
-		break;
-
-		default:
-		{
-			al_trace("No other scripts are currently supported\n");
-			return RUNSCRIPT_ERROR;
-		}
-	}
+	auto& data = get_script_engine_data(type, i);
+	set_current_script_engine_data(data, type, script, i);
 
 	// Because qst.cpp likes to write script_data without setting this.
 	curscript->meta.script_type = type;
@@ -24557,6 +24528,11 @@ int32_t run_script(ScriptType type, word script, int32_t i)
 		}
 		else
 		{
+			// Retain the script handler because if deleted while running, terrible things can happen (crash),
+			// as the jit runtimes often write to it. Typically a script won't delete its own script handle,
+			// but scripts can run nested, so lets capture a temporary retaining reference as part of the
+			// call stack.
+			auto retainer = data.jitted_script;
 			result = jit_run_script(jitted_script);
 		}
 	}

src/zc/ffscript.h
@@ -1893,7 +1893,7 @@ struct ScriptEngineData {
 	refInfo ref;
 	int32_t stack[MAX_SCRIPT_REGISTERS];
 	bounded_vec<word, int32_t> ret_stack {65535};
-	std::unique_ptr<JittedScriptHandle> jitted_script;
+	std::shared_ptr<JittedScriptHandle> jitted_script;
 	// This is used as a boolean for all but ScriptType::Item.
 	byte doscript = true;
 	bool waitdraw;