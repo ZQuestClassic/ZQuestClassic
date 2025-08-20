75c45c19ac01d6dfed0049e4369b17388622ef1c
Connor Clark <cjamcl@gmail.com>
2025-08-19 22:57:42 -0700

    fix(zc)!: draw minimap for regions correctly with dmap xoff

--- src/subscr.cpp
+++ src/subscr.cpp
@@ -2289,7 +2289,7 @@ void drawgrid(BITMAP *dest,int32_t x,int32_t y,int32_t c1,int32_t c2)
 		
 		for(int32_t x2=0; x2<=7; ++x2)
 		{
-			int scrx = (x2 - thedmap.xoff);
+			int scrx = x2 + thedmap.xoff;
 			int scrid = scrx + y2*0x10;
 			
 			int x_1 = x2*8, x_2 = x_1+6;
