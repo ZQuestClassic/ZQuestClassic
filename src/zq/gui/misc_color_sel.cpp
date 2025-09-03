39ed9f4fcb467da25a89fdfcdad781c73415ab3f
Emily Venezia <emilygamergirl99@gmail.com>
2025-09-03 01:26:49 -0400

    fix(zq): subscreen editor color selectors not showing cset 12/13 properly
    (cherry picked from commit 491824681dcdb2b95048f8be787f77e12c11a457)

--- src/zq/gui/misc_color_sel.cpp
+++ src/zq/gui/misc_color_sel.cpp
@@ -10,7 +10,7 @@ namespace GUI
 {
 static bool isCSet(int32_t val)
 {
-	return val <= 11;
+	return val <= 13;
 }
 static const ListData colTyList
 {
@@ -55,7 +55,7 @@ static const ListData miscColList
 MiscColorSel::MiscColorSel(): onUpdate(NULL), c1(0), c2(0)
 {
 	setFitParent(true);
-	for(auto q = 0; q <= 11; ++q)
+	for(auto q = 0; q <= 13; ++q)
 	{
 		c2s[q] = 0;
 	}
