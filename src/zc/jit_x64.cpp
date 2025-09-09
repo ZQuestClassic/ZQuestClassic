a9299cf6d0c6caadeb3d40a105cd1761fc88d3e1
Connor Clark <cjamcl@gmail.com>
2025-09-08 20:43:08 -0700

    feat(zc): optimize ORR/ORV/ORR32/ORV32 in x64 jit
    (cherry picked from commit 34ead2e3b2e4ece09d8499cada89f2c5a6fb8315)

--- src/zc/jit_x64.cpp
+++ src/zc/jit_x64.cpp
@@ -397,6 +397,10 @@ static bool command_is_compiled(int command)
 	case MULTR:
 	case MULTV:
 	case NOP:
+	case ORR:
+	case ORR32:
+	case ORV:
+	case ORV32:
 	case SETR:
 	case SETV:
 	case STORED:
@@ -972,6 +976,49 @@ JittedFunction jit_compile_script(script_data *script)
 			div_10000(cc, val2);
 			cc.and_(val, val2);
 			cc.imul(val, 10000);
+
+			set_z_register(state, cc, vStackIndex, arg1, val);
+		}
+		break;
+		case ORV:
+		{
+			x86::Gp val = get_z_register(state, cc, vStackIndex, arg1);
+
+			div_10000(cc, val);
+			cc.or_(val, arg2 / 10000);
+			cc.imul(val, 10000);
+
+			set_z_register(state, cc, vStackIndex, arg1, val);
+		}
+		break;
+		case ORR:
+		{
+			x86::Gp val = get_z_register(state, cc, vStackIndex, arg1);
+			x86::Gp val2 = get_z_register(state, cc, vStackIndex, arg2);
+
+			div_10000(cc, val);
+			div_10000(cc, val2);
+			cc.or_(val, val2);
+			cc.imul(val, 10000);
+
+			set_z_register(state, cc, vStackIndex, arg1, val);
+		}
+		break;
+		case ORR32:
+		{
+			x86::Gp val = get_z_register(state, cc, vStackIndex, arg1);
+			x86::Gp val2 = get_z_register(state, cc, vStackIndex, arg2);
+
+			cc.or_(val, val2);
+
+			set_z_register(state, cc, vStackIndex, arg1, val);
+		}
+		break;
+		case ORV32:
+		{
+			x86::Gp val = get_z_register(state, cc, vStackIndex, arg1);
+
+			cc.or_(val, arg2);
 
 			set_z_register(state, cc, vStackIndex, arg1, val);
 		}
