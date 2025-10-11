eae75f591060bde749f20aacc0a9acda698deb65
Connor Clark <cjamcl@gmail.com>
2025-10-11 14:38:19 -0700

    fix(zscript): load initial screen before onSaveLoad global script
    When the "Use Old Global Init and SaveLoad Timing" compat QR is off, the
    onSaveLoad global script ran before the initial screen even loaded. That
    made accessing anything on `Screen` do the wrong thing.
    
    Now, this global script can access the initially loaded screen.
    
    (cherry picked from commit cf7b17d3ee599790db2d72c64c5d5c96a3637998)

--- src/zc/zelda.cpp
+++ src/zc/zelda.cpp
@@ -2003,14 +2003,14 @@ int32_t init_game()
 		FFCore.usr_music_volume = 10000 * 100;
 	}
 	
-	//FFCore.init(); ///Initialise new ffscript engine core. 
+	loadscr(0,currdmap,currscr,-1,false);
+
 	if(!firstplay && !get_qr(qr_OLD_INIT_SCRIPT_TIMING))
 	{
 		ZScriptVersion::RunScript(ScriptType::Global, GLOBAL_SCRIPT_ONSAVELOAD, GLOBAL_SCRIPT_ONSAVELOAD); //Do this after global arrays have been loaded
 		FFCore.deallocateAllScriptOwned(ScriptType::Global, GLOBAL_SCRIPT_ONSAVELOAD);
 	}
-	//loadscr(0,currscr,up);
-	loadscr(0,currdmap,currscr,-1,false);
+
 	putscr(scrollbuf,0,0,&tmpscr[0]);
 	putscrdoors(scrollbuf,0,0,&tmpscr[0]);
 	
