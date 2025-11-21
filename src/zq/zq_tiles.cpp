ZQuestClassic/ZQuestClassic

commit 92971ff29d1c2f93ad08e74bd63b2876cf3d25a9

fix(zq): remove frame of lag when opening tiles dialog

src/zq/zq_tiles.cpp
@@ -1636,7 +1636,6 @@ void draw_edit_scr(int32_t tile,int32_t flip,int32_t cs,byte *oldtile,bool creat
 		}
 	}
 	
-	custom_vsync();
 	blit(screen2,screen,0,0,0,0,zq_screen_w,zq_screen_w);
 	update_tool_cursor();
 }
@@ -2143,7 +2142,6 @@ void edit_tile(int32_t tile,int32_t flip,int32_t &cs)
 		invcol=makecol8(255 - tpal[0].r, 255 - tpal[0].g, 255 - tpal[0].b);
 	}
 	
-	custom_vsync();
 	zc_set_palette(tpal);
 	draw_edit_scr(tile,flip,cs,oldtile, true);
 	
@@ -3166,8 +3164,8 @@ void edit_tile(int32_t tile,int32_t flip,int32_t &cs)
 		
 		if(redraw)
 		{
-			custom_vsync();
 			draw_edit_scr(tile,flip,cs,oldtile, false);
+			custom_vsync();
 		}
 		else
 		{
@@ -5641,7 +5639,6 @@ void tile_info_0(int32_t tile,int32_t tile2,int32_t cs,int32_t copy,int32_t copy
 	int32_t screen_xofs=window_xofs+6;
 	int32_t screen_yofs=window_yofs+25;
 	
-	custom_vsync();
 	blit(screen2,screen,0,0,screen_xofs,screen_yofs,w,h);
 }
 
@@ -5734,7 +5731,6 @@ void tile_info_1(int32_t oldtile,int32_t oldflip,int32_t oldcs,int32_t tile,int3
 	int32_t screen_xofs=window_xofs+6;
 	int32_t screen_yofs=window_yofs+25;
 	
-	custom_vsync();
 	blit(screen2,screen,0,0,screen_xofs,screen_yofs,w,h);
 }
 /*
@@ -10707,7 +10703,6 @@ void combo_info(int32_t tile,int32_t tile2,int32_t cs,int32_t copy,int32_t copyc
 	int32_t screen_xofs=window_xofs+6;
 	int32_t screen_yofs=window_yofs+25;
 	
-	custom_vsync();
 	blit(screen2,screen,0,0,screen_xofs,screen_yofs,w,h);
 }
 

commit 3fa22cf718b9cd3ac8629af8bdf55f4a5b434957

fix(zq)!: combo page not opening
regressed recently in 92971ff

src/zq/zq_tiles.cpp
@@ -10705,6 +10705,7 @@ void combo_info(int32_t tile,int32_t tile2,int32_t cs,int32_t copy,int32_t copyc
 	int32_t screen_xofs=window_xofs+6;
 	int32_t screen_yofs=window_yofs+25;
 	
+	custom_vsync();
 	blit(screen2,screen,0,0,screen_xofs,screen_yofs,w,h);
 }
 