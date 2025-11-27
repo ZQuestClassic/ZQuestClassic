ZQuestClassic/ZQuestClassic

commit d7438d4f3b1c69586b9c9509662815126c120172

feat(zq): render screen editor in multiple palettes, higher resolution
The screen editor can now render multiple screens in different palettes
at the same time. It also renders at a much higher resolution. This
makes the zoom out feature much better.

In case there are any regressions, this can be disabled via in the View
menu: just uncheck "High Quality Screen Rendering".

src/zq/render.cpp
@@ -57,6 +57,7 @@ static void init_render_tree()
 	rti_screen.bitmap = create_a5_bitmap(screen->w, screen->h);
 	rti_screen.a4_bitmap = screen;
 	rti_screen.visible = true;
+	rti_screen.transparency_index = 0;
 
 	rti_root.add_child(&rti_screen);
 	rti_root.add_child(&rti_dialogs);

resources/base_config/zquest.cfg
@@ -105,6 +105,7 @@ save_paths = 1
 cycle_on = 1
 vsync = 1
 showfps = 0
+high_quality_screen_rendering = 1 #? web = 0
 
 combo_brush = 0
 float_brush = 0

src/dialog/zq_options.cpp
@@ -847,7 +847,7 @@ std::shared_ptr<GUI::Widget> OptionsDialog::view()
 					ROW_CHECK(OPT_CUSTOMFONT, "Custom Fonts"),
 					ROW_CHECK(OPT_TOOLTIPS, "Enable Tooltips"),
 					ROW_CHECK(OPT_TOOLTIP_HIGHLIGHT, "Tooltips Highlight Target"),
-					ROW_CHECK(OPT_NEXTPREVIEW, "No Next-Screen Preview"),
+					ROW_CHECK_I(OPT_NEXTPREVIEW, "No Next-Screen Preview", "If disabled, a row/column of the neighboring screens will show in Compact mode (only if zoom level is <= 2x2, and High Quality Screen Rendering is off)"),
 					ROW_CHECK(OPT_DISABLE_LPAL_SHORTCUT, "Disable Level Palette Shortcuts"),
 					ROW_CHECK(OPT_DISABLE_COMPILE_CONSOLE, "Internal Compile Window")
 				),

src/zq/render_map_view.cpp
@@ -1,0 +1,150 @@
+#include "zq/render_map_view.h"
+#include "drawing.h"
+#include "tiles.h"
+#include "zq/render.h"
+#include "zq/zq_class.h"
+#include "zq/zquest.h"
+#include <algorithm>
+
+static MapViewRTI rti_map_view;
+
+void MapViewRTI::render(bool bitmap_resized)
+{
+	// This mode is used by "View Map", via "mapview_open".
+	if (view_map_mode)
+	{
+		MapCursor previous_cursor = Map.getCursor();
+		Map.setViewSize(1);
+
+		BITMAP* bmap4_single = create_bitmap_ex(8,256,176);
+		set_bitmap_create_flags(true);
+		ALLEGRO_BITMAP* bmap5_single = al_create_bitmap(256,176);
+		for(int32_t y=0; y<8; y++)
+		{
+			for(int32_t x=0; x<16; x++)
+			{
+				clear_bitmap(bmap4_single);
+				Map.setCurrScr(y*16+x);
+				Map.draw(bmap4_single, 0, 0, flags, -1, y*16+x, -1);
+				stretch_blit(bmap4_single, bmap4_single, 0, 0, 0, 0, 256, 176, 256, 176);
+				all_render_a5_bitmap(bmap4_single, bmap5_single);
+				al_draw_scaled_bitmap(bmap5_single, 0, 0, 256, 176, sw * x, sh * y, sw, sh, 0);
+			}
+		}
+
+		Map.setCursor(previous_cursor);
+		destroy_bitmap(bmap4_single);
+		al_destroy_bitmap(bmap5_single);
+		return;
+	}
+
+	// Otherwise, this is rendering for the screens editor if "High Quality Screen Rendering" is enabled.
+
+	int view_scr_x = Map.getViewScr() % 16;
+	int view_scr_y = Map.getViewScr() / 16;
+	int size = Map.getViewSize();
+	auto& transform = get_transform();
+
+	extern int ActiveLayerHighlight;
+	int highlight_layer = ActiveLayerHighlight ? CurrentLayer : -1;
+
+	extern int LayerDitherBG, LayerDitherSz;
+	int layer_dither_bg = LayerDitherBG;
+	int layer_dither_sz = LayerDitherSz;
+
+	MapCursor previous_cursor = Map.getCursor();
+	Map.setViewSize(1);
+
+	static BITMAP* bmap4_single = create_bitmap_ex(8,256,176);
+	set_bitmap_create_flags(true);
+	static ALLEGRO_BITMAP* bmap5_single = al_create_bitmap(256,176);
+
+	struct ScreenColorInfo {
+		int screen, x, y, color;
+	};
+	std::vector<ScreenColorInfo> screens_to_draw;
+
+	for (int y = 0; y < size; y++)
+	{
+		for (int x = 0; x < size; x++)
+		{
+			int mx = view_scr_x + x;
+			int my = view_scr_y + y;
+			if (mx < 0 || mx >= 16 || my < 0 || my >= 9)
+				continue;
+
+			int screen = my*16 + mx;
+			if (screen >= MAPSCRS)
+				continue;
+
+			screens_to_draw.push_back(ScreenColorInfo{screen, x, y, Map.getcolor(screen)});
+		}
+	}
+
+	// Sort by palette, because switching palettes has non-trivial overhead.
+	std::sort(screens_to_draw.begin(), screens_to_draw.end(), [](const auto& a, const auto& b) {
+		return a.color < b.color;
+	});
+
+	for (auto& [screen, x, y, _] : screens_to_draw)
+	{
+		if (layer_dither_bg > -1)
+		{
+			if (layer_dither_sz > 0)
+			{
+				clear_to_color(mapscreenbmp, 0);
+				ditherblit(mapscreenbmp, nullptr, vc(layer_dither_bg), dithChecker, layer_dither_sz);
+			}
+			else
+			{
+				clear_to_color(mapscreenbmp, vc(layer_dither_bg));
+			}
+		}
+
+		Map.setCurrScr(screen);
+
+		int xoff = x * 256 * transform.xscale;
+		int yoff = y * 176 * transform.yscale;
+		combotile_add_x = transform.x + xoff;
+		combotile_add_y = transform.y + yoff;
+		combotile_mul_x = transform.xscale;
+		combotile_mul_y = transform.yscale;
+		Map.draw(bmap4_single, 0, 0, flags, -1, screen, highlight_layer);
+		combotile_add_x = 0;
+		combotile_add_y = 0;
+		combotile_mul_x = 1;
+		combotile_mul_y = 1;
+
+		all_render_a5_bitmap(bmap4_single, bmap5_single);
+		al_draw_bitmap(bmap5_single, 256 * x, 176 * y, 0);
+	}
+
+	Map.setCursor(previous_cursor);
+}
+
+MapViewRTI* mapview_get_rti()
+{
+	return &rti_map_view;
+}
+
+void mapview_open(int flags, int sw, int sh, int bw, int bh)
+{
+	MapViewRTI* rti_map_view = mapview_get_rti();
+	rti_map_view->view_map_mode = true;
+	rti_map_view->flags = flags;
+	rti_map_view->sw = sw;
+	rti_map_view->sh = sh;
+	rti_map_view->set_size(bw, bh);
+	rti_map_view->dirty = true;
+	get_root_rti()->add_child(rti_map_view);
+	render_zq();
+}
+
+void mapview_close()
+{
+	rti_map_view.view_map_mode = false;
+	rti_map_view.remove();
+
+	void reload_zq_gui();
+	reload_zq_gui();
+}

src/zq/render_map_view.h
@@ -1,0 +1,24 @@
+#ifndef RENDER_MAP_VIEW_H_
+#define RENDER_MAP_VIEW_H_
+
+#include "base/render.h"
+
+class MapViewRTI : public RenderTreeItem
+{
+public:
+	MapViewRTI(): RenderTreeItem("map_view")
+	{
+	}
+
+	int sw, sh, flags;
+	bool view_map_mode;
+
+private:
+	void render(bool bitmap_resized) override;
+};
+
+MapViewRTI* mapview_get_rti();
+void mapview_open(int flags, int sw, int sh, int bw, int bh);
+void mapview_close();
+
+#endif

src/zq/zq_class.cpp
@@ -442,7 +442,6 @@ void zmap::setCursor(MapCursor new_cursor)
 	reset_combo_animations2();
 	mmap_mark_dirty();
 	regions_mark_dirty();
-	refresh(rALL);
 }
 
 bool zmap::isValidPosition(ComboPosition pos) const
@@ -687,7 +686,12 @@ void zmap::setcolor(int color, mapscr* scr)
 
 int32_t zmap::getcolor()
 {
-	mapscr& scr = prv_mode ? prvscr : screens[cursor.screen];
+	return getcolor(cursor.screen);
+}
+
+int32_t zmap::getcolor(int screen)
+{
+	mapscr& scr = prv_mode ? prvscr : screens[screen];
 	
 	if (scr.valid & mVALID)
 		return scr.color;

src/zq/zq_class.h
@@ -389,6 +389,7 @@ public:
     void refresh_color();
 	void setcolor(int color, mapscr* scr = nullptr);
     int32_t getcolor();
+	int32_t getcolor(int screen);
     void resetflags();
     word tcmbdat(int32_t pos);
     word tcmbcset(int32_t pos);

src/zq/zq_misc.cpp
@@ -1,5 +1,6 @@
 #include "base/qrs.h"
 #include "base/packfile.h"
+#include "base/render.h"
 #include "base/version.h"
 #include "base/zapp.h"
 #include "base/zc_alleg.h"
@@ -1024,33 +1025,35 @@ int32_t onShowWalkability()
 int32_t onPreviewMode()
 {
 	prv_mode=(prv_mode+1)%2;
+	return D_O_K;
+}
 
-	if(prv_mode)
+void handlePreviewMode()
+{
+	if (prv_mode)
 	{
-		popup_zqdialog_start();
-
+		// Save.
 		MapCursor previous_cursor = Map.getCursor();
-		Map.setViewSize(1);
+		bool tempcb=ComboBrush!=0;
 
 		void reload_zq_gui();
 		reload_zq_gui();
 
+		Map.setViewSize(1);
 		Map.set_prvscr(Map.getCurrMap(),Map.getCurrScr());		
 
 		mmap_set_zoom(false);
 		
-		bool tempcb=ComboBrush!=0;
 		ComboBrush=0;
 		restore_mouse();
 		clear_tooltip();
 		dopreview();
+
+		// Restore.
 		ComboBrush=tempcb;
 		Map.setCursor(previous_cursor);
 		reload_zq_gui();
-
-		popup_zqdialog_end();
 	}
-	return D_O_K;
 }
 
 int32_t onShowFlags()

commit d8e5a2b1c22db5dac251edc62574162fd7f6330c

fix(zq)!: correctly draw color 0 in combo selector
Regressed recently in d7438d4.

src/zq/render.cpp
@@ -1,4 +1,5 @@
 #include "zq/render.h"
+#include "base/pal_tables.h"
 #include "zq/render_tooltip.h"
 #include "base/render.h"
 #include "zconfig.h"
@@ -180,7 +181,7 @@ void render_zq()
 	configure_render_tree();
 	
 	al_set_target_backbuffer(all_get_display());
-	al_clear_to_color(al_map_rgb_f(0, 0, 0));
+	al_clear_to_color(al_map_rgb(RAMpal[0].r, RAMpal[0].g, RAMpal[0].b));
 	
 	render_tree_draw(&rti_root);
 	if (render_get_debug())