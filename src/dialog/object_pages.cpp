ZQuestClassic/ZQuestClassic

commit d7438d4f3b1c69586b9c9509662815126c120172

feat(zq): render screen editor in multiple palettes, higher resolution
The screen editor can now render multiple screens in different palettes
at the same time. It also renders at a much higher resolution. This
makes the zoom out feature much better.

In case there are any regressions, this can be disabled via in the View
menu: just uncheck "High Quality Screen Rendering".

src/zq/zq_class.cpp
@@ -442,7 +442,6 @@ void zmap::setCursor(MapCursor new_cursor)
 	reset_combo_animations2();
 	mmap_mark_dirty();
 	regions_mark_dirty();
-	refresh(rALL);
 }
 
 bool zmap::isValidPosition(ComboPosition pos) const
@@ -687,7 +686,12 @@ void zmap::setcolor(int color, mapscr* scr)
 
 int32_t zmap::getcolor()
 {
-	mapscr& scr = prv_mode ? prvscr : screens[cursor.screen];
+	return getcolor(cursor.screen);
+}
+
+int32_t zmap::getcolor(int screen)
+{
+	mapscr& scr = prv_mode ? prvscr : screens[screen];
 	
 	if (scr.valid & mVALID)
 		return scr.color;

resources/base_config/zquest.cfg
@@ -105,6 +105,7 @@ save_paths = 1
 cycle_on = 1
 vsync = 1
 showfps = 0
+high_quality_screen_rendering = 1 #? web = 0
 
 combo_brush = 0
 float_brush = 0

src/dialog/zq_options.cpp
@@ -847,7 +847,7 @@ std::shared_ptr<GUI::Widget> OptionsDialog::view()
 					ROW_CHECK(OPT_CUSTOMFONT, "Custom Fonts"),
 					ROW_CHECK(OPT_TOOLTIPS, "Enable Tooltips"),
 					ROW_CHECK(OPT_TOOLTIP_HIGHLIGHT, "Tooltips Highlight Target"),
-					ROW_CHECK(OPT_NEXTPREVIEW, "No Next-Screen Preview"),
+					ROW_CHECK_I(OPT_NEXTPREVIEW, "No Next-Screen Preview", "If disabled, a row/column of the neighboring screens will show in Compact mode (only if zoom level is <= 2x2, and High Quality Screen Rendering is off)"),
 					ROW_CHECK(OPT_DISABLE_LPAL_SHORTCUT, "Disable Level Palette Shortcuts"),
 					ROW_CHECK(OPT_DISABLE_COMPILE_CONSOLE, "Internal Compile Window")
 				),

src/zq/render.cpp
@@ -57,6 +57,7 @@ static void init_render_tree()
 	rti_screen.bitmap = create_a5_bitmap(screen->w, screen->h);
 	rti_screen.a4_bitmap = screen;
 	rti_screen.visible = true;
+	rti_screen.transparency_index = 0;
 
 	rti_root.add_child(&rti_screen);
 	rti_root.add_child(&rti_dialogs);

src/zq/render_map_view.cpp
@@ -1,0 +1,150 @@
+#include "zq/render_map_view.h"
+#include "drawing.h"
+#include "tiles.h"
+#include "zq/render.h"
+#include "zq/zq_class.h"
+#include "zq/zquest.h"
+#include <algorithm>
+
+static MapViewRTI rti_map_view;
+
+void MapViewRTI::render(bool bitmap_resized)
+{
+	// This mode is used by "View Map", via "mapview_open".
+	if (view_map_mode)
+	{
+		MapCursor previous_cursor = Map.getCursor();
+		Map.setViewSize(1);
+
+		BITMAP* bmap4_single = create_bitmap_ex(8,256,176);
+		set_bitmap_create_flags(true);
+		ALLEGRO_BITMAP* bmap5_single = al_create_bitmap(256,176);
+		for(int32_t y=0; y<8; y++)
+		{
+			for(int32_t x=0; x<16; x++)
+			{
+				clear_bitmap(bmap4_single);
+				Map.setCurrScr(y*16+x);
+				Map.draw(bmap4_single, 0, 0, flags, -1, y*16+x, -1);
+				stretch_blit(bmap4_single, bmap4_single, 0, 0, 0, 0, 256, 176, 256, 176);
+				all_render_a5_bitmap(bmap4_single, bmap5_single);
+				al_draw_scaled_bitmap(bmap5_single, 0, 0, 256, 176, sw * x, sh * y, sw, sh, 0);
+			}
+		}
+
+		Map.setCursor(previous_cursor);
+		destroy_bitmap(bmap4_single);
+		al_destroy_bitmap(bmap5_single);
+		return;
+	}
+
+	// Otherwise, this is rendering for the screens editor if "High Quality Screen Rendering" is enabled.
+
+	int view_scr_x = Map.getViewScr() % 16;
+	int view_scr_y = Map.getViewScr() / 16;
+	int size = Map.getViewSize();
+	auto& transform = get_transform();
+
+	extern int ActiveLayerHighlight;
+	int highlight_layer = ActiveLayerHighlight ? CurrentLayer : -1;
+
+	extern int LayerDitherBG, LayerDitherSz;
+	int layer_dither_bg = LayerDitherBG;
+	int layer_dither_sz = LayerDitherSz;
+
+	MapCursor previous_cursor = Map.getCursor();
+	Map.setViewSize(1);
+
+	static BITMAP* bmap4_single = create_bitmap_ex(8,256,176);
+	set_bitmap_create_flags(true);
+	static ALLEGRO_BITMAP* bmap5_single = al_create_bitmap(256,176);
+
+	struct ScreenColorInfo {
+		int screen, x, y, color;
+	};
+	std::vector<ScreenColorInfo> screens_to_draw;
+
+	for (int y = 0; y < size; y++)
+	{
+		for (int x = 0; x < size; x++)
+		{
+			int mx = view_scr_x + x;
+			int my = view_scr_y + y;
+			if (mx < 0 || mx >= 16 || my < 0 || my >= 9)
+				continue;
+
+			int screen = my*16 + mx;
+			if (screen >= MAPSCRS)
+				continue;
+
+			screens_to_draw.push_back(ScreenColorInfo{screen, x, y, Map.getcolor(screen)});
+		}
+	}
+
+	// Sort by palette, because switching palettes has non-trivial overhead.
+	std::sort(screens_to_draw.begin(), screens_to_draw.end(), [](const auto& a, const auto& b) {
+		return a.color < b.color;
+	});
+
+	for (auto& [screen, x, y, _] : screens_to_draw)
+	{
+		if (layer_dither_bg > -1)
+		{
+			if (layer_dither_sz > 0)
+			{
+				clear_to_color(mapscreenbmp, 0);
+				ditherblit(mapscreenbmp, nullptr, vc(layer_dither_bg), dithChecker, layer_dither_sz);
+			}
+			else
+			{
+				clear_to_color(mapscreenbmp, vc(layer_dither_bg));
+			}
+		}
+
+		Map.setCurrScr(screen);
+
+		int xoff = x * 256 * transform.xscale;
+		int yoff = y * 176 * transform.yscale;
+		combotile_add_x = transform.x + xoff;
+		combotile_add_y = transform.y + yoff;
+		combotile_mul_x = transform.xscale;
+		combotile_mul_y = transform.yscale;
+		Map.draw(bmap4_single, 0, 0, flags, -1, screen, highlight_layer);
+		combotile_add_x = 0;
+		combotile_add_y = 0;
+		combotile_mul_x = 1;
+		combotile_mul_y = 1;
+
+		all_render_a5_bitmap(bmap4_single, bmap5_single);
+		al_draw_bitmap(bmap5_single, 256 * x, 176 * y, 0);
+	}
+
+	Map.setCursor(previous_cursor);
+}
+
+MapViewRTI* mapview_get_rti()
+{
+	return &rti_map_view;
+}
+
+void mapview_open(int flags, int sw, int sh, int bw, int bh)
+{
+	MapViewRTI* rti_map_view = mapview_get_rti();
+	rti_map_view->view_map_mode = true;
+	rti_map_view->flags = flags;
+	rti_map_view->sw = sw;
+	rti_map_view->sh = sh;
+	rti_map_view->set_size(bw, bh);
+	rti_map_view->dirty = true;
+	get_root_rti()->add_child(rti_map_view);
+	render_zq();
+}
+
+void mapview_close()
+{
+	rti_map_view.view_map_mode = false;
+	rti_map_view.remove();
+
+	void reload_zq_gui();
+	reload_zq_gui();
+}

src/zq/render_map_view.h
@@ -1,0 +1,24 @@
+#ifndef RENDER_MAP_VIEW_H_
+#define RENDER_MAP_VIEW_H_
+
+#include "base/render.h"
+
+class MapViewRTI : public RenderTreeItem
+{
+public:
+	MapViewRTI(): RenderTreeItem("map_view")
+	{
+	}
+
+	int sw, sh, flags;
+	bool view_map_mode;
+
+private:
+	void render(bool bitmap_resized) override;
+};
+
+MapViewRTI* mapview_get_rti();
+void mapview_open(int flags, int sw, int sh, int bw, int bh);
+void mapview_close();
+
+#endif

src/zq/zq_class.h
@@ -389,6 +389,7 @@ public:
     void refresh_color();
 	void setcolor(int color, mapscr* scr = nullptr);
     int32_t getcolor();
+	int32_t getcolor(int screen);
     void resetflags();
     word tcmbdat(int32_t pos);
     word tcmbcset(int32_t pos);

commit 8f8e9dd73eb4c020232ab9800e0f8399192e56eb

fix(zq)!: prevent crash from static drawing
Closing the Map View with static background drawing enabled resulted in
drawing to a bitmap out-of-bounds, resulting in a crash. I added a
common function for drawing static noise and added some bounds checking.

Regressed recently in d7438d4.

src/dialog/object_pages.cpp
@@ -727,9 +727,7 @@ void ObjectTemplate::draw_null(BITMAP* dest, int x, int y, int w, int h) const
 			break;
 		}
 		case 1:
-			for(int dy = 0; dy < h; ++dy)
-				for(int dx = 0; dx < w; ++dx)
-					dest->line[dy+y][dx+x]=vc((((zc_oldrand()%100)/50)?0:8)+(((zc_oldrand()%100)/50)?0:7));
+			draw_static(dest, x, y, w, h);
 			break;
 		default:
 			for(int dy = 0; dy < h; ++dy)

src/gui/jwin.cpp
@@ -9165,6 +9165,24 @@ void draw_checkerboard(BITMAP* dest, int x, int y, int sz, optional<int> cb_sz, 
 	ditherrectfill(dest, x, y, x+sz-1, y+sz-1, vc(CheckerCol1), dithChecker, *cb_sz, ox, oy, vc(CheckerCol2));
 }
 
+void draw_static(BITMAP* dest, int x, int y, int w, int h)
+{
+	// Clip.
+	if (x < 0) { w += x; x = 0; }
+	if (y < 0) { h += y; y = 0; }
+	if (x + w > dest->w) w = dest->w - x;
+	if (y + h > dest->h) h = dest->h - y;
+	if (w <= 0 || h <= 0) return;
+
+	for(int32_t dy=0; dy<h; dy++)
+	{
+		for(int32_t dx=0; dx<w; dx++)
+		{
+			dest->line[y+dy][x+dx]=vc((((zc_oldrand()%100)/50)?0:8)+(((zc_oldrand()%100)/50)?0:7));
+		}
+	}
+}
+
 int32_t d_vsync_proc(int32_t msg,DIALOG *d,int32_t c)
 {
 	// This use to be what kept 60fps, but now render_timer_wait handles that.

src/gui/jwin.h
@@ -292,6 +292,7 @@ int32_t d_jwinbutton_proc(int32_t msg, DIALOG *d, int32_t c);
 void draw_x(BITMAP* dest, int x1, int y1, int x2, int y2, int color);
 void draw_check(BITMAP* dest, int x1, int y1, int x2, int y2, int c);
 void draw_checkerboard(BITMAP* dest, int basex, int basey, int sz, optional<int> cb_sz = nullopt, int x = 0, int y = 0);
+void draw_static(BITMAP* dest, int x, int y, int w, int h);
 
 int32_t d_vsync_proc(int32_t msg,DIALOG *,int32_t c);
 

src/zq/zq_class.cpp
@@ -11,6 +11,7 @@
 #include "base/zapp.h"
 #include "base/zdefs.h"
 #include "dialog/info.h"
+#include "gui/jwin.h"
 #include "metadata/metadata.h"
 
 #include "base/qrs.h"
@@ -3771,13 +3772,7 @@ void zmap::drawstaticblock(BITMAP* dest,int32_t x,int32_t y)
 	}
 	else if(InvalidBG == 1)
     {
-        for(int32_t dy=0; dy<16; dy++)
-        {
-            for(int32_t dx=0; dx<16; dx++)
-            {
-                dest->line[y+dy][x+dx]=vc((((zc_oldrand()%100)/50)?0:8)+(((zc_oldrand()%100)/50)?0:7));
-            }
-        }
+		draw_static(dest, x, y, 16, 16);
     }
     else
     {
@@ -3797,13 +3792,7 @@ void zmap::drawstaticcolumn(BITMAP* dest,int32_t x,int32_t y)
 	}
     else if(InvalidBG == 1)
     {
-        for(int32_t dy=0; dy<176; dy++)
-        {
-            for(int32_t dx=0; dx<16; dx++)
-            {
-                dest->line[y+dy][x+dx]=vc((((zc_oldrand()%100)/50)?0:8)+(((zc_oldrand()%100)/50)?0:7));
-            }
-        }
+		draw_static(dest, x, y, 16, 176);
     }
     else
     {
@@ -3823,13 +3812,7 @@ void zmap::drawstaticrow(BITMAP* dest,int32_t x,int32_t y)
 	}
     else if(InvalidBG == 1)
     {
-        for(int32_t dy=0; dy<16; dy++)
-        {
-            for(int32_t dx=0; dx<256; dx++)
-            {
-                dest->line[y+dy][x+dx]=vc((((zc_oldrand()%100)/50)?0:8)+(((zc_oldrand()%100)/50)?0:7));
-            }
-        }
+		draw_static(dest, x, y, 256, 16);
     }
     else
     {

src/zq/zq_tiles.cpp
@@ -5448,13 +5448,7 @@ void draw_tiles(BITMAP* dest,int32_t first,int32_t cs, bool large, bool true_emp
 				}
 				else if(InvalidBG == 1)
 				{
-					for(int32_t dy=0; dy<=l+1; dy++)
-					{
-						for(int32_t dx=0; dx<=l+1; dx++)
-						{
-							dest->line[dy+(y)][dx+(x)]=vc((((zc_oldrand()%100)/50)?0:8)+(((zc_oldrand()%100)/50)?0:7));
-						}
-					}
+					draw_static(dest, x, y, l+1, l+1);
 				}
 				else
 				{
@@ -5524,13 +5518,7 @@ void tile_info_0(int32_t tile,int32_t tile2,int32_t cs,int32_t copy,int32_t copy
 		}
 		else if(InvalidBG == 1)
 		{
-			for(int32_t dy=0; dy<16*mul; dy++)
-			{
-				for(int32_t dx=0; dx<16*mul; dx++)
-				{
-					screen2->line[(216*mul+yofs+dy)][36*mul+dx]=vc((((zc_oldrand()%100)/50)?0:8)+(((zc_oldrand()%100)/50)?0:7));
-				}
-			}
+			draw_static(screen2, 36*mul, 216*mul+yofs, 16*mul, 16*mul);
 		}
 		else
 		{
@@ -5622,13 +5610,7 @@ void tile_info_1(int32_t oldtile,int32_t oldflip,int32_t oldcs,int32_t tile,int3
 		}
 		else if(InvalidBG == 1)
 		{
-			for(int32_t dy=0; dy<16*mul; dy++)
-			{
-				for(int32_t dx=0; dx<16*mul; dx++)
-				{
-					screen2->line[216*mul+yofs+dy][124*mul+dx]=vc((((zc_oldrand()%100)/50)?0:8)+(((zc_oldrand()%100)/50)?0:7));
-				}
-			}
+			draw_static(screen2, 124*mul, 216*mul+yofs, 16*mul, 16*mul);
 		}
 		else
 		{
@@ -10536,13 +10518,7 @@ void combo_info(int32_t tile,int32_t tile2,int32_t cs,int32_t copy,int32_t copyc
 		}
 		else if(InvalidBG == 1)
 		{
-			for(int32_t dy=0; dy<16*mul; dy++)
-			{
-				for(int32_t dx=0; dx<16*mul; dx++)
-				{
-					screen2->line[(216*mul)+yofs+dy][(31*mul)+dx]=vc((((zc_oldrand()%100)/50)?0:8)+(((zc_oldrand()%100)/50)?0:7));
-				}
-			}
+			draw_static(screen2, 31*mul, 216*mul+yofs, 16*mul, 16*mul);
 		}
 		else
 		{
@@ -10592,13 +10568,7 @@ void combo_info(int32_t tile,int32_t tile2,int32_t cs,int32_t copy,int32_t copyc
 			}
 			else if(InvalidBG == 1)
 			{
-				for(int32_t dy=0; dy<16*mul; dy++)
-				{
-					for(int32_t dx=0; dx<16*mul; dx++)
-					{
-						screen2->line[(216*mul)+yofs+dy][(136*mul)+dx]=vc((((zc_oldrand()%100)/50)?0:8)+(((zc_oldrand()%100)/50)?0:7));
-					}
-				}
+				draw_static(screen2, 136*mul, 216*mul+yofs, 16*mul, 16*mul);
 			}
 			else
 			{

src/zq/zquest.cpp
@@ -5732,6 +5732,11 @@ static void draw_screenunit_map_screen(VisibleScreen visible_screen)
 	stretch_blit(mapscreenbmp, screen_bitmap, 0, 0, mapscreenbmp->w, mapscreenbmp->h, mapscreen_x + xoff, mapscreen_y + yoff, w, h);
 }
 
+static void draw_static_pos(const size_and_pos& sqr)
+{
+	draw_static(screen, sqr.x, sqr.y, sqr.w, sqr.h);
+}
+
 void draw_screenunit(int32_t unit, int32_t flags)
 {
 	FONT* tfont = font;
@@ -5776,13 +5781,7 @@ void draw_screenunit(int32_t unit, int32_t flags)
 						}
 						else if (InvalidBG == 1)
 						{
-							for(int32_t dy=0; dy<sqr.h; dy++)
-							{
-								for(int32_t dx=0; dx<sqr.w; dx++)
-								{
-									screen->line[dy+sqr.y][dx+sqr.x]=vc((((zc_oldrand()%100)/50)?0:8)+(((zc_oldrand()%100)/50)?0:7));
-								}
-							}
+							draw_static_pos(sqr);
 						}
 						else
 						{
@@ -6722,13 +6721,7 @@ void draw_screenunit(int32_t unit, int32_t flags)
 					}
 					else if(InvalidBG == 1)
 					{
-						for(int32_t dy=0; dy<combo_preview2.w; dy++)
-						{
-							for(int32_t dx=0; dx<combo_preview2.w; dx++)
-							{
-								screen->line[dy+combo_preview2.y][dx+combo_preview2.x]=vc((((zc_oldrand()%100)/50)?0:8)+(((zc_oldrand()%100)/50)?0:7));
-							}
-						}
+						draw_static_pos(combo_preview2);
 					}
 					else
 					{
@@ -6797,13 +6790,7 @@ void draw_screenunit(int32_t unit, int32_t flags)
 						}
 						else if(InvalidBG == 1)
 						{
-							for(int32_t dy=0; dy<sqr.h; dy++)
-							{
-								for(int32_t dx=0; dx<sqr.w; dx++)
-								{
-									screen->line[sqr.y+dy][sqr.x+dx]=vc((((zc_oldrand()%100)/50)?0:8)+(((zc_oldrand()%100)/50)?0:7));
-								}
-							}
+							draw_static_pos(sqr);
 						}
 						else
 						{