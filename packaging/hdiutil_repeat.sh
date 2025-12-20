2367fa8f91599a597bf2f1d169c6d6fd2c04a5dc
Connor Clark <cjamcl@gmail.com>
2025-12-20 14:50:59 -0800

    build!: retry more aggressively when hdiutil fails
    (cherry picked from commit e9a3d4197141b2de9c2d70a90bf3b3b57307dae1)

--- packaging/hdiutil_repeat.sh
+++ packaging/hdiutil_repeat.sh
@@ -37,20 +37,14 @@
 
 set -e
 
-if [ "$1" != "create" ]; then
-  # If it isn't an `hdiutil create` command, just run and exit normally
-  hdiutil "$@"
-  exit 0
-fi
-
-# For an `hdiutil create` command, try repeatedly, up to 10 times
+# Try repeatedly, up to 30 times.
 # This prevents spurious errors caused by a race condition with XProtect
 # See https://github.com/actions/runner-images/issues/7522
 i=0
 until
 hdiutil "$@"
 do
-if [ $i -eq 10 ]; then exit 1; fi
+if [ $i -eq 30 ]; then exit 1; fi
 i=$((i+1))
-sleep 1
+sleep 5
 done
