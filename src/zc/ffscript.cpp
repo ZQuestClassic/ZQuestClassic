ZQuestClassic/ZQuestClassic

commit 14287ea262139ed24271f09a3e0b41273cb5eca6

refactor(zscript)!: use internal traceStr for TraceToBase

src/zc/ffscript.cpp
@@ -32543,10 +32543,9 @@ void FFScript::do_tracetobase()
 		s2 += ss.str();
 		break;
 	}
-	TraceScriptIDs();
-	s2 += "\n";
-	al_trace("%s", s2.c_str());
-	
+
+	traceStr(s2);
+
 	if ( console_enabled ) 
 	{
 		zscript_coloured_console.safeprint((CConsoleLoggerEx::COLOR_WHITE | 

commit d8c28ccbb07a6227d55f38596d8b77416972ba01

fix(zscript)!: print newline after TraceToBase
Regressed recently in 14287ea262.

src/zc/ffscript.cpp
@@ -30932,13 +30932,8 @@ void FFScript::do_tracetobase()
 		break;
 	}
 
+	s2 += "\n";
 	traceStr(s2);
-
-	if ( console_enabled ) 
-	{
-		zscript_coloured_console.safeprint((CConsoleLoggerEx::COLOR_WHITE | 
-			CConsoleLoggerEx::COLOR_BACKGROUND_BLACK),s2.c_str());
-	}
 }
 
 int32_t FFScript::getHeroOTile(int32_t index1, int32_t index2)