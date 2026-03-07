426a42fdb1bd5cad5b0732bcf9bd63644d7b0988
Emily Venezia <emilygamergirl99@gmail.com>
2026-03-06 23:12:28 -0500

    feat(zq)!: add info buttons to cheat dialog

--- src/dialog/cheat_codes.cpp
+++ src/dialog/cheat_codes.cpp
@@ -1,4 +1,5 @@
 #include "cheat_codes.h"
+#include "externs.h"
 #include <gui/builder.h>
 
 extern ZCHEATS zcheats;
@@ -19,33 +20,44 @@ std::shared_ptr<GUI::Widget> CheatCodesDialog::view()
 		title = "Cheat Codes",
 		onClose = message::CANCEL,
 		Column(
-			enabledCB = Checkbox(
-				hAlign = 1.0,
-				text = "&Enable Cheats",
-				checked = zcheats.flags&1),
-			Columns<5>(
+			Row(
+				INFOBTN("If cheats are not enabled, the 'Cheat' menu"
+					" will not appear at all, so no codes can be entered."
+					"\nWhen using Test Mode to test a quest, cheats are"
+					" automatically enabled at level 4 for you, regardless"
+					" of the settings here."),
+				enabledCB = Checkbox(
+					text = "&Enable Cheats",
+					checked = zcheats.flags&1)
+			),
+			Rows<3>(
 				Label(text = "Level"),
+				Label(text = "Code", colSpan = 2),
+				
 				Label(text = "1"),
-				Label(text = "2"),
-				Label(text = "3"),
-				Label(text = "4"),
-
-				Label(
-					hAlign = 0.05,
-					text = "Code"),
 				textFields[0] = TextField(
 					maxLength = 40,
 					text = zcheats.codes[0],
 					focused = true),
+				INFOBTN("Refill life, bombs, arrows, money, magic"),
+				
+				Label(text = "2"),
 				textFields[1] = TextField(
 					maxLength = 40,
 					text = zcheats.codes[1]),
+				INFOBTN("Change max life, max bombs, max magic, toggle invincibility"),
+				
+				Label(text = "3"),
 				textFields[2] = TextField(
 					maxLength = 40,
 					text = zcheats.codes[2]),
+				INFOBTN("Edit Hero Data (most Init Data fields are editable, including owned items/counters)"),
+				
+				Label(text = "4"),
 				textFields[3] = TextField(
-					maxLength=40,
-					text = zcheats.codes[3])
+					maxLength = 40,
+					text = zcheats.codes[3]),
+				INFOBTN("Full Debug Cheats; warp to locations, toggle layer visibility, show hitboxes, trigger secrets, etc.")
 			),
 			Row(
 				vAlign = 1.0,
