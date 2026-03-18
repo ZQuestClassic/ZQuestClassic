7934637ab1502350647fb743bcb0a504c120dd35
Emily Venezia <emilygamergirl99@gmail.com>
2026-03-18 14:24:43 -0400

    fix(zq): Under Combo dialog broken for layers
    The 'map' being used for each layer was off-by-1, causing the dialog to edit the wrong screens when editing layers
    
    (cherry picked from commit 3a56e7b463ffc03aac85ebd153b227e661a6dd0d)

--- src/dialog/undercombo_dlg.cpp
+++ src/dialog/undercombo_dlg.cpp
@@ -21,7 +21,7 @@ static void get_screens(mapscr& s, mapscr* screens[7])
 	{
 		screens[layer] = nullptr;
 		if (s.layermap[layer-1] > 0)
-			screens[layer] = Map.AbsoluteScr(s.layermap[layer-1], s.layerscreen[layer-1]);
+			screens[layer] = Map.AbsoluteScr(s.layermap[layer-1]-1, s.layerscreen[layer-1]);
 	}
 }
 
@@ -88,8 +88,6 @@ shared_ptr<GUI::Widget> UnderComboDialog::view()
 	
 	while (layer < 7)
 	{
-		if (layer > 0)
-			g->add(HSeparator());
 		shared_ptr<GUI::Grid> subg = GUI::Internal::makeRowsColumns(2, whole_map ? 2 : 3);
 		subg->setHAlign(0.0);
 		
@@ -104,10 +102,12 @@ shared_ptr<GUI::Widget> UnderComboDialog::view()
 		subg->add(INFOBTN("The combo+cset set as the undercombo for the layer screen."
 			"\nFirst box selects combo, second box shows animated preview of the combo."));
 		
+		bool did_something = false;
 		for (int q = 0; q < 4 && layer < 7; ++layer)
 		{
 			if (!whole_map && !layer_enabled[layer])
 				continue;
+			did_something = true;
 			++q;
 			size_t idx = 0;
 			#define ADD(w) \
@@ -174,7 +174,13 @@ shared_ptr<GUI::Widget> UnderComboDialog::view()
 			
 			#undef ADD
 		}
-		g->add(subg);
+		
+		if (did_something) // don't add a blank row
+		{
+			if (layer > 0)
+				g->add(HSeparator());
+			g->add(subg);
+		}
 	}
 	
 	if (whole_map)
@@ -278,7 +284,7 @@ bool UnderComboDialog::save_results()
 				cur_screens[layer]->undercombo = undercombos[layer];
 				cur_screens[layer]->undercset = undercsets[layer];
 				if (layer > 0)
-					Map.AbsoluteScrMakeValid(cur_screens[0]->layermap[layer-1], cur_screens[0]->layerscreen[layer-1]);
+					Map.AbsoluteScrMakeValid(cur_screens[0]->layermap[layer-1]-1, cur_screens[0]->layerscreen[layer-1]);
 			}
 		}
 	}
