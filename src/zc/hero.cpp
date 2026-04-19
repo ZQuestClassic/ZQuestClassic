da5fd0a43a725bbf06e47ee99c73575b1c94a080
Emily Venezia <emilygamergirl99@gmail.com>
2026-04-19 14:10:09 -0400

    fix(zc): Hero sometimes drowning when drown qr is off

--- src/zc/hero.cpp
+++ src/zc/hero.cpp
@@ -14356,6 +14356,7 @@ bool HeroClass::try_drown()
 {
 	if(ladderx || laddery) return false;
 	if (walk_through_walls) return false;
+	if (!get_qr(qr_DROWN)) return false;
 	if(action == drowning || action == lavadrowning) return true;
 	int water = onWater(true);
 	if(!water) return false;
