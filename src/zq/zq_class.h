ZQuestClassic/ZQuestClassic

commit e660a63e5401fe801432df9751049469155f8647

fix(zq)!: prevent zoomed-out map view from showing out-of-bounds

src/zq/zq_class.h
@@ -42,6 +42,11 @@ struct MapCursor
 
 	void setSize(int new_size)
 	{
+		if (new_size < 1)
+			new_size = 1;
+		if (new_size > 16)
+			new_size = 16;
+
 		if (size == new_size)
 			return;
 
@@ -50,7 +55,8 @@ struct MapCursor
 	}
 
 private:
-	// Modify `viewscr` just enough to keep `screen` in bounds.
+	// Modify `viewscr` just enough to keep `screen` in bounds,
+	// doing a best-effort clamp against the 16x8 map edges.
 	void adjustViewScr()
 	{
 		int cx = screen % 16;
@@ -62,10 +68,21 @@ private:
 			vx = cx;
 		else if (cx >= vx + size)
 			vx = cx - size + 1;
+
 		if (cy < vy)
 			vy = cy;
 		else if (cy >= vy + size)
 			vy = cy - size + 1;
+
+		if (vx + size > 16)
+			vx = 16 - size;
+		if (vy + size > 8)
+			vy = 8 - size;
+
+		if (vx < 0)
+			vx = 0;
+		if (vy < 0)
+			vy = 0;
 
 		viewscr = vx + vy * 16;
 	}

src/zq/zq_class.cpp
@@ -6665,12 +6665,14 @@ int32_t load_quest(const char *filename, bool show_progress)
 			std::string qst_cfg_header = qst_cfg_header_from_path(filename);
 			int view_size = zc_get_config(qst_cfg_header.c_str(), "zoom_num_screens", 1);
 
-			Map.setCursor(MapCursor{
+			auto cursor = MapCursor{
 				.map = vbound(zinit.last_map, 0, map_count - 1),
 				.screen = zinit.last_screen,
 				.viewscr = zinit.last_screen,
 				.size = view_size,
-			});
+			};
+			cursor.setScreen(cursor.screen); // Force clamping.
+			Map.setCursor(cursor);
 
 			extern int32_t current_mappage;
 			current_mappage = 0;

commit 10e0779c143e57aeb222feff27c633ba3e604901

fix(zq)!: restore access to 0x80+ screens
Regressed recently in e660a63e.

src/zq/zq_class.h
@@ -76,8 +76,8 @@ private:
 
 		if (vx + size > 16)
 			vx = 16 - size;
-		if (vy + size > 8)
-			vy = 8 - size;
+		if (vy + size > 9)
+			vy = 9 - size;
 
 		if (vx < 0)
 			vx = 0;