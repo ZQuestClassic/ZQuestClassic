ZQuestClassic/ZQuestClassic

commit d653e722b486265541bd61762fa7b194f2fdef14

refactor(zq): format map, screen locations more consistently
* Format map + screen pairs using a common format: decimal for map, hex
  with a `0x` prefix for screen, and spaces for legibility. Example:
  `19 : 0x5A`.
* Fix an issue in quest report tools that incorrectly used hex to format
  the map component.

src/zq/zquest.cpp
@@ -1,4 +1,5 @@
 #include "allegro/gui.h"
+#include "base/check.h"
 #include "zalleg/files.h"
 #include "core/mapscr.h"
 #include "dialog/edit_region.h"
@@ -5085,7 +5086,7 @@ void tile_warp_notification(int32_t which, char *buf)
         else
             sprintf(buf2,"%d-%s",Map.CurrScr()->tilewarpdmap[which],DMaps[Map.CurrScr()->tilewarpdmap[which]].name);
             
-        sprintf(buf,"Tile Warp %c: %s, %02X", letter, buf2, Map.CurrScr()->tilewarpscr[which]);
+        sprintf(buf,"Tile Warp %c: %s, 0x%02X", letter, buf2, Map.CurrScr()->tilewarpscr[which]);
         break;
     }
     
@@ -5118,10 +5119,10 @@ void side_warp_notification(int32_t which, int32_t dir, char *buf)
         // Destination DMap name
         if(strlen(DMaps[Map.CurrScr()->sidewarpdmap[which]].name)==0)
         {
-            sprintf(buf,"Side Warp %c (%s): %d, %02X", letter, buf3, Map.CurrScr()->sidewarpdmap[which], Map.CurrScr()->sidewarpscr[which]);
+            sprintf(buf,"Side Warp %c (%s): %d, 0x%02X", letter, buf3, Map.CurrScr()->sidewarpdmap[which], Map.CurrScr()->sidewarpscr[which]);
         }
         else
-            sprintf(buf,"Side Warp %c (%s): %d-%s, %02X", letter, buf3, Map.CurrScr()->sidewarpdmap[which],DMaps[Map.CurrScr()->sidewarpdmap[which]].name, Map.CurrScr()->sidewarpscr[which]);
+            sprintf(buf,"Side Warp %c (%s): %d-%s, 0x%02X", letter, buf3, Map.CurrScr()->sidewarpdmap[which],DMaps[Map.CurrScr()->sidewarpdmap[which]].name, Map.CurrScr()->sidewarpscr[which]);
             
         break;
     }
@@ -6770,9 +6771,10 @@ void refresh(int32_t flags, bool update)
 
 		if (i>0 && mapscreen_valid_layers[i - 1] && num_screens_to_draw == 1)
 		{
+			std::string screen_str = formatMapScreenPair(Map.CurrScr()->layermap[i-1], Map.CurrScr()->layerscreen[i-1]);
 			if(is_compact)
-				sprintf(tbuf, "%s%d %d:%02X", (i==2 && Map.CurrScr()->flags7&fLAYER2BG) || (i==3 && Map.CurrScr()->flags7&fLAYER3BG) ? "-":"", i, Map.CurrScr()->layermap[i-1], Map.CurrScr()->layerscreen[i-1]);
-			else sprintf(tbuf, "%s%d (%d:%02X)", (i==2 && Map.CurrScr()->flags7&fLAYER2BG) || (i==3 && Map.CurrScr()->flags7&fLAYER3BG) ? "-":"", i, Map.CurrScr()->layermap[i-1], Map.CurrScr()->layerscreen[i-1]);
+				sprintf(tbuf, "%s%d %s", (i==2 && Map.CurrScr()->flags7&fLAYER2BG) || (i==3 && Map.CurrScr()->flags7&fLAYER3BG) ? "-":"", i, screen_str.c_str());
+			else sprintf(tbuf, "%s%d (%s)", (i==2 && Map.CurrScr()->flags7&fLAYER2BG) || (i==3 && Map.CurrScr()->flags7&fLAYER3BG) ? "-":"", i, screen_str.c_str());
 		}
 		else
 		{
@@ -6861,7 +6863,7 @@ void refresh(int32_t flags, bool update)
 			//This fixes Screen Info not displaying properly when changing maps. -Z 
 			//Needed to refresh the screen info. -Z ( 26th March, 2019 )
 			int32_t m = Map.getLayerTargetMultiple();
-			sprintf(buf,"Used as a layer by screen %d:%02X",Map.getLayerTargetMap(),Map.getLayerTargetScr());
+			sprintf(buf,"Used as a layer by screen %s", formatMapScreenPair(Map.getLayerTargetMap(), Map.getLayerTargetScr()).c_str());
 			char buf2[24];
 			
 			if(m>0)
@@ -6875,7 +6877,7 @@ void refresh(int32_t flags, bool update)
 		
 		if(Map.CurrScr()->nextmap)
 		{
-			sprintf(buf,"Screen State carries over to %d:%02X",Map.CurrScr()->nextmap,Map.CurrScr()->nextscr);
+			sprintf(buf,"Screen State carries over to %s",formatMapScreenPair(Map.CurrScr()->nextmap, Map.CurrScr()->nextscr).c_str());
 			show_screen_error(buf,i++,vc(15));
 		}
 		
@@ -10172,14 +10174,14 @@ void domouse()
 		for(int32_t btn=0; btn<mappage_count; ++btn)
 		{
 			if (btn == current_mappage) continue;
-			char tbuf[15];
-			sprintf(tbuf, "%d:%02X", map_page[btn].map+1, map_page[btn].screen);
+
+			std::string screen_str = formatMapScreenPair(map_page[btn].map+1, map_page[btn].screen);
 			auto& sqr = map_page_bar[btn];
 			if(sqr.rect(x,y))
 			{
-				if(do_layer_button_reset(sqr.x,sqr.y,sqr.w,sqr.h,tbuf,(btn==current_mappage?D_SELECTED:0)))
+				if(do_layer_button_reset(sqr.x,sqr.y,sqr.w,sqr.h,screen_str.c_str(),(btn==current_mappage?D_SELECTED:0)))
 				{
-					draw_layer_button(screen, sqr.x,sqr.y,sqr.w,sqr.h,tbuf,D_SELECTED);
+					draw_layer_button(screen, sqr.x,sqr.y,sqr.w,sqr.h,screen_str.c_str(),D_SELECTED);
 					
 					if (lclick)
 					{
@@ -10366,17 +10368,18 @@ void domouse()
 				}
 				else if (i != 0 && mapscreen_valid_layers[i - 1])
 				{
+					std::string screen_str = formatMapScreenPair(Map.CurrScr()->layermap[i-1], Map.CurrScr()->layerscreen[i-1]);
 					if (is_compact)
 					{
-						sprintf(tbuf, "%s%d %d:%02X",
+						sprintf(tbuf, "%s%d %s",
 							(i==2 && Map.CurrScr()->flags7&fLAYER2BG) || (i==3 && Map.CurrScr()->flags7&fLAYER3BG) ? "-":"",
-							i, Map.CurrScr()->layermap[i-1], Map.CurrScr()->layerscreen[i-1]);
+							i, screen_str.c_str());
 					}
 					else
 					{
-						sprintf(tbuf, "%s%d (%d:%02X)",
+						sprintf(tbuf, "%s%d (%s)",
 							(i==2 && Map.CurrScr()->flags7&fLAYER2BG) || (i==3 && Map.CurrScr()->flags7&fLAYER3BG) ? "-":"",
-							i, Map.CurrScr()->layermap[i-1], Map.CurrScr()->layerscreen[i-1]);
+							i, screen_str.c_str());
 					}
 				}
 				else
@@ -21806,6 +21809,15 @@ void ZQ_ClearQuestPath()
 {
 	zc_set_config("zquest","win_last_quest",(char const*)nullptr);
 	strcpy(filepath,"");
+}
+
+// map should be 1-indexed.
+// Formats map as decimal, screen as hex. Ex: `19 : 0x3`.
+std::string formatMapScreenPair(int map, int screen)
+{
+	DCHECK(map > 0);
+	DCHECK(screen >= 0);
+	return fmt::format("{} : 0x{:02X}", map, screen);
 }
 
 //FFCore

src/zq/questReport.cpp
@@ -202,13 +202,14 @@ void TileWarpsReport()
             {
                 if(!type_found)
                 {
-                    sprintf(buf, "The following screens have tile warp references set to the current screen (%X:%02X):\n", Map.getCurrMap()+1, Map.getCurrScr());
-                    quest_report_str+=buf;
+					quest_report_str += fmt::format(
+						"The following screens have tile warp references set to the current screen ({}):\n",
+						formatMapScreenPair(Map.getCurrMap()+1, Map.getCurrScr()));
                     type_found=true;
                 }
                 
                 buf[0]=0;
-                sprintf(buf, "%s %3d:%02X\n", palname_spaced(ts->color), m+1, s);
+                sprintf(buf, "%s %10s\n", palname_spaced(ts->color), formatMapScreenPair(m + 1, s).c_str());
                 quest_report_str+=buf;
             }
         }
@@ -268,13 +269,14 @@ void SideWarpsReport()
             {
                 if(!type_found)
                 {
-                    sprintf(buf, "The following screens have side warp references set to the current screen (%X:%02X):\n", Map.getCurrMap()+1, Map.getCurrScr());
-                    quest_report_str+=buf;
+                    quest_report_str += fmt::format(
+						"The following screens have side warp references set to the current screen ({}):\n",
+						formatMapScreenPair(Map.getCurrMap()+1, Map.getCurrScr()));
                     type_found=true;
                 }
                 
                 buf[0]=0;
-                sprintf(buf, "%s %3d:%02X\n", palname_spaced(ts->color), m+1, s);
+                sprintf(buf, "%s %10s\n", palname_spaced(ts->color), formatMapScreenPair(m + 1, s).c_str());
                 quest_report_str+=buf;
             }
         }
@@ -331,13 +333,14 @@ void LayersReport()
             {
                 if(!type_found)
                 {
-                    sprintf(buf, "The following screens use the current screen as a layer (%X:%02X):\n", Map.getCurrMap()+1, Map.getCurrScr());
-                    quest_report_str+=buf;
+					quest_report_str += fmt::format(
+						"The following screens use the current screen as a layer ({}):\n",
+						formatMapScreenPair(Map.getCurrMap()+1, Map.getCurrScr()));
                     type_found=true;
                 }
                 
                 buf[0]=0;
-                sprintf(buf, "%s %3d:%02X (layer %d)\n", palname_spaced(ts->color), m+1, s, layer_check[i]);
+                sprintf(buf, "%s %10s (layer %d)\n", palname_spaced(ts->color), formatMapScreenPair(m + 1, s).c_str(), layer_check[i]);
                 quest_report_str+=buf;
             }
         }
@@ -377,7 +380,7 @@ void integrityCheckSpecialItem()
                     
                 type_found=true;
                 buf[0]=0;
-                sprintf(buf, "%s %3d:%02X\n", palname_spaced(ts->color), m+1, s);
+                sprintf(buf, "%s %10s\n", palname_spaced(ts->color), formatMapScreenPair(m + 1, s).c_str());
                 quest_report_str+=buf;
             }
         }
@@ -454,7 +457,7 @@ void integrityCheckEnemiesItem()
                 }
                 
                 buf[0]=0;
-                sprintf(buf, "%s %3d:%02X\n", palname_spaced(ts->color), m+1, s);
+                sprintf(buf, "%s %10s\n", palname_spaced(ts->color), formatMapScreenPair(m + 1, s).c_str());
                 quest_report_str+=buf;
             }
         }
@@ -531,7 +534,7 @@ void integrityCheckEnemiesSecret()
                 }
                 
                 buf[0]=0;
-                sprintf(buf, "%s %3d:%02X\n", palname_spaced(ts->color), m+1, s);
+                sprintf(buf, "%s %10s\n", palname_spaced(ts->color), formatMapScreenPair(m + 1, s).c_str());
                 quest_report_str+=buf;
             }
         }
@@ -615,7 +618,10 @@ void integrityCheckTileWarpDestSquare()
                 }
                 
                 buf[0]=0;
-                sprintf(buf, "%s %3d:%02X (%3d:%02X)\n", palname_spaced(ts->color), m+1, s, ((warp_check[i]-1)/MAPSCRS)+1, (warp_check[i]-1)%MAPSCRS);
+                sprintf(buf, "%s %10s (%10s)\n",
+					palname_spaced(ts->color),
+					formatMapScreenPair(m + 1, s).c_str(),
+					formatMapScreenPair(((warp_check[i]-1)/MAPSCRS)+1, (warp_check[i]-1)%MAPSCRS).c_str());
                 quest_report_str+=buf;
             }
         }
@@ -736,7 +742,7 @@ void integrityCheckTileWarpDest()
                 }
                 
                 buf[0]=0;
-                sprintf(buf, "%s %3d:%02X %s%s%s%s%s\n", palname_spaced(ts->color), m+1, s, warpa ? "[A] ":"", warpb ? "[B] ":"",
+                sprintf(buf, "%s %10s %s%s%s%s%s\n", palname_spaced(ts->color), formatMapScreenPair(m + 1, s).c_str(), warpa ? "[A] ":"", warpb ? "[B] ":"",
                         warpc ? "[C] ":"", warpd ? "[D] ":"", warpr ? "[Random]":"");
                 quest_report_str+=buf;
             }
@@ -846,7 +852,7 @@ void integrityCheckSideWarpDest()
                 }
                 
                 buf[0]=0;
-                sprintf(buf, "%s %3d:%02X %s%s%s%s%s%s\n", palname_spaced(ts->color), m+1, s, warpa ? "[A] ":"", warpb ? "[B] ":"",
+                sprintf(buf, "%s %10s %s%s%s%s%s%s\n", palname_spaced(ts->color), formatMapScreenPair(m + 1, s).c_str(), warpa ? "[A] ":"", warpb ? "[B] ":"",
                         warpc ? "[C] ":"", warpd ? "[D] ":"", warpr ? "[Random]":"", warpt ? "[Triforce]" : "");
                 quest_report_str+=buf;
             }
@@ -926,7 +932,7 @@ void integrityCheckUnderCombo()
                 }
                 
                 buf[0]=0;
-                sprintf(buf, "%s %3d:%02X\n", palname_spaced(ts->color), m+1, s);
+                sprintf(buf, "%s %10s\n", palname_spaced(ts->color), formatMapScreenPair(m + 1, s).c_str());
                 quest_report_str+=buf;
             }
         }
@@ -1001,7 +1007,7 @@ void integrityCheckSaveCombo()
                 }
                 
                 buf[0]=0;
-                sprintf(buf, "%s %3d:%02X\n", palname_spaced(ts->color), m+1, s);
+                sprintf(buf, "%s %10s\n", palname_spaced(ts->color), formatMapScreenPair(m + 1, s).c_str());
                 quest_report_str+=buf;
             }
         }
@@ -1041,7 +1047,7 @@ void integrityCheckGuyNoString()
                 }
                 
                 buf[0]=0;
-                sprintf(buf, "%s %3d:%02X\n", palname_spaced(ts->color), m+1, s);
+                sprintf(buf, "%s %10s\n", palname_spaced(ts->color), formatMapScreenPair(m + 1, s).c_str());
                 quest_report_str+=buf;
             }
         }
@@ -1112,7 +1118,7 @@ void integrityCheckRoomNoGuy()
                 }
                 
                 buf[0]=0;
-                sprintf(buf, "%s %3d:%02X\n", palname_spaced(ts->color), m+1, s);
+                sprintf(buf, "%s %10s\n", palname_spaced(ts->color), formatMapScreenPair(m + 1, s).c_str());
                 quest_report_str+=buf;
             }
         }
@@ -1182,7 +1188,7 @@ void integrityCheckRoomNoString()
                 }
                 
                 buf[0]=0;
-                sprintf(buf, "%s %3d:%02X\n", palname_spaced(ts->color), m+1, s);
+                sprintf(buf, "%s %10s\n", palname_spaced(ts->color), formatMapScreenPair(m + 1, s).c_str());
                 quest_report_str+=buf;
                 type_found=true;
             }
@@ -1252,7 +1258,7 @@ void integrityCheckRoomNoGuyNoString()
                 }
                 
                 buf[0]=0;
-                sprintf(buf, "%s %3d:%02X\n", palname_spaced(ts->color), m+1, s);
+                sprintf(buf, "%s %10s\n", palname_spaced(ts->color), formatMapScreenPair(m + 1, s).c_str());
                 quest_report_str+=buf;
                 type_found=true;
             }
@@ -1299,7 +1305,7 @@ void integrityCheckItemWalkability()
                 }
                 
                 buf[0]=0;
-                sprintf(buf, "%s %3d:%02X\n", palname_spaced(ts->color), m+1, s);
+                sprintf(buf, "%s %10s\n", palname_spaced(ts->color), formatMapScreenPair(m + 1, s).c_str());
                 quest_report_str+=buf;
             }
         }
@@ -1371,7 +1377,10 @@ void integrityCheckTileWarpDestSquareWalkability()
                 }
                 
                 buf[0]=0;
-                sprintf(buf, "%s %3d:%02X (%3d:%02X)\n", palname_spaced(ts->color), m+1, s, ((warp_check[i]-1)/MAPSCRS)+1, (warp_check[i]-1)%MAPSCRS);
+                sprintf(buf, "%s %10s (%10s)\n",
+					palname_spaced(ts->color),
+					formatMapScreenPair(m + 1, s).c_str(),
+					formatMapScreenPair(((warp_check[i]-1)/MAPSCRS)+1, (warp_check[i]-1)%MAPSCRS).c_str());
                 quest_report_str+=buf;
             }
         }
@@ -1425,7 +1434,7 @@ void integrityCheckTileWarpDestScreenInvalid()
                     }
                     
                     buf[0]=0;
-                    sprintf(buf, "%s %3d:%02X\n", palname_spaced(ts->color), m+1, s);
+                    sprintf(buf, "%s %10s\n", palname_spaced(ts->color), formatMapScreenPair(m + 1, s).c_str());
                     quest_report_str+=buf;
                     type_found=true;
                 }
@@ -1906,10 +1915,9 @@ void enemyLocationReport()
                 tempnode=tempnode->next;
                 //add it to the list
                 buf[0]=0;
-                sprintf(buf, "%s %3d:%02X (%d)\n",
+                sprintf(buf, "%s %10s (%d)\n",
                         palname_spaced(tempnode->pal),
-                        tempnode->map,
-                        tempnode->screen,
+						formatMapScreenPair(tempnode->map, tempnode->screen).c_str(),
                         tempnode->list /* Possibly add more details later/never */);
                 quest_report_str+=buf;
             }
@@ -2061,7 +2069,9 @@ void scriptLocationReport()
                 tempnode=tempnode->next;
                 //add it to the list
                 buf[0]=0;
-                sprintf(buf, "%s %3d:%02X (Args: %d, %d, %d, %d, %d, %d, %d, %d)\n", palname_spaced(tempnode->pal), tempnode->map, tempnode->screen,
+                sprintf(buf, "%s %10s (Args: %d, %d, %d, %d, %d, %d, %d, %d)\n",
+						palname_spaced(tempnode->pal),
+						formatMapScreenPair(tempnode->map, tempnode->screen).c_str(),
                         tempnode->d[0]/10000,tempnode->d[1]/10000,tempnode->d[2]/10000,tempnode->d[3]/10000,
                         tempnode->d[4]/10000,tempnode->d[5]/10000,tempnode->d[6]/10000,tempnode->d[7]/10000);
                 quest_report_str+=buf;
@@ -2153,7 +2163,7 @@ void ComboLocationReport()
                 }
                 
                 buf[0]=0;
-                sprintf(buf, "%s %3d:%02X (%d use%s", palname_spaced(ts->color), m+1, s, uses, uses>1 ? "s" : "");
+                sprintf(buf, "%s %10s (%d use%s", palname_spaced(ts->color), formatMapScreenPair(m + 1, s).c_str(), uses, uses>1 ? "s" : "");
                 quest_report_str+=buf;
                 
                 if(secretuses>0)
@@ -2374,7 +2384,7 @@ void ComboTypeLocationReport()
                 tempnode=tempnode->next;
                 //add it to the list
                 buf[0]=0;
-                sprintf(buf, "%s %3d:%02X (%d uses", palname_spaced(tempnode->pal), tempnode->map, tempnode->screen, tempnode->uses);
+                sprintf(buf, "%s %10s (%d uses", palname_spaced(tempnode->pal), formatMapScreenPair(tempnode->map, tempnode->screen).c_str(), tempnode->uses);
                 quest_report_str+=buf;
                 
                 if(tempnode->secretuses>0)

src/zq/zquest.h
@@ -603,6 +603,7 @@ void draw_box(BITMAP* destbmp, size_and_pos* pos, char const* tipmsg, double txs
 void update_tooltip(int32_t x, int32_t y, size_and_pos const& sqr, char const* tipmsg, double scale = 1);
 void update_tooltip(int32_t x, int32_t y, int32_t trigger_x, int32_t trigger_y, int32_t trigger_w, int32_t trigger_h, char const* tipmsg, int fw = -1, int fh = -1, double scale = 1);
 void ZQ_ClearQuestPath();
+std::string formatMapScreenPair(int map, int screen);
 void cflag_help(int32_t id);
 void ctype_help(int32_t id);
 void questrev_help();

commit ec25c1647f4ae0e65ab54560c6cfec42cddc82b4

fix(zc)!: use shorter text on click for layers/bookmarks
Regressed recently in d653e72.

src/zq/zquest.cpp
@@ -10166,7 +10166,7 @@ void domouse()
 		{
 			if (btn == current_mappage) continue;
 
-			std::string screen_str = formatMapScreenPair(map_page[btn].map+1, map_page[btn].screen);
+			std::string screen_str = formatMapScreenPair(map_page[btn].map+1, map_page[btn].screen, true);
 			auto& sqr = map_page_bar[btn];
 			if(sqr.rect(x,y))
 			{
@@ -10359,7 +10359,7 @@ void domouse()
 				}
 				else if (i != 0 && mapscreen_valid_layers[i - 1])
 				{
-					std::string screen_str = formatMapScreenPair(Map.CurrScr()->layermap[i-1], Map.CurrScr()->layerscreen[i-1]);
+					std::string screen_str = formatMapScreenPair(Map.CurrScr()->layermap[i-1], Map.CurrScr()->layerscreen[i-1], true);
 					if (is_compact)
 					{
 						sprintf(tbuf, "%s%d %s",