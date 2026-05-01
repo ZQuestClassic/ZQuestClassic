93e16b8d3ddfa0cda0d971e68ec3c8ed452f573d
Connor Clark <cjamcl@gmail.com>
2026-04-30 19:19:34 -0700

    refactor(zscript): increase ZASM hang threshold
    Every set number of ZASM instructions, the scripting engine checks if
    the player hit F9 or F10 to allow them to quit during an infinite loop.
    The default was to check every 1000 instructions, but that is way too
    frequent. Now, the engine checks every 100k instructions. This reduces
    the amount of unnecessary overhead related to checking for key events.

--- src/zc/zelda.cpp
+++ src/zc/zelda.cpp
@@ -4557,7 +4557,7 @@ reload_for_replay_file:
 	DEBUG_PRINT_TO_CONSOLE = zc_get_config("ZSCRIPT", "print_zasm_to_console", false);
 	DEBUG_JIT_PRINT_ASM = zc_get_config("ZSCRIPT", "jit_print_asm", false) || used_switch(argc, argv, "-jit-print-asm");
 	DEBUG_JIT_EXIT_ON_COMPILE_FAIL = zc_get_config("ZSCRIPT", "jit_fatal_compile_errors", false) || used_switch(argc, argv, "-jit-fatal-compile-errors");
-	hangcount = zc_get_config("ZSCRIPT","ZASM_Hangcount",1000);
+	hangcount = zc_get_config("ZSCRIPT","ZASM_Hangcount",100000);
 	jit_set_enabled(is_feature_enabled("-jit", "ZSCRIPT", "jit", true));
 
 #ifdef __EMSCRIPTEN__
