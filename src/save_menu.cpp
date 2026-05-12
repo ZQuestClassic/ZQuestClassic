ZQuestClassic/ZQuestClassic

commit 790b0836f8a299ca66dee581d5d5c1edf21208e2

fix(zc)!: replays no longer desync when using custom save menu

src/save_menu.cpp
@@ -53,6 +53,7 @@ optional<byte> SaveMenu::run(optional<byte> cursor) const
 	{
 		if (use_music && unsigned(music-1) < quest_music.size())
 			quest_music[music-1].play();
+
 		while (!tick(cursor, clk))
 		{
 			if (Quit)
@@ -65,17 +66,11 @@ optional<byte> SaveMenu::run(optional<byte> cursor) const
 			advanceframe(false, true, false);
 			load_control_state();
 		}
-		
+
 		if (cursor)
 		{
 			SaveMenuOption const& opt = options[*cursor];
-			
-			if (opt.flags & SMENU_OPT_SAVE)
-				save_game(false);
-			
-			if (opt.gen_script)
-				FFCore.runGenericFrozenEngine(opt.gen_script);
-			
+
 			running = false;
 			if (Quit)
 				;
@@ -88,6 +83,14 @@ optional<byte> SaveMenu::run(optional<byte> cursor) const
 			else if (!is_dead && (opt.flags & SMENU_OPT_CANCEL))
 				Quit = 0;
 			else running = true;
+
+			// We must save the game in the outermost game loop (init_and_run_main_zplayer_loop) for
+			// replays to work.
+			if (opt.flags & SMENU_OPT_SAVE)
+				pending_save_from_save_menu = true;
+
+			if (opt.gen_script)
+				FFCore.runGenericFrozenEngine(opt.gen_script);
 		}
 		else if (Quit)
 			running = false;

src/zc/ffscript.cpp
@@ -29470,6 +29470,7 @@ void FFScript::runF6Engine()
 		}
 		else if (unsigned(QMisc.savemenu_f6 - 1) < NUM_SAVE_MENUS && QMisc.save_menus[QMisc.savemenu_f6 - 1].is_valid())
 		{
+			GameFlags &= ~GAMEFLAG_TRYQUIT;
 			GameFlags |= GAMEFLAG_F6SCRIPT_ACTIVE;
 			QMisc.save_menus[QMisc.savemenu_f6 - 1].run();
 			GameFlags &= ~GAMEFLAG_F6SCRIPT_ACTIVE;

src/zc/zc_sys.cpp
@@ -4290,9 +4290,11 @@ void advanceframe(bool allowwavy, bool sfxcleanup, bool allowF6Script)
 	if (GameFlags & GAMEFLAG_TRYQUIT)
 		replay_step_quit(0);
 	if(allowF6Script)
+	{
 		FFCore.runF6Engine();
-	if (Quit)
-		replay_step_quit(Quit);
+		if (Quit)
+			replay_step_quit(Quit);
+	}
 	
 #ifdef _WIN32
 	

src/zc/zelda.cpp
@@ -369,9 +369,10 @@ ZScriptArray localRAM[NUM_ZSCRIPT_ARRAYS];
 std::map<int32_t,ZScriptArray> objectRAM;
 ArrayOwner arrayOwner[NUM_ZSCRIPT_ARRAYS];
 
+bool pending_save_from_save_menu;
+
 //script bitmap drawing
 ZScriptDrawingRenderTarget* zscriptDrawingRenderTarget;
-
 
 void setZScriptVersion(int32_t s_version)
 {
@@ -1648,6 +1649,7 @@ void init_game_vars(bool is_cont_game = false)
 	{
 		visited[i]=-1;
 	}
+	pending_save_from_save_menu = false;
 }
 
 int32_t init_game()
@@ -4715,6 +4717,11 @@ reload_for_replay_file:
 					save_game(false);
 					Quit = qCONT;
 				}
+				else if (pending_save_from_save_menu)
+				{
+					save_game(false);
+				}
+				pending_save_from_save_menu = false;
 				
 				if (get_qr(qr_OLD_SCRIPT_VOLUME))
 				{

src/zc/zelda.h
@@ -423,6 +423,8 @@ extern std::string only_qstpath;
 extern volatile int32_t lastfps;
 extern volatile int32_t framecnt;
 
+extern bool pending_save_from_save_menu;
+
 // quest file data
 extern zquestheader QHeader;
 extern int32_t				   msg_strings_size;

commit 7ce4c277b7593a8904d1d69871b6f3c82a36fc01

fix(zc)!: ensure save+cancel works in custom save menu
Regressed recently in 790b083.

src/save_menu.cpp
@@ -90,10 +90,20 @@ optional<byte> SaveMenu::run(optional<byte> cursor) const
 				Quit = 0;
 			else running = true;
 
-			// We must save the game in the outermost game loop (init_and_run_main_zplayer_loop) for
-			// replays to work.
 			if (opt.flags & SMENU_OPT_SAVE)
-				pending_save_from_save_menu = true;
+			{
+				if (Quit)
+				{
+					// If we're quitting, we must save the game in the outermost game loop
+					// (init_and_run_main_zplayer_loop) for replays to work.
+					pending_save_from_save_menu = true;
+				}
+				else
+				{
+					// Otherwise just save it here.
+					save_game(false);
+				}
+			}
 		}
 		else if (Quit)
 			running = false;