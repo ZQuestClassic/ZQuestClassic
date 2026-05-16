4fc813cd26567c90c40438ee527e87e82b6c400c
Connor Clark <cjamcl@gmail.com>
2026-05-16 13:53:06 -0700

    misc!: remove debug info from changelog
    (cherry picked from commit b626406121c358fc96119444073597b31f066a63)

--- scripts/generate_changelog.py
+++ scripts/generate_changelog.py
@@ -394,6 +394,8 @@ def generate_changelog(from_sha: str, to_sha: str) -> str:
         if m:
             body = body[0 : m.start()].strip()
         body = re.sub(r'^Co-authored-by: .+', '', body, flags=re.MULTILINE).strip()
+        body = re.sub(r'^Signed-off-by: .+', '', body, flags=re.MULTILINE).strip()
+        body = re.sub(r'^Agent: .+', '', body, flags=re.MULTILINE).strip()
         body = re.sub(
             r'^\(cherry picked from commit .+\)$', '', body, flags=re.MULTILINE
         ).strip()
