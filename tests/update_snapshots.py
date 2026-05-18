4eed6efc1b294c9a1f03cc0d07fa1b388a0f815a
Emily Venezia <emilygamergirl99@gmail.com>
2026-05-18 17:09:55 -0400

    test!: optimize snapshot updating script

--- tests/update_snapshots.py
+++ tests/update_snapshots.py
@@ -7,18 +7,19 @@ from pathlib import Path
 script_dir = Path(os.path.dirname(os.path.realpath(__file__)))
 
 
-def run(path: Path):
-    print(f'running {path.name} --update')
+def run(path: Path, *args):
     run_args = [
         sys.executable,
         path,
         '--update',
+        *args,
     ]
+    print(f'running {path.name} {' '.join(run_args[2:])}')
     subprocess.check_call(run_args)
 
 
 # run(script_dir / 'test_jit.py')
 run(script_dir / 'test_optimize_zasm.py')
 run(script_dir / 'test_optimize_zasm_unit.py')
-run(script_dir / 'test_zscript.py')
+run(script_dir / 'test_zscript.py', '--', '-k', 'test_zscript_compiler_expected_zasm')
 run(script_dir / 'test_zscript_database.py')
