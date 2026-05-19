ZQuestClassic/ZQuestClassic

commit 9d742eafe281e4435f83cd86941e926e006c0594

fix(zc): prevent infinite loop in trig_trigger_groups

src/zc/combos.cpp
@@ -3048,6 +3048,8 @@ void trig_trigger_groups()
 		int cid = handle.data();
 
 		bool recheck = false;
+		std::set<int> visited_cids;
+		visited_cids.insert(cid);
 		do
 		{
 			if (!combo_cache.minis[cid].tgroup)
@@ -3063,7 +3065,7 @@ void trig_trigger_groups()
 				if(!ok && trig.trigger_flags.get(TRIGFLAG_TGROUP_GREATER))
 					ok = cpos_trig_group_count(trig.trig_group) > trig.trig_group_val;
 				if(!ok) continue;
-				
+
 				bool earlyret = false;
 				if(do_trigger_combo(handle, idx))
 					if(trig.trigger_flags.get(TRIGFLAG_CANCEL_TRIGGER))
@@ -3078,10 +3080,14 @@ void trig_trigger_groups()
 					else timer.updateData(cid2);
 				}
 				else timer.updateData(cid2);
-				
+
 				if(earlyret) break;
 				if (cid != cid2)
 				{
+					// Prevent infinite oscillation: if this cid was already visited, stop.
+					if (visited_cids.count(cid2))
+						break;
+					visited_cids.insert(cid2);
 					cid = cid2;
 					recheck = true;
 					break;

commit 60e417fa3ba0bc4b4d64b482c7135c82026e07fa

fix(zc)!: resolve performance regression in trig_trigger_groups
Recently regressed in 9d742ea.

Discord: https://discord.com/channels/876899628556091432/1506121429874376704

src/zc/combos.cpp
@@ -1,4 +1,5 @@
 #include "base/general.h"
+#include "base/util.h"
 #include "core/handles.h"
 #include "core/qrs.h"
 #include "core/dmap.h"
@@ -3031,6 +3032,7 @@ void trig_trigger_groups()
 	auto& combo_cache = combo_caches::trigger_group;
 
 	bool include_ffcs = true;
+	std::vector<int> visited_cids;
 	for_every_combo([&](const auto& handle) {
 		// changers don't contribute
 		if constexpr (requires { handle.ffc; })
@@ -3042,8 +3044,8 @@ void trig_trigger_groups()
 		int cid = handle.data();
 
 		bool recheck = false;
-		std::set<int> visited_cids;
-		visited_cids.insert(cid);
+		visited_cids.clear();
+		visited_cids.push_back(cid);
 		do
 		{
 			if (!combo_cache.minis[cid].tgroup)
@@ -3079,9 +3081,9 @@ void trig_trigger_groups()
 				if (cid != cid2)
 				{
 					// Prevent infinite oscillation: if this cid was already visited, stop.
-					if (visited_cids.count(cid2))
+					if (util::contains(visited_cids, cid2))
 						break;
-					visited_cids.insert(cid2);
+					visited_cids.push_back(cid2);
 					cid = cid2;
 					recheck = true;
 					break;