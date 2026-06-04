87a84351e386f6c4daab745e977f145170d01dc0
Emily Venezia <emilygamergirl99@gmail.com>
2026-06-04 02:26:17 -0400

    fix: buggy solid platform sideview ffc collision
    when moving at angles, the collision for riding the platform was getting confused
    with the collision for solid objects pushing the player, knocking the player
    off the platform weirdly.

--- src/core/qst_rules.cpp
+++ src/core/qst_rules.cpp
@@ -866,6 +866,7 @@ int32_t readrules(PACKFILE *f, zquestheader *Header)
 	{
 		set_qr(qr_CHESTS_SIGNS_LOCKBLOCKS_ABOVE_2, 1);
 		set_qr(qr_KNOCKBACK_THROUGH_PLATFORMS, 1);
+		set_qr(qr_BROKEN_SIDEVIEW_SOLID_FFC_COLLISION, 1);
 	}
 
 	set_qr(qr_ANIMATECUSTOMWEAPONS,0);
