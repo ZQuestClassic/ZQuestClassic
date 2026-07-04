ZQuestClassic/ZQuestClassic

commit acb7c848170c77210c385586d38bd2b0d2e8fd0b

refactor: Init Data / Gamedata
Change how Init Data is stored and saved to the quest file (and rearrange the dialog slightly)
    - Allow setting mcguffin level state for all levels in init data/cheats instead of just levels 1-8
    - Init data health can be in HP instead of just in Hearts
    - Can set init values for script counters 26-100 (was missing before) 
- Change how data is stored and saved to the save file

src/gamedata.cpp
@@ -67,17 +67,12 @@ void gamedata::load_user_objects()
 
 void gamedata::clear_genscript()
 {
-	memset(gen_doscript, 0, sizeof(gen_doscript));
-	memset(gen_exitState, 0, sizeof(gen_exitState));
-	memset(gen_reloadState, 0, sizeof(gen_reloadState));
-	memset(gen_eventstate, 0, sizeof(gen_eventstate));
-	memset(gen_initd, 0, sizeof(gen_initd));
-	memset(gen_dataSize, 0, sizeof(gen_dataSize));
-	for(size_t q = 0; q < NUMSCRIPTSGENERIC; ++q)
-	{
-		gen_data[q].clear();
-		gen_data[q].resize(0);
-	}
+	gen_doscript.clear();
+	gen_exitState.clear();
+	gen_reloadState.clear();
+	gen_eventstate.clear();
+	gen_initd.clear();
+	gen_data.clear();
 }
 
 const char *gamedata::get_qstpath() const
@@ -563,14 +558,14 @@ byte gamedata::get_continue_scrn() const
 }
 void gamedata::set_continue_scrn(byte s)
 {
-    if(!isclearing && _continue_scrn != s) Z_eventlog("Continue screen set to %x\n", s);
+    if(_continue_scrn != s) Z_eventlog("Continue screen set to %x\n", s);
     
     _continue_scrn=s;
     return;
 }
 void gamedata::change_continue_scrn(int16_t s)
 {
-    if(!isclearing && s!=0) Z_eventlog("Continue screen set to %x\n", _continue_scrn+s);
+    if(s!=0) Z_eventlog("Continue screen set to %x\n", _continue_scrn+s);
     
     _continue_scrn+=s;
     return;
@@ -582,14 +577,14 @@ word gamedata::get_continue_dmap() const
 }
 void gamedata::set_continue_dmap(word d)
 {
-    if(!isclearing && _continue_dmap!=d) Z_eventlog("Continue DMap set to %d\n", d);
+    if(_continue_dmap!=d) Z_eventlog("Continue DMap set to %d\n", d);
     
     _continue_dmap=d;
     return;
 }
 void gamedata::change_continue_dmap(int16_t d)
 {
-    if(!isclearing && d!=0) Z_eventlog("Continue DMap set to %d\n", _continue_dmap+d);
+    if(d!=0) Z_eventlog("Continue DMap set to %d\n", _continue_dmap+d);
     
     _continue_dmap+=d;
     return;
@@ -675,11 +670,6 @@ byte gamedata::get_lkeys()
 {
     return lvlkeys[dlevel];
 }
-
-/*bool gamedata::get_item(int32_t id)
-{
-return item[id];
-}*/
 
 byte gamedata::get_hcp_per_hc()
 {
@@ -876,7 +866,7 @@ void gamedata::set_item(int32_t id, bool value)
 
 void gamedata::set_item_no_flush(int32_t id, bool value)
 {
-    if(!isclearing && !(value == item[id]))
+    if(value != item[id])
         Z_eventlog("%sed item %i: %s\n", value ? "Gain" : "Remov", id, item_string[id]); //Gain + ed, Remov + ed. %s + ed.
         
     item[id]=value;

src/gamedata.h
@@ -4,48 +4,12 @@
 #include "base/general.h"
 #include "base/ints.h"
 #include "base/zc_array.h"
+#include "base/containers.h"
 #include "user_object.h"
 
 #define DIDCHEAT_BIT 0x80
 #define NUM_GSWITCHES 256
 #define MAX_MI (MAXDMAPS*MAPSCRSNORMAL)
-
-enum
-{
-	crNONE = -1,
-	crLIFE, crMONEY, crBOMBS, crARROWS, crMAGIC,
-	crKEYS, crSBOMBS, crCUSTOM1, crCUSTOM2, crCUSTOM3,
-	crCUSTOM4, crCUSTOM5, crCUSTOM6, crCUSTOM7, crCUSTOM8,
-	crCUSTOM9, crCUSTOM10, crCUSTOM11, crCUSTOM12, crCUSTOM13,
-	crCUSTOM14, crCUSTOM15, crCUSTOM16, crCUSTOM17, crCUSTOM18,
-	crCUSTOM19, crCUSTOM20, crCUSTOM21, crCUSTOM22, crCUSTOM23,
-	crCUSTOM24, crCUSTOM25, crCUSTOM26, crCUSTOM27, crCUSTOM28,
-	crCUSTOM29, crCUSTOM30, crCUSTOM31, crCUSTOM32, crCUSTOM33,
-	crCUSTOM34, crCUSTOM35, crCUSTOM36, crCUSTOM37, crCUSTOM38,
-	crCUSTOM39, crCUSTOM40, crCUSTOM41, crCUSTOM42, crCUSTOM43,
-	crCUSTOM44, crCUSTOM45, crCUSTOM46, crCUSTOM47, crCUSTOM48,
-	crCUSTOM49, crCUSTOM50, crCUSTOM51, crCUSTOM52, crCUSTOM53,
-	crCUSTOM54, crCUSTOM55, crCUSTOM56, crCUSTOM57, crCUSTOM58,
-	crCUSTOM59, crCUSTOM60, crCUSTOM61, crCUSTOM62, crCUSTOM63,
-	crCUSTOM64, crCUSTOM65, crCUSTOM66, crCUSTOM67, crCUSTOM68,
-	crCUSTOM69, crCUSTOM70, crCUSTOM71, crCUSTOM72, crCUSTOM73,
-	crCUSTOM74, crCUSTOM75, crCUSTOM76, crCUSTOM77, crCUSTOM78,
-	crCUSTOM79, crCUSTOM80, crCUSTOM81, crCUSTOM82, crCUSTOM83,
-	crCUSTOM84, crCUSTOM85, crCUSTOM86, crCUSTOM87, crCUSTOM88,
-	crCUSTOM89, crCUSTOM90, crCUSTOM91, crCUSTOM92, crCUSTOM93,
-	crCUSTOM94, crCUSTOM95, crCUSTOM96, crCUSTOM97, crCUSTOM98,
-	crCUSTOM99, crCUSTOM100, MAX_COUNTERS
-};
-enum generic_ind
-{
-	genHCP, genMDRAINRATE, genCANSLASH, genWLEVEL,
-	genHCP_PER_HC, genCONTHP, genCONTHP_IS_PERC, genHP_PER_HEART,
-	genMP_PER_BLOCK, genHERO_DMG_MULT, genENE_DMG_MULT,
-	genDITH_TYPE, genDITH_ARG, genDITH_PERC, genLIGHT_RAD,genTDARK_PERC,genDARK_COL,
-	genWATER_GRAV, genSIDESWIM_UP, genSIDESWIM_SIDE, genSIDESWIM_DOWN, genSIDESWIM_JUMP,
-	genBUNNY_LTM, genSWITCHSTYLE, genLAST,
-	genMAX = 256
-};
 
 #define MAX_SAVED_PORTALS 10000
 struct savedportal
@@ -104,78 +68,55 @@ struct gamedata
 	bool operator==(const gamedata&) const = default;
 
 	gamedata_header header;
-	byte  /*_wlevel,*/_cheat;
-	bool  item[MAXITEMS];
-	byte  items_off[MAXITEMS];
-	word _maxcounter[MAX_COUNTERS];	// 0 - life, 1 - rupees, 2 - bombs, 3 - arrows, 4 - magic, 5 - keys, 6-super bombs
+	byte _cheat;
+	bool item[MAXITEMS];
+	byte items_off[MAXITEMS];
+	word _maxcounter[MAX_COUNTERS];
 	word _counter[MAX_COUNTERS];
 	int16_t _dcounter[MAX_COUNTERS];
 	
-	char  version[17];
-	byte  lvlitems[MAXLEVELS];
-	byte  lvlkeys[MAXLEVELS];
-	dword lvlswitches[MAXLEVELS];
-	byte  _continue_scrn;
-	word  _continue_dmap;
-	int32_t _generic[genMAX];	// Generic gamedata. See enum above this struct for indexes.
-	byte  visited[MAXDMAPS];
-	byte  bmaps[MAXDMAPS*MAPSCRSNORMAL];                      // the dungeon progress maps
-	word  maps[MAXMAPS*MAPSCRSNORMAL];                       // info on map changes, items taken, etc.
-	byte  guys[MAXMAPS*MAPSCRSNORMAL];                       // guy counts (though dungeon guys are reset on entry)
-	bool item_messages_played[MAXITEMS];  //Each field is set when an item pickup message plays the first time per session
-	int32_t  screen_d[MAX_MI][8];                // script-controlled screen variables
-	int32_t  global_d[MAX_SCRIPT_REGISTERS];                                      // script-controlled global variables
-	std::vector< ZCArray <int32_t> > globalRAM;
+	char version[17];
+	bounded_vec<word,byte> lvlitems {MAXLEVELS, 0};
+	bounded_vec<word,byte> lvlkeys {MAXLEVELS, 0};
+	bounded_vec<word,dword> lvlswitches {MAXLEVELS, 0};
+	byte _continue_scrn;
+	word _continue_dmap;
+	bounded_vec<word,int32_t> _generic {genMAX, 0}; // Generic gamedata. See enum above this struct for indexes.
+	byte visited[MAXDMAPS];
+	bounded_vec<dword,byte> bmaps {MAX_MI, 0}; // the dungeon progress maps
+	bounded_vec<dword,word> maps {MAXSCRSNORMAL, 0}; // info on map changes, items taken, etc.
+	bounded_vec<dword,byte> guys {MAXSCRSNORMAL, 0}; // guy counts (enemy kill progress)
+	bool item_messages_played[MAXITEMS]; //Each field is set when an item pickup message plays the first time per session
+	bounded_map<dword,bounded_vec<byte,int32_t>> screen_d {MAX_MI, {8, 0}}; // script-controlled screen variables
+	int32_t global_d[MAX_SCRIPT_REGISTERS]; // script-controlled global variables
+	std::vector<ZCArray<int32_t>> globalRAM;
 	
 	word awpn = 255, bwpn = 255, xwpn = 255, ywpn = 255;
 	int16_t abtn_itm = -1, bbtn_itm = -1, xbtn_itm = -1, ybtn_itm = -1;
 	int16_t forced_awpn = -1, forced_bwpn = -1, forced_xwpn = -1, forced_ywpn = -1;
-	bool isclearing; // The gamedata is being cleared
-	//115456 (260)
+	
 	byte bottleSlots[256];
 	
 	savedportal saved_mirror_portal;
 	
 	byte swim_mult = 1, swim_div = 1;
 	
-	bool gen_doscript[NUMSCRIPTSGENERIC];
-	word gen_exitState[NUMSCRIPTSGENERIC];
-	word gen_reloadState[NUMSCRIPTSGENERIC];
-	int32_t gen_initd[NUMSCRIPTSGENERIC][8];
-	int32_t gen_dataSize[NUMSCRIPTSGENERIC];
-	std::vector<int32_t> gen_data[NUMSCRIPTSGENERIC];
-	uint32_t gen_eventstate[NUMSCRIPTSGENERIC];
+	bitstring gen_doscript;
+	bounded_map<word,word> gen_exitState {NUMSCRIPTSGENERIC, 0};
+	bounded_map<word,word> gen_reloadState {NUMSCRIPTSGENERIC, 0};
+	bounded_map<word,bounded_vec<byte,int32_t>> gen_initd {NUMSCRIPTSGENERIC, {8, 0}};
+	bounded_map<word,uint32_t> gen_eventstate {NUMSCRIPTSGENERIC, 0};
+	bounded_map<word,bounded_map<dword,int32_t>> gen_data {NUMSCRIPTSGENERIC, {0, 0}};
 	
-	uint32_t xstates[MAXMAPS*MAPSCRSNORMAL];
+	bounded_map<dword,uint32_t> xstates {MAXSCRSNORMAL, 0};
 	
-	int32_t gswitch_timers[NUM_GSWITCHES];
-	int16_t OverrideItems[itype_max] = {-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,
-		-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,
-		-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,
-		-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,
-		-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,
-		-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,
-		-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,
-		-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,
-		-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,
-		-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,
-		-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,
-		-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,
-		-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,
-		-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,
-		-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,
-		-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,
-		-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,
-		-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,
-		-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,
-		-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,
-		-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,
-		-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2};
+	bounded_map<word,int32_t> gswitch_timers {NUM_GSWITCHES, 0};
+	bounded_map<word,int16_t> OverrideItems {itype_max, -2};
 
 	std::vector<saved_user_object> user_objects;
 	std::vector<savedportal> user_portals;
 	
-	std::vector<int32_t> screen_data[MAXMAPS*MAPSCRS];
+	bounded_map<dword,bounded_map<dword,int32_t>> screen_data {MAXSCRS, {0, 0}};
 	
 	size_t scriptDataSize(int32_t indx) const
 	{
@@ -188,9 +129,7 @@ struct gamedata
 		if(unsigned(indx) >= MAXMAPS*MAPSCRS)
 			return;
 		sz = vbound(sz, 0, 214748);
-		if(screen_data[indx].size() == size_t(sz))
-			return;
-		screen_data[indx].resize(sz, 0);
+		screen_data[indx].resize(sz);
 	}
 	
 	void Clear();
@@ -415,6 +354,28 @@ struct gamedata
 	savedportal* getSavedPortal(int32_t uid);
 
 	bool should_show_time();
+	
+	void normalize()
+	{
+		lvlitems.normalize();
+		lvlkeys.normalize();
+		lvlswitches.normalize();
+		_generic.normalize();
+		bmaps.normalize();
+		maps.normalize();
+		guys.normalize();
+		screen_d.normalize();
+		gen_doscript.normalize();
+		gen_exitState.normalize();
+		gen_reloadState.normalize();
+		gen_initd.normalize();
+		gen_eventstate.normalize();
+		gen_data.normalize();
+		xstates.normalize();
+		gswitch_timers.normalize();
+		OverrideItems.normalize();
+		screen_data.normalize();
+	}
 };
 
 extern gamedata *game;

src/base/containers.h
@@ -4,7 +4,7 @@
 #include "base/headers.h"
 #include <stdexcept>
 
-template<class Sz,typename T>
+template<typename Sz,typename T>
 class bounded_container
 {
 public:
@@ -33,7 +33,138 @@ protected:
 	obj_type default_val;
 };
 
-template<class Sz,class T>
+template<typename Sz,typename T>
+class bounded_map : public bounded_container<Sz,T>
+{
+public:
+	typedef Sz size_type;
+	typedef T obj_type;
+	typedef map<size_type,obj_type> cont_t;
+	typedef bounded_map<size_type,obj_type> bound_t;
+	
+	bounded_map() : bounded_container<Sz,T>(0) {}
+	bounded_map(size_type tsz) : bounded_container<Sz,T>(tsz) {}
+	bounded_map(size_type tsz, obj_type dv) : bounded_container<Sz,T>(tsz,dv) {}
+	virtual ~bounded_map() = default;
+	
+	obj_type& at(size_type ind)
+	{
+		if(ind < this->true_sz)
+		{
+			if(cont.contains(ind))
+				return cont[ind];
+			return (cont[ind] = this->default_val);
+		}
+		throw std::out_of_range("Bad bounded_map access");
+	}
+
+	obj_type const& at(size_type ind) const
+	{
+		if(ind < this->true_sz)
+		{
+			if(cont.contains(ind))
+				return cont.at(ind);
+			return this->default_val;
+		}
+		throw std::out_of_range("Bad bounded_map access");
+	}
+
+	obj_type& operator[](size_type ind)
+	{
+		return at(ind);
+	}
+
+	obj_type const& operator[](size_type ind) const
+	{
+		return at(ind);
+	}
+	
+	bounded_map(bound_t const& other)
+	{
+		*this = other;
+	}
+	bound_t& operator=(bound_t const& other)
+	{
+		cont = other.cont;
+		this->true_sz = other.true_sz;
+		normalize();
+		return *this;
+	}
+	bound_t& operator=(map<size_type,obj_type> const& other)
+	{
+		cont = other;
+		normalize();
+		return *this;
+	}
+	bound_t& operator=(vector<obj_type> const& other)
+	{
+		cont.clear();
+		for(size_t q = 0; q < other.size(); ++q)
+		{
+			if(other[q] != this->default_val)
+				cont[q] = other[q];
+		}
+		normalize();
+		return *this;
+	}
+	
+	cont_t const& inner() const {return cont;}
+	size_type capacity() const {return cont.size();}
+	virtual void clear() {cont.clear();}
+	
+	virtual void normalize();
+	
+	bool inner_empty() const {return cont.empty();}
+	
+	bool erase(size_type const& key)
+	{
+		return cont.erase(key) > 0;
+	}
+	bool contains(size_type const& key) const
+	{
+		return key < this->true_sz && cont.contains(key);
+	}
+	
+	bool operator==(bound_t const& other) const
+	{
+		if(other.size() != this->size())
+			return false;
+		if(other.default_val != this->default_val)
+			return false;
+		map<size_type,bool> keys;
+		for(auto [k,v] : cont)
+			keys[k] = true;
+		for(auto [k,v] : other.cont)
+			keys[k] = true;
+		for(auto [k,b] : keys)
+			if(other[k] != (*this)[k])
+				return false;
+		return true;
+	}
+	
+	optional<size_type> firstKey() const
+	{
+		for(auto it = cont.begin(); it != cont.end(); ++it)
+		{
+			if(it->first < this->true_sz)
+				return it->first;
+		}
+		return nullopt;
+	}
+	optional<size_type> lastKey() const
+	{
+		for(auto it = cont.rbegin(); it != cont.rend(); ++it)
+		{
+			if(it->first < this->true_sz)
+				return it->first;
+		}
+		return nullopt;
+	}
+private:
+	cont_t cont;
+};
+
+template<typename Sz,typename T>
 class bounded_vec : public bounded_container<Sz,T>
 {
 public:
@@ -119,135 +250,72 @@ public:
 	
 	bool inner_empty() const {return cont.empty();}
 	
-	bool operator==(bound_t const& other)
+	bool operator==(bound_t const& other) const
 	{
-		return other.size() == this->size() && other.cont == this->cont;
+		if(other.size() != this->size())
+			return false;
+		if(other.default_val != this->default_val)
+			return false;
+		size_t cap = std::max(other.capacity(), this->capacity());
+		for(size_t q = 0; q < cap; ++q)
+			if(other[q] != (*this)[q])
+				return false;
+		return true;
 	}
-	bool operator!=(bound_t const& other)
+	
+	bounded_map<size_type,obj_type> as_bmap() const
 	{
-		return !(other == *this);
+		bounded_map<size_type,obj_type> ret(this->true_sz, this->default_val);
+		ret = cont; //overloaded for std::vector
+		return ret;
 	}
 private:
 	cont_t cont;
 };
 
-template<class Sz,class T>
-class bounded_map : public bounded_container<Sz,T>
+class bitstring
 {
 public:
-	typedef Sz size_type;
-	typedef T obj_type;
-	typedef map<size_type,obj_type> cont_t;
-	typedef bounded_map<size_type,obj_type> bound_t;
-	
-	bounded_map() : bounded_container<Sz,T>(0) {}
-	bounded_map(size_type tsz) : bounded_container<Sz,T>(tsz) {}
-	bounded_map(size_type tsz, obj_type dv) : bounded_container<Sz,T>(tsz,dv) {}
-	virtual ~bounded_map() = default;
-	
-	obj_type& at(size_type ind)
+	bool get(size_t ind) const
 	{
-		if(ind < this->true_sz)
-		{
-			if(cont.contains(ind))
-				return cont[ind];
-			return (cont[ind] = this->default_val);
-		}
-		throw std::out_of_range("Bad bounded_map access");
+		return cont[ind/8] & (1 << ind%8);
 	}
-
-	obj_type const& at(size_type ind) const
+	void set(size_t ind, bool state)
 	{
-		if(ind < this->true_sz)
-		{
-			if(cont.contains(ind))
-				return cont.at(ind);
-			return this->default_val;
-		}
-		throw std::out_of_range("Bad bounded_map access");
+		if(state)
+			cont[ind/8] |= (1 << ind%8);
+		else if(get(ind))
+			cont[ind/8] &= ~(1 << ind%8);
 	}
-
-	obj_type& operator[](size_type ind)
-	{
-		return at(ind);
-	}
-
-	obj_type const& operator[](size_type ind) const
-	{
-		return at(ind);
-	}
-	
-	bounded_map(bound_t const& other)
-	{
-		*this = other;
-	}
-	bound_t& operator=(bound_t const& other)
-	{
-		cont = other.cont;
-		this->true_sz = other.true_sz;
-		normalize();
-		return *this;
-	}
-	bound_t& operator=(map<size_type,obj_type> const& other)
-	{
-		cont = other;
-		normalize();
-		return *this;
-	}
-	
-	cont_t const& inner() const {return cont;}
-	size_type capacity() const {return cont.size();}
-	virtual void clear() {cont.clear();}
-	
-	virtual void normalize();
-	
-	bool inner_empty() const {return cont.empty();}
-	
-	bool erase(size_type const& key)
-	{
-		return cont.erase(key) > 0;
-	}
-	bool contains(size_type const& key) const
-	{
-		return key < this->true_sz && cont.contains(key);
-	}
-	
-	optional<size_type> firstKey() const
-	{
-		for(auto it = cont.begin(); it != cont.end(); ++it)
-		{
-			if(it->first < this->true_sz)
-				return it->first;
-		}
-		return nullopt;
-	}
-	optional<size_type> lastKey() const
-	{
-		for(auto it = cont.rbegin(); it != cont.rend(); ++it)
-		{
-			if(it->first < this->true_sz)
-				return it->first;
-		}
-		return nullopt;
-	}
+	void normalize() {cont.normalize();}
+	void clear() {cont.clear();}
+	bounded_vec<word,byte>& inner() {return cont;}
+	bounded_vec<word,byte> const& inner() const {return cont;}
+	bool operator==(bitstring const& other) const = default;
 private:
-	cont_t cont;
+	bounded_vec<word,byte> cont {65535};
 };
 
-template<class T>
+template<typename T>
 void _do_normalize(T& v){}
-template<class Sz,class T>
+template<>
+inline void _do_normalize<bitstring>(bitstring& v)
+{
+	v.normalize();
+}
+template<typename Sz,typename T>
 void _do_normalize(bounded_vec<Sz,T>& v)
 {
 	v.normalize();
 }
-template<class Sz,class T>
+template<typename Sz,typename T>
 void _do_normalize(bounded_map<Sz,T>& v)
 {
 	v.normalize();
 }
 
-template<class Sz,class T>
+
+template<typename Sz,typename T>
 void bounded_vec<Sz,T>::normalize()
 {
 	if(cont.size() > this->true_sz)
@@ -258,7 +326,7 @@ void bounded_vec<Sz,T>::normalize()
 	for(auto& v : cont)
 		_do_normalize(v);
 }
-template<class Sz,class T>
+template<typename Sz,typename T>
 void bounded_map<Sz,T>::normalize()
 {
 	for(auto it = cont.begin(); it != cont.end();)

src/base/general.h
@@ -117,6 +117,9 @@ int wrap(int x,int low,int high);
 #define MAPSCRSNORMAL              128
 #define MAPSCRS192b136             132
 #define MAPSCRS                    136
+#define MAXSCRSNORMAL              (MAXMAPS*MAPSCRSNORMAL)
+#define MAXSCRS                    (MAXMAPS*MAPSCRS)
+
 #define TEMPLATES                  8
 #define TEMPLATE                   131
 #define TEMPLATE2                  132
@@ -238,6 +241,44 @@ enum // used for gamedata ITEMS
 	
 	itype_maxusable,
 	itype_max=512
+};
+
+enum
+{
+	crNONE = -1,
+	crLIFE, crMONEY, crBOMBS, crARROWS, crMAGIC,
+	crKEYS, crSBOMBS, crCUSTOM1, crCUSTOM2, crCUSTOM3,
+	crCUSTOM4, crCUSTOM5, crCUSTOM6, crCUSTOM7, crCUSTOM8,
+	crCUSTOM9, crCUSTOM10, crCUSTOM11, crCUSTOM12, crCUSTOM13,
+	crCUSTOM14, crCUSTOM15, crCUSTOM16, crCUSTOM17, crCUSTOM18,
+	crCUSTOM19, crCUSTOM20, crCUSTOM21, crCUSTOM22, crCUSTOM23,
+	crCUSTOM24, crCUSTOM25, crCUSTOM26, crCUSTOM27, crCUSTOM28,
+	crCUSTOM29, crCUSTOM30, crCUSTOM31, crCUSTOM32, crCUSTOM33,
+	crCUSTOM34, crCUSTOM35, crCUSTOM36, crCUSTOM37, crCUSTOM38,
+	crCUSTOM39, crCUSTOM40, crCUSTOM41, crCUSTOM42, crCUSTOM43,
+	crCUSTOM44, crCUSTOM45, crCUSTOM46, crCUSTOM47, crCUSTOM48,
+	crCUSTOM49, crCUSTOM50, crCUSTOM51, crCUSTOM52, crCUSTOM53,
+	crCUSTOM54, crCUSTOM55, crCUSTOM56, crCUSTOM57, crCUSTOM58,
+	crCUSTOM59, crCUSTOM60, crCUSTOM61, crCUSTOM62, crCUSTOM63,
+	crCUSTOM64, crCUSTOM65, crCUSTOM66, crCUSTOM67, crCUSTOM68,
+	crCUSTOM69, crCUSTOM70, crCUSTOM71, crCUSTOM72, crCUSTOM73,
+	crCUSTOM74, crCUSTOM75, crCUSTOM76, crCUSTOM77, crCUSTOM78,
+	crCUSTOM79, crCUSTOM80, crCUSTOM81, crCUSTOM82, crCUSTOM83,
+	crCUSTOM84, crCUSTOM85, crCUSTOM86, crCUSTOM87, crCUSTOM88,
+	crCUSTOM89, crCUSTOM90, crCUSTOM91, crCUSTOM92, crCUSTOM93,
+	crCUSTOM94, crCUSTOM95, crCUSTOM96, crCUSTOM97, crCUSTOM98,
+	crCUSTOM99, crCUSTOM100, MAX_COUNTERS
+};
+
+enum generic_ind //game->generic[]
+{
+	genHCP, genMDRAINRATE, genCANSLASH, genWLEVEL,
+	genHCP_PER_HC, genCONTHP, genCONTHP_IS_PERC, genHP_PER_HEART,
+	genMP_PER_BLOCK, genHERO_DMG_MULT, genENE_DMG_MULT,
+	genDITH_TYPE, genDITH_ARG, genDITH_PERC, genLIGHT_RAD,genTDARK_PERC,genDARK_COL,
+	genWATER_GRAV, genSIDESWIM_UP, genSIDESWIM_SIDE, genSIDESWIM_DOWN, genSIDESWIM_JUMP,
+	genBUNNY_LTM, genSWITCHSTYLE, genLAST,
+	genMAX = 256
 };
 
 enum class ScriptType {

src/base/initdata.cpp
@@ -4,16 +4,12 @@ zinitdata zinit;
 
 void zinitdata::clear_genscript()
 {
-	memset(gen_doscript, 0, sizeof(gen_doscript));
-	memset(gen_exitState, 0, sizeof(gen_exitState));
-	memset(gen_reloadState, 0, sizeof(gen_reloadState));
-	memset(gen_eventstate, 0, sizeof(gen_eventstate));
-	memset(gen_initd, 0, sizeof(gen_initd));
-	for(size_t q = 0; q < NUMSCRIPTSGENERIC; ++q)
-	{
-		gen_data[q].clear();
-		gen_data[q].resize(0);
-	}
+	gen_doscript.clear();
+	gen_exitState.clear();
+	gen_reloadState.clear();
+	gen_eventstate.clear();
+	gen_initd.clear();
+	gen_data.clear();
 }
 void zinitdata::clear()
 {

src/base/initdata.h
@@ -5,33 +5,32 @@
 #include "base/containers.h"
 #include <vector>
 
-#define MAXSCRS (MAXMAPS*MAPSCRS)
+enum
+{
+	INIT_FL_CONTPERCENT,
+	INIT_FL_CANSLASH,
+	INIT_FL_MAX
+};
 struct zinitdata
 {
-	bool items[256];
+	byte items[MAXITEMS/8];
 	
-	byte hc;
-	word start_heart, cont_heart;
-	byte hcp, hcp_per_hc = 4, keys;
-	word rupies;
-	byte triforce; // bit flags
-	byte map[64];
-	byte compass[64];
-	byte boss_key[64];
-	byte misc[16];
+	byte map[MAXLEVELS/8];
+	byte compass[MAXLEVELS/8];
+	byte boss_key[MAXLEVELS/8];
+	byte mcguffin[MAXLEVELS/8];
+	bounded_vec<word,byte> level_keys {MAXLEVELS};
 	
-	byte last_map; //last map worked on
+	word counter[MAX_COUNTERS];
+	word mcounter[MAX_COUNTERS] = {0, 255, 0, 0, 0, 255}; // crMONEY/crKEYS = 255
 	
-	byte last_screen; //last screen worked on
-	word max_magic;
-	word magic;
 	byte bomb_ratio = 4; // ratio of super bombs to bombs
-	byte msg_more_x, msg_more_y, msg_more_is_offset;
-	byte subscreen;
-	word start_dmap;
-	byte heroAnimationStyle;
+	byte hcp, hcp_per_hc = 4;
+	word cont_heart; // continue health
+
+	byte hp_per_heart, magic_per_block, hero_damage_multiplier, ene_damage_multiplier;
+	byte dither_type, dither_arg, dither_percent, def_lightrad, transdark_percent, darkcol;
 	
-	byte level_keys[MAXLEVELS];
 	int32_t ss_grid_x = 8;
 	int32_t ss_grid_y = 8;
 	int32_t ss_grid_xofs;
@@ -40,46 +39,54 @@ struct zinitdata
 	int32_t ss_bbox_1_color = 15;
 	int32_t ss_bbox_2_color = 7;
 	int32_t ss_flags;
-	byte subscreen_style;
-	byte usecustomsfx;
-	word max_rupees = 255, max_keys = 255;
-	byte gravity = 16; //Deprecated!
-	int32_t gravity2 = 1600; //Bumping this up to an int32_t.
+	
+	bitstring flags;
+	
+	byte last_map, last_screen; //last editor map/screen
+	byte msg_more_x, msg_more_y, msg_more_is_offset, msg_speed = 5;
+	
+	int32_t gravity = 1600, swimgravity = 5;
 	word terminalv = 320;
-	byte msg_speed = 5;
-	byte transition_type; // Can't edit, yet.
-	byte jump_hero_layer_threshold = 255; // Hero is drawn above layer 3 if z > this.
-	byte hero_swim_speed;
+	byte hero_swim_speed; //old movement still needs
+	byte hero_swim_mult = 2, hero_swim_div = 3; //new movement
 	
-	word bombs, super_bombs, max_bombs, max_sbombs, arrows, max_arrows, heroStep, subscrSpeed = 1, heroSideswimUpStep, heroSideswimSideStep, heroSideswimDownStep;
-	
+	word heroSideswimUpStep, heroSideswimSideStep, heroSideswimDownStep;
 	int32_t exitWaterJump;
-
-	byte hp_per_heart, magic_per_block, hero_damage_multiplier, ene_damage_multiplier;
 	
-	word scrcnt[25], scrmaxcnt[25]; //Script counter start/max -Em
-	
-	int32_t swimgravity;
-	
-	byte dither_type, dither_arg, dither_percent, def_lightrad, transdark_percent, darkcol;
-	
+	word heroStep;
+	byte heroAnimationStyle;
+	byte jump_hero_layer_threshold = 255; // Hero is drawn above layer 3 if z > this.
 	int32_t bunny_ltm;
-	byte switchhookstyle;
 	
-	byte magicdrainrate;
-	
-	byte hero_swim_mult = 2, hero_swim_div = 3;
-	
-	bool gen_doscript[NUMSCRIPTSGENERIC];
-	word gen_exitState[NUMSCRIPTSGENERIC];
-	word gen_reloadState[NUMSCRIPTSGENERIC];
-	int32_t gen_initd[NUMSCRIPTSGENERIC][8];
-	uint32_t gen_eventstate[NUMSCRIPTSGENERIC];
-	
-	bounded_vec<word,bounded_vec<uint32_t,int32_t>> gen_data {NUMSCRIPTSGENERIC, {0}};
-	bounded_vec<uint32_t,bounded_vec<uint32_t,int32_t>> screen_data {MAXSCRS, {0}};
+	word start_dmap;
+	word subscrSpeed = 1;
+	byte switchhookstyle, magicdrainrate;
 	
 	zfix shove_offset = 6.5_zf;
+	
+	bitstring gen_doscript;
+	bounded_map<word,word> gen_exitState {NUMSCRIPTSGENERIC};
+	bounded_map<word,word> gen_reloadState {NUMSCRIPTSGENERIC};
+	bounded_map<word,bounded_vec<byte,int32_t>> gen_initd {NUMSCRIPTSGENERIC, {8}};
+	bounded_map<word,uint32_t> gen_eventstate {NUMSCRIPTSGENERIC};
+	bounded_map<word,bounded_map<dword,int32_t>> gen_data {NUMSCRIPTSGENERIC, {0}};
+	bounded_map<dword,bounded_map<dword,int32_t>> screen_data {MAXSCRS, {0}};
+	
+	bool get_item(size_t ind) const {return get_bit(items,ind);}
+	void set_item(size_t ind, bool st) {set_bit(items,ind,st);}
+	
+	void normalize()
+	{
+		level_keys.normalize();
+		flags.normalize();
+		gen_doscript.normalize();
+		gen_exitState.normalize();
+		gen_reloadState.normalize();
+		gen_initd.normalize();
+		gen_eventstate.normalize();
+		gen_data.normalize();
+		screen_data.normalize();
+	}
 	
 	void clear_genscript();
 	

commit 3e6f640d4bf75da695a5cf2f666ffe1c5d9a8f7f

fix: continue hearts being improperly bounded
This lead to high continue heart values wrapping around to lower values.
Regressed in `acb7c84`.

src/dialog/init_data.cpp
@@ -381,7 +381,7 @@ std::shared_ptr<GUI::Widget> InitDataDialog::view()
 							fitParent = true,
 							onValChangedFunc = [&](GUI::TextField::type,std::string_view,int32_t val)
 							{
-								local_zinit.counter[crLIFE] = val*HEART_FACTOR;
+								local_zinit.counter[crLIFE] = std::min(65535, val*HEART_FACTOR);
 							}
 						),
 						TextField(maxLength = 5, type = GUI::TextField::type::INT_DECIMAL,
@@ -389,7 +389,7 @@ std::shared_ptr<GUI::Widget> InitDataDialog::view()
 							fitParent = true,
 							onValChangedFunc = [&](GUI::TextField::type,std::string_view,int32_t val)
 							{
-								local_zinit.mcounter[crLIFE] = val*HEART_FACTOR;
+								local_zinit.mcounter[crLIFE] = std::min(65535, val*HEART_FACTOR);
 							}
 						),
 						TextField(maxLength = 5, type = GUI::TextField::type::INT_DECIMAL,
@@ -397,7 +397,7 @@ std::shared_ptr<GUI::Widget> InitDataDialog::view()
 							fitParent = true,
 							onValChangedFunc = [&](GUI::TextField::type,std::string_view,int32_t val)
 							{
-								local_zinit.cont_heart = val*HEART_FACTOR2;
+								local_zinit.cont_heart = std::min(65535, val*HEART_FACTOR2);
 							}
 						),
 						Button(text = "HP Per Heart", colSpan = 2, maxheight = 1.5_em, fitParent = true,

src/gamedata.cpp
@@ -720,12 +720,12 @@ void gamedata::set_hcp_per_hc(byte val)
     set_generic(val, 4);
 }
 
-byte gamedata::get_cont_hearts()
+word gamedata::get_cont_hearts()
 {
     return get_generic(5);
 }
 
-void gamedata::set_cont_hearts(byte val)
+void gamedata::set_cont_hearts(word val)
 {
     set_generic(val, 5);
 }

src/gamedata.h
@@ -248,8 +248,8 @@ public:
 	byte get_hcp_per_hc();
 	void set_hcp_per_hc(byte val);
 	
-	byte get_cont_hearts();
-	void set_cont_hearts(byte val);
+	word get_cont_hearts();
+	void set_cont_hearts(word val);
 	
 	bool get_cont_percent();
 	void set_cont_percent(bool ispercent);