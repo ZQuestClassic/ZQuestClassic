ae59abdcb5fd4da84c9d5d876f2f739e0ec661f4
Connor Clark <cjamcl@gmail.com>
2026-07-22 20:13:13 -0700

    fix(zq): wrong npc IDs listed by tile move overwrite protection
    When moving tiles onto tiles used by enemies, the overwrite protection
    warning listed each enemy by its position in the alphabetized enemy
    list instead of by its actual npc ID (the name shown next to it was
    correct).
    
    Regressed in 2.55.2 (6aae56ece8).
    
    end changelog
    
    The loop over the alphabetized 'bie' list also now stops at bie_cnt
    instead of scanning all eMAXGUYS entries; the tail entries are
    zero-initialized and pointed at npc 0, which has no tiles, so that
    part was harmless.
    
    Discord: https://discord.com/channels/876899628556091432/1528685961238679602
    (cherry picked from commit 2032f995d3e4b59faad7447f16c07a3b5e5141f1)

--- src/zq/zq_tiles.cpp
+++ src/zq/zq_tiles.cpp
@@ -56,6 +56,7 @@ static bool massRecolorSetup(int32_t cset);
 static void massRecolorApply(int32_t tile);
 extern int32_t last_droplist_sel;
 extern int32_t TilePgCursorCol, CmbPgCursorCol;
+extern int32_t bie_cnt;
 
 int32_t ex=0;
 int32_t nextcombo_fake_click=0;
@@ -6746,9 +6747,11 @@ bool _handle_tile_move(TileMoveProcess dest_process, optional<TileMoveProcess> s
 			));
 		build_bie_list(false);
 		bool newtiles=get_qr(qr_NEWENEMYTILES)!=0;
-		for(int u=0; u<eMAXGUYS; u++)
+		for(int u=0; u<bie_cnt; u++)
 		{
-			guydata& enemy=guysbuf[bie[u].i];
+			// 'bie' is alphabetized; 'id' is the real npc id.
+			int id = bie[u].i;
+			guydata& enemy=guysbuf[id];
 			bool darknut=false;
 			int32_t gleeok=0;
 			
@@ -6764,9 +6767,9 @@ bool _handle_tile_move(TileMoveProcess dest_process, optional<TileMoveProcess> s
 			}
 			
 			// Dummied out enemies
-			if(bie[u].i>=eOCTO1S && bie[u].i<e177)
+			if(id>=eOCTO1S && id<e177)
 			{
-				if(old_guy_string[bie[u].i][strlen(old_guy_string[bie[u].i])-1]==' ')
+				if(old_guy_string[id][strlen(old_guy_string[id])-1]==' ')
 				{
 					continue;
 				}
@@ -6802,7 +6805,7 @@ bool _handle_tile_move(TileMoveProcess dest_process, optional<TileMoveProcess> s
 					rects.emplace_back(c+TILES_PER_ROW*r, 20, 3);
 					rects.emplace_back(c+TILES_PER_ROW*(r+3), 16, 6);
 				}
-				movelist->add_tile(&enemy.e_tile, enemy.e_width, enemy.e_height, fmt::format("Enemy {} ({}) 'New'", u, bie[u].s),
+				movelist->add_tile(&enemy.e_tile, enemy.e_width, enemy.e_height, fmt::format("Enemy {} ({}) 'New'", id, bie[u].s),
 					false, 0, 0, rects);
 			}
 			else
@@ -6811,11 +6814,11 @@ bool _handle_tile_move(TileMoveProcess dest_process, optional<TileMoveProcess> s
 				{
 					continue;
 				}
-				movelist->add_tile(&enemy.tile, enemy.width, enemy.height, fmt::format("Enemy {} ({}) 'Old'", u, bie[u].s));
+				movelist->add_tile(&enemy.tile, enemy.width, enemy.height, fmt::format("Enemy {} ({}) 'Old'", id, bie[u].s));
 				
 				if(guysbuf[bie[u].i].s_tile!=0)
 				{
-					movelist->add_tile(&enemy.s_tile, enemy.s_width, enemy.s_height, fmt::format("Enemy {} ({}) 'Special'", u, bie[u].s));
+					movelist->add_tile(&enemy.s_tile, enemy.s_width, enemy.s_height, fmt::format("Enemy {} ({}) 'Special'", id, bie[u].s));
 				}
 			}
 		}
