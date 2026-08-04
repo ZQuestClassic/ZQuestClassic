fff8427df0c029e1a7809073a536eff5238e7808
Emily Venezia <emilygamergirl99@gmail.com>
2026-08-04 03:41:01 -0400

    misc: improve checklist dialog per-column calculation
    The dialog now attempts to create columns as evenly sized as possible,
    up to a max of 10 entries per column.
    
    (cherry picked from commit bc4c0bafad436d6507bf08f22f8d562411fdf44d)

--- src/dialog/checklist.cpp
+++ src/dialog/checklist.cpp
@@ -20,7 +20,27 @@ std::shared_ptr<GUI::Widget> ChecklistDialog::view()
 {
 	using namespace GUI::Builder;
 	using namespace GUI::Props;
-	auto grid = Columns<10>();
+	// Aim for columns as evenly sized as possible, up to 10 entries per column.
+	size_t per_col = 10;
+	{
+		auto numflags = flagnames.size();
+		size_t best_per_col = 0;
+		size_t best_remainder = numflags;
+		for (size_t q = vbound(int(numflags/2), 4, 10); q > 3; --q)
+		{
+			auto rem = numflags % q;
+			if (rem < best_remainder)
+			{
+				best_per_col = q;
+				best_remainder = rem;
+			}
+			if (!rem)
+				break;
+		}
+		if (best_per_col > 0)
+			per_col = best_per_col;
+	}
+	auto grid = GUI::Grid::columns(per_col);
 	for(uint q = 0; q < flagnames.size(); ++q)
 	{
 		grid->add(Checkbox(text = flagnames[q], hAlign = 0.0,
