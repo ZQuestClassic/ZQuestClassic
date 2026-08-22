a918ac167dbe1c51149dce5bf7347eeb6d21e536
Connor Clark <cjamcl@gmail.com>
2026-08-21 18:34:11 -0700

    fix(zc): cellar enemies beyond the fourth spawning at garbage positions
    When a DMap uses 'Use Enemy List for Cellar Enemies', item cellar and
    passageway screens can spawn up to 10 enemies, but only 4 spawn
    positions exist - enemies 5 through 10 read positions from
    out-of-bounds memory. They now cycle through the same 4 columns.
    
    Bug introduced when custom cellar enemies were added in 2.55-alpha-83
    (a7a416007e).
    
    (cherry picked from commit ee3422d94f54327de6e90993c894470be73803ff)

--- src/zc/guys.cpp
+++ src/zc/guys.cpp
@@ -20099,7 +20099,7 @@ void loadenemies()
 				if ( tmpscr->enemy[i] )
 				{
 					int32_t preguycount = guys.Count();
-					addenemy(dngn_enemy_x[i],96,tmpscr->enemy[i],-14-i);
+					addenemy(dngn_enemy_x[i%4],96,tmpscr->enemy[i],-14-i);
 					if (guys.Count() > preguycount)
 					{
 						if (!get_qr(qr_ENEMIES_DONT_SCRIPT_FIRST_FRAME))
