connorjclark/ZeldaClassic

feat(zc): simpler controller binding UI
In the control scheme binding dialog, the movement sticks are now picked
from a dropdown listing the connected controller's actual sticks (Dpad,
Left Stick, Right Stick, ...) instead of a press-to-bind flow. The
press-to-bind flow remains when no controller is connected.

The Control Schemes dialog gains a "Connected Gamepads" section showing
each controller and which scheme it is assigned to, with a dropdown to
reassign or set back to '(Auto)'. Assignment changes apply immediately.

src/zc/control_scheme.cpp
@@ -159,6 +159,8 @@ void cleanup_control_schemes() // make sure this cleans up before allegro exits?
 // is remembered as `[Controls] gamepad__<guid>` in zc.cfg. Scheme priority is
 // quest-specific > gamepad > global.
 
+static string poll_last_guid = "\n"; // impossible value; the first poll always evaluates
+
 static string joystick_guid_str(ALLEGRO_JOYSTICK* joy)
 {
 	ALLEGRO_JOYSTICK_GUID guid = al_get_joystick_guid(joy);
@@ -211,11 +213,10 @@ void poll_gamepad_scheme()
 	int index = active_control_scheme ? active_control_scheme->joystick_index : 0;
 	ALLEGRO_JOYSTICK* joy = index < al_get_num_joysticks() ? al_get_joystick(index) : nullptr;
 
-	static string last_guid = "\n"; // impossible value; first call always evaluates
 	string guid = joy ? joystick_guid_str(joy) : "";
-	if (guid == last_guid)
+	if (guid == poll_last_guid)
 		return;
-	last_guid = guid;
+	poll_last_guid = guid;
 
 	optional<string> prev = gamepad_control_scheme_name;
 	gamepad_control_scheme_name = nullopt;
@@ -250,6 +251,39 @@ void poll_gamepad_scheme()
 	}
 	if (gamepad_control_scheme_name != prev)
 		refresh_control_scheme();
+}
+
+static optional<string> gamepad_assignment_key(int joy_index)
+{
+	if (joy_index < 0 || joy_index >= al_get_num_joysticks())
+		return nullopt;
+	ALLEGRO_JOYSTICK* joy = al_get_joystick(joy_index);
+	if (!joy)
+		return nullopt;
+	return fmt::format("gamepad__{}", joystick_guid_str(joy));
+}
+
+optional<string> get_gamepad_assigned_scheme(int joy_index)
+{
+	auto key = gamepad_assignment_key(joy_index);
+	if (!key)
+		return nullopt;
+	const char* assigned = zc_get_config(ctrl_sect, key->c_str(), nullptr);
+	if (assigned && assigned[0] && control_schemes.contains(assigned))
+		return string(assigned);
+	return nullopt;
+}
+
+void set_gamepad_assigned_scheme(int joy_index, string const& name)
+{
+	auto key = gamepad_assignment_key(joy_index);
+	if (!key)
+		return;
+	// An empty name clears the assignment; the next poll re-assigns
+	// automatically.
+	zc_set_config(ctrl_sect, key->c_str(), name.empty() ? nullptr : name.c_str());
+	poll_last_guid = "\n"; // force re-evaluation
+	poll_gamepad_scheme();
 }
 
 bool activate_control_scheme(string const& name)

ZQuestClassic/ZQuestClassic

Left Stick, Right Stick, ...) instead of a press-to-bind flow. Without a
connected controller the stick bindings are shown read-only, as before.
(cherry picked from commit c2ab39b31c72543b2e4513857a333f9c3e3f5857)

src/zc/control_scheme.cpp
@@ -163,6 +163,8 @@ void cleanup_control_schemes() // make sure this cleans up before allegro exits?
 // `[Controls] gamepad__<identity>` in zc.cfg. Scheme priority is
 // quest-specific > gamepad > global.
 
+static string poll_last_identity = "\n"; // impossible value; the first poll always evaluates
+
 // Stable identity for a controller, used in the config key that remembers its
 // scheme assignment. Normally the GUID; some drivers (e.g. allegro's native
 // XInput driver on Windows) report an all-zero GUID, so fall back to the
@@ -308,11 +310,10 @@ void poll_gamepad_scheme()
 		index = active_control_scheme->joystick_index;
 	ALLEGRO_JOYSTICK* joy = index < al_get_num_joysticks() ? al_get_joystick(index) : nullptr;
 
-	static string last_identity = "\n"; // impossible value; first call always evaluates
 	string identity = joy ? joystick_identity_str(joy) : "";
-	if (identity == last_identity)
+	if (identity == poll_last_identity)
 		return;
-	last_identity = identity;
+	poll_last_identity = identity;
 
 	optional<string> prev = gamepad_control_scheme_name;
 	gamepad_control_scheme_name = nullopt;
@@ -347,6 +348,44 @@ void poll_gamepad_scheme()
 	}
 	if (gamepad_control_scheme_name != prev)
 		refresh_control_scheme();
+}
+
+static optional<string> gamepad_assignment_key(int joy_index)
+{
+	if (joy_index < 0 || joy_index >= al_get_num_joysticks())
+		return nullopt;
+
+	ALLEGRO_JOYSTICK* joy = al_get_joystick(joy_index);
+	if (!joy)
+		return nullopt;
+
+	return fmt::format("gamepad__{}", joystick_identity_str(joy));
+}
+
+optional<string> get_gamepad_assigned_scheme(int joy_index)
+{
+	auto key = gamepad_assignment_key(joy_index);
+	if (!key)
+		return nullopt;
+
+	const char* assigned = zc_get_config(ctrl_sect, key->c_str(), nullptr);
+	if (assigned && assigned[0] && control_schemes.contains(assigned))
+		return string(assigned);
+
+	return nullopt;
+}
+
+void set_gamepad_assigned_scheme(int joy_index, string const& name)
+{
+	auto key = gamepad_assignment_key(joy_index);
+	if (!key)
+		return;
+
+	// An empty name clears the assignment; the next poll re-assigns
+	// automatically.
+	zc_set_config(ctrl_sect, key->c_str(), name.empty() ? nullptr : name.c_str());
+	poll_last_identity = "\n"; // force re-evaluation
+	poll_gamepad_scheme();
 }
 
 bool activate_control_scheme(string const& name)