fdc12b510d4e48bd7e9d5ce79febcd3c65b1669c
Emily Venezia <emilygamergirl99@gmail.com>
2026-09-14 01:36:44 -0400

    fix(zscript): player move functions no longer advance pit state
    This caused using functions like `Hero->Move()` many times to cause you to
    rapidly fall into pits, causing you to take rapid damage in some instances.

--- src/zc/hero.cpp
+++ src/zc/hero.cpp
@@ -18453,13 +18453,14 @@ bool HeroClass::movexy(zfix dx, zfix dy, bool kb, bool ign_sv, bool shove, bool 
 	if(dx && dy)
 		shove = false;
 	bool checkladder = dy < 0;
-	auto check_drown_fall = [&]()
+	bool no_pit_sidefx = earlyret || replay_version_check(68);
+	auto check_drown_fall = [&, no_pit_sidefx]()
 		{
 			if (replay_compat_pitslide_bug())
 				return false;
 			if (!ladderx && !laddery)
 			{
-				if (earlyret) // no side effects
+				if (no_pit_sidefx) // no side effects
 				{
 					if (check_pitslide() == -2 || onWater(true))
 						return true;
