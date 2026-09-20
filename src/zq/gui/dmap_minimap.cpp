connorjclark/ZeldaClassic

refactor!: render true-color GUI layers through the render tree
RenderTreeItem gains a render_cb, so owners of a plain layer can hand
the framework a draw callback and mark `dirty` when their source data
changes, instead of drawing imperatively into the layer's bitmap. The
framework then schedules, targets, clears, and observes the redraw
itself - no render_mark_dirty / zc_set_target_bitmap bookkeeping to
remember.

Migrated: the dmap editor's minimap (drawxmap now draws to the current
target), the quest browser's icon layer, and the spinner arc (which no
longer needs its own full-screen a5 dialog layer or the erase-previous-
arc trick). The zq_cset pickers keep their imperative drawing - their
modal loops predate the render tree and are covered by custom_vsync's
conservative mark.

add_dlg_layer now returns a plain RenderTreeItem instead of a
LegacyBitmapRTI with a null a4_bitmap - the legacy render() override
would swallow render_cb and the a4 pass would consume the dirty flag.
Layers start non-dirty so the draw pass never clears content that
imperative callers already drew.

src/zq/gui/dmap_minimap.cpp
@@ -1,6 +1,7 @@
 #include "zq/gui/dmap_minimap.h"
 #include "gui/common.h"
 #include "gui/dialog.h"
+#include "zalleg/render.h"
 #include "gui/dialog_runner.h"
 #include "gui/size.h"
 #include "gui/jwin.h"
@@ -9,7 +10,7 @@
 
 using namespace GUI;
 
-void drawxmap(ALLEGRO_BITMAP* dest, int32_t themap, int32_t xoff, bool large, int dx, int dy);
+void drawxmap(int32_t themap, int32_t xoff, bool large, int dx, int dy);
 
 namespace GUI
 {
@@ -24,6 +25,13 @@ namespace GUI
 		if (msg == MSG_START && !widg->rti)
 		{
 			widg->rti = add_dlg_layer();
+			// The map itself is true-color, so it renders on this a5 layer. The DIALOG
+			// pointer is stable while the dialog runs, which is also this layer's lifetime.
+			widg->rti->render_cb = [widg, d](RenderTreeItem*, bool) {
+				int32_t x = d->x + 6 + 3;
+				int32_t y = d->y + 9 + 3;
+				drawxmap(widg->cur_map - 1, widg->offset, widg->small_dmap, x, y);
+			};
 		}
 		else if (msg == MSG_END && widg->rti)
 		{
@@ -60,7 +68,7 @@ namespace GUI
 			}
 
 			jwin_draw_frame(screen, (x - frame_thickness) + 1, (y - frame_thickness) + 1, 180, 84, FR_DEEP);
-			drawxmap(widg->rti->bitmap, widg->cur_map-1, widg->offset, widg->small_dmap, x, y);
+			widg->rti->dirty = true;
 		}
 
 		return ret;

ZQuestClassic/ZQuestClassic

(cherry picked from commit 97980e81d9290226ecc1f045e13fc0b3cb9efa27)

src/zq/gui/dmap_minimap.cpp
@@ -1,6 +1,7 @@
 #include "zq/gui/dmap_minimap.h"
 #include "gui/common.h"
 #include "gui/dialog.h"
+#include "base/render.h"
 #include "gui/dialog_runner.h"
 #include "gui/size.h"
 #include "gui/jwin.h"
@@ -9,7 +10,7 @@
 
 using namespace GUI;
 
-void drawxmap(ALLEGRO_BITMAP* dest, int32_t themap, int32_t xoff, bool large, int dx, int dy);
+void drawxmap(int32_t themap, int32_t xoff, bool large, int dx, int dy);
 
 namespace GUI
 {
@@ -25,6 +26,13 @@ namespace GUI
 		if (msg == MSG_START && !widg->rti)
 		{
 			widg->rti = add_dlg_layer();
+			// The map itself is true-color, so it renders on this a5 layer. The DIALOG
+			// pointer is stable while the dialog runs, which is also this layer's lifetime.
+			widg->rti->render_cb = [widg, d](RenderTreeItem*, bool) {
+				int32_t x = d->x + 6 + 3;
+				int32_t y = d->y + 9 + 3;
+				drawxmap(widg->cur_map - 1, widg->offset, widg->small_dmap, x, y);
+			};
 		}
 		else if (msg == MSG_END && widg->rti)
 		{
@@ -61,8 +69,7 @@ namespace GUI
 			}
 
 			jwin_draw_frame(screen, (x - frame_thickness) + 1, (y - frame_thickness) + 1, 180, 84, FR_DEEP);
-			drawxmap(widg->rti->bitmap, widg->cur_map-1, widg->offset, widg->small_dmap, x, y);
-			xmap_drawn = true;
+			widg->rti->dirty = true;
 		}
 
 		return ret;